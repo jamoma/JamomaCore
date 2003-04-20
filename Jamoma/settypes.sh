8e812f19e7952b439f074367cb4c15ec94e220e0
tap <tap@464221e2-0a0f-0410-983b-f8d57d210c17>
2003-04-20 02:26:31 +0000

    Sync
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/trunk@13 464221e2-0a0f-0410-983b-f8d57d210c17

--- Jamoma/settypes.sh
+++ Jamoma/settypes.sh
@@ -1,14 +1,24 @@
 #!/bin/tcsh
+#
+# This script is to set the attribute fork of all Max documents and libraries
+# required for Jamoma. It is neccessary because cvs strips off the file attribute
+# forks.
+#
+# by Tim Place, 19 April 2003
 
-/developer/tools/setfile -t iLaF -c max2 third_party-OSX/otudp
-/developer/tools/setfile -t iLaF -c max2 third_party-OSX/OpenSoundControl
-/developer/tools/setfile -t iLaF -c max2 third_party-OSX/OSC-route
-/developer/tools/setfile -t iLaF -c max2 third_party-OSX/shell
 
-/developer/tools/setfile -t iLaF -c max2 third_party-OS9/otudp
-/developer/tools/setfile -t iLaF -c max2 third_party-OS9/OpenSoundControl
-/developer/tools/setfile -t iLaF -c max2 third_party-OS9/OSC-route
-/developer/tools/setfile -t iLaF -c max2 third_party-OS9/myip
+# CONFIGURATION FILES
+/developer/tools/setfile -t TEXT -c max2 configurations/jam.core.proto
+
+# HELP FILES
+/developer/tools/setfile -t maxb -c max2 docs/jam.attributes.help
+
+# EXAMPLES
+/developer/tools/setfile -t TEXT -c max2 examples/untitled.txt
+
+# MODULES
+/developer/tools/setfile -t TEXT -c max2 modules/jam.filter.mod
+
 
 
 
