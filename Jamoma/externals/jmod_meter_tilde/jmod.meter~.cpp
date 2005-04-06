76ea338aa43d97dc7a0240d39fa8325246be5eb6
tap <tap@464221e2-0a0f-0410-983b-f8d57d210c17>
2005-04-06 15:04:29 +0000

    making progress on the new level meter external
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/trunk@115 464221e2-0a0f-0410-983b-f8d57d210c17

--- Jamoma/externals/jmod_meter_tilde/jmod.meter~.cpp
+++ Jamoma/externals/jmod_meter_tilde/jmod.meter~.cpp
@@ -1,50 +1,52 @@
-/* 
-	Jade2 Menubar window dragger thingy
-	based on:
-	xgui - a blank user interface object (ppc only -- for Max 4)
-		1999-2001 dudas hack
-		
-	additional code from jhno
-	
-	hacked into this form by Tim Place
-		
-*/
+// External Object for Jamoma: signal level meter 
+// By Timothy Place, Copyright © 2005
+// License: GNU LGPL
 
-
-#include "ext.h"					// Max Header
-#include "ext_common.h"				// includes the MIN macro
-#include "ext_strings.h"			// String Functions
-#include "commonsyms.h"				// Common symbols used by the Max 4.5 API
-#include "ext_obex.h"				// Max Object Extensions (attributes) Header
+#include "ext.h"				// Max Header
+#include "ext_common.h"			// includes the MIN macro
+#include "ext_strings.h"		// String Functions
+#include "commonsyms.h"			// Common symbols used by the Max 4.5 API
+#include "ext_obex.h"			// Max Object Extensions (attributes) Header
 #include "ext_user.h"
 #include "ext_wind.h"
 
-#define RES_ID		10101		// ID of our STR# and SICN resources
-
+// Macros and Constants
+#define RES_ID		10120		// ID of our SICN resources
 #define MAXWIDTH 	1024L		// maximum width and height of user object in pixels
-#define MAXHEIGHT	512L			//    (defines maximum offscreen canvas allocation)
+#define MAXHEIGHT	512L		//		(defines maximum offscreen canvas allocation)
 #define MINWIDTH 	64L			// minimum width and height
-#define MINHEIGHT	5L			
+#define MINHEIGHT	5L			//		...
 #define DEFWIDTH 	200			// default width and height
-#define DEFHEIGHT	12	
+#define DEFHEIGHT	12			//		...
 
-typedef struct windowdrag{
-	t_box		x_box;
-	void		*obex;
-	Rect 		x_rect;			// for comparing with x_box.b_rect
-} t_windowdrag;
+// Data Structure for our object
+typedef struct meter{
+	t_box		x_box;			// required box structure for all ui externs
+	void		*obex;			// max 4.5 object extensions
+	void		*qelem;			// queue element to defer drawing commands
+	
+	
+	Rect 		rect;			// for comparing with x_box.b_rect in the update() method
+} t_meter;
 
-void windowdrag_update(t_windowdrag *x);
-void windowdrag_click(t_windowdrag *x, Point pt, short modifiers);
-void windowdrag_psave(t_windowdrag *x, void *w);
-void windowdrag_free(t_windowdrag *x);
-void *windowdrag_new(t_symbol *s, short argc, t_atom *argv);
-void *windowdrag_menu(void *p, long x, long y, long font);
+// Prototypes for our methods:
+void *meter_new(t_symbol *s, short argc, t_atom *argv);					// Object life...
+void *meter_menu(void *p, long x, long y, long font);
+void meter_free(t_meter *x);
+void meter_assist(t_meter *x, void *b, long msg, long arg, char *dst);	// Max Methods...
+void meter_bang(t_meter *x);
+void meter_update(t_meter *x);											// UI Methods...
+void meter_click(t_meter *x, Point pt, short modifiers);
+void meter_psave(t_meter *x, void *w);
+void meter_qfn(t_meter *x);												// Draw Routines...
+void meter_draw(t_meter *x);
 
-t_class *windowdrag_class;
+// Globals
+t_class *meter_class;
 
 
-/*==========================================================================*/
+/************************************************************************************/
+// Main() Function
 
 void main(void)
 {	
@@ -54,66 +56,153 @@ void main(void)
 	
 	common_symbols_init();
 
-	c = class_new("jmod.meter~",(method)windowdrag_new, (method)windowdrag_free, (short)sizeof(t_windowdrag), (method)windowdrag_menu, A_GIMME, 0);
-	class_obexoffset_set(c, calcoffset(t_windowdrag, obex));
+	c = class_new("jmod.meter~",(method)meter_new, (method)meter_free, (short)sizeof(t_meter), (method)meter_menu, A_GIMME, 0);
+	class_obexoffset_set(c, calcoffset(t_meter, obex));
 
-	class_addmethod(c, (method)windowdrag_click,	"click", A_LONG, 0L);
- 	class_addmethod(c, (method)windowdrag_update, 	"update", A_FLOAT, 0L);
- 	class_addmethod(c, (method)windowdrag_psave,	"psave", A_CANT, 0L);	
-//	class_addmethod(c, (method)windowdrag_assist, 	"assist", A_CANT, 0L); 
+	class_addmethod(c, (method)meter_click,		"click", A_LONG, 0L);
+ 	class_addmethod(c, (method)meter_update, 	"update", A_FLOAT, 0L);
+ 	class_addmethod(c, (method)meter_psave,		"psave", A_CANT, 0L);	
+	class_addmethod(c, (method)meter_assist, 	"assist", A_CANT, 0L); 
+	class_addmethod(c, (method)meter_bang, 		"bang", 0L); 
 
 	// Finalize our class
 	class_register(CLASS_BOX, c);
-	windowdrag_class = c;
+	meter_class = c;
 }
 
 
-/*==========================================================================*/
-void windowdrag_update(t_windowdrag *x)
+/************************************************************************************/
+// Object Life
+
+// when the UI external is read-in from a patcher file...
+void *meter_new(t_symbol *s, short argc, t_atom *argv)
 {
-	;
+	t_meter *x;
+	void *patcher;
+	long x_coord, y_coord, width, height;
+	//short bw, bh, flags;
+	short flags;
+	
+	x = (t_meter*)object_alloc(meter_class);
+	if(x){
+		patcher = argv[0].a_w.w_obj;					// patcher
+		x_coord = argv[1].a_w.w_long;					// x coord
+		y_coord = argv[2].a_w.w_long;					// y coord
+		width = argv[3].a_w.w_long;						// width
+		height = argv[4].a_w.w_long;					// height
+			
+		width = CLIP(width, MINWIDTH, MAXWIDTH); 		// constrain to min and max size
+		height = CLIP(height, MINHEIGHT, MAXHEIGHT);	// constrain to min and max size
+		
+		// set flags with which our t_box struct will be initialized:
+		//	F_DRAWFIRSTIN - Draw the first inlet
+		//	F_NODRAWBOX - don't draw the box
+		//	F_GROWBOTH - can grow independently in both width and height
+		//	F_DRAWINLAST - draws inlets after box draws
+		//	F_SAVVY - tells Max any queue function you make calls box_enddraw()
+		flags = F_DRAWFIRSTIN | F_NODRAWBOX | F_GROWBOTH | F_DRAWINLAST | F_SAVVY;
+	
+		// now actually initialize the t_box structure
+		box_new((t_box *)x, (t_patcher *)patcher, flags, x_coord, y_coord, x_coord + width, y_coord + height);
+
+		// Reassign the box's firstin field to point to our new object
+		x->x_box.b_firstin = (void *)x;
+		
+		// Cache rect for comparisons when the user decides to re-size the object
+		x->rect = x->x_box.b_rect;
+
+		// Create our queue element for defering calls to the draw function
+		x->qelem = qelem_new(x, (method)meter_qfn);
+		
+		// Finish it up...
+		box_ready((t_box *)x);
+	}
+	else
+		error("jmod.meter~ - could not create instance");
+	return(x);
 }
 
-/* ========================================================================== */
-// if the user clicks on out object's rect...
 
-void windowdrag_click(t_windowdrag *x, Point pt, short modifiers)
+// if the user selects our object from the toolbar/menu, we supply the _new method with default values
+void *meter_menu(void *p, long x, long y, long font)
+{
+	t_atom argv[5];		// reduced to 5 on 24 nov 2004
+	
+	SETOBJ(argv, (t_object *)p);				// patcher
+	SETLONG(argv+1, x);							// x coord
+	SETLONG(argv+2, y);							// y coord
+	SETLONG(argv+3, DEFWIDTH);					// width
+	SETLONG(argv+4, DEFHEIGHT);					// height
+	
+	return meter_new(gensym("jmod.meter~"), 5, argv);
+}
+
+
+// delete
+void meter_free(t_meter *x)
+{
+	qelem_free(x->qelem);
+	box_free((box *)x);
+}
+
+
+/************************************************************************************/
+// Methods bound to input/inlets
+
+// Method for Assistance Messages
+void meter_assist(t_meter *x, void *b, long msg, long arg, char *dst)
+{
+	if(msg==1) 						// Inlet
+		strcpy(dst, "Input");
+	else if(msg==2){ 				// Outlets
+		switch(arg){
+			case 0: strcpy(dst, "Output"); break;
+			//case 1: strcpy(dst, "Attribute Stuff"); break;
+ 		}
+ 	}		
+}
+
+
+// Method: bang
+void meter_bang(t_meter *x)
+{
+	qelem_set(x->qelem);
+}
+
+
+/************************************************************************************/
+// Required UI Object Methods
+
+// Sent when our object needs to be redrawn
+//	(this is already at low priority and does not need to be deferred)
+void meter_update(t_meter *x)
+{
+	short width_old = x->rect.right - x->rect.left;
+	short height_old = x->rect.bottom - x->rect.top;
+	short width_new = x->x_box.b_rect.right - x->x_box.b_rect.left;
+	short height_new = x->x_box.b_rect.bottom - x->x_box.b_rect.top;
+
+	if(/*!x->x_offscreen ||*/ height_new != height_old || width_new != width_old) {
+		width_new = CLIP(width_new, MINWIDTH, MAXWIDTH); // constrain to min and max size
+		height_new = CLIP(height_new, MINHEIGHT, MAXHEIGHT);
+		box_size(&x->x_box, width_new, height_new);	// this function actually resizes out t_box structure
+		x->rect = x->x_box.b_rect;
+		
+		//xgui_allocoffscreen(x); // (existing offsceen is disposed inside)
+	}
+	meter_draw(x);
+}
+
+
+// If the user clicks on the object
+void meter_click(t_meter *x, Point pt, short modifiers)
 { 
-   WindowPtr wp;
-   Point /*pt,*/ rpt;
-   t_wind *twp;
-   Rect r;
-   short h, v;
-   t_wind *p;
-
-   SetRect(&r,0,0,15555,15555); // whatever. just really big. space.
-   LocalToGlobal(&pt);
-
-	if ((p = (t_wind*)x->x_box.b_patcher->p_wind) 
-		&& (wp = (OpaqueWindowPtr *)wind_syswind(p)))
-	{
-      DragWindow(wp,pt,&r);
-      
-
-/* COMMENTED OUT BY JB      
-      GetMouse(&rpt);
-      LocalToGlobal(&rpt);
-      h = rpt.h - pt.h;
-      v = rpt.v - pt.v;
-      p->w_x1 += h;
-      p->w_y1 += v;
-      p->w_x2 += h;
-      p->w_y2 += v;
-*/
-
-   }
+	;	// Ignore the clicks for the time being - maybe clear the peak hold in the future...
 }
 
 
-/*==========================================================================*/
-// save our UI object's location and appearance with the patcher...
-
-void windowdrag_psave(t_windowdrag *x, void *w)
+// Save our UI object's location and appearance with the patcher...
+void meter_psave(t_meter *x, void *w)
 {
 	Rect r = x->x_box.b_rect;
 	t_atom argv[16];
@@ -124,72 +213,63 @@ void windowdrag_psave(t_windowdrag *x, void *w)
 		SETSYM(argv+1,gensym("hidden"));
 		inc = 1;
 	}
-	SETSYM(argv+1+inc,gensym("user"));
-	SETSYM(argv+2+inc,ob_sym(x));
-	SETLONG(argv+3+inc,r.left);
-	SETLONG(argv+4+inc,r.top);
-	SETLONG(argv+5+inc,r.right - r.left);	// width
-	SETLONG(argv+6+inc,r.bottom - r.top);	// height
+	SETSYM(argv+1+inc, gensym("user"));
+	SETSYM(argv+2+inc, ob_sym(x));
+	SETLONG(argv+3+inc, r.left);			// x
+	SETLONG(argv+4+inc, r.top);				// y
+	SETLONG(argv+5+inc, r.right - r.left);	// width
+	SETLONG(argv+6+inc, r.bottom - r.top);	// height
 	
-	binbuf_insert(w,0L,7+inc,argv);
+	binbuf_insert(w, 0L, 7+inc, argv);
 }
 
 
-/*--------------------------------------------------------------------------*/
-void windowdrag_free(t_windowdrag *x)
+/************************************************************************************/
+// Drawing Routines
+
+// The deferred routine called by our Qelem
+void meter_qfn(t_meter *x)
 {
-	box_free((box *)x);
-}
-
-
-/*--------------------------------------------------------------------------*/
-// when the UI external is read-in from a patcher file...
-
-void *windowdrag_new(t_symbol *s, short argc, t_atom *argv)
-{
-	t_windowdrag *x;
-	void *patcher;
-	long bx, by, width, height;
-	short bw, bh, flags;
+	GrafPtr	gp = patcher_setport(x->x_box.b_patcher);
 	
-	x = (t_windowdrag*)object_alloc(windowdrag_class);
-	if(x){
-		patcher = argv[0].a_w.w_obj;
-		bx = argv[1].a_w.w_long;
-		by = argv[2].a_w.w_long;
-		width = argv[3].a_w.w_long;
-		height = argv[4].a_w.w_long;	
-			
-		bw = CLIP(width, MINWIDTH, MAXWIDTH); // constrain to min and max size
-		bh = CLIP(height, MINHEIGHT, MAXHEIGHT);
-		flags = F_DRAWFIRSTIN | F_NODRAWBOX | F_GROWBOTH | F_DRAWINLAST | F_SAVVY;
-		box_new((t_box *)x, (t_patcher *)patcher, flags, bx, by, bx+bw, by+bh);
-		x->x_box.b_firstin = (void *)x;
-		x->x_rect = x->x_box.b_rect;
-				
-		box_ready((t_box *)x);
+	if(gp){				// if the pointer is valid...
+		if(!box_nodraw((t_box *)x)){
+			meter_draw(x);
+			box_enddraw((t_box *)x);
+		}
 	}
-	else
-		error("tap.windowdrag - could not create instance");
-	return(x);
+	SetPort(gp);
 }
 
 
-
-/*--------------------------------------------------------------------------*/
-// if the user selects our object from the toolbar/menu, 
-//		we need to supply the _new method with default values
-
-void *windowdrag_menu(void *p, long x, long y, long font)
+// The actual drawing routine
+void meter_draw(t_meter *x)
 {
-//	t_atom argv[16];
-	t_atom argv[5];		// reduced to 5 on 24 nov 2004
+	GrafPtr		curPort;
+	GDHandle	curDevice;
+	RGBColor	frgb;
+	Rect		rect_ui = x->x_box.b_rect;
+	Rect		rect_temp;
+	short		i;
+	short		width_ui = x->x_box.b_rect.right - x->x_box.b_rect.left;
+	short		left = x->x_box.b_rect.left;
+	//float		ratio;
 	
-	SETOBJ(argv, (t_object *)p);
-	SETLONG(argv+1,x);
-	SETLONG(argv+2,y);
-	SETLONG(argv+3,DEFWIDTH);
-	SETLONG(argv+4,DEFHEIGHT);
-	return windowdrag_new(gensym("windowdrag"),5,argv);
+	GetGWorld((CGrafPtr *)&curPort, &curDevice);
+	PenMode(srcCopy);
+	
+	frgb.blue = 0;
+	frgb.green = 65535;
+	rect_temp.top = x->x_box.b_rect.top;
+	rect_temp.bottom = x->x_box.b_rect.bottom;
+
+	for(i=0; i<width_ui; i++){
+		rect_temp.left = left + i;
+		rect_temp.right = rect_temp.left + 1;
+		frgb.red = (i * 65535) / width_ui;
+		
+		RGBForeColor(&frgb);
+		PaintRect(&rect_temp);
+	}
 }
 
