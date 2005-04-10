a2a4454bc46dc07a83d7d91013097c5c41b9ac69
tap <tap@464221e2-0a0f-0410-983b-f8d57d210c17>
2005-04-10 01:47:22 +0000

    Compiled jmod.change external for Windows.  I still couldn't get the project to compile correctly, so I gave up on using the commonsyms.c defined symbols and just defined my own.
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/trunk@145 464221e2-0a0f-0410-983b-f8d57d210c17

--- Jamoma/library/externals/jmod_change/jmod.change.cpp
+++ Jamoma/library/externals/jmod_change/jmod.change.cpp
@@ -35,8 +35,9 @@ void atom_copy(t_atom *dst, t_atom *src);
 bool atom_compare(Atom *in1, Atom *in2);
 
 // Globals
-t_class *change_class;			// Required. Global pointing to this class
-
+t_class		*change_class;			// Required. Global pointing to this class
+t_symbol	*ps_nothing;
+t_symbol	*ps_dumpout;
 
 /************************************************************************************/
 // Main() Function
@@ -65,6 +66,10 @@ void main(void)				// main recieves a copy of the Max function macros table
 	// Finalize our class
 	class_register(CLASS_BOX, c);
 	change_class = c;
+	
+	// Initialize Globals
+	ps_nothing = gensym("nothing");
+	ps_dumpout = gensym("dumpout");
 }
 
 
@@ -77,7 +82,7 @@ void *change_new(void)
 	t_change *x;									// Declare an object (based on our struct)
 	x = (t_change *)object_alloc(change_class);		// Create object, store pointer to it (get 1 inlet free)
 	if(x){
-		object_obex_store((void *)x, _sym_dumpout, (object *)outlet_new(x,NULL));	// dumpout	
+		object_obex_store((void *)x, ps_dumpout, (object *)outlet_new(x,NULL));	// dumpout	
 		x->change_Out[1] = bangout(x);				// Create Outlets (right to left order)
 	    x->change_Out[0] = outlet_new(x, 0);		//	...
 
@@ -86,7 +91,7 @@ void *change_new(void)
 
 	// Init the temp storage
 	for(i=0; i<MAX_LIST_SIZE; i++)
-		atom_setsym(&(x->last_input_list[i]), _sym_nothing);			
+		atom_setsym(&(x->last_input_list[i]), ps_nothing);			
 	}
 	return (x);										// Return pointer to our instance
 }
