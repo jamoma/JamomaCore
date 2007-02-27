d90713a6b6e63c075eb1b69ba23a2653be8622d6
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2007-02-27 03:10:09 +0000

    added pure virtual definitions for attr_set() and attr_get()
    git-svn-id: http://ttblue.googlecode.com/svn/trunk@73 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- includes/tt_audio_base.h
+++ includes/tt_audio_base.h
@@ -50,7 +50,10 @@ class tt_audio_base:public tt_base{
 		static int get_global_vectorsize();
 		void set_vectorsize(int value);
 		int get_vectorsize();
-				
+		
+		// ATTRIBUTES: pure virtual functions -- must define these in derived classes!
+		virtual void 				set_attr(tt_selector sel, tt_attribute_value val)	= 0	// Set Attributes
+		virtual tt_attribute_value 	get_attr(tt_selector sel)							= 0	// Get Attributes
 		
 		// Attempt to knock out denormalized floats; inlined here for speed
 		static double anti_denormal(double value)
