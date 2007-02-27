6414d0107293351eea1a0424b4fbba545c0c3765
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2007-02-27 03:10:31 +0000

    implementations moved out into the cpp files
    git-svn-id: http://ttblue.googlecode.com/svn/trunk@74 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- includes/tt_add.h
+++ includes/tt_add.h
@@ -2,8 +2,8 @@
  *******************************************************
  *		VECTOR ADDITION
  *******************************************************
- *		Tap.Tools Blue Object
- *		copyright © 2003 by Timothy A. Place
+ *		TTBlue Object
+ *		Copyright © 2003 by Timothy A. Place
  *
  */
 
@@ -16,9 +16,7 @@
 
 
 /********************************************************
-	CLASS INTERFACE/IMPLEMENTATION
-
-	The entire class is implemented inline for speed.
+	CLASS INTERFACE
  ********************************************************/
 
 class tt_add:public tt_audio_base{
@@ -26,27 +24,11 @@ class tt_add:public tt_audio_base{
 	private:
 		
 	public:
-		tt_add(void)	// Constructor		
-		{
-			;
-		}
+		tt_add(void);												// Constructor		
+		~tt_add(void);												// Destructor
+		// tt_add operator = (const tt_add &tt_add_source)			// use the generic c++ copy constructor
 
-		~tt_add(void)		// Destructor
-		{
-			;
-		}
-		
-		// use the generic c++ copy constructor
-		// tt_add operator = (const tt_add &tt_add_source)
-
-
-		void dsp_vector_calc(tt_audio_signal *in1, tt_audio_signal *in2, tt_audio_signal *out)
-		{
-			temp_vs = in1->vectorsize;
-			while(temp_vs--)
-				*out->vector++ = (*in1->vector++) + (*in2->vector++);
-			in1->reset(); in2->reset(); out->reset();
-		}
+		void dsp_vector_calc(tt_audio_signal *in1, tt_audio_signal *in2, tt_audio_signal *out);
 };
 
 #endif // TT_ADD_H
