8594f728782d760d18a8ef1239a7cbe1cf39588c
watsond <watsond@464221e2-0a0f-0410-983b-f8d57d210c17>
2007-03-21 10:50:53 +0000

    and more doxyification
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@1738 464221e2-0a0f-0410-983b-f8d57d210c17

--- Externals/common/jcom.sendreceive.h
+++ Externals/common/jcom.sendreceive.h
@@ -12,28 +12,29 @@
 
 typedef void (*t_receive_callback)(t_symbol *name, t_symbol *msg, short argc, t_atom *argv);
 
-// Receive Object
+/** Receive Object */
 typedef struct _receive{
-	t_object					ob;							// REQUIRED: Our object
-	void						*obex;						// REQUIRED: Object Extensions used by Jitter/Attribute stuff
-	void						*outlet;					// Need one for each outlet
-	t_symbol					*attr_name;					// ATTRIBUTE: name
-	t_receive_callback			receive_master_callback;	// These all point to the same shared function
+	t_object					ob;				///< REQUIRED: Our object
+	void						*obex;			///< REQUIRED: Object Extensions used by Jitter/Attribute stuff
+	void						*outlet;		///< Need one for each outlet
+	t_symbol					*attr_name;		///< ATTRIBUTE: name
+	t_receive_callback			receive_master_callback;	///< These all point to the same shared function
 	t_receive_obex_callback		receive_obex_callback;
 	void						*receive_obex_callback_arg;
 } t_receive;
 
-// Set up a linked list for maintaining pointers to each instance of the jcom.receive external
+/** A linked list for maintaining pointers to each instance of the jcom.receive 
+ * external */
 typedef struct _receiver{
 	t_receive		*object;
 	_receiver		*next;
 } t_receiver;
 
-// Send Object
+/** Send Object */
 typedef struct _send{
-	t_object		ob;					// REQUIRED: Our object
-	void			*obex;				// REQUIRED: Object Extensions used by Jitter/Attribute stuff
-	t_symbol		*attr_name;			// ATTRIBUTE: name
+	t_object		ob;			///< REQUIRED: Our object
+	void			*obex;		///< REQUIRED: Object Extensions used by Jitter/Attribute stuff
+	t_symbol		*attr_name;	///< ATTRIBUTE: name
 } t_send;
 
 #endif // #ifndef __JCOM_SENDRECEIVE_H__
