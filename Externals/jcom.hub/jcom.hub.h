5031bff7aec9bb9a9023a5b3deff200eb388e1e4
watsond <watsond@464221e2-0a0f-0410-983b-f8d57d210c17>
2007-03-21 10:32:39 +0000

    more doxyifying
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@1733 464221e2-0a0f-0410-983b-f8d57d210c17

--- Externals/jcom.hub/jcom.hub.h
+++ Externals/jcom.hub/jcom.hub.h
@@ -24,12 +24,12 @@ enum outlets{
 	k_num_outlets
 };
 
-
+/** Linked list of subscribers to jcom.hub */
 typedef struct _subscriber{
-	void			*object;		// the max object instance that represents this subscriber
-	t_symbol		*name;			// name of the parameter/message/return/remote
-	t_symbol		*type;			// examples: subscribe_parameter, subscribe_message, subscribe_in, subscribe_out, subscribe_remote, subscribe_return
-	_subscriber		*next;			// next subscriber in the linked list
+	void			*object;	///< the max object instance that represents this subscriber
+	t_symbol		*name;		///< name of the parameter/message/return/remote
+	t_symbol		*type;		///< examples: subscribe_parameter, subscribe_message, subscribe_in, subscribe_out, subscribe_remote, subscribe_return
+	_subscriber		*next;		///< next subscriber in the linked list
 } t_subscriber;
 
 
@@ -43,45 +43,49 @@ typedef struct _preset_item{
 	_preset_item	*next;
 } t_preset_item;
 
-
+/** Linked list of loaded presets */
 typedef struct _preset{
 	long			number;
 	t_symbol		*name;
-	t_preset_item	*item;			// a preset may have N items (stored as a linked list)
+	t_preset_item	*item;		///< a preset may have N items (stored as a linked list)
 	_preset			*next;
 } t_preset;
 
-
-typedef struct _hub{							// Data Structure for this object
-	t_object		ob;							// REQUIRED: Our object
-	void			*obex;						// REQUIRED: Object Extensions used by Jitter/Attribute stuff
-	void			*outlets[k_num_outlets];	// outlet array
-	t_patcher		*container;					// the owning patcher
-	t_subscriber	*subscriber;				// top of the linked list of parameters
-	t_preset		*preset;					// top of the linked list of presets
-	long			num_parameters;				// count used for working with presets
-	void			*init_qelem;				// qelem used for initialization messages
-	void			*in_object;					// cache the jcom.in object directly for quick access
-	void			*out_object;				// cache the jcom.out object directly for quick access
-	void			*meter_object[16];			// cache any meter objects so they can be handed to jcom.out
-	void			*preview_object;			// cache the remote for sending jitter matrix preview frames
-	void			*gui_object;				// cache the jcom.remote object in the gui for quick access
-	t_symbol		*attr_name;					// ATTRIBUTE: module name
-	t_symbol		*attr_type;					// ATTRIBUTE: what kind of module is this?  (audio, video, control, etc.)
-	t_symbol		*attr_size;					// ATTRIBUTE: gui size
-	t_symbol		*attr_description;			// ATTRIBUTE: textual description of this module
-	t_symbol		*attr_skin;					// ATTRIBUTE: skin
-	t_symbol		*attr_algorithm_type;		// ATTRIBUTE: poly, blue, jitter, etc.
+/** The Hub. jcom.hub performs a large part of the automagic part of Jamoma.  This includes
+ * things such as saving and recalling presets, resizing modules to one of the standard sizes
+ * forwarding of control messages to jcom.in and so on.  */
+typedef struct _hub{							///< Data Structure for this object
+	t_object		ob;							///< REQUIRED: Our object
+	void			*obex;						///< REQUIRED: Object Extensions used by Jitter/Attribute stuff
+	void			*outlets[k_num_outlets];	///< outlet array
+	t_patcher		*container;					///< the owning patcher
+	t_subscriber	*subscriber;				///< top of the linked list of parameters
+	t_preset		*preset;					///< top of the linked list of presets
+	long			num_parameters;				///< count used for working with presets
+	void			*init_qelem;				///< qelem used for initialization messages
+	void			*in_object;					///< cache the jcom.in object directly for quick access
+	void			*out_object;				///< cache the jcom.out object directly for quick access
+	void			*meter_object[16];			///< cache any meter objects so they can be handed to jcom.out
+	void			*preview_object;			///< cache the remote for sending jitter matrix preview frames
+	void			*gui_object;				///< cache the jcom.remote object in the gui for quick access
+	t_symbol		*attr_name;					///< ATTRIBUTE: module name
+	t_symbol		*attr_type;					///< ATTRIBUTE: what kind of module is this?  (audio, video, control, etc.)
+	t_symbol		*attr_size;					///< ATTRIBUTE: gui size
+	t_symbol		*attr_description;			///< ATTRIBUTE: textual description of this module
+	t_symbol		*attr_skin;					///< ATTRIBUTE: skin
+	t_symbol		*attr_algorithm_type;		///< ATTRIBUTE: control, jitter, etc.
 	long			attr_inspector;
-	t_object		*jcom_send;					// jcom.send and jcom.receive objects for remote communication
+	t_object		*jcom_send;					///< jcom.send and jcom.receive objects for remote communication
 	t_object		*jcom_receive;				//	...
-	t_symbol		*osc_name;					// the OSC name of this module for remote communication
+	t_symbol		*osc_name;					///< the OSC name of this module for remote communication
 } t_hub;
 
 
 // Prototypes for our methods:
+/** The jcom.hub constructor */
 void*		hub_new(t_symbol *s, long argc, t_atom *argv);
 void		hub_examine_context(t_hub *x);
+/** The hub deconstructor, free's any memory used by the hub */
 void		hub_free(t_hub *x);
 void		hub_assist(t_hub *x, void *b, long msg, long arg, char *dst);
 void		hub_symbol(t_hub *x, t_symbol *msg, short argc, t_atom *argv);
@@ -104,7 +108,12 @@ void 		hub_init(t_hub *x);
 void 		hub_qfn_init(t_hub *x);
 void 		hub_gui_build(t_hub *x);
 void		hub_module_view_alg(t_hub *x);
+/** Freeze updating of the UI
+ * @param x a pointer to the hub
+ * @param value non-zero to freeze UI updates, 0 to enable updating the UI */
 void 		hub_ui_freeze(t_hub *x, long value);
+/** Refresh the UI
+ * @param x a pointer to the hub */
 void 		hub_ui_refresh(t_hub *x);
 void		hub_receive_callback(void *x, t_symbol *msg, short argc, t_atom *argv);
 // These are in jcom.hub.autodoc.cpp
@@ -122,7 +131,11 @@ void 		hub_preset_recall(t_hub *x, t_symbol *msg, short argc, t_atom *argv);	// 
 void 		hub_preset_store(t_hub *x, t_symbol *msg, short argc, t_atom *argv);	// number & optional name
 void 		hub_preset_default(t_hub *x);
 void 		hub_presets_clear(t_hub *x);
+/** Dump all presets
+ * @param x a pointer to the hub */
 void 		hub_presets_dump(t_hub *x);
+/** Adds presets to the GUI menu
+ * @param x a pointer to the hub */
 void		hub_preset_buildmenu(t_hub *x);
 
 
