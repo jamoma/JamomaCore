f9c229a823d93f992c800410548b2ef3e123561b
watsond <watsond@464221e2-0a0f-0410-983b-f8d57d210c17>
2007-03-21 11:12:29 +0000

    use jcom.out.h
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@1740 464221e2-0a0f-0410-983b-f8d57d210c17

--- Externals/jcom.out/jcom.out.cpp
+++ Externals/jcom.out/jcom.out.cpp
@@ -26,30 +26,9 @@
 #include "ext_obex.h"				// Max Object Extensions (attributes) Header
 #include "jcom.core.h"
 #include "jcom.io.h"
+#include "jcom.out.h"
 
 #define POLL_INTERVAL	150			// metro time
-
-
-// Prototypes for methods
-void *out_new(t_symbol *s, short argc, t_atom *argv);				// New Object Creation Method
-void out_subscribe(t_out *x);
-void out_algorithm_message(t_out *x, t_symbol *msg, short argc, t_atom *argv);
-void out_dispatched(t_out *x, t_symbol *msg, short argc, t_atom *argv);
-void out_sendbypassedvalue(t_out *x, short inletnum, t_symbol *msg, short argc, t_atom *argv);
-void out_sendlastvalue(t_out *x);
-void out_anything(t_out *x, t_symbol *msg, short argc, t_atom *argv);
-void out_free(t_out *x);											// Object Deletion Method
-void out_release(t_out *x);											// Hub Deletion
-void out_dsp(t_out *x, t_signal **sp, short *count);				// DSP Method
-void out_assist(t_out *x, void *b, long m, long a, char *s);		// Assistance Method
-t_int *out_perform(t_int *w);										// MSP Perform Method
-void out_alloc(t_out *x, int vector_size);
-void out_link_to_in_object(t_out *x, t_in *y);
-void out_unlink(t_out *x);
-void out_register_meter(t_out *x, int meternum, void *meter_object);
-void out_register_preview(t_out *x, void *preview_object);
-void update_meters(t_out *x);
-
 
 // Globals
 t_class		*out_class;					// Required. Global pointing to this class
