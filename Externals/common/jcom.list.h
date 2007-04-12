24b8b7ef6b3f87276fd5069d605cab3564f0b9d2
watsond <watsond@464221e2-0a0f-0410-983b-f8d57d210c17>
2007-04-12 10:12:52 +0000

    initial attempt at creating a template class to manage linked lists
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@1945 464221e2-0a0f-0410-983b-f8d57d210c17

---
+++ Externals/common/jcom.list.h
@@ -0,0 +1,87 @@
+/* 
+ * Class for handling linked lists. 
+ * By Dave Watson Copyright © 2007
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#ifndef __JCOM_LIST_H_
+#define __JCOM_LIST_H__
+
+#include <list>
+using namespace std;
+
+/** A template class for managing linked lists */
+template<class T> 
+class jcomList {
+public:
+	jcomList() { };
+	~jcomList() { };
+
+	/** Put an item at the front of the list
+	 * @param x the item to add to the list
+	*/
+	void push_front(const T& x) 
+	{ ll.push_front(x); }
+	/** Put an item at the back of the list
+	 * @param x the item to add to the list
+	*/
+	void push_back(const T& x)
+	{ ll.push_back(x); }
+	
+	/** Remove an item from the list if it exists.
+	 * @param x the item in the list 
+	*/
+	void remove(const T& x)
+	{
+		typename list<T>::iterator i = find(ll.begin(), ll.end(), x);
+		if(i != ll.end())
+			ll.erase(i);
+	}
+	
+	bool find(const T& x) 
+	{
+		return find(ll.begin(), ll.end(), x) != ll.end();
+	}
+	
+	T& element(const T& x) 
+	{
+		typename list<T>::iterator i = find(ll.begin(), ll.end(), x);
+		return *i;
+	}
+	
+	/** Merges a new item into the list sorting it according to the function object comp
+	 * @param x the new item to merge
+	 * @param comp A function object to determine where in the list the new item should
+	 * be placed
+	*/
+	template<typename Compare>
+	void merge(const T& x, Compare comp)
+	{
+		list<T> m;
+		m.push_front(x);
+		ll.merge(m, comp);
+	}
+	/** @return the number of elements in the list */	
+	int size() { return ll.size(); }
+	/** @return true if the list is empty, false otherwise */
+	bool empty() const { return ll.empty(); }
+	/** Removes the list item at position.  
+	 * @param position the position of the list item to remove
+	 * @return an updated iterator pointing to the position that followed the removed item
+	 */
+	typename list<T>::iterator erase(typename list<T>::iterator position) { return ll.erase(position); }
+	/** @return an iterator to the front of the list */
+	typename list<T>::iterator begin() { return ll.begin(); }
+	/** @return an iterator to one past the end of the list */
+	typename list<T>::iterator end() { return ll.end(); }
+	
+private:
+	list<T> ll;
+
+};
+
+
+
+#endif // __JCOM_LIST_H__
