fb5da6e393fb1e5e064ab2a7f2a91ba2c4481342
watsond <watsond@464221e2-0a0f-0410-983b-f8d57d210c17>
2007-09-22 22:42:46 +0000

    initial makefiles for building on windows using gcc under cygwin, still needs some tweaking; nor is every external currently being built when you do a make
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@2247 464221e2-0a0f-0410-983b-f8d57d210c17

---
+++ Externals/infra/common.mk
@@ -0,0 +1,85 @@
+# common stuff
+
+# compilers
+CC	= gcc
+CXX = gcc
+
+LOCAL_DEPS = -MM
+ALL_DEPS = -M
+DEP_CHECK_TYPE = ${ALL_DEPS}
+
+WIN_DEFINES = -DWIN_VERSION -DWIN_EXT_VERSION 
+
+MAX_INCLUDES = -I/maxmspsdk/c74support/max-includes \
+   -I/maxmspsdk/c74support/msp-includes \
+   -I/maxmspsdk/c74support/common
+
+MAX_LIB_INCLUDES = -L/maxmspsdk/c74support/max-includes \
+   -L/maxmspsdk/c74support/msp-includes \
+   -L/maxmspsdk/c74support/common
+
+OPTIMIZATION_FLAGS = -O2
+# c compiler flags
+RELEASE_CFLAGS = ${OPTIMIZATION_FLAGS} -Wall -shared -mno-cygwin $(WIN_DEFINES) $(MAX_INCLUDES)
+# c++ compiler flags
+RELEASE_CXXFLAGS = ${OPTIMIZATION_FLAGS} -Wall -shared -mno-cygwin $(WIN_DEFINES) $(MAX_INCLUDES)
+
+DEBUG_CFLAGS = -g -Wall -shared -mno-cygwin $(WIN_DEFINES) $(MAX_INCLUDES)
+DEBUG_CXXFLAGS = -g -Wall -shared -mno-cygwin $(WIN_DEFINES) $(MAX_INCLUDES)
+
+INSTALL_DIR = ../../Jamoma/library/externals/windows
+
+MAX_LIBS = -lMaxAPI -lMaxAudio
+
+ifeq "$(MAKECMDGOALS)" "release"
+	CFLAGS ?= $(RELEASE_CFLAGS)
+	CXXFLAGS ?= $(RELEASE_CXXFLAGS)
+endif
+ifeq "$(MAKECMDGOALS)" "debug"
+	CFLAGS ?= $(DEBUG_CFLAGS)
+	CXXFLAGS ?= $(DEBUG_CXXFLAGS)
+endif
+
+
+# suffix rules
+#-------------------------------------------------------------------------------
+.SUFFIX:
+.SUFFIX: .o .c .cpp .C .d
+
+%.o: %.cpp 
+	${CXX} ${CXXFLAGS} $(EXTRA_CXXFLAGS) -c $< -o ./${*}.o
+
+%.o: %.c
+	${CC} ${CFLAGS} $(EXTRA_CFLAGS) -c $< -o ./${*}.o
+
+%.o: %.C 
+	${CC} ${CFLAGS} $(EXTRA_CFLAGS) -c $< -o ./${*}.o
+
+
+# These little blurbs take advantage of sed to translate a target like
+# main.o: main.c defs.h   
+#
+# into
+# main.o main.d: main.c defs.h
+#
+# This makes each of our compiler generated dependency files depend on all of the
+# source and header files needed to make an object file.  This means our 
+# dependencies will always be up to date.
+%.d: %.c
+	@set -e; rm -f $@; \
+	$(CC) -M $(CFLAGS) $(EXTRA_CFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+
+%.d: %.cpp
+	@set -e; rm -f $@; \
+	$(CXX) -M $(CXXFLAGS) $(EXTRA_CXXFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+
+%.d: %.C
+	@set -e; rm -f $@; \
+	$(CC) -M $(CLAGS) $(EXTRA_CFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+	
---
+++ Externals/projekt.mk
@@ -0,0 +1,73 @@
+COMMON_INCLUDES = -I../common
+
+MAX_INCLUDES = -I/maxmspsdk/c74support/max-includes \
+   -I/maxmspsdk/c74support/msp-includes \
+   -I/maxmspsdk/c74support/common
+
+MAX_LIB_INCLUDES = -L/maxmspsdk/c74support/max-includes \
+   -L/maxmspsdk/c74support/msp-includes \
+   -L/maxmspsdk/c74support/common
+
+COMMON_SOURCE = jcom.core.cpp /maxmspsdk/c74support/max-includes/common/commonsyms.c
+JCOM_CORE_OBJS = jcom.core.o commonsyms.o
+CORE_OBJS = ../common/jcom.core.o ../common/commonsyms.o
+			
+# projects to be built
+JCOM_CHANGE_SOURCE = jcom.change.cpp jcom.change.def
+JCOM_CHANGE_OBJS = ${JCOM_CHANGE_SOURCE:.cpp=.o}
+
+JCOM_COLORSPACE_SOURCE = jcom.colorspace.cpp jcom.colorspace.def
+JCOM_COLORSPACE_OBJS = ${JCOM_COLORSPACE_SOURCE:.cpp=.o}
+
+JCOM_CUBIC_INTERPOLATE_SOURCE = jcom.cubic_interpolate.cpp jcom.cubic_interpolate.def
+JCOM_CUBIC_INTERPOLATE_OBJS = ${JCOM_CUBIC_INTERPOLATE_SOURCE:.cpp=.o}
+
+JCOM_DELTA_SOURCE = jcom.delta.cpp jcom.delta.def
+JCOM_DELTA_OBJS = ${JCOM_DELTA_SOURCE:.cpp=.o}
+
+JCOM_DELTA2_SOURCE = jcom.delta2.cpp jcom.delta2.def
+JCOM_DELTA2_OBJS = ${JCOM_DELTA2_SOURCE:.cpp=.o}
+
+JCOM_EQUALS_SOURCE = jcom.equals.cpp jcom.equals.def
+JCOM_EQUALS_OBJS = ${JCOM_EQUALS_SOURCE:.cpp=.o}
+
+JCOM_GAIN~_SOURCE = jcom.gain~.cpp jcom.gain~.def
+JCOM_GAIN~_OBJS = ${JCOM_GAIN~_SOURCE:.cpp=.o}
+
+JCOM_GANG_SOURCE = jcom.gang.cpp jcom.gang.def
+JCOM_GANG_OBJS = ${JCOM_GANG_SOURCE:.cpp=.o}
+
+JCOM_HUB_SOURCE = jcom.hub.cpp jcom.hub.autodoc.cpp jcom.hub.internals.cpp jcom.hub.presets.cpp jcom.hub.def
+JCOM_HUB_OBJS = ${JCOM_HUB_SOURCE:.cpp=.o}
+
+JCOM_IN_SOURCE = jcom.in.cpp jcom.in.def
+JCOM_IN_OBJS = ${JCOM_IN_SOURCE:.cpp=.o}
+
+JCOM_INIT_SOURCE = jcom.init.cpp jcom.init.def
+JCOM_INIT_OBJS = ${JCOM_INIT_SOURCE:.cpp=.o}
+
+JCOM_LIMITER~_SOURCE = jcom.limiter~.cpp jcom.limiter~.def
+JCOM_LIMITER~_OBJS = ${JCOM_LIMITER~_SOURCE:.cpp=.o}
+
+JCOM_METER~_SOURCE = jcom.meter~.cpp jcom.meter~.def
+JCOM_METER~_OBJS = ${JCOM_METER~_SOURCE:.cpp=.o}
+
+JCOM_MKDIR_SOURCE = jcom.mkdir.cpp jcom.mkdir.def
+JCOM_MKDIR_OBJS = ${JCOM_MKDIR_SOURCE:.cpp=.o}
+
+JCOM_OSCROUTE_SOURCE = jcom.oscroute.cpp jcom.oscroute.def
+JCOM_OSCROUTE_OBJS = ${JCOM_OSCROUTE_SOURCE:.cpp=.o}
+
+JCOM_OUT_SOURCE = jcom.out.cpp jcom.out.def
+JCOM_OUT_OBJS = ${JCOM_OUT_SOURCE:.cpp=.o}
+
+JCOM_PARAMETER_SOURCE = jcom.parameter.cpp jcom.parameter.clip.cpp jcom.parameter.def
+JCOM_PARAMETER_OBJS = ${JCOM_PARAMETER_SOURCE:.cpp=.o}
+
+
+
+ALL_OBJECTS = ${JCOM_CHANGE_OBJS} ${JCOM_COLORCHANGE_OBJS} ${JCOM_CUBIC_INTERPOLATE_OBJS} \
+			  ${JCOM_DELTA_OBJS} ${JCOM_DELTA2_OBJS} ${JCOM_GAIN~_OBJS} ${JCOM_GANG_OBJS} \
+			  ${JCOM_HUB_OBJS}, ${JCOM_IN_OBJS}, ${JCOM_INIT_OBJS}, ${JCOM_LIMITER~_OBJS} \
+			  ${JCOM_METER~_OBJS} ${JCOM_MKDIR_OBJS} ${JCOM_OSCROUTE_OBJS} ${JCOM_OUT_OBJS} \
+			  ${JCOM_PARAMETER_OBJS}
