89c71f2c0587b69607ae019ae5d63d8dbec98e8a
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2007-10-11 02:20:57 +0000

    main() now works with the latest build of the MaxObject framework.
    Added a wrapper class that creates the inlet and outlet for the class.  Maybe there is a way to do that more generically?
    
    
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@151 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- examples/tt.degrade~/main.m
+++ examples/tt.degrade~/main.m
@@ -1,12 +1,35 @@
 //	main.m
-//	Create the entry-point and wrapper for this class to be used as anexternal object for Max/MSP.
+//	Create the entry-point and wrapper for this class to be used as an external object for Max/MSP.
 //	Copyright © 2007 by Timothy Place
 //	License: GNU LGPL
 
-#import "MaxObject.h"
+#import "TTDegrade.h"
 
 // Entry Point when loaded by Max
 int main(void)
-{	
-	return [MaxObject createMaxClassWithName:"tt.degrade~" fromObjcClassWithName:"TTDegradeObject"];
+{
+	[MaxObject createMaxClassWithName:"tt.degrade~" fromObjcClassWithName:"TTDegradeMax"];
+	return 0;
 }
+
+
+// Max Wrapper Class
+
+@interface TTDegradeMax : TTDegradeObject
+{}
+@end
+
+@implementation TTDegradeMax
+
+- (id) initWithObject:(t_object *)x name:(t_symbol *)s numArgs:(long)argc andValues:(t_atom *)argv
+{
+	self = [super init];
+	if(self != nil){
+		maxobject = x;
+		[self createInletWithIndex:0	named:"signalIn"	withAssistanceMessage:"(signal) Input"];
+		[self createOutletWithIndex:0	named:"signalOut"	withAssistanceMessage:"(signal) Output"];
+	}
+	return self;
+}
+
+@end
