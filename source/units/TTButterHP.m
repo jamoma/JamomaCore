541f5d3a1dd742c977f8463479a462bb8d1fbe02
lossius <lossius@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2007-10-18 12:37:43 +0000

    Adding header and source for TTButterHP: 2nd order Butterworth high pass filter.
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@181 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ source/units/TTButterHP.m
@@ -0,0 +1,95 @@
+//	TTButterHPObject
+//	2nd order IIR Butterworth high pass filter
+//	Copyright © 2007 by Trond Lossius & Timothy A. Place
+//	License: GNU LGPL
+
+#import "TTButterHP.h"
+
+
+@implementation TTButterHPObject
+
+- (id)init
+{
+	[super init];
+	[self clearMessage];
+	[self setFloat:1000.0 forKey:@"frequencyAttribute"];
+	return self;
+}	
+
+
+- (void) dealloc
+{
+	[super dealloc];
+}
+
+
+- (TTErr) clearMessage
+{
+	short i;
+
+	for(i=0; i<TT_MAX_NUM_CHANNELS; i++){
+		x1[i] = x2[i] = y1[i] = y2[i] = 0;
+	}
+	return TT_ERR_NONE;
+}
+
+
+// accessor for frequencyAttribute
+- (void) setFrequencyAttribute:(float)newValue
+{
+	frequencyAttribute = newValue;
+	c = tan( TTPi*frequencyAttribute*srr );		// srr is the reciprocal of the sample-rate (1/sr)
+	a0 = 1 / (1 + TTSqrt2*c + c*c);
+	a1 = -2*a0;
+	a2 = a0;
+	b1 = 2*a0*( c*c - 1 );
+	b2 = a0 * (1 - TTSqrt2*c + c*c);	
+}
+
+
+// override the inherited sample-rate setter so we can update the cutoff frequency if needed
+- (void) setSr:(long)newValue
+{
+	if(newValue != sr){
+		[super setLong:newValue forKey:@"sr"];				// manually call the inherited accessor, since we still want it
+		[self setFrequencyAttribute:frequencyAttribute];	// send the same frequency to ourself, but with a new SR it will update the coefficients
+	}
+}
+
+
+// DSP LOOP
+- (TTErr) processAudioWithInput:(TTAudioSignal *)audioIn andOutput:(TTAudioSignal *)audioOut
+{
+	short			vs;
+	float			*in,
+					*out;
+	short			numChannels = [TTAudioSignal GetMinNumChannelsForASignal:audioIn andAnotherSignal:audioOut];
+	short			channel;
+	TTSampleValue	x0, y0;
+
+	if(bypassAttribute)
+		return [super processAudioWithInput:audioIn andOutput:audioOut];
+
+	// If there are more inputs than outputs, use the last input to set the frequency
+	if(audioIn->numChannels > numChannels)
+		[self setFrequencyAttribute:(*audioIn->vectors[audioIn->numChannels - 1]) ];
+
+	for(channel=0; channel<numChannels; channel++){
+		in = audioIn->vectors[channel];
+		out = audioOut->vectors[channel];
+		vs = audioIn->vs;
+		
+		while(vs--){
+			x0 = *in++;
+			y0 = TTAntiDenormal(a0*x0 - a1*x1[channel] + a2*x2[channel] - b1*y1[channel] - b2*y2[channel]);
+			x2[channel] = x1[channel];
+			x1[channel] = x0;
+			y2[channel] = y1[channel];
+			*out++ = y1[channel] = y0;
+		}
+	}
+	return TT_ERR_NONE;
+}
+
+
+@end
