30a9c7447f8b5b504071f91cfe2b2aa69b73bf17
tap <tap@464221e2-0a0f-0410-983b-f8d57d210c17>
2007-11-16 13:36:10 +0000

    a few things I missed
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@2395 464221e2-0a0f-0410-983b-f8d57d210c17

--- SourceCode/MaxObjects/projekt.mk
+++ SourceCode/MaxObjects/projekt.mk
@@ -3,8 +3,8 @@ COMMON_INCLUDES = -I../common
 MAX_INCLUDES = -I/maxmspsdk/c74support/max-includes \
    -I/maxmspsdk/c74support/msp-includes \
    -I/maxmspsdk/c74support/common \
-   -I../Externals_3rd_Party/ttblue/dsplib \
-   -I../../ramplib/ramplib_api
+   -I../ThirdParty/ttblue/dsplib \
+   -I../../RampLib/ramplib_api
 
 MAX_LIB_INCLUDES = -L/maxmspsdk/c74support/max-includes \
    -L/maxmspsdk/c74support/msp-includes \
@@ -15,9 +15,9 @@ JCOM_CORE_OBJS = jcom.core.o commonsyms.o
 CORE_OBJS = ../common/jcom.core.o ../common/commonsyms.o
 JCOM_CORE_AUDIO_OBJS = ../common/jcom.core~.o ../common/commonsyms.o
 
-TT_SOURCE = ../../../Externals_3rd_Party/ttblue/dsplib/tt_audio_base.cpp
+TT_SOURCE = ../../../ThirdParty/ttblue/dsplib/tt_audio_base.cpp
 TT_CORE_OBJS = tt_audio_base.o
-TT_OBJS = ../../Externals_3rd_Party/ttblue/dsplib/tt_audio_base.o
+TT_OBJS = ../../ThirdParty/ttblue/dsplib/tt_audio_base.o
 
 # projects to be built
 JCOM_CHANGE_SOURCE = jcom.change.cpp jcom.change.def
@@ -81,7 +81,7 @@ JCOM_MESSAGE_OBJS = ${JCOM_MESSAGE_SOURCE:.cpp=.o}
 JCOM_PASS_SOURCE = jcom.pass.cpp jcom.pass.def
 JCOM_PASS_OBJS = ${JCOM_PASS_SOURCE:.cpp=.o}
 
-JCOM_RAMP_SOURCE = jcom.ramp.cpp ../../ramplib/ramplib_api/RampUnitWrap.cpp jcom.ramp.def
+JCOM_RAMP_SOURCE = jcom.ramp.cpp ../../RampLib/ramplib_api/RampUnitWrap.cpp jcom.ramp.def
 JCOM_RAMP_OBJS = ${JCOM_RAMP_SOURCE:.cpp=.o}
 
 JCOM_RECEIVE_SOURCE = jcom.receive.cpp jcom.receivemaster.cpp jcom.receive.def
