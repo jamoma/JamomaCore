91dc11c825fedb57b962a26d50249341f5db132a
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-01-07 03:29:53 +0000

    Adding the one-pole lowpass filter.
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@252 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ source/units/TTLowpassOnePole.cpp
@@ -0,0 +1,88 @@
+/* 
+ * TTBlue 1-Pole Lowpass Filter Object
+ * Copyright © 2008, Tim Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "TTLowpassOnePole.h"
+
+
+TTLowpassOnePole::TTLowpassOnePole(TTUInt8 newMaxNumChannels)
+	: TTAudioObject::TTAudioObject(newMaxNumChannels),
+	feedback(NULL)
+{
+	// register parameters
+	registerParameter(TT("frequency"),	kTypeFloat64, &attrFrequency, (TTSetterMethod)&TTLowpassOnePole::setFrequency);
+
+	// register for notifications from the parent class so we can allocate memory as required
+	registerMessage(TT("updateMaxNumChannels"), (TTMethod)&TTLowpassOnePole::updateMaxNumChannels);
+	// make the clear method available to the outside world
+	registerMessage(TT("clear"), (TTMethod)&TTLowpassOnePole::clear);
+
+	// Set Defaults...
+	setParameterValue(TT("maxNumChannels"),	newMaxNumChannels);			// This parameter is inherited
+	setParameterValue(TT("frequency"),		4000.0);
+	setProcess((TTProcessMethod)&TTLowpassOnePole::processAudio);
+}
+
+
+TTLowpassOnePole::~TTLowpassOnePole()
+{
+	free(feedback);
+}
+
+
+TTErr TTLowpassOnePole::updateMaxNumChannels()
+{
+	if(feedback)
+		free(feedback);
+	feedback = (TTFloat64*)malloc(sizeof(TTFloat64) * maxNumChannels);
+	clear();
+	return kTTErrNone;
+}
+
+
+TTErr TTLowpassOnePole::clear()
+{
+	for(short i=0; i<maxNumChannels; i++)
+		feedback[i] = 0.0;
+	return kTTErrNone;
+}
+
+
+TTErr TTLowpassOnePole::setFrequency(TTValue& newValue)
+{	
+	TTFloat64	radians;
+
+	attrFrequency = newValue;
+	radians = hertzToRadians(attrFrequency);
+	coefficient = clip(radians / kTTPi, 0.0, 1.0);
+	
+	return kTTErrNone;
+}
+
+
+TTErr TTLowpassOnePole::processAudio(TTAudioSignal& in, TTAudioSignal& out)
+{
+	short			vs;
+	TTSampleValue	*inSample,
+					*outSample;
+	short			numchannels = TTAudioSignal::getMinChannelCount(in, out);
+	short			channel;
+
+	// This outside loop works through each channel one at a time
+	for(channel=0; channel<numchannels; channel++){
+		inSample = in.sampleVectors[channel];
+		outSample = out.sampleVectors[channel];
+		vs = in.vs;
+		
+		// This inner loop works through each sample within the channel one at a time
+		while(vs--){
+			*outSample++ = feedback[channel] = antiDenormal((*inSample++ * coefficient) + (feedback[channel] * (1.0 - coefficient)));
+		}
+	}
+	return kTTErrNone;
+}
+
