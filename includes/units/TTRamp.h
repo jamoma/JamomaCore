f9056c9eafa5ff3fc91b55d15e1718505e8c63ab
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-01-16 03:34:36 +0000

    TTRamp object ported.  The example project is a stub.  TTRamp is used by jcom.out~ in Jamoma, but it was missed when we compiled a list of things Jamoma uses.
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@285 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ includes/units/TTRamp.h
@@ -0,0 +1,63 @@
+/* 
+ * TTBlue Ramp Generator
+ * Copyright © 2008, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#ifndef __TT_RAMP_H__
+#define __TT_RAMP_H__
+
+#include "TTAudioObject.h"
+
+/**	TTRamp is an audio processor that provides a soft saturation
+ *	or overdrive effect to "warm" a sound up.
+ */ 
+class TTRamp : public TTAudioObject {
+private:
+	enum{
+		kUP = 0,
+		kDOWN
+	}
+	
+	TTSymbol			attrMode;				///< mode: sample_accurate or vector_accurate
+	TTFloat32 			attrRampTime;			///< ramp time in milliseconds
+	TTUInt32			rampSamples;			///< ramp time in samples
+	TTFloat32			attrCurrentValue;		///< 
+	TTFloat32			attrDestinationValue;	///< 
+	TTFloat64			step;					///< 
+	TTBoolean			direction;				///< 0 = ramp up, 1 = ramp down
+
+	/**	Setter for the mode attribute.  The options are 'sample' and 'vector' which determine 
+		the desired accuracy of the ramp that is generated. */
+	TTErr setMode(const TTValue& value);
+
+	/**	Stop the current ramp.			*/
+	TTErr stop();
+
+	/** Internal method for setting which process method to use. */
+	void setupProcess();
+
+	/**	Internal method called when the ramp is triggered (by setting the rampTime) */
+	void setStep();
+
+	/**	Process method -- This method is special!  It only sets the first sample of the vector and does not use it's input!	*/
+	TTErr processVectorAccurateDown(TTAudioSignal& in, TTAudioSignal& out);
+	/**	Process method -- This method is special!  It only sets the first sample of the vector and does not use it's input!	*/
+	TTErr processVectorAccurateUp(TTAudioSignal& in, TTAudioSignal& out);
+	/**	Process method 	*/
+	TTErr processSampleAccurateDown(TTAudioSignal& in, TTAudioSignal& out);
+	/**	Process method 	*/
+	TTErr processSampleAccurateUp(TTAudioSignal& in, TTAudioSignal& out);
+
+public:
+	/**	Constructor. */
+	TTRamp(TTUInt8 maxNumChannels);
+
+	/**	Destructor. */
+	~TTRamp();
+};
+
+
+#endif // __TT_RAMP_H__
---
+++ source/units/TTRamp.cpp
@@ -0,0 +1,193 @@
+/* 
+ * TTBlue Ramp Generator
+ * Copyright © 2008, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "TTRamp.h"
+
+
+TTRamp::TTRamp(TTUInt8 newMaxNumChannels)
+	: TTAudioObject::TTAudioObject(newMaxNumChannels),
+	attrCurrentValue(0), attrDestinationValue(0), step(0), direction(0)
+{
+	registerAttribute(TT("rampTime"),			kTypeFloat32,	&attrRampTime,	(TTSetterMethod)&TTRamp::setRampTime);
+	registerAttribute(TT("currentValue"),		kTypeFloat32,	&attrCurrentValue);
+	registerAttribute(TT("destinationValue"),	kTypeFloat32,	&attrDestinationValue);
+	registerAttribute(TT("mode"),				kTypeSymbol,	&attrMode,		(TTSetterMethod)&TTRamp::setMode);
+	
+	registerMessage(TT("stop"), (TTMethod)&TTRamp::stop);	
+	registerMessage(TT("rampTimeInSamples"), (TTMethod)&TTRamp::rampInSamples);	
+
+	setAttributeValue(TT("mode"), TT("vector"));
+}
+
+
+TTRamp::~TTRamp()
+{
+	;
+}
+
+
+TTErr TTRamp::rampInSamples(const TTValue& newValue)
+{
+	rampSamples = newValue;
+	if(rampSamples == 0){
+		step = 0;
+		attrCurrentValue = attrDestinationValue;
+		direction = 0;
+	}
+	else{
+		attrRampTime = 1000.0 * (rampSamples / TTFloat32(sr));
+		setStep();		
+	}
+	setupProcess();
+	return kTTErrNone;
+}
+
+
+TTErr TTRamp::setRampTime(const TTValue& newValue)
+{
+	attrRampTime = newValue;
+	if((attrRampTime <= 0.0 + TTAntiDenormalValue) && (attrRampTime >= 0.0 - TTAntiDenormalValue)){
+		step = 0;
+		attrCurrentValue = attrDestinationValue;
+		direction = 0;
+	}
+	else{
+		rampSamples = long((attrRampTime * 0.001) * sr);
+		setStep();		
+	}
+	setupProcess();
+	return kTTErrNone;
+}
+
+
+TTErr TTRamp::setMode(const TTValue& newValue)
+{
+	attrMode = newValue;
+	setupProcess();
+	return kTTErrNone;
+}
+
+
+TTErr TTRamp::stop()
+{
+	step = 0;
+	return kTTErrNone;
+}
+
+
+void TTRamp::setupProcess()
+{
+	if((mode == TT("sample")) && (direction == kUP))
+		setProcess((TTProcessMethod)&TTRamp::processSampleAccurateUp);
+	else if((mode == TT("sample")) && (direction == kDOWN))
+		setProcess((TTProcessMethod)&TTRamp::processSampleAccurateDown);
+	else if((mode == TT("vector")) && (direction == kUP))
+		setProcess((TTProcessMethod)&TTRamp::processVectorAccurateUp);
+	else
+		setProcess((TTProcessMethod)&TTRamp::processVectorAccurateDown);
+}
+
+
+void TTRamp::setStep()
+{
+	step = (attrDestinationValue - attrCurrentValue) / double(ramp_samps);
+	direction = (step < 0);
+}
+
+
+TTErr TTRamp::processVectorAccurateDown(TTAudioSignal& in, TTAudioSignal& out)
+{
+	TTSampleValue	*outSample;
+	short			numchannels = out.numChannels;
+	short			channel;
+
+	for(channel=0; channel<numchannels; channel++){
+		outSample = out.sampleVectors[channel];
+		if(step){
+			attrCurrentValue += (step * out->vectorsize);
+			if(attrCurrentValue <= attrDestinationValue){
+				step = 0;
+				attrCurrentValue = attrDestinationValue;	// clamp
+			}
+		}
+		*outSample = attrCurrentValue;
+	}
+	return kTTErrNone;
+}
+
+
+TTErr TTRamp::processVectorAccurateUp(TTAudioSignal& in, TTAudioSignal& out)
+{
+	TTSampleValue	*outSample;
+	short			numchannels = out.numChannels;
+	short			channel;
+
+	for(channel=0; channel<numchannels; channel++){
+		outSample = out.sampleVectors[channel];
+		if(step){
+			attrCurrentValue += (step * out->vectorsize);
+			if(attrCurrentValue >= attrDestinationValue){
+				step = 0;
+				attrCurrentValue = attrDestinationValue;	// clamp
+			}
+		}
+		*outSample = attrCurrentValue;
+	}
+	return kTTErrNone;
+}
+
+
+TTErr TTRamp::processSampleAccurateDown(TTAudioSignal& in, TTAudioSignal& out)
+{
+	TTSampleValue	*outSample;
+	short			numchannels = out.numChannels;
+	short			channel;
+	short			vs;
+
+	for(channel=0; channel<numchannels; channel++){
+		vs = out.vs;
+		outSample = out.sampleVectors[channel];
+		while(vs--){
+			if(step){
+				attrCurrentValue += step;
+				if(attrCurrentValue <= attrDestinationValue){
+					step = 0;
+					attrCurrentValue = attrDestinationValue; // clamp
+				}
+			}
+			*outSample++ = attrCurrentValue;	
+		}
+	}
+	return kTTErrNone;
+}
+
+
+TTErr TTRamp::processSampleAccurateUp(TTAudioSignal& in, TTAudioSignal& out)
+{
+	TTSampleValue	*outSample;
+	short			numchannels = out.numChannels;
+	short			channel;
+	short			vs;
+
+	for(channel=0; channel<numchannels; channel++){
+		vs = out.vs;
+		outSample = out.sampleVectors[channel];
+		while(vs--){
+			if(step){
+				attrCurrentValue += step;
+				if(attrCurrentValue >= attrDestinationValue){
+					step = 0;
+					attrCurrentValue = attrDestinationValue; // clamp
+				}
+			}
+			*outSample++ = attrCurrentValue;	
+		}
+	}
+	return kTTErrNone;
+}
+
