c2ef95f740a36a6119c5ba35cc04145ff5d24f8c
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-02-23 21:17:15 +0000

    Starting to fill in the details for the (still non-functional) TTList class.
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@328 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- includes/core/TTList.h
+++ includes/core/TTList.h
@@ -10,14 +10,58 @@
 #define __TT_LIST_H__
 
 #include "TTElement.h"
+#include "TTValue.h"
 
 /****************************************************************************************************/
 // Class Specification
 
+typedef struct TTListItem {
+	TTValue*		value;
+	TTListItem*		prev;
+	TTListItem*		next;
+};
+	
+
 class TTList : TTElement {
+private:
+	TTValue*		list;
+	TTUInt32		size;			///< number of items stored in the list
+	TTListItem*		head;
+	TTListItem*		tail;
+	TTListItem*		currentItem;
+	
+	/** utility used by both remove() and clear()  */
+	void doRemove(TTListItem* item);
+	
 public:
 	TTList();
-	virtual ~TTList();	
+	virtual ~TTList();
+
+	/** return the number of values in the list. */
+	TTUInt32 getSize();
+	
+	/** return the number of values in the list. */
+	TTValue* getHead();
+	
+	/** return the number of values in the list. */
+	TTValue* getTail();
+	
+	/** appends a value to the list. */
+	void append(TTValue* newValue);
+
+// how to know where to insert it?	
+//	void insert(TTValue* newItem);
+
+	/** remove a specified item. */
+	void remove(TTValue* value);
+	void remove(TTListItem* item);
+	
+	/** remove all items from the list */
+	void clear();
+
+	/** find the first item with this value, and return it. */
+	TTListItem* findFirst(TTValue* value);
+	
 };
 
 
