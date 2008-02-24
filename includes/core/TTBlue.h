58f328298a6082766d0b1d82f22c1280622d815d
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-02-24 03:00:43 +0000

    New file to represent the entry-point for the entire TTBlue library, now that we are treating it as a library instead just a box full of parts.  The new init function in here isn't doing anything yet, but it will shortly, at which point we will want to be sure it is called before doing other things when we use TTBlue.
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@334 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ includes/core/TTBlue.h
@@ -0,0 +1,22 @@
+/* 
+ * TTBlue Library
+ * Copyright © 2008, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#ifndef __TT_BLUE_H__
+#define __TT_BLUE_H__
+
+#include "TTElement.h"
+#include "TTGlobal.h"
+#include "TTThread.h"
+#include "TTSymbolCache.h"
+#include "TTValueCache.h"
+
+
+void TTBlueInit();
+
+
+#endif // __TT_BLUE_H__
