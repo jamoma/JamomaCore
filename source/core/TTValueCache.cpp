e533da17afcea999aa15c6df2f311c15a0fe171a
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-02-24 02:56:08 +0000

    fixing header comment
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@333 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- source/core/TTValueCache.cpp
+++ source/core/TTValueCache.cpp
@@ -1,5 +1,5 @@
 /* 
- * TTBlue Class for representing Values
+ * TTBlue Class for caching common values for speed
  * Copyright © 2008, Timothy Place
  * 
  * License: This code is licensed under the terms of the GNU LGPL
