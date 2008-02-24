75390e3206b2126e94e7fcb043db02f747639af1
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-02-24 02:55:17 +0000

    New pair of files for a "symbol cache".  This is a place we can cache commonly used symbols so that we don't have to constantly do lookups in the symbol table (which we usually do with the TT macro).
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@332 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ includes/core/TTSymbolCache.h
@@ -0,0 +1,24 @@
+/* 
+ * TTBlue Class for caching common symbols for speed
+ * Copyright © 2008, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#ifndef __TT_VALUE_CACHE_H__
+#define __TT_VALUE_CACHE_H__
+
+#include "TTValue.h"
+
+
+extern TTValue	kTTSym_processInPlace;
+extern TTValue	kTTSym_bypass;
+extern TTValue	kTTSym_sr;
+extern TTValue	kTTSym_maxNumChannels;
+extern TTValue	kTTSym_updateMaxNumChannels;
+extern TTValue	kTTSym_updateSr;
+
+
+#endif // __TT_VALUE_CACHE_H__
+
---
+++ source/core/TTSymbolCache.cpp
@@ -0,0 +1,18 @@
+/* 
+ * TTBlue Class for caching common symbols for speed
+ * Copyright © 2008, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "TTValueCache.h"
+
+
+TTValue	kTTSym_processInPlace("processInPlace");
+TTValue	kTTSym_bypass("bypass");
+TTValue	kTTSym_sr("sr");
+TTValue	kTTSym_maxNumChannels("maxNumChannels");
+TTValue	kTTSym_updateMaxNumChannels("updateMaxNumChannels");
+TTValue	kTTSym_updateSr("updateSr");
+
