1a7deef085b3805fd2470e7b14fefb89f182e54e
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-03-09 17:21:12 +0000

    rely on search path to find svnversion now
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@337 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- examples/common/infoplist_versionmaker.pl
+++ examples/common/infoplist_versionmaker.pl
@@ -5,7 +5,7 @@ use strict;
 die "$0: Must be run from Xcode" unless $ENV{"BUILT_PRODUCTS_DIR"};
 
 # Get the current subversion revision number and use it to set the CFBundleVersion value
-my $REV = `/usr/local/bin/svnversion -n ./`;
+my $REV = `svnversion -n ./`;
 my $INFO = "$ENV{BUILT_PRODUCTS_DIR}/$ENV{WRAPPER_NAME}/Contents/Info.plist";
 
 my $version = $REV;
