b2ccd257f7b7bf7c5284ebc0643290f447c33137
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-05-17 12:17:03 +0000

    TTPhasor ported from version 0.3
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@413 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ includes/units/TTPhasor.h
@@ -0,0 +1,56 @@
+/* 
+ * TTBlue Cycling Ramp Generator
+ * Copyright © 2003, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#ifndef __TT_PHASOR_H__
+#define __TT_PHASOR_H__
+
+#include "TTAudioObject.h"
+
+/**	TTRamp is an audio processor that provides a soft saturation
+ *	or overdrive effect to "warm" a sound up.
+ */ 
+TTCLASS TTPhasor : public TTAudioObject {
+private:	
+	TTFloat64 			attrFrequency;			///< ramp time in milliseconds
+	TTFloat64			attrGain;				///< gain in dB
+	TTSampleValue		attrPhase;				///< current location in the ramp
+	
+	TTFloat64			rampMilliseconds;		///< ramp time in milliseconds
+	TTUInt32			rampSamples;			///< ramp time in samples
+	TTFloat64			step;					///< step size
+	TTFloat64			linearGain;				///< actual gain coefficient derived from attrGain
+
+	/** Receives notifications when there are changes to the inherited 
+		sr attribute.						*/
+	TTErr updateSr();
+
+	/**	Setter for the frequency attribute in Hertz.   */
+	TTErr setFrequency(const TTAttribute&, const TTValue& newValue);
+
+	/**	Setter the gain attribute in dB.   */
+	TTErr setGain(const TTAttribute&, const TTValue& newValue);
+
+	/**	Getter for the gain attribute.		*/
+	TTErr getGain(const TTAttribute&, TTValue& value);
+
+	/**	Internal method called when the ramp is triggered (by setting the rampTime) */
+	void setStep();
+
+	/**	Process method -- This method is special!  It does not use it's input!	*/
+	TTErr processAudio(TTAudioSignal& in, TTAudioSignal& out);
+
+public:
+	/**	Constructor. */
+	TTPhasor(TTUInt8 maxNumChannels);
+
+	/**	Destructor. */
+	~TTPhasor();
+};
+
+
+#endif // __TT_PHASOR_H__
