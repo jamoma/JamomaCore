da93e9321e98742289834fa2030f701bfef402d2
tim <tim@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-05-18 01:05:07 +0000

    Initial TTPulseSub class (ported from version 0.3).  It's doubtful that this works yet, as it is untested.  Particularly w/r/t multichannel operation.  But it should be close.
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@429 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ includes/units/TTPulseSub.h
@@ -0,0 +1,58 @@
+/* 
+ * TTBlue Pulse-based Envelope Substitution
+ * Copyright © 2004, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#ifndef __TT_PULSESUB_H__
+#define __TT_PULSESUB_H__
+
+#include "TTAudioObject.h"
+#include "TTPhasor.h"
+#include "TTAdsr.h"
+#include "TTOperator.h"
+
+
+/**	TTPulseSub generates a repeating ADSR envelope which is then applied to the gain of an input signal.
+ */ 
+TTCLASS TTPulseSub : public TTAudioObject {
+private:
+	TTFloat64	attrAttack;
+	TTFloat64	attrDecay;
+	TTFloat64	attrSustain;
+	TTFloat64	attrRelease;
+	TTBoolean	attrTrigger;
+	TTSymbol&	attrMode;
+
+	TTOperator*		offset;
+	TTPhasor*		phasor;
+	TTAdsr*			env_gen;
+	TTOperator*		scaler;
+	TTAudioSignal*	sig1;
+	TTAudioSignal*	sig2;
+	
+
+	TTErr updateSr();
+	
+	TTErr setTrigger(const TTAttribute&, const TTValue& newValue);
+	TTErr setAttack(const TTAttribute&, const TTValue& newValue);
+	TTErr setDecay(const TTAttribute&, const TTValue& newValue);
+	TTErr setSustain(const TTAttribute&, const TTValue& newValue);
+	TTErr setRelease(const TTAttribute&, const TTValue& newValue);
+	TTErr setMode(const TTAttribute&, const TTValue& newValue);
+	
+	TTErr processAudio(TTAudioSignal& in, TTAudioSignal& out);
+	
+		
+	
+public:
+	TTPulseSub(TTUInt8 newMaxNumChannels);
+	~TTPulseSub();
+	
+
+};
+
+#endif // __TT_PULSESUB_H__
+
