84a01dd093ef16cc6dec6fdc2bdb9941b55255f4
watsond <watsond@464221e2-0a0f-0410-983b-f8d57d210c17>
2008-06-16 17:50:12 +0000

    update windows install scripts for Jamoma 0.5 and Max 5
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@3502 464221e2-0a0f-0410-983b-f8d57d210c17

--- Installers/Windows/makeInstaller.sh
+++ Installers/Windows/makeInstaller.sh
@@ -13,10 +13,19 @@ mkdir root/Cycling\ \'74/max-startup
 mkdir root/patches
 mkdir root/patches/extras
 mkdir root/support
+mkdir root/Cycling\ \'74/default-definitions
+mkdir root/Cycling\ \'74/default-settings
+mkdir root/Cycling\ \'74/extensions
 
 cp -r ../../Jamoma root/Cycling\ \'74
 mv root/Cycling\ \'74/Jamoma/library/externals/windows/jcom.meter~.mxe root/Cycling\ \'74/max-startup/jcom.meter~.mxe
-cp root/Cycling\ \'74/Jamoma/documentation/jamoma-overview.mxt root/patches/extras/jamoma-overview.mxt
+mv root/Cycling\ \'74/Jamoma/library/externals/windows/jcom.loader.mxe root/Cycling\ \'74/extensions/jcom.loader.mxe
+cp root/Cycling\ \'74/Jamoma/documentation/jamoma-overview.maxpat root/patches/extras/jamoma-overview.maxpat
+cp root/Cycling\ \'74/Jamoma/JamomaDark.maxdefaults root/Cycling\ \'74/default-settings/JamomaDark.maxdefaults
+cp root/Cycling\ \'74/Jamoma/JamomaLight.maxdefaults root/Cycling\ \'74/default-settings/JamomaLight.maxdefaults
+cp root/Cycling\ \'74/Jamoma/jcom.ui.maxdefines root/Cycling\ \'74/default-definitions/jcom.ui.maxdefines
+mv root/Cycling\ \'74/Jamoma/library/externals/windows/JamomaFramework.dll root/support
+mv root/Cycling\ \'74/Jamoma/library/externals/windows/TTBlue.dll root/support
 mv root/Cycling\ \'74/Jamoma/library/third-party/win-support/iconv.dll root/support
 mv root/Cycling\ \'74/Jamoma/library/third-party/win-support/libxml2.dll root/support
 mv root/Cycling\ \'74/Jamoma/library/third-party/win-support/zlib1.dll root/support
@@ -28,10 +37,10 @@ echo "making candle with paraffin"
 ../wix/Paraffin.exe -dir root/support\\       -custom JamomaSupport -g -direXclude .svn -ext .WXS JamomaSupport.wxs
 
 echo "now making the installer"
-../wix/candle.exe -dvar.ProductVersion="0.4.6" -dvar.ProductName="Jamoma 0.4.6" /nologo JamomaC74.wxs
-../wix/candle.exe -dvar.ProductVersion="0.4.6" -dvar.ProductName="Jamoma 0.4.6" /nologo JamomaPatches.wxs
-../wix/candle.exe -dvar.ProductVersion="0.4.6" -dvar.ProductName="Jamoma 0.4.6" /nologo JamomaSupport.wxs
-../wix/candle.exe -dvar.ProductVersion="0.4.6" -dvar.ProductName="Jamoma 0.4.6" /nologo main.wxs 
-../wix/candle.exe -dvar.ProductVersion="0.4.6" -dvar.ProductName="Jamoma 0.4.6" /nologo ui.wxs 
+../wix/candle.exe -dvar.ProductVersion="0.5" -dvar.ProductName="Jamoma 0.5" /nologo JamomaC74.wxs
+../wix/candle.exe -dvar.ProductVersion="0.5" -dvar.ProductName="Jamoma 0.5" /nologo JamomaPatches.wxs
+../wix/candle.exe -dvar.ProductVersion="0.5" -dvar.ProductName="Jamoma 0.5" /nologo JamomaSupport.wxs
+../wix/candle.exe -dvar.ProductVersion="0.5" -dvar.ProductName="Jamoma 0.5" /nologo main.wxs 
+../wix/candle.exe -dvar.ProductVersion="0.5" -dvar.ProductName="Jamoma 0.5" /nologo ui.wxs 
 ../wix/light.exe /nologo /out Jamoma.msi main.wixobj JamomaC74.wixobj JamomaPatches.wixobj JamomaSupport.wixobj ui.wixobj ..\\wix\\wixui.wixlib -loc ..\\wix\\WixUI_en-us.wxl
 
