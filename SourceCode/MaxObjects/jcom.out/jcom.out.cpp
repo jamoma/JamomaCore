e851dca82c127b3aa2d589720d47c6fb67243dda
lossius <lossius@464221e2-0a0f-0410-983b-f8d57d210c17>
2008-06-17 15:47:07 +0000

    Attempt at getting sound working in jcom.out~ for the input module, but it didn't quite work...
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@3543 464221e2-0a0f-0410-983b-f8d57d210c17

--- SourceCode/MaxObjects/jcom.out/jcom.out.cpp
+++ SourceCode/MaxObjects/jcom.out/jcom.out.cpp
@@ -147,6 +147,7 @@ void *out_new(t_symbol *s, long argc, t_atom *argv)
 		jcom_core_subscriber_new_common(&x->common, jps__jcom_out__, jps_subscribe_out);
 		jcom_core_subscriber_setcustomsubscribe_method(&x->common, &out_subscribe);
 		
+		x->gain->setAttributeValue(TT("linearGain"), 1.0);
 		attr_args_process(x, argc, argv);					// handle attribute args				
 		defer_low(x, (method)jcom_core_subscriber_subscribe, 0, 0, 0);
 	}
