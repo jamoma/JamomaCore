eb53e3fe54ec815dba6f057b1de8999828263952
tap <tap@464221e2-0a0f-0410-983b-f8d57d210c17>
2008-06-25 11:49:26 +0000

    Merging 16-bit channel counting from the TTBlue repository.
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@3678 464221e2-0a0f-0410-983b-f8d57d210c17

--- SourceCode/ThirdParty/TTBlue/source/core/TTAudioObject.cpp
+++ SourceCode/ThirdParty/TTBlue/source/core/TTAudioObject.cpp
@@ -19,7 +19,7 @@ static const double kGainMidiPowerInv = 1./kGainMidiPower;
 
 /****************************************************************************************************/
 
-TTAudioObject::TTAudioObject(const char* name, TTUInt8 newMaxNumChannels)
+TTAudioObject::TTAudioObject(const char* name, TTUInt16 newMaxNumChannels)
 	: TTObject(name)
 {
 	registerAttribute(TT("maxNumChannels"), kTypeUInt8,		&maxNumChannels,	(TTSetterMethod)&TTAudioObject::setMaxNumChannels);
@@ -45,8 +45,10 @@ TTAudioObject::~TTAudioObject()
 
 TTErr TTAudioObject::setMaxNumChannels(const TTValue& newValue)
 {
+	TTValue	oldMaxNumChannels = maxNumChannels;
+	
 	maxNumChannels = newValue;
-	sendMessage(TT("updateMaxNumChannels"));
+	sendMessage(TT("updateMaxNumChannels"), oldMaxNumChannels);
 	return kTTErrNone;
 }
 
