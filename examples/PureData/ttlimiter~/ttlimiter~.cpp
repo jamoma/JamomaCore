f7f15609f85586a42f29dce6f076bc40354e7770
tim@electrotap.com <tim@electrotap.com@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-07-03 05:07:30 +0000

    A working example of a TTBlue object (the limiter) in Pd. It isn't sexy, but it works...
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@542 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ examples/PureData/ttlimiter~/ttlimiter~.cpp
@@ -0,0 +1,116 @@
+/*
+ * ttlimiter~ Object for Pure Data
+ * Written by Tim Place
+ * Copyright © 2008
+ * 
+ */
+
+#include "m_pd.h"
+#include "TTBlueAPI.h"
+
+
+// Types & Structs
+typedef struct _ttlimiter {
+	t_object		obj;
+	t_outlet*		outlet;			// outlet for status and version messages
+	t_float			f;				// dummy for signal in first inlet
+	TTLimiter*		limiter;		// the TTBlue limiter object
+	TTAudioSignal*	audioIn;
+	TTAudioSignal*	audioOut;
+} t_ttlimiter;
+
+
+// Prototypes
+extern "C" {
+void ttlimiter_tilde_setup();
+void* ttlimiter_new(t_symbol *s, long ac, t_atom *at);
+void ttlimiter_free(t_ttlimiter *x);
+void ttlimiter_setpreamp(t_ttlimiter *x, double f);
+t_int* ttlimiter_perform(t_int *w);
+void ttlimiter_dsp(t_ttlimiter *x, t_signal **sp);
+}
+
+// Statics & Globals
+static t_class *ttlimiter_class;
+
+
+// Class Definition
+void ttlimiter_tilde_setup(void) 
+{	
+	TTBlueInit();
+	
+	ttlimiter_class = class_new(gensym("ttlimiter~"), (t_newmethod)ttlimiter_new, (t_method)ttlimiter_free, 
+		sizeof(t_ttlimiter), 0, A_GIMME, 0);
+
+	CLASS_MAINSIGNALIN(ttlimiter_class, t_ttlimiter, f);
+    class_addmethod(ttlimiter_class, (t_method)ttlimiter_dsp, gensym("dsp"), A_NULL);
+	class_addmethod(ttlimiter_class, (t_method)ttlimiter_setpreamp,	gensym("preamp"), A_FLOAT, 0);
+
+    class_sethelpsymbol(ttlimiter_class, gensym("help-ttlimiter~.pd"));
+}
+
+
+// Life Cycle
+
+void *ttlimiter_new(t_symbol *s, long ac, t_atom *at)
+{
+	t_ttlimiter *x = (t_ttlimiter*)pd_new(ttlimiter_class);
+
+	if(x){
+		outlet_new(&x->obj, gensym("signal"));	// Create new signal outlet
+		x->limiter = new TTLimiter(1);			// Just a mono limiter for now...
+		x->audioIn = new TTAudioSignal(1);
+		x->audioOut = new TTAudioSignal(1);
+	}
+	return(x);
+}
+
+
+void ttlimiter_free(t_ttlimiter *x)
+{
+	delete x->limiter;
+}
+
+
+// Methods
+
+// Method for Posting Status
+void ttlimiter_setpreamp(t_ttlimiter *x, float f)
+{
+	x->limiter->setAttributeValue(TT("preamp"), f);
+}
+
+
+// Perform (signal) Method
+t_int *ttlimiter_perform(t_int *w)
+{
+	t_ttlimiter*	x = (t_ttlimiter*)(w[1]);		
+	t_float*		in = (t_float*)(w[2]);
+	t_float*		out = (t_float*)(w[3]);
+	//int			n = (int)(w[4]);
+	//TTUInt8		numChannels = x->audioIn->getNumChannels();
+	TTUInt16		vs = x->audioIn->getVectorSize();
+
+	x->audioIn->setVector(0, vs, (t_float*)in);
+	x->limiter->process(x->audioIn, x->audioOut);
+	x->audioOut->getVector(0, vs, (t_float*)out);
+	
+	return (w+5);
+}
+
+
+// DSP Method
+void ttlimiter_dsp(t_ttlimiter *x, t_signal **sp)
+{
+	x->audioIn->setNumChannels(1);
+	x->audioOut->setNumChannels(1);
+	x->audioIn->setVectorSize(sp[0]->s_n);
+	x->audioOut->setVectorSize(sp[0]->s_n);
+	//audioIn will be set in the perform method
+	x->audioOut->alloc();
+	
+	x->limiter->setAttributeValue(TT("sr"), sp[0]->s_sr);
+	
+    dsp_add(ttlimiter_perform, 4, x, sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
+}
+
