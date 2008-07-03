e05f21974dfabf9ef75902207006a166380eba16
tim@electrotap.com <tim@electrotap.com@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-07-03 00:32:43 +0000

    A working VST plug-in based on TTOverdrive.
    Having realized what a disaster VST3 is, this is now done using VST2.4.  This example plug was tested in Live 7.0.7 on the Mac and seems to be doing what it is supposed to be doing.
    
    
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@541 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- examples/VST/BlueSaturation/source/BlueSaturation.cpp
+++ examples/VST/BlueSaturation/source/BlueSaturation.cpp
@@ -1,390 +1,209 @@
-//------------------------------------------------------------------------
-// Project     : VST SDK
-// Version     : 3.0
-//
-// Category    : Examples
-// Filename    : again.cpp
-// Created by  : Steinberg, 04/2005
-// Modified    : $Date: 2008/01/16 07:48:35 $
-// Description : BlueSaturation Example for VST SDK 3.0
-//-----------------------------------------------------------------------------
-// LICENSE
-// © 2008, Steinberg Media Technologies GmbH, All Rights Reserved
-//-----------------------------------------------------------------------------
-// This Software Development Kit may not be distributed in parts or its entirety  
-// without prior written agreement by Steinberg Media Technologies GmbH. 
-// This SDK must not be used to re-engineer or manipulate any technology used  
-// in any Steinberg or Third-party application or software module, 
-// unless permitted by law.
-// Neither the name of the Steinberg Media Technologies nor the names of its
-// contributors may be used to endorse or promote products derived from this 
-// software without specific prior written permission.
-// 
-// THIS SDK IS PROVIDED BY STEINBERG MEDIA TECHNOLOGIES GMBH "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL STEINBERG MEDIA TECHNOLOGIES GMBH BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-// OF THE POSSIBILITY OF SUCH DAMAGE.
-//-----------------------------------------------------------------------------
+#include "BlueSaturation.h"
 
-//------------------------------------------------------------------------
-#define INIT_CLASS_IID
-// This macro definition modifies the behavior of DECLARE_CLASS_IID (funknown.h)
-// and produces the actual symbols for all interface identifiers.
-// It must be defined before including the interface headers and
-// in only one source file!
-//------------------------------------------------------------------------
 
-#include "BLueSaturation.h"
-#include "BLueSaturationParamids.h"
-#include "BLueSaturationEditor.h" // here in order to produce all interface identifiers required by editor
-
-#include "pluginterfaces/base/ibstream.h"
-#include "pluginterfaces/vst/ivstparameterchanges.h"
-#include "pluginterfaces/vst/ivstevents.h"
-
-#include <stdio.h>
-
-#define AGAIN_TEST 1 // this allows to enable the communication example between again and its controller
-
-//------------------------------------------------------------------------
-// BlueSaturation Implementation
-//------------------------------------------------------------------------
-BlueSaturation::BlueSaturation ()
-: fGain (1.f)
-, fGainReduction (0.f)
-, fVuPPMOld (0.f)
-, currentProcessMode (-1) // -1 means not initialized
-, bHalfGain (false)
-, bBypass (false)
+AudioEffect* createEffectInstance(audioMasterCallback audioMaster)
 {
-	// register its editor class (the same than used in againentry.cpp)
-	setControllerClass (FUID (gControllerUID));
+	return new BlueSaturation(audioMaster);
 }
 
-//------------------------------------------------------------------------
-tresult PLUGIN_API BlueSaturation::initialize (FUnknown* context)
+
+/***************************************************************************/
+
+BlueSaturation::BlueSaturation(audioMasterCallback audioMaster)
+	: AudioEffectX(audioMaster, kNumPresets, kNumParameters)
 {
-	//---always initialize the parent-------
-	tresult result = AudioEffect::initialize (context);
-	// if everything Ok, continue
-	if (result != kResultOk)
-		return result;
-
-	//---create Audio In/Out busses------
-	// we want a stereo Input and a Stereo Output
-	addAudioInput  (USTRING ("Stereo In"),  SpeakerArr::kStereo);
-	addAudioOutput (USTRING ("Stereo Out"), SpeakerArr::kStereo);
-
-	//---create Midi In/Out busses (1 bus with only 1 channel)------
-	addEventInput (USTRING ("Midi In"), 1);
-
-	return kResultOk;
-}
-
-//------------------------------------------------------------------------
-tresult PLUGIN_API BlueSaturation::terminate  ()
-{
-	// nothing to do here yet...except calling our parent terminate
-	return AudioEffect::terminate ();
-}
-
-//------------------------------------------------------------------------
-tresult PLUGIN_API BlueSaturation::setActive (TBool state)
-{
-#if AGAIN_TEST
-	if (state)
-		sendTextMessage ("BlueSaturation::setActive (true)");
-	else
-		sendTextMessage ("BlueSaturation::setActive (false)");
-#endif
+	TTBlueInit();
 	
-	// reset the VuMeter value
-	fVuPPMOld = 0.f;
+	setNumInputs(2);		// stereo in
+	setNumOutputs(2);		// stereo out
+	setUniqueID('TTOv');	// identify
+	canProcessReplacing();	// supports replacing output
+	canDoubleReplacing();	// supports double precision processing
 
-	// call our parent setActive
-	return AudioEffect::setActive (state);
-}
+	//fGain = 1.f;			// default to 0 dB
+	mOverdrive = new TTOverdrive(2);
 
-//------------------------------------------------------------------------
-tresult PLUGIN_API BlueSaturation::process (ProcessData& data)
-{
-	// finally the process function
-	// In this example there are 4 steps:
-	// 1) Read inputs parameters coming from host (in order to adapt our model values)
-	// 2) Read inputs events coming from host (we apply a gain reduction depending of the velocity of pressed key)
-	// 3) Process the gain of the input buffer to the output buffer
-	// 4) Write the new VUmeter value to the output Parameters queue
+	mInput = new TTAudioSignal(2);
+	mInput->setNumChannels(2);
 
-
-	//---1) Read inputs parameter changes-----------
-	IParameterChanges* paramChanges = data.inputParameterChanges;
-	if (paramChanges)
-	{
-		int32 numParamsChanged = paramChanges->getParameterCount ();
-		// for each parameter which are some changes in this audio block:
-		for (int32 i = 0; i < numParamsChanged; i++)
-		{
-			IParamValueQueue* paramQueue = paramChanges->getParameterData (i);
-			if (paramQueue)
-			{
-				int32 offsetSamples;
-				double value;
-				int32 numPoints = paramQueue->getPointCount ();				
-				switch (paramQueue->getParameterId ())
-				{
-					case kGainId:
-						// we use in this example only the last point of the queue.
-						// in some wanted case for specific kind of parameter it makes sense to retrieve all points
-						// and process the whole audio block in small blocks.
-						if (paramQueue->getPoint (numPoints - 1,  offsetSamples, value) == kResultTrue)
-							fGain = (float)value;
-						break;
-
-					case kBypassId:
-						if (paramQueue->getPoint (numPoints - 1,  offsetSamples, value) == kResultTrue)
-							bBypass = (value > 0.5f);
-						break;
-				}
-			}
-		}
-	}
+	mOutput = new TTAudioSignal(2);
+	mOutput->setNumChannels(2);
 	
-	//---2) Read input events-------------
-	IEventList* eventList = data.inputEvents;
-	if (eventList) 
-	{
-		int32 numEvent = eventList->getEventCount ();
-		for (int32 i = 0; i < numEvent; i++)
-		{
-			Event event;
-			if (eventList->getEvent (i, event) == kResultOk)
-			{
-				switch (event.type)
-				{
-					//----------------------
-					case Event::kNoteOnEvent:
-						// use the velocity as gain modifier
-						fGainReduction = event.noteOn.velocity;
-						break;
-					
-					//----------------------
-					case Event::kNoteOffEvent:
-						// noteOff reset the reduction
-						fGainReduction = 0.f;
-						break;
-				}
-			}
-		}
-	}
-		
-	//-------------------------------------
-	//---3) Process Audio---------------------
-	//-------------------------------------
-	if (data.numInputs == 0 || data.numOutputs == 0)
-	{
-		// nothing to do
-		return kResultOk;
-	}
+	mParameterList = new BlueParameter[kNumParameters];
 
-	//---check if silence---------------
-	// normally we have to check each channel (simplification)
-	if (data.inputs[0].silenceFlags)
-	{
-		// mark output silence too
-		data.outputs[0].silenceFlags = data.inputs[0].silenceFlags;
-		
-		// nothing to do
-		return kResultOk;
-	}
+	strncpy(mParameterList[kParameterDrive].name, "drive", 256);
+	mParameterList[kParameterDrive].scaling = 9.0;
+	mParameterList[kParameterDrive].offset = 1.0;
 
-	// (simplification) we suppose in this example that we have the same input channel count than the output
-	int32 numChannels = data.inputs[0].numChannels;
+	strncpy(mParameterList[kParameterPreamp].name, "preamp", 256);
+	mParameterList[kParameterPreamp].scaling = 96.0;
+	mParameterList[kParameterPreamp].offset = -78.0;
 
-	//---get audio buffers----------------
-	float** in  = data.inputs[0].channelBuffers32;
-	float** out = data.outputs[0].channelBuffers32;
+	strncpy(mParameterList[kParameterMode].name, "mode", 256);
+	mParameterList[kParameterMode].scaling = 2.0;		// just round this one
+	mParameterList[kParameterMode].offset = 0.5;
+
+	strncpy(mParameterList[kParameterBlockDC].name, "dcblocker", 256);
+	mParameterList[kParameterBlockDC].scaling = 1.0;	// just round this one
+	mParameterList[kParameterBlockDC].offset = 0.5;
 	
-	//---in bypass mode outputs should be like inputs-----
-	if (bBypass)
-	{
-		int32 sampleFrames = data.numSamples;
-		for (int32 i = 0; i < numChannels; i++)
-		{
-			// dont need to be copied if the buffers are the same
-			if (in[i] != out[i])
-				memcpy (out[i], in[i], sampleFrames * sizeof (float));
-		}
-		// in this example we dont update the VuMeter in Bypass
+	vst_strncpy(programName, "Default", kVstMaxProgNameLen);	// default program name
+}
+
+BlueSaturation::~BlueSaturation()
+{
+	delete mOverdrive;
+	delete mInput;
+	delete mOutput;
+}
+
+
+
+void BlueSaturation::setProgramName(char* name)
+{
+	vst_strncpy(programName, name, kVstMaxProgNameLen);
+}
+
+
+void BlueSaturation::getProgramName(char* name)
+{
+	vst_strncpy(name, programName, kVstMaxProgNameLen);
+}
+
+
+void BlueSaturation::setParameter(VstInt32 index, float value)
+{
+	int			i;
+	TTFloat32	temp;
+	
+	switch (index) {
+		case kParameterDrive:
+			temp = value * mParameterList[index].scaling;
+			temp += mParameterList[index].offset;
+			mOverdrive->setAttributeValue(TT("drive"), temp);
+			break;
+		case kParameterPreamp:
+			temp = value * mParameterList[index].scaling;
+			temp += mParameterList[index].offset;
+			mOverdrive->setAttributeValue(TT("preamp"), temp);
+			break;
+		case kParameterMode:
+			temp = value * mParameterList[index].scaling;
+			temp += mParameterList[index].offset;
+			i = temp;
+			mOverdrive->setAttributeValue(TT("mode"), i);
+			break;
+		case kParameterBlockDC:
+			temp = value * mParameterList[index].scaling;
+			temp += mParameterList[index].offset;
+			i = temp;
+			mOverdrive->setAttributeValue(TT("dcBlocker"), i);
+			break;
+		default:
+			break;
 	}
-	else
-	{
-		float fVuPPM = 0.f;
+}
 
-		//---apply gain factor----------
-		float gain = (fGain - fGainReduction);
-		if (gain < 0)
-		{
-			// output is silent
-			for (int32 i = 0; i < numChannels; i++)
-			{
-				int32 sampleFrames = data.numSamples;
-				float* ptr = out[i];
-				while (--sampleFrames >= 0)
-				{
-					(*ptr++) = 0.f;
-				}
-			}
-		}
-		else
-		{
-			if (bHalfGain)
-				gain = gain * 0.5f;
 
-			// in real plugin it would be better to do dezippering to avoid jump (click) in gain value
-			for (int32 i = 0; i < numChannels; i++)
-			{
-				int32 sampleFrames = data.numSamples;
-				float* ptrIn  = in[i];
-				float* ptrOut = out[i];
-				float tmp;
-				while (--sampleFrames >= 0)
-				{
-					// apply gain
-					tmp = (*ptrIn++) * gain;
-					(*ptrOut++) = tmp;
+float BlueSaturation::getParameter(VstInt32 index)
+{
+	float	value = 0;
 
-					// check only positiv values
-					if (tmp > fVuPPM)
-						fVuPPM = tmp;
-				}
-			}
-		}
-
-		//---3) Write outputs parameter changes-----------
-		IParameterChanges* paramChanges = data.outputParameterChanges;
-		// a new value of VuMeter will be send to the host 
-		// (the host will send it back in sync to our controller for updating our editor)
-		if (paramChanges && fVuPPMOld != fVuPPM)
-		{
-			int32 index = 0;
-			IParamValueQueue* paramQueue = paramChanges->addParameterData (kVuPPMId, index);
-			if (paramQueue)
-			{
-				int32 index2 = 0;
-				paramQueue->addPoint (0, fVuPPM, index2); 
-			}
-		}
-		fVuPPMOld = fVuPPM;
+	switch (index) {
+		case kParameterDrive:
+			mOverdrive->getAttributeValue(TT("drive"), value);
+			value -= mParameterList[index].offset;
+			value /= mParameterList[index].scaling;
+			break;
+		case kParameterPreamp:
+			mOverdrive->getAttributeValue(TT("preamp"), value);
+			value -= mParameterList[index].offset;
+			value /= mParameterList[index].scaling;
+			break;
+		case kParameterMode:
+			mOverdrive->getAttributeValue(TT("mode"), value);
+			break;
+		case kParameterBlockDC:
+			mOverdrive->getAttributeValue(TT("dcBlocker"), value);
+			break;
+		default:
+			break;
 	}
-
-	return kResultOk;
+	return value;
 }
 
-//------------------------------------------------------------------------
-tresult BlueSaturation::receiveText (const char* text)
+
+void BlueSaturation::getParameterName(VstInt32 index, char* label)
 {
-	// received from Controller
-	fprintf (stderr, "[BlueSaturation] received: ");
-	fprintf (stderr, text);
-	fprintf (stderr, "\n");
+	strncpy(label, mParameterList[index].name, kVstMaxParamStrLen);
+}
+
+
+void BlueSaturation::getParameterDisplay(VstInt32 index, char* text)
+{
+	//dB2string (fGain, text, kVstMaxParamStrLen);
+}
+
+
+void BlueSaturation::getParameterLabel(VstInt32 index, char* label)
+{
+	if(index == kParameterPreamp)
+		vst_strncpy(label, "dB", kVstMaxParamStrLen);
+}
+
+
+bool BlueSaturation::getEffectName(char* name)
+{
+	vst_strncpy(name, "BlueSaturation", kVstMaxEffectNameLen);
+	return true;
+}
+
+
+bool BlueSaturation::getProductString(char* text)
+{
+	vst_strncpy(text, "BlueSaturation", kVstMaxProductStrLen);
+	return true;
+}
+
+
+bool BlueSaturation::getVendorString(char* text)
+{
+	vst_strncpy(text, "TTBlue (ttblue.org)", kVstMaxVendorStrLen);
+	return true;
+}
+
+
+VstInt32 BlueSaturation::getVendorVersion()
+{ 
+	return 1000; 
+}
+
+
+
+void BlueSaturation::processReplacing(float** inputs, float** outputs, VstInt32 sampleFrames)
+{
+	mInput->setVector(0, sampleFrames, (TTFloat32*)inputs[0]);
+	mInput->setVector(1, sampleFrames, (TTFloat32*)inputs[1]);
+	mOutput->setVectorSize(sampleFrames);
+	mOutput->alloc();
 	
-	bHalfGain = !bHalfGain;
-
-	return kResultOk;
-}
-
-//------------------------------------------------------------------------
-tresult PLUGIN_API BlueSaturation::setState (IBStream* state)
-{
-	// called when we load a preset, the model has to be reloaded
-
-	float savedGain = 0.f;
-	if (state->read (&savedGain, 4) != kResultOk)
-		return kResultFalse;
-
-	float savedGainReduction = 0.f;
-	if (state->read (&savedGainReduction, 4) != kResultOk)
-		return kResultFalse;
-
-#if BYTEORDER == kBigEndian
-	SWAP_32 (savedGain)
-	SWAP_32 (savedGainReduction)
-#endif
+	mOverdrive->process(mInput, mOutput);
 	
-	fGain = savedGain;
-	fGainReduction = savedGainReduction;
-
-	return kResultOk;
+	mOutput->getVector(0, sampleFrames, (TTFloat32*)outputs[0]);
+	mOutput->getVector(1, sampleFrames, (TTFloat32*)outputs[1]);
 }
 
-//------------------------------------------------------------------------
-tresult PLUGIN_API BlueSaturation::getState (IBStream* state)
+
+void BlueSaturation::processDoubleReplacing(double** inputs, double** outputs, VstInt32 sampleFrames)
 {
-	// here we need to save the model
-
-	float savedGain = fGain;
-	float savedGainReduction = fGainReduction;
-
-#if BYTEORDER == kBigEndian
-	SWAP_32 (savedGain)
-	SWAP_32 (savedGainReduction)
-#endif
-
-	state->write (&savedGain, 4);
-	return state->write (&savedGainReduction, 4);
-}
-
-//------------------------------------------------------------------------
-tresult PLUGIN_API BlueSaturation::setupProcessing (ProcessSetup& newSetup)
-{
-	// called before the process call, always in a disable state (not active)
+	mInput->setVector(0, sampleFrames, (TTFloat64*)inputs[0]);
+	mInput->setVector(1, sampleFrames, (TTFloat64*)inputs[1]);
+	mOutput->setVectorSize(sampleFrames);
+	mOutput->alloc();
 	
-	// here we keep a trace of the processing mode (offline,...) for example.
-	currentProcessMode = newSetup.processMode;
+	mOverdrive->process(mInput, mOutput);
 	
-	return AudioEffect::setupProcessing (newSetup);
+	mOutput->getVector(0, sampleFrames, (TTFloat64*)outputs[0]);
+	mOutput->getVector(1, sampleFrames, (TTFloat64*)outputs[1]);
 }
 
-//------------------------------------------------------------------------
-tresult PLUGIN_API BlueSaturation::setBusArrangements (SpeakerArrangement* inputs, int32 numIns, SpeakerArrangement* outputs, int32 numOuts)
-{
-	if (numIns == 1 && numOuts == 1)
-	{
-		if (inputs[0] == SpeakerArr::kMono && outputs[0] == SpeakerArr::kMono)
-		{
-			AudioBus* bus = (AudioBus*)(audioInputs.at (0));
-			if (bus)
-			{
-				if (bus->getArrangement () != SpeakerArr::kMono)
-				{
-					removeAudioBusses ();
-					addAudioInput  (USTRING ("Mono In"),  SpeakerArr::kMono);
-					addAudioOutput (USTRING ("Mono Out"), SpeakerArr::kMono);
-				}
-				return kResultOk;
-			}
-		}
-		else
-		{
-			AudioBus* bus = (AudioBus*)(audioInputs.at (0));
-			if (bus)
-			{
-				if (bus->getArrangement () != SpeakerArr::kStereo)
-				{
-					removeAudioBusses ();
-					addAudioInput  (USTRING ("Stereo In"),  SpeakerArr::kStereo);
-					addAudioOutput (USTRING ("Stereo Out"), SpeakerArr::kStereo);
-				}
-				return kResultOk;
-			}
-		}
-	}
-	return kResultFalse;
-
-}
--- examples/VST/BlueSaturation/source/BlueSaturation.h
+++ examples/VST/BlueSaturation/source/BlueSaturation.h
@@ -1,115 +1,60 @@
-//------------------------------------------------------------------------
-// Project     : VST SDK
-// Version     : 3.0
-//
-// Category    : Examples
-// Filename    : again.h
-// Created by  : Steinberg, 04/2005
-// Modified    : $Date: 2008/01/09 12:51:05 $
-// Description : BlueSaturation Example for VST SDK 3.0
-//               Simple gain plugin with gain, bypass values and 1 midi input
-//-----------------------------------------------------------------------------
-// LICENSE
-// © 2008, Steinberg Media Technologies GmbH, All Rights Reserved
-//-----------------------------------------------------------------------------
-// This Software Development Kit may not be distributed in parts or its entirety  
-// without prior written agreement by Steinberg Media Technologies GmbH. 
-// This SDK must not be used to re-engineer or manipulate any technology used  
-// in any Steinberg or Third-party application or software module, 
-// unless permitted by law.
-// Neither the name of the Steinberg Media Technologies nor the names of its
-// contributors may be used to endorse or promote products derived from this 
-// software without specific prior written permission.
-// 
-// THIS SDK IS PROVIDED BY STEINBERG MEDIA TECHNOLOGIES GMBH "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-// IN NO EVENT SHALL STEINBERG MEDIA TECHNOLOGIES GMBH BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
-// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
-// OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-// OF THE POSSIBILITY OF SUCH DAMAGE.
-//-----------------------------------------------------------------------------
-
 #ifndef __bluesaturation__
 #define __bluesaturation__
 
-#include "public.sdk/source/vst/vstaudioeffect.h"
+#include "public.sdk/source/vst2.x/audioeffectx.h"
+#include "TTBlueAPI.h"
 
-using namespace Steinberg::Vst;
 
-//------------------------------------------------------------------------
-// define the UIDs for the component and the controller.
-// Note: be sure that you generate unique ID, you can use Create GUID from Visual Studio
-#define gComponentUID1 0x94e8de5f
-#define gComponentUID2 0xa2554f53
-#define gComponentUID3 0xa6fae413
-#define gComponentUID4 0x4c935a18
-#define gComponentUID  gComponentUID1, gComponentUID2, gComponentUID3, gComponentUID4
-
-#define gControllerUID1 0xE39D5B65
-#define gControllerUID2 0xE7AF42FA
-#define gControllerUID3 0x943F4AC8
-#define gControllerUID4 0x51EB04F0
-#define gControllerUID gControllerUID1, gControllerUID2, gControllerUID3, gControllerUID4
-
-//------------------------------------------------------------------------
-// BlueSaturation: directly derived from the helper class AudioEffect
-//------------------------------------------------------------------------
-class BlueSaturation: public AudioEffect
-{
-public:
-	BlueSaturation ();
-//------------------------------------------------------------------------
-// create function required for plugin factory,
-// it will be called to create new instances of this plugin
-//------------------------------------------------------------------------
-	static FUnknown* createInstance (void* context)
-	{
-		return (IAudioProcessor*)new BlueSaturation;
-	}
-
-//------------------------------------------------------------------------
-// AudioEffect overrides:
-//------------------------------------------------------------------------
-	/** called at first after contructor */
-	tresult PLUGIN_API initialize (FUnknown* context);
-	
-	/** called at the end before destructor */
-	tresult PLUGIN_API terminate  ();
-	
-	/** On/Off of the plugin */
-	tresult PLUGIN_API setActive (TBool state);
-
-	/** here we go...the process call */
-	tresult PLUGIN_API process (ProcessData& data);
-
-	/** test of a communication way between controller and component */
-	tresult receiveText (const char* text);
-	
-	/** for persistence */
-	tresult PLUGIN_API setState (IBStream* state);
-	tresult PLUGIN_API getState (IBStream* state);
-
-	/** will be called before any process call */
-	tresult PLUGIN_API setupProcessing (ProcessSetup& newSetup);
-
-	/** Bus arrangement managing: in this example the again will be mono for mono input/output and stereo for other arrangements. */
-	tresult PLUGIN_API setBusArrangements (SpeakerArrangement* inputs, int32 numIns, SpeakerArrangement* outputs, int32 numOuts);
-
-//------------------------------------------------------------------------
-private:
-	// our model values
-	float fGain;
-	float fGainReduction;
-	float fVuPPMOld;
-
-	int32 currentProcessMode;
-
-	bool  bHalfGain;
-	bool  bBypass;
+enum {
+	kParameterDrive = 0,
+	kParameterPreamp,
+	kParameterMode,
+	kParameterBlockDC,
+	kNumParameters
 };
 
-#endif //__bluesaturation__
+typedef struct {
+	char		name[256];
+	TTFloat32	scaling;
+	TTFloat32	offset;
+} BlueParameter;
+
+#define kNumPresets 1
+
+
+//-------------------------------------------------------------------------------------------------------
+class BlueSaturation : public AudioEffectX
+{
+public:
+	BlueSaturation(audioMasterCallback audioMaster);
+	~BlueSaturation();
+
+	// Processing
+	virtual void processReplacing(float** inputs, float** outputs, VstInt32 sampleFrames);
+	virtual void processDoubleReplacing(double** inputs, double** outputs, VstInt32 sampleFrames);
+
+	// Program
+	virtual void setProgramName(char* name);
+	virtual void getProgramName(char* name);
+
+	// Parameters
+	virtual void setParameter(VstInt32 index, float value);
+	virtual float getParameter(VstInt32 index);
+	virtual void getParameterLabel(VstInt32 index, char* label);
+	virtual void getParameterDisplay(VstInt32 index, char* text);
+	virtual void getParameterName(VstInt32 index, char* text);
+
+	virtual bool getEffectName(char* name);
+	virtual bool getVendorString(char* text);
+	virtual bool getProductString(char* text);
+	virtual VstInt32 getVendorVersion();
+
+protected:	
+	TTOverdrive*		mOverdrive;
+	TTAudioSignal*		mInput;
+	TTAudioSignal*		mOutput;
+	BlueParameter*		mParameterList;
+	char				programName[kVstMaxProgNameLen+1];
+};
+
+#endif // __bluesaturation__
