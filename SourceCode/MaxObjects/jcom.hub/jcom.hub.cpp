01f613d1ff91d4cc1b0e0ebe46264c4a0e23a27c
bltzr <bltzr@464221e2-0a0f-0410-983b-f8d57d210c17>
2008-07-06 16:27:29 +0000

    changes dots to undescores in the automatic module OSC naming
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@3744 464221e2-0a0f-0410-983b-f8d57d210c17

--- SourceCode/MaxObjects/jcom.hub/jcom.hub.cpp
+++ SourceCode/MaxObjects/jcom.hub/jcom.hub.cpp
@@ -1196,7 +1196,9 @@ t_max_err hub_attr_setname(t_hub* x, t_object* attr, long argc, t_atom* argv)
 		
 		// search for illegal characters as specified by the OSC standard and replace them
 		for(i=0; i<strlen(name); i++){
-			if(name[i] == '[')
+			if(name[i] == '.')
+				name[i] = '_';
+			else if(name[i] == '[')
 				name[i] = '.';
 			else if(name[i] == ']')
 				name[i] = 0;
