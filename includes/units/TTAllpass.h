497b20fcfc8fa752d40b1a3a58498199603c8876
tim@electrotap.com <tim@electrotap.com@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-07-12 16:11:46 +0000

    Adding the TTAllpass class: an allpass filter ported from tt_allpass. It isn't quite ready, so the body is commented-out.
    I'm committing this now however so that everything required to build the current Tap.Tools beta (which I'm working on) will be committed and we can tag it.
    
    
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@578 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ includes/units/TTAllpass.h
@@ -0,0 +1,74 @@
+/* 
+ * TTBlue Allpass Filter Object
+ * Copyright © 2003, Tim Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#ifndef __TT_ALLPASS_H__
+#define __TT_ALLPASS_H__
+
+#include "TTAudioObject.h"
+
+
+/**	An allpass filter. */
+class TTEXPORT TTAllpass : public TTAudioObject {
+private:
+	TTFloat64		delay;				///< Attribute: delay time in milliseconds
+	TTFloat64		delayMax;			///< Maximum delay time in milliseconds (how long is the buffer?)
+	TTUInt32		delayInSamples;		///< Delay time in samples (internal)		
+	TTUInt32		delayInSamplesMax;	///< size of the delay buffers
+	TTFloat64		gain;				///< Attribute: gain in db
+	TTFloat64		linearGain;			///< Attribute: linear gain coefficient
+	
+	TTSampleVector*	feedforward;		///< An array of feedforward sample buffers, one for each channel
+	TTSampleVector*	feedback;			///< An array of feedback sample buffers, one for each channel
+	
+	TTSampleVector*	ffEndPtr;			///< pointer to the last sample in the feedforward buffer (for each channel)
+	TTSampleVector*	fbEndPtr;			///< pointer to the last sample in the feedback buffer (for each channel)
+	TTSampleVector*	ffInPtr;			///< record pointer into the feedforward delay buffer (for each channel)
+	TTSampleVector*	fbInPtr;			///< record pointer into the feedback delay buffer (for each channel)
+	TTSampleVector*	ffOutPtr;			///< playback pointer from the feedforward delay buffer (for each channel)
+	TTSampleVector*	fbOutPtr;			///< playback pointer from the feedback delay buffer (for each channel)
+
+	// Notifications
+	TTErr updateMaxNumChannels(const TTValue& oldMaxNumChannels);
+	TTErr updateSr();
+
+	/** Internal initialization of buffers and pointers etc. */
+	void init();
+	
+	/** Internally used to free buffers. */
+	void chuck(TTUInt16 numChannels);
+	
+	/**	Standard audio processing method as used by TTBlue objects. */
+	TTErr processAudio(TTAudioSignal& in, TTAudioSignal& out);
+
+public:
+
+	/**	Constructor. */
+	TTAllpass(TTUInt16 newMaxNumChannels);
+
+	/**	Destructor. */
+	~TTAllpass();
+	
+	/** Attribute accessor. */
+	TTErr setdelay(const TTValue& newValue);
+	
+	/** Attribute accessor. */
+	TTErr setgain(const TTValue& newValue);
+	
+	/** Attribute accessor. */
+	TTErr setlinearGain(const TTValue& newValue);
+	
+	/**	This algorithm uses an IIR filter, meaning that it relies on feedback.  If the filter should
+	 *	not be producing any signal (such as turning audio off and then back on in a host) or if the
+	 *	feedback has become corrupted (such as might happen if a NaN is fed in) then it may be 
+	 *	neccesary to clear the filter by calling this method.
+	 *	@return Returns a TTErr error code.												*/
+	TTErr clear();
+};
+
+
+#endif // __TT_ALLPASS_H__
---
+++ source/units/TTAllpass.cpp
@@ -0,0 +1,224 @@
+/* 
+ * TTBlue Allpass Filter Object
+ * Copyright © 2003, Tim Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+/*
+#include "TTAllpass.h"
+#define thisTTClass TTAllpass
+
+
+TTAllpass::TTAllpass(TTUInt16 newMaxNumChannels)
+	: TTAudioObject("filter.allpass", newMaxNumChannels),
+	delayMax(0), delayInSamples(0), delayInSamplesMax(0),
+	feedforward(NULL), feedback(NULL),
+	ffEndPtr(NULL),	fbEndPtr(NULL),	
+	ffInPtr(NULL), fbInPtr(NULL),
+	ffOutPtr(NULL), fbOutPtr(NULL)
+{
+	// register attributes
+	registerAttributeWithSetter(delay,		kTypeFloat64);
+	registerAttributeWithSetter(gain,		kTypeFloat64);
+	registerAttributeWithSetter(linearGain,	kTypeFloat64);
+
+	// register methods
+	registerMessageSimple(clear);
+
+	// register for notifications
+	registerMessageWithArgument(updateMaxNumChannels);
+	registerMessageSimple(updateSr);
+
+	// Set Defaults...
+	setAttributeValue(TT("maxNumChannels"),	newMaxNumChannels);			// This attribute is inherited
+	setAttributeValue(TT("linearGain"),	1.0);
+	setAttributeValue(TT("delay"),		0.0);
+	setProcessMethod(processAudio);
+	
+//	 init(milliseconds);
+//	 set_attr(k_delay_ms, 0.0);
+//	 set_attr(k_gain, 0.0);	 
+}
+
+
+TTAllpass::~TTAllpass()
+{
+	chuck(maxNumChannels);
+}
+
+
+void TTAllpass::init()
+{
+	delayMax = milliseconds;
+	delayInSamplesMax = long(delayMax * (sr / 1000.0));
+
+	feedforward = new TTSampleVector[maxNumChannels];
+	feedback = new TTSampleVector[maxNumChannels];
+	
+	for(TTUInt16 channel=0; channel<maxNumChannels; channel++){
+		feedforward[channel] = new TTSampleValue[delayInSamplesMax];
+		feedback[channel] = new TTSampleValue[delayInSamplesMax];
+	}
+
+	ffEndPtr = new TTSampleVector[maxNumChannels];
+	fbEndPtr = new TTSampleVector[maxNumChannels];
+	ffInPtr = new TTSampleVector[maxNumChannels];
+	fbInPtr = new TTSampleVector[maxNumChannels];
+	ffOutPtr = new TTSampleVector[maxNumChannels];
+	fbOutPtr = new TTSampleVector[maxNumChannels];
+	
+	
+	clear();
+}
+
+
+void TTAllpass::chuck(TTUInt16 numChannels)
+{
+	for(TTUInt16 channel=0; channel<numChannels; channel++){
+		delete[] feedforward[channel];
+		delete[] feedback[channel];
+	}
+	delete[] feedforward;
+	delete[] feedback;
+	
+	delete[] ffEndPtr;
+	delete[] fbEndPtr;
+	delete[] ffInPtr;
+	delete[] fbInPtr;
+	delete[] ffOutPtr;
+	delete[] fbOutPtr;
+}
+
+
+TTErr TTAllpass::updateMaxNumChannels(const TTValue& oldMaxNumChannels)
+{
+	chuck(oldMaxNumChannels);
+	init();
+
+	clear();
+	return kTTErrNone;
+}
+
+
+TTErr TTAllpass::updateSr()
+{
+	init(delayMax);					// re-alloc memory
+	return setAttributeValue(TT("delay"), delay);
+}
+
+
+TTErr TTAllpass::clear()
+{
+	for(TTUInt16 channel=0; channel<maxNumChannels; channel++){
+		memset(feedforward[channel], 0, sizeof(TTSampleValue) * delayInSamplesMax);
+		memset(feedback[channel],    0, sizeof(TTSampleValue) * delayInSamplesMax);
+	}
+	
+	ffInPtr = feedforward;
+	ffEndPtr = feedforward + delayInSamples;
+	ffOutPtr = ffInPtr - delayInSamples;
+	if(ffOutPtr < feedforward)
+		ffOutPtr = ffEndPtr + (ffOutPtr - feedforward) + 1;
+	
+	fbInPtr = feedback;
+	fbEndPtr = feedback + delayInSamples;
+	fbOutPtr = fbInPtr - delayInSamples;
+	if(fbOutPtr < feedback)
+		fbOutPtr = fbEndPtr + (fbOutPtr - feedback) + 1;
+
+	return kTTErrNone;
+}
+
+
+TTErr TTAllpass::setgain(const TTValue& newValue)
+{	
+	gain = newValue;
+	linearGain = dbToLinear(gain);
+	return kTTErrNone;
+}
+
+
+TTErr TTAllpass::setlinearGain(const TTValue& newValue)
+{	
+	linearGain = newValue;
+	gain = linearToDb(linearGain);
+	return kTTErrNone;
+}
+
+
+TTErr TTAllpass::setdelay(const TTValue& newValue)
+{
+	delay = TTClip<TTFloat64>(newValue, 0.0, delayMax);
+	delayInSamples = long(delay * (sr / 1000.0));
+	for(TTUInt16 channel=0; channel<maxNumChannels; channel++){
+		ffEndPtr[channel] = feedforward[channel] + delayInSamples;
+		fbEndPtr[channel] = feedback[channel] + delayInSamples;	
+	}
+	return kTTErrNone;
+}
+
+
+TTErr TTAllpass::processAudio(TTAudioSignal& in, TTAudioSignal& out)
+{
+	TTUInt16		vs;
+	TTSampleValue*	inSample;
+	TTSampleValue*	outSample;
+	TTSampleValue	tempSample;
+	TTUInt16		numchannels = TTAudioSignal::getMinChannelCount(in, out);
+	TTUInt16		channel;
+
+	// This outside loop works through each channel one at a time
+	for(channel=0; channel<numchannels; channel++){
+		inSample = in.sampleVectors[channel];
+		outSample = out.sampleVectors[channel];
+		vs = in.getVectorSize();
+		
+		// This inner loop works through each sample within the channel one at a time
+		while(vs--){
+			tempSample = *inSample++;
+			tempSample = antiDenormal((feedback1[channel] * coefficientA) - (feedback2[channel] * coefficientB) + (tempSample * coefficientC));
+			*outSample++ = tempSample;
+			feedback2[channel] = feedback1[channel];
+			feedback1[channel] = tempSample;
+		}
+	}
+	return kTTErrNone;
+}
+
+
+// DSP LOOP
+TT_INLINE void tt_allpass::dsp_vector_calc(tt_audio_signal *in, tt_audio_signal *out)
+{
+	tt_sample_value temp;
+	temp_vs = in->vectorsize;
+	
+	while(temp_vs--){
+		temp = anti_denormal(*in->vector++);	// Input	
+		*ff_in_ptr = temp;		// Store the input in the feedforward buffer
+		
+		// Apply the filter
+		temp = anti_denormal((*ff_out_ptr) + (temp * (-gain)) + (*fb_out_ptr * gain));	
+		
+		*fb_in_ptr = temp;		// Store the output in the feedback buffer
+		*out->vector++ = temp;	// Output
+		
+		// Move the heads
+		ff_in_ptr++;
+		ff_out_ptr++;
+		fb_in_ptr++;
+		fb_out_ptr++;
+		
+		// Wrap the heads
+		if(ff_in_ptr > ff_end_ptr)
+			ff_in_ptr = ff_buffer;
+		if(ff_out_ptr > ff_end_ptr)
+			ff_out_ptr = ff_buffer;	
+		if(fb_in_ptr > fb_end_ptr)
+			fb_in_ptr = fb_buffer;
+		if(fb_out_ptr > fb_end_ptr)
+			fb_out_ptr = fb_buffer;	
+	}
+	in->reset(); out->reset();
+}
+*/
