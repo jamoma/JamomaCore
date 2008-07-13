4216a5d21527a3a708ad5e764abfa8c725e0a79d
tim@electrotap.com <tim@electrotap.com@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-07-13 17:04:43 +0000

    Initial implementation of TTHash -- a hash table data-storage object for TTBlue.  This implementation is minimal, but it works, and it works well enough to closes Issue 8.
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@593 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ includes/core/TTHash.h
@@ -0,0 +1,67 @@
+/* 
+ * TTBlue Hash Table Class
+ * Copyright © 2008, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#ifndef __TT_HASH_H__
+#define __TT_HASH_H__
+
+#include "TTElement.h"
+#include "TTValue.h"
+#include <hash_map.h>
+using namespace std;
+
+
+/**	A type that contains a key and a value. */
+typedef TTSymbol*										TTSymbolPtr;
+typedef pair<TTPtrSizedInt,TTValue>						TTKeyVal;
+typedef hash_map<TTPtrSizedInt,TTValue>::const_iterator TTHashIter;
+
+
+
+/****************************************************************************************************/
+// Class Specification
+
+/**	The standard TTBlue hash table.  
+	Maintains a collection of TTValue objects indexed by TTSymbol pointers.
+	The TTValue objects can themselves include TTSymbol pointers, TTObjects, simple values, polymorphic arrays, etc.
+ 
+	TTHash is implemented as a light wrapper class around the STL hash_map templates.
+	While we could inherit from hash_map and then override the appropriate things, there are a few reasons to just contain the hash_map and wrap it.
+	- For example, we may later make TTHash inherit from TTObject and want to address it with messages.
+	- This approach isolates coders from having to worry about an template craziness
+	- This approach offers the possibility, should the need ever arise, to change the underlying implementation away from STL.
+ 
+ 
+*/
+class TTEXPORT TTHash : TTElement {
+private:
+	hash_map<TTPtrSizedInt,TTValue>	hashMap;
+	
+public:
+	TTHash();
+	virtual ~TTHash();
+
+	/** Insert an item into the hash table. */
+	TTErr insert(const TTSymbolPtr key, const TTValue& value);
+	
+	/** Find the value for the given key. */
+	TTErr lookup(const TTSymbolPtr key, TTValue& value);
+	
+	/** Remove an item from the hash table. */
+	TTErr erase(const TTSymbolPtr key);
+	
+	/** Remove all items from the hash table. */
+	TTErr clear();
+	
+	/** Get an array of all of the keys for the hash table. */
+	TTErr getKeys(TTValue& hashKeys);
+	
+};
+
+
+#endif // __TT_HASH_H__
+
---
+++ source/core/TTHash.cpp
@@ -0,0 +1,74 @@
+/* 
+ * TTBlue Hash Table Class
+ * Copyright © 2008, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "TTHash.h"
+#include "TTSymbolTable.h"
+#include "TTMutex.h"
+
+static TTMutex* sListMutex=NULL;
+
+/****************************************************************************************************/
+
+TTHash::TTHash()
+{
+	if(!sListMutex)
+		sListMutex = new TTMutex(false);
+
+}
+
+
+TTHash::~TTHash()
+{
+	hashMap.clear();
+}
+
+
+TTErr TTHash::insert(const TTSymbolPtr key, const TTValue& value)
+{
+	hashMap.insert(TTKeyVal(TTPtrSizedInt(key), value));
+	return kTTErrNone;
+}
+
+
+TTErr TTHash::lookup(const TTSymbolPtr key, TTValue& value)
+{
+	TTHashIter iter;
+
+	iter = hashMap.find(TTPtrSizedInt(key));
+	if(iter == hashMap.end())
+		return kTTErrGeneric;
+	else{
+		value = iter->second;
+		return kTTErrNone;
+	}
+}
+
+
+TTErr TTHash::erase(const TTSymbolPtr key)
+{
+	hashMap.erase(TTPtrSizedInt(key));
+	return kTTErrNone;
+}
+
+
+TTErr TTHash::clear()
+{
+	hashMap.clear();
+	return kTTErrNone;
+}
+
+
+TTErr TTHash::getKeys(TTValue& hashKeys)
+{
+	TTHashIter	iter;
+	
+	hashKeys.clear();
+	for(iter = hashMap.begin(); iter != hashMap.end(); iter++)	
+		hashKeys.append(TTSymbolPtr(iter->first));
+	return kTTErrNone;
+}
