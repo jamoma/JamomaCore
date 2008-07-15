74465210670155b317118ba0490ef6ab8bf6582b
tim@electrotap.com <tim@electrotap.com@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-07-15 02:05:43 +0000

    A greatly simplified implementation of TTSymbol, based on two things:
    1. Now that we understand how we are using the symbol table and passing symbols, we can get rid of all of the extra complexity to support features that are no longer usable or desirable.
    
    2. Now we keep the internal copy of the string using the STL std::string instead of a C-string.  This makes the code simple, cleaner, and more flexible.
    
    
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@609 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- includes/core/TTSymbol.h
+++ includes/core/TTSymbol.h
@@ -19,38 +19,45 @@
 	The TTSymbol class is used to represent a string and efficiently pass and compare that string.
 	At the moment the implementation is somewhat crude and really slow and it should be improved in the future.
  
-	Note that generally speaking you should not create a TTSymbol.  Instead, TTSymbol objects should only
-	by created by the TTSymbolTable that you are using.  Because of this we *always* pass TTSymbols as
-	pointers into the symbol table rather than as references or copies.
+	Note that generally speaking you should not directly create a TTSymbol!
+	Instead, TTSymbol objects should only by created by the TTSymbolTable that you are using.  
+	Because of this we *always* pass TTSymbols as pointers (#TTSymbolPtr) into the symbol table rather than as references or copies.
+ 
+	Also, if you don't need the fast lookup capabilities of the symbol table (such as for message or attribute lookup)
+	then consider passing a #TTString instead.
 */
 class TTEXPORT TTSymbol : public TTElement {
 private:
-	TTString				string;				///< the actual string represented by this symbol
+	string					theString;			///< the actual string represented by this symbol
 	TTUInt32				id;					///< a unique identifier for the given string
-	TTUInt32				size;				///< how many chars in the string
-	TTBoolean				locallyOwned;		///< is the string allocated or just a pointer that we don't own?
 
 	/** used by the constructors to create the new symbol */
-	void init(const char* newString, TTInt32 newId);
+	void init(const string& newString, TTInt32 newId);
 
 public:
-	TTSymbol();
-	TTSymbol(const char* newString);
-	
-	/** This constructor is intended only for use by the TTSymbolTable object when creating new symbols
+	/** The constructor is intended only for use by the TTSymbolTable object when creating new symbols
 		in the table.  Perhaps this could be made private and then the class made a friend... */
-	TTSymbol(const char*, TTInt32 newId);
+	TTSymbol(const string& newString, TTInt32 newId);
 
 	virtual	~TTSymbol();
 	
 	/** Copy Constructor */
 	TTSymbol(const TTSymbol& oldSymbol);
 
-	const TTString getString();
+	/**	Return a pointer to the internal string. */
+	const string getString();
+
+	/**	Return a pointer to the internal string as a C-string. */
+	const char* getCString();
+	
+	/**	Return this symbol's unique id. */
 	const TTUInt32 getId();
+	
+	/**	Compare this symbol with another symbol.  */
 	TTBoolean compare(TTSymbol& anotherSymbol);
 	
 	// make sure this is a friend so that it can access the private members of the other
+	/** Compare two symbols for equality. */
 	inline friend bool operator == (const TTSymbol& symbol1, const TTSymbol& symbol2)
 	{
 		if(symbol1.id == symbol2.id)
@@ -59,9 +66,16 @@ public:
 			return false;
 	}
 	
-	operator TTString() const
+	/** Cast a symbol to a C-string. */
+	operator const char*() const
 	{
-		return string;
+		return theString.c_str();
+	}
+	
+	/** Cast a symbol to a std::string. */
+	operator string() const
+	{
+		return theString;
 	}
 };
 
--- source/core/TTSymbol.cpp
+++ source/core/TTSymbol.cpp
@@ -12,21 +12,7 @@
 
 /****************************************************************************************************/
 
-TTSymbol::TTSymbol()
-	: string(NULL), size(0)
-{
-	init("", -1);
-}
-
-TTSymbol::TTSymbol(const char* newString)
-	: string(NULL), size(0)
-{
-	init(newString, -1);
-}
-
-
-TTSymbol::TTSymbol(const char* newString, TTInt32 newId)
-	: string(NULL), size(0)
+TTSymbol::TTSymbol(const string& newString, TTInt32 newId)
 {
 	init(newString, newId);
 }
@@ -34,54 +20,34 @@ TTSymbol::TTSymbol(const char* newString, TTInt32 newId)
 
 TTSymbol::~TTSymbol()
 {
-	// We can't free the string here, because it is referenced by other TTSymbols
-	// only the when the symbol table itself is freed is it actually safe to free this string
-	if(locallyOwned)
-		delete [] string;
-	string = NULL;
+	;
 }
 
 
 // Copy Constructor
 TTSymbol::TTSymbol(const TTSymbol& oldSymbol)
 {
-	// deleting this causes problems, for example when creating a TTCrossfade object
-	//if(locallyOwned)
-	//	delete [] string;
 	id = oldSymbol.id;
-	size = oldSymbol.size;
-	string = new char[size];
-	memcpy(string, oldSymbol.string, size);
+	theString = oldSymbol.theString;
 }
 
 
-void TTSymbol::init(const char* newString, TTInt32 newId)
+void TTSymbol::init(const string& newString, TTInt32 newId)
 {
-	const TTSymbol	*existingSymbol = NULL;
-
-	// 1. Copy the string
-	if(locallyOwned)
-		delete [] string;
-	locallyOwned = true;
-	size = strlen(newString) + 1;
-	string = new char[size];
-	memcpy(string, newString, size-1);
-	string[size-1] = 0;
-
-	// 2. Look for this symbol in the symbol table (it should already exist)
-	if(newId < 0){
-		existingSymbol = ttSymbolTable->lookup(newString);
-		id = existingSymbol->id;
-	}
-	else{	// This symbol is being added to the symbol table with the given id
-		id = newId;
-	}
+	theString = newString;
+	id = newId;
 }
 
 
-const TTString TTSymbol::getString()
+const string TTSymbol::getString()
 {
-	return string;
+	return theString;
+}
+
+
+const char* TTSymbol::getCString()
+{
+	return theString.c_str();
 }
 
 
