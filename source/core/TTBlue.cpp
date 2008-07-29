c01c4928cd3716572dc9e4c1eace884845ea60e5
tim@electrotap.com <tim@electrotap.com@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-07-29 19:24:16 +0000

    We are now registering all of the internal classes so that they can be queried and instantiated using the environment.
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@654 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- source/core/TTBlue.cpp
+++ source/core/TTBlue.cpp
@@ -45,6 +45,52 @@ void TTBlueInit()
 
 
 /****************************************************************************************************/
+
+// Analysis
+#include "TTZerocross.h"
+
+// Data
+
+// Dynamics
+#include "TTBalance.h"
+#include "TTGain.h"
+#include "TTLimiter.h"
+#include "TTPulseSub.h"
+
+// Generators
+#include "TTAdsr.h"
+#include "TTNoise.h"
+#include "TTPhasor.h"
+#include "TTRamp.h"
+#include "TTWavetable.h"
+
+// Filtering
+#include "TTAllpass.h"
+#include "TTAverage.h"
+#include "TTBandpassButterworth2.h"
+#include "TTBandrejectButterworth2.h"
+#include "TTDCBlock.h"
+
+#include "TTHighpassButterworth1.h"
+#include "TTHighpassButterworth2.h"
+#include "TTHighpassButterworth3.h"
+#include "TTHighpassButterworth4.h"
+#include "TTHighpassLinkwitzRiley2.h"
+#include "TTHighpassLinkwitzRiley4.h"
+
+#include "TTLowpassButterworth1.h"
+#include "TTLowpassButterworth2.h"
+#include "TTLowpassButterworth3.h"
+#include "TTLowpassButterworth4.h"
+#include "TTLowpassLinkwitzRiley2.h"
+#include "TTLowpassLinkwitzRiley4.h"
+
+#include "TTLowpassOnePole.h"
+#include "TTLowpassTwoPole.h"
+#include "TTLowpassFourPole.h"
+#include "TTSvf.h"
+
+// Misc
 #include "TTBuffer.h"
 #include "TTCrossfade.h"
 #include "TTDegrade.h"
@@ -52,20 +98,154 @@ void TTBlueInit()
 #include "TTOperator.h"
 #include "TTOverdrive.h"
 
+
 void TTBlueRegisterInternalClasses()
 {
-	TTClassRegister(TT("buffer"),		"audio, buffer, storage",			&TTBlueInstantiateInternalClass);
-	TTClassRegister(TT("crossfade"),	"audio, processor, mixing",			&TTBlueInstantiateInternalClass);
-	TTClassRegister(TT("degrade"),		"audio, processor, distortion",		&TTBlueInstantiateInternalClass);
-	TTClassRegister(TT("delay"),		"audio, processor, delay",			&TTBlueInstantiateInternalClass);
-	TTClassRegister(TT("operator"),		"audio, processor, math",			&TTBlueInstantiateInternalClass);
-	TTClassRegister(TT("overdrive"),	"audio, processor",					&TTBlueInstantiateInternalClass);
+	// Analysis
+	TTClassRegister(TT("zerocross"),					"audio, analysis, frequency",										&TTBlueInstantiateInternalClass);
+	
+	
+	// Data
+	
+	
+	// Dynamics
+	TTClassRegister(TT("balance"),						"audio, processor, dynamics",										&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("gain"),							"audio, processor, dynamics",										&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("limiter"),						"audio, processor, dynamics, limiter",								&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("pulsesub"),						"audio, processor, dynamics, envelope",								&TTBlueInstantiateInternalClass);
+	
+	
+	// Generators
+	TTClassRegister(TT("adsr"),							"audio, generator, envelope",										&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("noise"),						"audio, generator, noise",											&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("phasor"),						"audio, generator, oscillator",										&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("ramp"),							"audio, generator",													&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("wavetable"),					"audio, generator, oscillator, buffer",								&TTBlueInstantiateInternalClass);
+	
+	
+	// Filtering
+	TTClassRegister(TT("allpass"),						"audio, processor, filter, allpass",								&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("average"),						"audio, processor, filter, lowpass, envelope",						&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("bandpass.butterworth.2"),		"audio, processor, filter, bandpass, butterworth",					&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("bandreject.butterworth.2"),		"audio, processor, filter, notch",									&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("dcblock"),						"audio, processor, filter",											&TTBlueInstantiateInternalClass);
+
+	TTClassRegister(TT("highpass.butterworth.1"),		"audio, processor, filter, highpass, butterworth",					&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("highpass.butterworth.2"),		"audio, processor, filter, highpass, butterworth",					&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("highpass.butterworth.3"),		"audio, processor, filter, highpass, butterworth",					&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("highpass.butterworth.4"),		"audio, processor, filter, highpass, butterworth",					&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("highpass.linkwitzriley.2"),		"audio, processor, filter, highpass",								&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("highpass.linkwitzriley.4"),		"audio, processor, filter, highpass, crossover",					&TTBlueInstantiateInternalClass);
+
+	TTClassRegister(TT("lowpass.butterworth.1"),		"audio, processor, filter, lowpass, butterworth",					&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("lowpass.butterworth.2"),		"audio, processor, filter, lowpass, butterworth",					&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("lowpass.butterworth.3"),		"audio, processor, filter, lowpass, butterworth",					&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("lowpass.butterworth.4"),		"audio, processor, filter, lowpass, butterworth",					&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("lowpass.linkwitzriley.2"),		"audio, processor, filter, lowpass",								&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("lowpass.linkwitzriley.4"),		"audio, processor, filter, lowpass, crossover",						&TTBlueInstantiateInternalClass);
+	
+	TTClassRegister(TT("lowpass.1"),					"audio, processor, filter, lowpass",								&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("lowpass.2"),					"audio, processor, filter, lowpass",								&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("lowpass.4"),					"audio, processor, filter, lowpass",								&TTBlueInstantiateInternalClass);
+
+	TTClassRegister(TT("svf"),							"audio, processor, filter, lowpass, highpass, bandpass, notch",		&TTBlueInstantiateInternalClass);
+	
+	
+	// Misc
+	TTClassRegister(TT("buffer"),						"audio, buffer, storage",											&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("crossfade"),					"audio, processor, mixing",											&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("degrade"),						"audio, processor, distortion",										&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("delay"),						"audio, processor, delay",											&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("operator"),						"audio, processor, math",											&TTBlueInstantiateInternalClass);
+	TTClassRegister(TT("overdrive"),					"audio, processor",													&TTBlueInstantiateInternalClass);
 }
 
 
 TTObject* TTBlueInstantiateInternalClass(TTSymbol* className, TTValue& arguments)
 {
-	if(className == TT("buffer"))
+	// Analysis
+	if(className == TT("zerocross"))
+		return new TTZerocross(arguments);
+	
+	
+	// Data
+	
+	
+	// Dynamics
+	else if(className == TT("balance"))
+		return new TTBalance(arguments);
+	else if(className == TT("gain"))
+		return new TTGain(arguments);
+	else if(className == TT("limiter"))
+		return new TTLimiter(arguments);
+	else if(className == TT("pulsesub"))
+		return new TTPulseSub(arguments);
+	
+	
+	// Generators
+	else if(className == TT("adsr"))
+		return new TTAdsr(arguments);
+	else if(className == TT("noise"))
+		return new TTNoise(arguments);
+	else if(className == TT("phasor"))
+		return new TTPhasor(arguments);
+	else if(className == TT("ramp"))
+		return new TTRamp(arguments);
+	else if(className == TT("wavetable"))
+		return new TTWavetable(arguments);
+	
+	
+	// Filtering
+	else if(className == TT("allpass"))
+		return new TTAllpass(arguments);
+	else if(className == TT("average"))
+		return new TTAverage(arguments);
+	else if(className == TT("bandpass.butterworth.2"))
+		return new TTBandpassButterworth2(arguments);
+	else if(className == TT("bandreject.butterworth.2"))
+		return new TTBandRejectButterworth2(arguments);
+	else if(className == TT("dcblock"))
+		return new TTDCBlock(arguments);
+	
+	else if(className == TT("highpass.butterworth.1"))
+		return new TTHighpassButterworth1(arguments);
+	else if(className == TT("highpass.butterworth.2"))
+		return new TTHighpassButterworth2(arguments);
+	else if(className == TT("highpass.butterworth.3"))
+		return new TTHighpassButterworth3(arguments);
+	else if(className == TT("highpass.butterworth.4"))
+		return new TTHighpassButterworth4(arguments);
+	else if(className == TT("highpass.linkwitzriley.2"))
+		return new TTHighpassLinkwitzRiley2(arguments);
+	else if(className == TT("highpass.linkwitzriley.4"))
+		return new TTHighpassLinkwitzRiley4(arguments);
+	
+	else if(className == TT("lowpass.butterworth.1"))
+		return new TTLowpassButterworth1(arguments);
+	else if(className == TT("lowpass.butterworth.2"))
+		return new TTLowpassButterworth2(arguments);
+	else if(className == TT("lowpass.butterworth.3"))
+		return new TTLowpassButterworth3(arguments);
+	else if(className == TT("lowpass.butterworth.4"))
+		return new TTLowpassButterworth4(arguments);
+	else if(className == TT("lowpass.linkwitzriley.2"))
+		return new TTLowpassLinkwitzRiley2(arguments);
+	else if(className == TT("lowpass.linkwitzriley.4"))
+		return new TTLowpassLinkwitzRiley4(arguments);
+	
+	else if(className == TT("lowpass.1"))
+		return new TTLowpassOnePole(arguments);
+	else if(className == TT("lowpass.2"))
+		return new TTLowpassTwoPole(arguments);
+	else if(className == TT("lowpass.4"))
+		return new TTLowpassFourPole(arguments);
+	
+	else if(className == TT("svf"))
+		return new TTSvf(arguments);
+	
+	
+	// Misc
+	else if(className == TT("buffer"))
 		return new TTBuffer;
 	else if(className == TT("crossfade"))
 		return new TTCrossfade(arguments);
