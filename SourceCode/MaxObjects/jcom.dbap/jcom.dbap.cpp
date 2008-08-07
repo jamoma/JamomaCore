86c7f6efb3e75a3efb134228b146d3cb6d797ff7
lossius <lossius@464221e2-0a0f-0410-983b-f8d57d210c17>
2008-08-07 10:45:47 +0000

    Initial commit of c code for jcom.dbap. Far from done yet.
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@3855 464221e2-0a0f-0410-983b-f8d57d210c17

---
+++ SourceCode/MaxObjects/jcom.dbap/jcom.dbap.cpp
@@ -0,0 +1,414 @@
+/* 
+ * jcom.dbap
+ * External for Jamoma: DBAP - Distance Based Amplitude Panning
+ * By Trond Lossius, Copyright © 2006
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "Jamoma.h"
+
+
+
+const long MAX_NUM_SPEAKERS = 32;
+
+
+typedef struct _xyz{
+	float		x;										///< x position
+	float		y;										///< y position
+	float		z;										///< z position
+} t_xyz;												///< Cartesian coordinate of a point
+	
+
+typedef struct _dbap{									///< Data structure for this object 
+	t_object	ob;										///< Must always be the first field; used by Max
+	t_xyz		speaker_position[MAX_NUM_SPEAKERS];		///< Array of speaker positions
+	t_xyz		source_position;						///< Positions of the virtual source
+	long		attr_dimensions;						///< Number of dimensions of the speaker and source system
+	float		attr_blur;								///< Spatial bluriness ratio
+	float		attr_rolloff;							///< Set rolloff with distance in dB.
+	long		attr_num_speakers;						///< number of speakers used
+	float		a;										///< Constant: Exponent controlling amplitude dependance on distance. Depends on attr_rolloff
+	void		*outlet;								////< Pointer to outlet. Need one for each outlet
+} t_dbap;
+
+// Prototypes for methods: need a method for each incoming message
+
+
+void *dbap_new(t_symbol *msg, long argc, t_atom *argv);
+t_max_err dbap_setstep(t_dbap *x, void *attr, long argc, t_atom *argv);
+
+/** Set the position of the nth speaker. */
+void dbap_speaker(t_dbap *x, void *attr, long argc, t_atom *argv);
+
+/** Set the position of the nth virtual source. */
+void dbap_source(t_dbap *x, void *attr, long argc, t_atom *argv);
+
+/** Display assist strings while patching. */
+void dbap_assist(t_dbap *x, void *b, long msg, long arg, char *dst);
+
+/** Get number of dimensions of the system. */
+t_max_err dbap_attr_getdimensions(t_dbap *x, void *attr, long *argc, t_atom **argv);
+
+/** Set number of dimensions of the system. */
+t_max_err dbap_attr_setdimensions(t_dbap *x, void *attr, long argc, t_atom *argv);
+
+/** Get the number of speakers of the system. */
+t_max_err dbap_attr_getnum_speakers(t_dbap *x, void *attr, long *argc, t_atom **argv);
+
+/** Set the number of speakers of the system. */
+t_max_err dbap_attr_setnum_speakers(t_dbap *x, void *attr, long argc, t_atom *argv);
+
+/** Get spatial blur coefficient */
+t_max_err dbap_attr_getblur(t_dbap *x, void *attr, long *argc, t_atom **argv);
+
+/** Set spatial blur coefficient */
+t_max_err dbap_attr_setblur(t_dbap *x, void *attr, long argc, t_atom *argv);
+
+/** Get rolloff in dB */
+t_max_err dbap_attr_getrolloff(t_dbap *x, void *attr, long *argc, t_atom **argv);
+
+/** Set rolloff in dB */
+t_max_err dbap_attr_setrolloff(t_dbap *x, void *attr, long argc, t_atom *argv);
+
+/** Calculate matrix coefficients for most recently received position for the 1D case. */
+void dbap_calculate1D(t_dbap *x, long n);
+
+/** Calculate matrix coefficients for most recently received position for the 2D case. */
+void dbap_calculate2D(t_dbap *x, long n);
+
+/** Calculate matrix coefficients for most recently received position for the 3D case. */
+void dbap_calculate3D(t_dbap *x, long n);
+
+
+
+
+// Globals
+t_class		*this_class;				// Required. Global pointing to this class 
+
+
+
+/************************************************************************************/
+// Main() Function
+
+int main(void)
+{	
+	long attrflags = 0;
+	t_class *c;
+	t_object *attr;
+
+	jamoma_init();
+	common_symbols_init();
+
+	// Define our class
+	c = class_new("jcom.dbap",(method)dbap_new, (method)0L, sizeof(t_dbap), 
+		(method)0L, A_GIMME, 0);		
+
+	// Make methods accessible for our class: 
+	class_addmethod(c, (method)dbap_speaker,			"speaker",	A_GIMME,	0);
+	class_addmethod(c, (method)dbap_speaker,			"source",	A_GIMME,	0);
+	class_addmethod(c, (method)dbap_assist,				"assist",	A_CANT,		0);  
+	class_addmethod(c, (method)object_obex_dumpout,		"dumpout",	0);  
+
+	// Add attributes to our class:	
+	jamoma_class_attr_new(c, 	"dimensions", 	_sym_long, (method)dbap_attr_setdimensions, (method)dbap_attr_getdimensions);
+	CLASS_ATTR_ENUM(c,			"dimensions",	0,	"1 2 3");
+	
+	jamoma_class_attr_new(c, 	"num_speakers", _sym_long, (method)dbap_attr_setnum_speakers, (method)dbap_attr_getnum_speakers);
+	
+	jamoma_class_attr_new(c, 	"blur",			_sym_float32, (method)dbap_attr_setblur, (method)dbap_attr_getblur);
+	
+	jamoma_class_attr_new(c, 	"rolloff",		_sym_float32, (method)dbap_attr_setrolloff, (method)dbap_attr_getrolloff);
+	
+	// Finalize our class
+	class_register(CLASS_BOX, c);
+	this_class = c;	
+	return 0;
+}
+
+
+/************************************************************************************/
+// Object Life
+
+void *dbap_new(t_symbol *msg, long argc, t_atom *argv)
+{
+	t_dbap *x;
+	long i;
+	
+	x = (t_dbap *)object_alloc(this_class);	// create the new instance and return a pointer to it
+	
+	if(x){
+    	object_obex_store(x, _sym_dumpout, (object *)outlet_new(x,NULL));	// dumpout
+		x->outlet = outlet_new(x, 0);				// Create outlet
+		
+		// Initializing and setting defaults for attributes.
+		x->attr_num_speakers = 1;					// default value
+		x->attr_dimensions = 2;						// two-dimensional by default
+		x->attr_rolloff = 6;						// 6 dB rolloff by default
+		x->attr_blur = 0.01;						// %TODO: Calculate rms of distance from mean position of speakers, and use that.
+		for (i=0; i<MAX_NUM_SPEAKERS; i++) {
+			x->speaker_position[i].x = 0.;
+			x->speaker_position[i].y = 0.;
+			x->speaker_position[i].z = 0.;
+		}
+		attr_args_process(x, argc, argv);			// handle attribute args
+	}
+	return (x);										// return the pointer
+}
+
+
+/************************************************************************************/
+// Methods bound to input/inlets
+
+
+// set position of a speaker
+void dbap_speaker(t_dbap *x, void *attr, long argc, t_atom *argv)
+{
+	long n;
+	
+	if (argc >= (x->attr_dimensions + 1)) {
+		n = atom_getlong(argv);
+		if ( (n<0) || (n>=MAX_NUM_SPEAKERS) ) {
+			error("Invalid arguments for speaker.");
+			return;
+		}
+		switch (x->attr_dimensions)
+		{
+			case 1:
+				n = atom_getlong(argv);
+				x->speaker_position[n].x = atom_getfloat(argv+1);
+				break;
+			case 2:
+				n = atom_getlong(argv);
+				x->speaker_position[n].x = atom_getfloat(argv+1);
+				x->speaker_position[n].y = atom_getfloat(argv+2);
+				break;
+			case 3:
+				n = atom_getlong(argv);
+				x->speaker_position[n].x = atom_getfloat(argv+1);
+				x->speaker_position[n].y = atom_getfloat(argv+2);
+				x->speaker_position[n].z = atom_getfloat(argv+3);
+				break;
+		}
+	}
+	else
+		error("Invalid arguments for speaker.");
+}
+
+
+// set source position and calculate output
+void dbap_source(t_dbap *x, void *attr, long argc, t_atom *argv)
+{
+	long n;
+	
+	if (argc >= (x->attr_dimensions + 1)) {
+		n = atom_getlong(argv);
+		if ( (n<0) || (n>=MAX_NUM_SPEAKERS) ) {
+			error("Invalid arguments for source.");
+			return;
+		}
+		switch (x->attr_dimensions)
+		{
+			case 1:
+				n = atom_getlong(argv);
+				x->source_position.x = atom_getfloat(argv+1);
+				dbap_calculate1D(x,n);
+				break;
+			case 2:
+				n = atom_getlong(argv);
+				x->source_position.x = atom_getfloat(argv+1);
+				x->source_position.y = atom_getfloat(argv+2);
+				dbap_calculate2D(x,n);
+				break;
+			case 3:
+				n = atom_getlong(argv);
+				x->source_position.x = atom_getfloat(argv+1);
+				x->source_position.y = atom_getfloat(argv+2);
+				x->source_position.z = atom_getfloat(argv+3);
+				dbap_calculate3D(x,n);
+				break;
+		}
+	}
+	else
+		error("Invalid arguments for source.");
+}
+
+
+// Method for Assistance Messages
+void dbap_assist(t_dbap *x, void *b, long msg, long arg, char *dst)	// Display assistance messages
+{
+	if(msg==1)
+	{ 
+		switch(arg)
+		{
+			case 0: sprintf(dst, "set source and speaker positions");
+				break;	
+		}
+	}
+	else if(msg==2)
+	{
+		switch(arg)
+		{
+			case 0: sprintf(dst, "(list) messages for matrix~"); break;
+		}
+	}
+}
+
+
+// ATTRIBUTE: dimensions (1-3)
+t_max_err dbap_attr_getdimensions(t_dbap *x, void *attr, long *argc, t_atom **argv)
+{
+	*argc = 1;
+	if (!(*argv)) // otherwise use memory passed in
+		*argv = (t_atom *)sysmem_newptr(sizeof(t_atom));
+	atom_setlong(*argv, x->attr_dimensions);
+	return MAX_ERR_NONE;	
+}
+
+t_max_err dbap_attr_setdimensions(t_dbap *x, void *attr, long argc, t_atom *argv)
+{
+	long n;
+
+	if(argc && argv) {
+		n = atom_getlong(argv);
+		if (n<1) n = 1;
+		if (n>3) n = 3;
+		x->attr_dimensions = n;
+	}
+	return MAX_ERR_NONE;
+}
+
+
+
+// ATTRIBUTE: number of speakers
+t_max_err dbap_attr_getnum_speakers(t_dbap *x, void *attr, long *argc, t_atom **argv)
+{
+	*argc = 1;
+	if (!(*argv)) // otherwise use memory passed in
+		*argv = (t_atom *)sysmem_newptr(sizeof(t_atom));
+	atom_setlong(*argv, x->attr_num_speakers);
+	return MAX_ERR_NONE;	
+}
+
+
+t_max_err dbap_attr_setnum_speakers(t_dbap *x, void *attr, long argc, t_atom *argv)
+{
+	long n;
+	
+	if(argc && argv) {	
+		n = atom_getlong(argv);
+		if (n<0) 
+			n = 0;
+		if (n>MAX_NUM_SPEAKERS) 
+			n = MAX_NUM_SPEAKERS;		
+		x->attr_num_speakers = n;
+	}	
+	return MAX_ERR_NONE;
+}
+
+
+
+// ATTRIBUTE: spatial bluriness
+t_max_err dbap_attr_getblur(t_dbap *x, void *attr, long *argc, t_atom **argv)
+{
+	*argc = 1;
+	if (!(*argv)) // otherwise use memory passed in
+		*argv = (t_atom *)sysmem_newptr(sizeof(t_atom));
+	atom_setfloat(*argv, x->attr_blur);
+	return MAX_ERR_NONE;	
+}
+
+
+t_max_err dbap_attr_setblur(t_dbap *x, void *attr, long argc, t_atom *argv)
+{
+	float f;
+	
+	if(argc && argv) {	
+		f = atom_getfloat(argv);
+		if (f<0.01) 
+			f = 0.01;	
+		x->attr_blur = f;
+	}	
+	return MAX_ERR_NONE;
+}
+
+
+
+
+// ATTRIBUTE: rolloff
+t_max_err dbap_attr_getrolloff(t_dbap *x, void *attr, long *argc, t_atom **argv)
+{
+	*argc = 1;
+	if (!(*argv)) // otherwise use memory passed in
+		*argv = (t_atom *)sysmem_newptr(sizeof(t_atom));
+	atom_setfloat(*argv, x->attr_rolloff);
+	return MAX_ERR_NONE;	
+}
+
+
+t_max_err dbap_attr_setrolloff(t_dbap *x, void *attr, long argc, t_atom *argv)
+{
+	float f;
+	
+	post("Setting rollof");
+	if(argc && argv) {	
+		f = atom_getfloat(argv);
+		if (f<=0.0) {
+			error("Invalid argument for rolloff. Must be > 0");
+			return MAX_ERR_NONE;;
+		}	
+		x->attr_rolloff = f;
+		x->a = log(x->attr_rolloff)/log(2.);
+	}	
+	return MAX_ERR_NONE;
+}
+
+
+
+/************************************************************************************/
+// Calculations
+
+
+void dbap_calculate1D(t_dbap *x, long n)
+{
+	
+}
+
+
+void dbap_calculate2D(t_dbap *x, long n)
+{
+	float k;						// Scaling coefficient
+	float k2inv;					// Inverse square of the scaling constant k
+	float dx, dy;					// Distance vector
+	float r2;						// Bluriness ratio 
+	float dia[MAX_NUM_SPEAKERS];	// Distance to ith speaker to the power of x->a.
+	t_atom a[3];					// Output array of atoms
+
+	
+	long i;
+
+	r2 = x->attr_blur*x->attr_blur;
+	k2inv = 0;
+	for (i=0; i<x->attr_num_speakers; i++) {
+		dx = x->source_position.x - x->speaker_position[i].x;
+		dy = x->source_position.y - x->speaker_position[i].y;
+		dia[i] = pow(dx*dx + dy*dy + r2, 0.5*x->a);
+		k2inv = k2inv + 1./(dia[i]*dia[i]);
+	}
+	k = sqrt(1./k2inv);
+
+	atom_setlong(&a[0], n);
+	
+	for (i=0; i<x->attr_num_speakers; i++) {
+		atom_setlong(&a[1], i);
+		atom_setfloat(&a[2], k/dia[i]);
+		outlet_anything(x->outlet, _sym_list, 3, a);
+	}	
+}
+
+
+void dbap_calculate3D(t_dbap *x, long n)
+{
+	
+}
---
+++ Tests/jcom.dbap.test.maxpat
@@ -0,0 +1,195 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"rect" : [ 25.0, 69.0, 640.0, 506.0 ],
+		"bglocked" : 0,
+		"defrect" : [ 25.0, 69.0, 640.0, 506.0 ],
+		"openrect" : [ 0.0, 0.0, 0.0, 0.0 ],
+		"openinpresentation" : 0,
+		"default_fontsize" : 10.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Verdana",
+		"gridonopen" : 0,
+		"gridsize" : [ 5.0, 5.0 ],
+		"gridsnaponopen" : 0,
+		"toolbarvisible" : 1,
+		"boxanimatetime" : 200,
+		"imprint" : 0,
+		"metadata" : [  ],
+		"boxes" : [ 			{
+				"box" : 				{
+					"maxclass" : "number",
+					"id" : "obj-11",
+					"fontname" : "Verdana",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"patching_rect" : [ 325.0, 78.0, 50.0, 19.0 ],
+					"fontsize" : 10.0,
+					"outlettype" : [ "int", "bang" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "flonum",
+					"id" : "obj-8",
+					"fontname" : "Verdana",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"patching_rect" : [ 240.0, 85.0, 50.0, 19.0 ],
+					"fontsize" : 10.0,
+					"outlettype" : [ "float", "bang" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "message",
+					"id" : "obj-4",
+					"fontname" : "Verdana",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"patching_rect" : [ 350.0, 310.0, 164.0, 17.0 ],
+					"fontsize" : 10.0,
+					"outlettype" : [ "" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "newobj",
+					"text" : "jcom.dbap",
+					"id" : "obj-1",
+					"fontname" : "Verdana",
+					"numinlets" : 1,
+					"numoutlets" : 2,
+					"patching_rect" : [ 300.0, 210.0, 63.0, 19.0 ],
+					"fontsize" : 10.0,
+					"outlettype" : [ "", "" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "message",
+					"text" : "getnum_speakers",
+					"id" : "obj-2",
+					"fontname" : "Verdana",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"patching_rect" : [ 200.0, 180.0, 100.0, 17.0 ],
+					"fontsize" : 10.0,
+					"outlettype" : [ "" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "message",
+					"text" : "blur/get $1",
+					"id" : "obj-5",
+					"fontname" : "Verdana",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"patching_rect" : [ 425.0, 120.0, 65.0, 17.0 ],
+					"fontsize" : 10.0,
+					"outlettype" : [ "" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "message",
+					"text" : "blur $1",
+					"id" : "obj-6",
+					"fontname" : "Verdana",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"patching_rect" : [ 240.0, 120.0, 50.0, 17.0 ],
+					"fontsize" : 10.0,
+					"outlettype" : [ "" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "message",
+					"text" : "dimensions $1",
+					"id" : "obj-9",
+					"fontname" : "Verdana",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"patching_rect" : [ 315.0, 120.0, 81.0, 17.0 ],
+					"fontsize" : 10.0,
+					"outlettype" : [ "" ]
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"source" : [ "obj-11", 0 ],
+					"destination" : [ "obj-9", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-9", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-8", 0 ],
+					"destination" : [ "obj-6", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-6", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-5", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-1", 1 ],
+					"destination" : [ "obj-4", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-2", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+ ]
+	}
+
+}
