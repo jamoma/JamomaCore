633ac35f33f7f5c3c12c91242c7c239b186f09b6
tim@electrotap.com <tim@electrotap.com@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-08-17 14:46:12 +0000

    Cleaning up old comments.
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@702 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- includes/core/TTSymbolTable.h
+++ includes/core/TTSymbolTable.h
@@ -11,24 +11,17 @@
 
 #include "TTElement.h"
 #include "TTSymbol.h"
-class TTValue;
 
 #ifdef TT_PLATFORM_MAC
 #include <hash_map.h>
-//#define TT_HASH_FUNCTION hash
 #else
 #include <hash_map>
 using namespace stdext;	// Visual Studio 2008 puts the hash_map in this namespace
-//#define TT_HASH_FUNCTION stdext::hash_value
 #endif
 
 
 /** This macro is defined as a shortcut for doing a lookup in the symbol table. */
 #define TT ttSymbolTable->lookup
-
-
-/** A type that represents the key as a C-String and the value as a pointer to the matching TTSymbol object. */
-typedef pair<const char*, TTSymbolPtr>																	TTSymbolTablePair;
 
 
 /** A simple helper class used by TTSymbolTable for comparing hash_map keys.  */
@@ -56,37 +49,34 @@ public:
 };
 #endif
 
+/** A type that represents the key as a C-String and the value as a pointer to the matching TTSymbol object. */
+typedef pair<const char*, TTSymbolPtr>										TTSymbolTablePair;
 
-/** An iterator for the STL hash_map used by TTSymbolTable. */
+/** A hash_map type used by TTSymbolTable. */
 #ifdef TT_PLATFORM_WIN
-//typedef hash_map<const char*, TTSymbolPtr, TTStringCompare >	TTSymbolTableHash;
-
-//class TTSymbolTableHash : public stdext::hash_map <char*, TTSymbolPtr, TTStringCompare>
-//{};
-typedef hash_map<TTString, TTSymbolPtr>	TTSymbolTableHash;
-
-//typedef hash_map<const char*, TTSymbolPtr >	TTSymbolTableHash;
-//typedef hash_map<const char*, TTSymbolPtr, hash_compare<TTStringCompare> >	TTSymbolTableHash;
-//typedef hash_map<const char*, TTSymbolPtr, hash_compare<TTStringCompare> >::const_iterator	TTSymbolTableIter;
+typedef hash_map<TTString, TTSymbolPtr>										TTSymbolTableHash;
 #else
 typedef hash_map<const char*, TTSymbolPtr, hash<char*>, TTStringCompare>	TTSymbolTableHash;
-//typedef hash_map<const char*, TTSymbolPtr, hash<char*>, TTStringCompare>::const_iterator	TTSymbolTableIter;
 #endif
 
+/** An iterator for the STL hash_map used by TTSymbolTable. */
 typedef TTSymbolTableHash::const_iterator									TTSymbolTableIter;
+
+
+// Forward declarations
+class TTValue;
 
 
 /****************************************************************************************************/
 // Class Specifications
 
-
 /**
 	The TTSymbolTable class is hash_map (based on Stroustrup pp 497) that keeps 
-	commonly used strings in a table so that we can refer to them simply as a pointers for fast comparison.
-*/
+	commonly used strings in a table so that we can refer to them simply as a pointers for fast comparison.	
+ */
+
 class TTEXPORT TTSymbolTable : public TTElement {
 private:
-//	hash_map<const char*, TTSymbolPtr, TT_HASH_FUNCTION<char*>, TTStringCompare>	symbolTable;	///< The symbol table, implemented internally as an STL hash_map.
 	TTSymbolTableHash*	symbolTable;	///< The symbol table, implemented internally as an STL hash_map.
 
 public:
@@ -105,7 +95,9 @@ public:
 	void dump(TTValue& allSymbols);
 };
 
+
 extern TTEXPORT TTSymbolTable* ttSymbolTable;		///< The global table of symbols
+
 
 #endif // __TT_SYMBOLTABLE_H__
 
