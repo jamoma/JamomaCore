4c5dda45e0663ab3d49cb256fa247102db314ff9
tim@electrotap.com <tim@electrotap.com@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2008-10-26 17:56:23 +0000

    The lydbaer source object is in it's own file now.
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@811 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ extensions/Lydbaer/lydbaerSource.cpp
@@ -0,0 +1,36 @@
+/* 
+ * Lydbaer Extensions for TTBlue
+ * Creates a wrapper for TTAudioObjects that can be used to build an audio processing graph.
+ * Copyright © 2008, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "lydbaer.h"
+#define thisTTClass LydbaerSource
+
+
+LydbaerSource::LydbaerSource(TTUInt16 newMaxNumChannels)
+: TTAudioObject("lydbaer.source", newMaxNumChannels)
+{
+	buffer = new TTAudioSignal(newMaxNumChannels);
+	setProcessMethod(processAudio);
+}
+
+
+LydbaerSource::~LydbaerSource()
+{
+	delete buffer;
+}
+
+
+TTErr LydbaerSource::processAudio(TTAudioSignal& unused, TTAudioSignal& out)
+{
+	return TTAudioSignal::copy(*buffer, out);
+}
+
+
+// Register our class with the TTBlue environment
+TT_CLASS_SETUP("lydbaer.source", "audio, wrapper", LydbaerSource);
+
