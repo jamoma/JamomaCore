c3819f2a1a80ba489e2f698b7c08c65bb3bee2cc
tap <tap@464221e2-0a0f-0410-983b-f8d57d210c17>
2009-01-14 03:24:35 +0000

    eliminate warnings
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@4474 464221e2-0a0f-0410-983b-f8d57d210c17

--- SourceCode/MaxObjects/jcom.oscinstance/jcom.oscinstance.cpp
+++ SourceCode/MaxObjects/jcom.oscinstance/jcom.oscinstance.cpp
@@ -1,7 +1,7 @@
 /* 
  * jcom.oscinstance
  * External for Jamoma: retrieve instance numbers or ids from osc messages
- * By Trond Lossius, Copyright © 2005
+ * By Trond Lossius, Copyright ï¿½ 2005
  * 
  * License: This code is licensed under the terms of the GNU LGPL
  * http://www.gnu.org/licenses/lgpl.html 
@@ -47,9 +47,7 @@ t_class		*oscinstance_class;				// Required: Global pointer for our class
 
 int JAMOMA_EXPORT_MAXOBJ main(void)
 {
-	long attrflags = 0;
 	t_class *c;
-	t_object *attr;
 	
 	jamoma_init();
 	common_symbols_init();
