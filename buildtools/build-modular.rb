be1ab02899b2a5614183117e98dac6f02d55419e
tdelahogue@yahoo.fr <tdelahogue@yahoo.fr@cf56d150-baf5-11dd-bdb3-db5ec97ac920>
2009-01-19 18:57:15 +0000

    adding the creation of a Modular folder in ../logs folder to store the _build.log and the _error.log files
    git-svn-id: http://jatuuls.googlecode.com/svn/branches/active@10 cf56d150-baf5-11dd-bdb3-db5ec97ac920

--- buildtools/build-modular.rb
+++ buildtools/build-modular.rb
@@ -50,6 +50,7 @@ puts "  "
 
 @svn_root = "../"
 @build_root = @svn_root + "../Modular/Jamoma/library/externals"
+@log_root = "../logs/Modular"
 @fail_array = Array.new
 @zerolink = false
 
@@ -98,11 +99,11 @@ end
 
 def create_logs
   # set up log files and ensure that the build_root is there
-  `mkdir -p #{@build_root}` if !FileTest.exist?(@build_root)
-  @build_log = File.new("#{@build_root}/_build.log", "w")
+  `mkdir -p #{@log_root}` if !FileTest.exist?(@log_root)
+  @build_log = File.new("#{@log_root}/_build.log", "w")
   @build_log.write("MAX BUILD LOG: #{`date`}\n\n")
   @build_log.flush
-  @error_log = File.new("#{@build_root}/_error.log", "w")
+  @error_log = File.new("#{@log_root}/_error.log", "w")
   @error_log.write("MAX BUILD ERROR LOG: #{`date`}\n\n")
   @error_log.flush
   trap("SIGINT") { die }
