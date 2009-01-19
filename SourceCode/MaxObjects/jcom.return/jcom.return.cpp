e8af1b9c865aaba71bcf3057d3402bc55b488089
tap <tap@464221e2-0a0f-0410-983b-f8d57d210c17>
2009-01-19 11:18:59 +0000

    build fix
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/active@4512 464221e2-0a0f-0410-983b-f8d57d210c17

--- SourceCode/MaxObjects/jcom.return/jcom.return.cpp
+++ SourceCode/MaxObjects/jcom.return/jcom.return.cpp
@@ -115,7 +115,7 @@ void *return_new(t_symbol *s, long argc, t_atom *argv)
 		
 		attr_args_process(x, argc, argv);
 		defer_low(x, (method)jcom_core_subscriber_subscribe, 0, 0, 0);
-		return_makesend(x, 0, 0, 0);
+		return_makesend(x);
 	}
 	return (x);
 }
