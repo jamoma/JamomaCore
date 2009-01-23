9da53bfcb42142a9fbc7a07bde784d4b44b17305
tim@electrotap.com <tim@electrotap.com@cf56d150-baf5-11dd-bdb3-db5ec97ac920>
2009-01-23 11:28:31 +0000

    fix for arg processing
    git-svn-id: http://jatuuls.googlecode.com/svn/branches/active@17 cf56d150-baf5-11dd-bdb3-db5ec97ac920

--- buildtools/build.rb
+++ buildtools/build.rb
@@ -9,11 +9,11 @@ if(ARGV.length == 0)
   puts "usage: "
   puts "build.rb <required:configuration> <optional:clean>"
   exit 0;
-elsif(ARGV.length > 2)
+elsif(ARGV.length > 1)
   `ruby build-dsp.rb ${ARGV[1]} ${ARGV[2]}`
   `ruby build-multicore.rb ${ARGV[1]} ${ARGV[2]}`
   `ruby build-modular.rb ${ARGV[1]} ${ARGV[2]}`
-elsif(ARGV.length > 1)
+elsif(ARGV.length > 0)
   `ruby build-dsp.rb ${ARGV[1]}`
   `ruby build-multicore.rb ${ARGV[1]}`
   `ruby build-modular.rb ${ARGV[1]}`
