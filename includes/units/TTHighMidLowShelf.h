b9d0a66d5a1e4cc408602e41a546b701d45d86ae
nils.peters <nils.peters@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2009-05-27 19:22:04 +0000

    initial commit of a high-mid-low shelf filter realized with one Biquad, notworking yet
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@987 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

---
+++ includes/units/TTHighMidLowShelf.h
@@ -0,0 +1,59 @@
+/* 
+ * TTBlue High-Mid-Low shelf filter 
+ * portd by Nils Peters 2009, from the PD external hml_shelf~ by Thomas Musil
+ * 
+ */
+
+//#ifndef __TT_HIGHPASS_BUTTERWORTH_2_H__
+//#define __TT_HIGHPASS_BUTTERWORTH_2_H__
+
+#ifndef _HIMIDLOW_H_
+#define _HIMIDLOW_H_
+
+#include "TTAudioObject.h"
+
+class TTEXPORT TTHighMidLowShelf : public TTAudioObject {
+protected:
+	TTFloat64		frequencyLm, frequencyMh, gainL, gainM, gainH;///< filter parameter
+	TTFloat64		a0, a1, a2, b1, b2;		///< filter coefficients
+	TTFloat64		*xm1;
+	TTFloat64		*xm2;
+	TTFloat64		*xm0;
+	//TTFloat64		*ym2;						// previous input and output samples
+
+	/**	Receives notifications when there are changes to the inherited 
+		maxNumChannels attribute.  This allocates memory for xm1, xm2, ym1, and ym2 
+		so that each channel's previous values are remembered.		*/
+	TTErr updateMaxNumChannels(const TTValue& oldMaxNumChannels);
+
+	/** Receives notifications when there are changes to the inherited 
+		sr attribute.						*/
+	TTErr updateSr();
+
+	/**	Standard audio processing method as used by TTBlue objects. */
+	TTErr processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs);
+
+private: //TODO: What is the difference between private and protected ??
+	TTFloat64 lf_, mf_, hf_, lg_, mg_, hg_;
+	void init();
+	
+public:
+
+	/**	Constructor. */
+	TTHighMidLowShelf(TTUInt16 newMaxNumChannels);
+
+	/**	Destructor. */
+	virtual ~TTHighMidLowShelf();
+
+	/**	Setter for the filter attribute. */
+	TTErr setgainL(const TTValue& value);
+	TTErr setgainM(const TTValue& value);
+	TTErr setgainH(const TTValue& value);
+	TTErr setfrequencyLm(const TTValue& value);
+	TTErr setfrequencyMh(const TTValue& value);
+	
+	TTErr clear();
+};
+
+
+#endif //  _HIMIDLOW_H_
---
+++ source/units/TTHighMidLowShelf.cpp
@@ -0,0 +1,270 @@
+/* 
+ * TTBlue High-Mid-Low shelf filter 
+ * portd by Nils Peters 2009, from the PD external hml_shelf~ by Thomas Musil
+ * 
+ */
+
+#include "TTHighMidLowShelf.h"
+#define thisTTClass TTHighMidLowShelf // TODO: what is that 
+
+
+TTHighMidLowShelf::TTHighMidLowShelf(TTUInt16 newMaxNumChannels)
+	: TTAudioObject("filter.highpass.butterworth", newMaxNumChannels), 
+	xm1(NULL), xm2(NULL), xm0(NULL)//, ym1(NULL), ym2(NULL)//TODO: this might need to be changed
+{
+	// register attributes
+	registerAttributeWithSetter(frequencyLm,	kTypeFloat64);
+	addAttributeProperty(frequencyLm,			range,			TTValue(10.0, sr*0.475));
+	addAttributeProperty(frequencyLm,			rangeChecking,	TT("clip"));
+    
+	registerAttributeWithSetter(frequencyMh,	kTypeFloat64);
+	addAttributeProperty(frequencyMh,			range,			TTValue(10.0, sr*0.475));
+	addAttributeProperty(frequencyMh,			rangeChecking,	TT("clip"));
+	
+	registerAttributeWithSetter(gainL,			kTypeFloat64);
+	addAttributeProperty(gainL,					range,			TTValue(0.0, 1.0));
+	addAttributeProperty(gainL,					rangeChecking,	TT("clip"));
+	
+	registerAttributeWithSetter(gainM,			kTypeFloat64);
+	addAttributeProperty(gainM,					range,			TTValue(0.0, 1.0));
+	addAttributeProperty(gainM,					rangeChecking,	TT("clip"));
+	
+	registerAttributeWithSetter(gainH,			kTypeFloat64);
+	addAttributeProperty(gainH,					range,			TTValue(0.0, 1.0));
+	addAttributeProperty(gainH,					rangeChecking,	TT("clip"));
+	
+	// register for notifications from the parent class so we can allocate memory as required
+	registerMessageWithArgument(updateMaxNumChannels);
+	// register for notifications from the parent class so we can recalculate coefficients as required
+	registerMessageSimple(updateSr);
+	// make the clear method available to the outside world
+	registerMessageSimple(clear);
+
+	// Set Defaults...
+	setAttributeValue(TT("maxNumChannels"),	newMaxNumChannels);			// This attribute is inherited
+	setAttributeValue(TT("frequencyLm"),		100.0);
+	setAttributeValue(TT("frequencyMh"),		3000.0);
+	setAttributeValue(TT("gainL"),		1.0);
+	setAttributeValue(TT("gainM"),		1.0);
+	setAttributeValue(TT("gainH"),		1.0);
+	setProcessMethod(processAudio);
+}
+
+
+
+TTHighMidLowShelf::~TTHighMidLowShelf()
+{
+	delete[] xm1;
+	delete[] xm2;
+	delete[] xm0;
+	//delete[] ym2;
+}
+
+
+
+
+TTErr TTHighMidLowShelf::updateMaxNumChannels(const TTValue& oldMaxNumChannels)
+{
+	delete[] xm1;
+	delete[] xm2;
+	delete[] xm0;
+	//delete[] ym2;
+	
+	xm1 = new TTFloat64[maxNumChannels];
+	xm2 = new TTFloat64[maxNumChannels];
+	xm0 = new TTFloat64[maxNumChannels];
+	//ym2 = new TTFloat64[maxNumChannels];
+	
+	clear();
+	return kTTErrNone;
+}
+
+
+TTErr TTHighMidLowShelf::updateSr()
+{ //TODO: this might need to be changed
+	//TTValue	v(frequency);
+	//return setfrequency(v);
+	init();
+	return kTTErrNone;
+}
+
+
+TTErr TTHighMidLowShelf::clear()
+{
+	short i;
+
+	for(i=0; i<maxNumChannels; i++){
+		xm1[i] = 0.0;
+		xm2[i] = 0.0;
+		xm0[i] = 0.0;
+		//ym2[i] = 0.0;
+	}
+	return kTTErrNone;
+}
+
+TTErr TTHighMidLowShelf::setfrequencyLm(const TTValue& newValue)
+{
+    if (newValue < hf_)
+    {
+		lf_ = newValue;
+        mf_ = sqrt(hf_) * sqrt(lf_); // mf_ depends on hf_ and lf_
+        init();
+	}
+    /*else if (globWarningFlag)
+       error("HiMidLow: Low shelf frequency must be less than high shelf frequency %f.", hf_);*/
+    return kTTErrNone;   
+}
+
+TTErr TTHighMidLowShelf::setfrequencyMh(const TTValue& newValue)
+{
+    if (newValue > lf_)
+    {
+		hf_ = newValue;
+		mf_ = sqrt(hf_) * sqrt(lf_); // mf_ depends on hf_ and lf_
+		init();
+	}
+    /*else if (globWarningFlag)
+        error("HiMidLow: High shelf frequency must be greater than low shelf frequency %f.", lf_);*/
+	return kTTErrNone; 
+}
+
+TTErr TTHighMidLowShelf::setgainL(const TTValue& newValue)
+{   
+    //newLg = percentToGain(newLg);	// convert from percentage to gain
+    	
+    if (lg_ != newValue)
+    {
+        lg_ = newValue;
+        init();
+    }
+	return kTTErrNone; 	
+}
+
+
+
+TTErr TTHighMidLowShelf::setgainM(const TTValue& newValue)
+{   
+    //newLg = percentToGain(newLg);	// convert from percentage to gain
+	
+	if (mg_ != newValue)
+    {
+        mg_ = newValue;
+        init();
+    }
+	return kTTErrNone; 	
+}
+
+TTErr TTHighMidLowShelf::setgainH(const TTValue& newValue)
+{   
+    //newLg = percentToGain(newLg);	// convert from percentage to gain
+	
+	if (hg_ != newValue)
+    {
+        hg_ = newValue;
+        init();
+    }
+	return kTTErrNone; 	
+}
+
+
+
+/*TTErr TTHighMidLowShelf::setfrequency(const TTValue& newValue)
+{	
+	frequency = newValue;
+
+	c = tan( kTTPi*(frequency/sr ) );
+	a0 = 1 / (1 + kTTSqrt2*c + c*c);
+	a1 = -2*a0;
+	a2 = a0;
+	b1 = 2*a0*( c*c - 1 );
+	b2 = a0 * (1 - kTTSqrt2*c + c*c);
+	return kTTErrNone;
+}*/
+
+TTErr TTHighMidLowShelf::init()
+{
+    double f = mf_ * sr;  //was sr_ TODO: is sr in Hz or in radians, becasue this code needs sr = M_PI / Samplerate[Hz]
+    double rf = sqrt(hf_) / sqrt(lf_); 
+    double l = cos(f) / sin(f);
+    double invHg = 1.0 / hg_;
+    double invLg = 1.0 / lg_;
+    double invMg = 1.0 / mg_;
+    double k1 = rf * l;
+    double k2 = l / rf;
+    double k3 = l * l;
+    double k4 = k3 * hg_;
+    double k5 = k3 * invHg;
+    double k6 = invLg + k5;
+    double k7 = invMg * k1 +  k2 * invLg * invHg * mg_;
+    double k8 = lg_ + k4;
+    double k9 = (mg_ * k1) + (k2 * lg_ * hg_ * invMg);
+    double k10 = 1.0 / (k6 + k7);
+	
+    double tempb2 = k10 * (k7 - k6);
+    double tempb1 = k10 * 2.0 * (k5 - invLg);
+    double tempa2 = k10 * (k8 - k9);
+    double tempa1 = k10 * 2.0 * (lg_ - k4);
+    double tempa0 = k10 * (k8 + k9);
+	
+    /* stability check */
+	
+    double discriminant = tempb1 * tempb1 + 4.0 * tempb2;
+    if(tempb1 <= -1.9999996)
+        tempb1 = -1.9999996;
+    else if(tempb1 >= 1.9999996)
+        tempb1 = 1.9999996;
+	
+    if(tempb2 <= -0.9999998)
+        tempb2 = -0.9999998;
+    else if(tempb2 >= 0.9999998)
+        tempb2 = 0.9999998;
+	
+    if(discriminant >= 0.0)
+    {
+        if(0.9999998 - tempb1 - tempb2 < 0.0)
+            tempb2 = 0.9999998 - tempb1;
+        if(0.9999998 + tempb1 - tempb2 < 0.0)
+            tempb2 = 0.9999998 + tempb1;
+    }
+	
+    a0 = tempa0;
+    a1 = tempa1;
+    a2 = tempa2;
+    b1 = tempb1;
+    b2 = tempb2;
+	
+	return kTTErrNone;
+}
+
+
+
+TTErr TTHighMidLowShelf::processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs)
+{
+	TTAudioSignal&	in = inputs->getSignal(0);
+	TTAudioSignal&	out = outputs->getSignal(0);
+	TTUInt16		vs;
+	TTSampleValue	*inSample,
+					*outSample;
+	TTFloat64		tempx,
+					tempy;
+	TTUInt16		numchannels = TTAudioSignal::getMinChannelCount(in, out);
+	TTUInt16		channel;
+
+	// This outside loop works through each channel one at a time
+	for(channel=0; channel<numchannels; channel++){
+		inSample = in.sampleVectors[channel];
+		outSample = out.sampleVectors[channel];
+		vs = in.getVectorSize();
+		
+		// This inner loop works through each sample within the channel one at a time
+		while(vs--){
+			tempx = *inSample++;
+			xm0[channel] = tempx + b1 * xm1[channel] + b2 * xm2[channel]; //TODO: not sure if we need TTAntiDenormal here
+			tempy        = TTAntiDenormal(a0 * xm0[channel] + a1 * xm1[channel] + a2 * xm2[channel]);
+			xm2[channel] = xm1[channel];
+			xm1[channel] = xm0[channel];
+			*outSample++ = tempy;
+		}
+	}
+	return kTTErrNone;
+}
