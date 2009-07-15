f79cf082455e13ef3359b9a79416b67c1e2983f3
tdelahogue <tdelahogue@464221e2-0a0f-0410-983b-f8d57d210c17>
2009-07-15 14:42:12 +0000

    Adding a jcom.minuit external (based on the NodeLib)
    git-svn-id: https://jamoma.svn.sourceforge.net/svnroot/jamoma/branches/experimental@5592 464221e2-0a0f-0410-983b-f8d57d210c17

---
+++ SourceCode/MaxObjects/jcom.minuit/jcom.minuit.cpp
@@ -0,0 +1,86 @@
+/* 
+ * jcom.node
+ * External for Jamoma : ...
+ * By Théo de la Hogue, Copyright 2009
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+#include "jcom.minuit.h"
+
+// Globals
+t_class		*minuit_class;
+
+// implementation
+#if 0
+#pragma mark -
+#pragma mark Class Definition
+#endif 0
+
+int JAMOMA_EXPORT_MAXOBJ main(void)
+{
+	t_class *c;
+	
+	jamoma_init();
+	common_symbols_init();
+
+	// Define our class
+	c = class_new("jcom.minuit",(method)minuit_new, (method)minuit_free, (long)sizeof(t_minuit), 0L, A_GIMME, 0);
+
+	// add methods
+	class_addmethod(c, (method)minuit_notify,			"notify",		A_CANT, 0);
+	class_addmethod(c, (method)minuit_assist,			"assist",		A_CANT, 0);
+
+	// Finalize our class
+	class_register(CLASS_BOX, c);
+	node_class = c;
+	return 0;
+}
+
+#if 0
+#pragma mark -
+#pragma mark Life Cycle
+#endif 0
+
+void *node_new(t_symbol *name, long argc, t_atom *argv)
+{
+	t_node *x;
+
+	x = (t_node*)object_alloc(node_class);
+
+	if(x){
+
+		// default : get the root of the tree
+		x->p_node = jamoma_node_init();
+
+	}
+	return x;
+}
+
+void node_free(t_node *x)
+{
+	;
+}
+
+#if 0
+#pragma mark -
+#pragma mark Methods
+#endif 0
+
+t_max_err node_notify(t_node *x, t_symbol *s, t_symbol *msg, void *sender, void *data)
+{
+	object_post((t_object *)x, "notification : %s", msg->s_name);
+	return MAX_ERR_NONE;
+}
+
+void node_assist(t_node *x, void *b, long msg, long arg, char *dst)
+{
+	if (msg == ASSIST_INLET) { // inlet
+		//if(arg == 0)
+		//strcpy(dst, "I am inlet 0");
+	} 
+	else {	// outlet
+		//if(arg == 0)
+		//strcpy(dst, "I am outlet 0");
+	}		
+}
