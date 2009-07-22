6772675c398233384931d930fb57ec89658766df
tim@electrotap.com <tim@electrotap.com@bc2ad3ca-9a4d-0410-8d14-35da9c29b421>
2009-07-22 20:59:09 +0000

    SpatDisplay: lots of progress on drawing the display.  Currently organized with 2 separate lists: one for sources and one for microphones.  To make the mouse handling easier these may get merged into a single TTList in a future rev.  Testing this display largely requires Vimic code that is only present on my local machine at the moment...
    git-svn-id: http://ttblue.googlecode.com/svn/branches/active@1086 bc2ad3ca-9a4d-0410-8d14-35da9c29b421

--- extensions/SpatDisplay/SpatDisplay.cpp
+++ extensions/SpatDisplay/SpatDisplay.cpp
@@ -15,14 +15,44 @@
 #define thisTTClassTags		"graphics, widget, spatialization"
 
 
+/**	An entity within the SpatDisplay that is located in 3d space. 
+	Examples include mic positions, source locations, and speaker coords. */
+class SpatDisplayEntity : TTElement {
+public:
+	TTFloat64		x;				///< x
+	TTFloat64		y;				///< y 
+	TTFloat64		z;				///< z  
+	TTFloat64		yaw;			///< rotation on the horizontal plane
+	TTFloat64		pitch;			///< rotation on the z axis
+	TTFloat64		directivity;	///< how directional is the entity
+	TTFloat64		intensity;		///< the intensity of the signal for the entity
+	
+	SpatDisplayEntity():
+	x(0), y(0), z(0), yaw(0), pitch(0), directivity(0), intensity(0)
+	{
+		;
+	}
+};
+
+
 /**	A basic user interface for spatial positioning. */
 class SpatDisplay : TTObject {
 	TTCLASS_SETUP(SpatDisplay)
 
-	TTObjectPtr		graphicsSurface;
-	TTFloat64		width;
-	TTFloat64		height;
-	TTFloat64		position; // range [0.0, 1.0]	
+	TTObjectPtr			graphicsSurface;
+	TTGraphicsContext*	g;
+	TTListPtr			sources;
+	TTListPtr			microphones;
+	TTFloat64			width;
+	TTFloat64			height;
+	
+	
+	TTErr setnumSources(const TTValue& newValue);
+	TTErr getnumSources(TTValue& returnedValue);
+	TTErr setnumMicrophones(const TTValue& newValue);
+	TTErr getnumMicrophones(TTValue& returnedValue);
+	TTErr drawSource(const TTValue& v);
+	TTErr drawMicrophone(const TTValue& v);
 	
 	TTErr paint();
 	TTErr draw(const TTValue& v);
@@ -47,8 +77,15 @@ TT_OBJECT_CONSTRUCTOR,
 	TTObjectInstantiate(TT("TTGraphicsSurface"), &graphicsSurface, arguments);	// create the surface we will draw to
 	graphicsSurface->registerObserverForNotifications(*this);			// cause this object to receive 'draw' messages from the window
 	
+	sources = new TTList;
+	microphones = new TTList;
+			
 	//registerAttributeSimple(mode, kTypeSymbol);
-	registerAttributeSimple(position, kTypeFloat64);
+	//registerAttributeSimple(position, kTypeFloat64);
+	registerAttributeWithSetterAndGetter(numSources, kTypeUInt16);
+	registerAttributeWithSetterAndGetter(numMicrophones, kTypeUInt16);
+	registerMessageWithArgument(drawSource);
+	registerMessageWithArgument(drawMicrophone);
 	
 	registerMessageSimple(paint);				// send my the Max object to tell our surface to paint
 	registerMessageWithArgument(draw);			// callback from TTGraphics surface
@@ -70,7 +107,62 @@ SpatDisplay::~SpatDisplay()
 {
 	graphicsSurface->unregisterObserverForNotifications(*this);
 	TTObjectRelease(&graphicsSurface);
+	delete sources;
+	delete microphones;
 }
+
+
+TTErr SpatDisplay::setnumSources(const TTValue& newValue)
+{
+	TTUInt16	oldNumSources = sources->getSize();
+	TTUInt16	newNumSources = newValue;
+	
+	// we could try to preseve the old source data here
+	// for now we are just going to be lazy and obliterate it
+	if (oldNumSources)
+		sources->free();
+		// FIXME: memory leak: we free the TTValue, but not the entity pointed to by the value!
+	
+	for (TTUInt16 i=0; i<newNumSources; i++) {
+		SpatDisplayEntity*	source = new SpatDisplayEntity;
+		TTValue*			v = new TTValue(TTPtr(source));
+		
+		sources->append(v);
+	}
+	return kTTErrNone;
+}
+
+TTErr SpatDisplay::getnumSources(TTValue& returnedValue)
+{
+	returnedValue = sources->getSize();
+	return kTTErrNone;
+}
+
+
+TTErr SpatDisplay::setnumMicrophones(const TTValue& newValue)
+{
+	TTUInt16	oldNumMicrophones = microphones->getSize();
+	TTUInt16	newNumMicrophones = newValue;
+	
+	// we could try to preseve the old source data here
+	// for now we are just going to be lazy and obliterate it
+	if (oldNumMicrophones)
+		microphones->clear();
+	
+	for (TTUInt16 i=0; i<newNumMicrophones; i++) {
+		SpatDisplayEntity*	mic = new SpatDisplayEntity;
+		microphones->append(TTPtr(mic));
+	}
+	return kTTErrNone;
+}
+
+TTErr SpatDisplay::getnumMicrophones(TTValue& returnedValue)
+{
+	returnedValue = microphones->getSize();
+	return kTTErrNone;
+}
+
+
 
 
 TTErr SpatDisplay::paint()
@@ -82,55 +174,76 @@ TTErr SpatDisplay::paint()
 
 TTErr SpatDisplay::draw(const TTValue& v)
 {
-	TTGraphicsContext* gc = (TTGraphicsContext*)TTPtr(v);
-	char cstr[16];
+	char				cstr[16];
+	TTFloat64			xc = width/2.0;
+	TTFloat64			yc = height/2.0;
+	TTGraphicsColor		fillColor(0.1, 0.1, 0.1, 1.0);
+	TTGraphicsColor		borderColor(0.4, 0.4, 0.4, 1.0);
+	TTFloat64			f;
+	
+	g = (TTGraphicsContext*)TTPtr(v);
 	
 	// Draw background
-	gc->setSourceRGBA(0.2, 0.2, 0.2, 1.0);
-	gc->rectangle(0.0, 0.0, width, height);
-	gc->fill();
+	g->setSourceRGBA(0.2, 0.2, 0.2, 1.0);
+	g->rectangle(0.0, 0.0, width, height);
+	g->fill();
 	
 	// Draw text
-	gc->selectFontFace("Helvetica Neue", TT_FONT_SLANT_NORMAL, TT_FONT_WEIGHT_NORMAL);
-	gc->setFontSize(18.0);
-	gc->setSourceRGBA(0.8, 0.8, 0.8, 1.0);
-	gc->moveTo(10.0, 20.0);
-	gc->showText("Spat Display");
+	g->selectFontFace("Helvetica Neue", TT_FONT_SLANT_NORMAL, TT_FONT_WEIGHT_NORMAL);
+	g->setFontSize(18.0);
+	g->setSourceRGBA(0.8, 0.8, 0.8, 1.0);
+	g->moveTo(10.0, 20.0);
+	g->showText("Spat Display");
 	
-	gc->setFontSize(12.0);
-	gc->moveTo(width-100.0, height-60.0);
-	snprintf(cstr, 16, "%ld Hz", (long)(position*20000.0));
-	gc->showText(cstr);
+	g->setFontSize(12.0);
+	g->moveTo(width-100.0, height-60.0);
+	snprintf(cstr, 16, "Hi there...");
+	g->showText(cstr);
+		
+	// draw unit circle and grid
+	g->filledCircle(xc, yc, (height/2.0) * 0.75, 3.0, fillColor, borderColor);
+	fillColor.set(0.15, 0.15, 0.15, 1.0);
+	g->filledCircle(xc, yc, (height/2.0) * 0.6,  3.0, fillColor, borderColor);
+	g->filledCircle(xc, yc, (height/2.0) * 0.45, 1.0, fillColor, borderColor);
+	g->filledCircle(xc, yc, (height/2.0) * 0.3,  1.0, fillColor, borderColor);
+	g->filledCircle(xc, yc, (height/2.0) * 0.15, 1.0, fillColor, borderColor);
 	
-	// position attr is [0.0, 1.0], and here we convert it into an angle in degrees [0.0, 360.0]
-	double deg = 180.0 - (position * 360.0) + 90.0;
+	f = sin(kTTPi*0.25);
+	f = height * 0.6 * 0.5 * f;
+	g->lineSegment(xc, height*0.2, xc, height*0.8, 1.0, borderColor);
+	g->lineSegment(xc-(height*0.6*0.5), yc, xc+(height*0.6*0.5), yc, 1.0, borderColor);
+	g->lineSegment(xc-f, yc-f, xc+f, yc+f, 1.0, borderColor);
+	g->lineSegment(xc+f, yc-f, xc- f, yc+f, 1.0, borderColor);
 	
-	// draw dial
-	double xc = width/2.0;
-	double yc = height/2.0;
-	double radius = (height/2.0) * 0.8;
-	double angle1 = (360.0 - deg) * (kTTPi/180.0);  // angles for cairo are specified in radians
-	double angle2 = 90.0 * (kTTPi/180.0);
-	
-	gc->moveTo(width/2.0, height/2.0);
-	gc->setSourceRGBA(0.4, 0.4, 0.4, 1.0);
-	gc->setLineWidth(10.0);
-	gc->arc(xc, yc, radius, angle2, angle1);
-	gc->stroke();
-	
-	gc->setSourceRGBA(1.0, 0.2, 0.2, 0.6);
-	gc->setLineWidth(6.0);
-	gc->arc(xc, yc, 10.0, 0, 2.0*kTTPi);
-	gc->fill();
-	
-	gc->arc(xc, yc, radius, angle1, angle1);
-	gc->lineTo(xc, yc);
-	gc->arc(xc, yc, radius, angle2, angle2);
-	gc->lineTo(xc, yc);
-	gc->stroke();
-	
+	// draw moveable entities
+	sources->iterate(this, TT("drawSource"));
+	microphones->iterate(this, TT("drawMicrophone"));
+		
 	return kTTErrNone;
 }
+
+
+TTErr SpatDisplay::drawSource(const TTValue& v)
+{
+	SpatDisplayEntity*	source = (SpatDisplayEntity*)(TTPtr(v));
+	TTGraphicsColor		fillColor(0.1, 0.5, 0.1, 1.0);
+	TTGraphicsColor		borderColor(0.1, 0.7, 0.1, 1.0);
+
+	g->filledCircle((width*0.5)+source->x, (height*0.5)+source->y, 3.0, 2.0, fillColor, borderColor);
+	return kTTErrNone;
+}
+
+
+TTErr SpatDisplay::drawMicrophone(const TTValue& v)
+{
+	SpatDisplayEntity*	mic = (SpatDisplayEntity*)(TTPtr(v));
+	TTGraphicsColor		fillColor(0.4, 0.1, 0.1, 1.0);
+	TTGraphicsColor		borderColor(0.7, 0.1, 0.1, 1.0);
+	
+	g->filledCircle((width*0.5)+mic->x, (height*0.5)+mic->y, 3.0, 2.0, fillColor, borderColor);
+	return kTTErrNone;
+}
+
 
 
 TTErr SpatDisplay::getData(TTValue& v)
