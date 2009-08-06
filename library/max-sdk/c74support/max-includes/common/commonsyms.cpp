78afacdbe513137793c03dfe3fb5c220090a5bd2
tap <tim@electrotap.com>
2009-08-06 11:23:41 -0700

    resolving conflict

--- library/max-sdk/c74support/max-includes/common/commonsyms.cpp
+++ library/max-sdk/c74support/max-includes/common/commonsyms.cpp
@@ -1,7 +1,1 @@
-<<<<<<< HEAD:library/max-sdk/c74support/max-includes/common/commonsyms.cpp
-// This wraps commonsyms.c as a C++ file to avoid dumb warnings in Xcode
-=======
-// This wrapps commonsyms.c as a C++ file to avoid dumb warnings in Xcode
->>>>>>> active:library/max-sdk/c74support/max-includes/common/commonsyms.cpp
-
 #include "commonsyms.c"
