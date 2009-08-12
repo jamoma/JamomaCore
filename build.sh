3416c5c2f76c6d26a92406fffcd1f75d486326e8
Timothy Place <tim@electrotap.com>
2009-08-12 10:30:44 -0500

    fixes to the script for building everything

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-ruby ../Modules/Foundation/build.rb $1 $2
-ruby ../Modules/DSP/build.rb $1 $2
-ruby ../Modules/Graphics/build.rb $1 $2
-ruby ../Modules/Multicore/build.rb $1 $2
-ruby ../Modules/Modular/build.rb $1 $2
+cd ..
+JAMOMA_PATH=`pwd`
+
+cd $JAMOMA_PATH/Modules/Foundation
+ruby build.rb $1 $2
+
+cd $JAMOMA_PATH/Modules/DSP
+ruby build.rb $1 $2
+
+cd $JAMOMA_PATH/Modules/Graphics
+ruby build.rb $1 $2
+
+cd $JAMOMA_PATH/Modules/Multicore
+ruby build.rb $1 $2
+
+cd $JAMOMA_PATH/Modules/Modular
+ruby build.rb $1 $2
