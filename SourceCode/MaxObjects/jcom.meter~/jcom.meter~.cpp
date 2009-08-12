ba99676d47345b4d8dd80cabbd291ec41d452b7e
Timothy Place <tim@electrotap.com>
2009-08-12 16:29:59 -0500

    most projects are now updated and building against the new frameworks

--- SourceCode/MaxObjects/jcom.meter~/jcom.meter~.cpp
+++ SourceCode/MaxObjects/jcom.meter~/jcom.meter~.cpp
@@ -16,7 +16,7 @@
 #include "jpatcher_api.h"	// jpatcher_api.h must come before z_dsp.h (in Jamoma.h)
 #include "jgraphics.h"
 #include "Jamoma.h"
-#include "TTBlue.h"
+#include "TTDSP.h"
 
 
 // Constants
