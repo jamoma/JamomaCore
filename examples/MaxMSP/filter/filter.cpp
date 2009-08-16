8adad34ab7aa9de0395fce8737d09d8a36587092
Timothy Place <tim@electrotap.com>
2009-08-16 12:14:12 -0500

    filter≈ object is now compiling properly

--- examples/MaxMSP/filter/filter.cpp
+++ examples/MaxMSP/filter/filter.cpp
@@ -8,14 +8,18 @@
  */
 
 #include "maxMulticore.h"
-#define thisTTClass TTFilter
+
+#define thisTTClass			TTFilter
+#define thisTTClassName		"filter"
+#define thisTTClassTags		"audio, processor, filter, multicore"
 
 
 
 // For the filter≈ object, we wish to create our own class, 
 // which then encapsulates the various filters available in TTBlue.
 
-class TTFilter : public TTAudioObject {
+TTAUDIOCLASS(TTFilter)
+
 protected:
 	TTAudioObjectPtr	actualFilterObject;		///< The actual filter object that this object is currently wrapping
 	TTFloat64			frequency;				///< The center or cutoff frequency of the filter
@@ -23,35 +27,7 @@ protected:
 	TTSymbolPtr			type;					///< The name of the current filter type
 	
 public:
-	
-	// Constructor
-	TTFilter(TTUInt16 newMaxNumChannels)
-		: TTAudioObject("filter", newMaxNumChannels), actualFilterObject(NULL)
-	{
-		registerAttributeWithSetter(frequency, kTypeFloat64);
-		registerAttributeWithSetter(q, kTypeFloat64);
-		registerAttributeWithSetter(type, kTypeSymbol);
 		
-		registerMessageWithArgument(getTypes);
-		registerMessageSimple(clear);
-
-		registerMessageWithArgument(updateMaxNumChannels);
-		registerMessageSimple(updateSr);
-		
-		setAttributeValue(TT("maxNumChannels"), newMaxNumChannels);
-		setAttributeValue(TT("type"), TT("lowpass.1"));
-		setAttributeValue(TT("frequency"), 1000.0);
-		setAttributeValue(TT("q"), 1.0);
-		setProcessMethod(processAudio);
-	}
-	
-	// Destructor
-	virtual ~TTFilter()
-	{
-		;
-	}
-	
-	
 	TTErr setfrequency(const TTValue& newValue)
 	{	
 		frequency = newValue;
@@ -123,24 +99,43 @@ public:
 };
 
 
-
-// Because we have created this overdriveExtended class outside of the main TTBlue framework, 
-// we have to define our own factory method that is used to create a new instance of our object.
-
-TTObjectPtr instantiateTTFilter(TTSymbolPtr className, TTValue& arguments)
+TT_AUDIO_CONSTRUCTOR_EXPORT,
+	actualFilterObject(NULL)
 {
-	return new TTFilter(arguments);
+	registerAttributeWithSetter(frequency, kTypeFloat64);
+	registerAttributeWithSetter(q, kTypeFloat64);
+	registerAttributeWithSetter(type, kTypeSymbol);
+	
+	registerMessageWithArgument(getTypes);
+	registerMessageSimple(clear);
+	
+	registerMessageWithArgument(updateMaxNumChannels);
+	registerMessageSimple(updateSr);
+	
+	setAttributeValue(TT("maxNumChannels"), arguments);
+	setAttributeValue(TT("type"), TT("lowpass.1"));
+	setAttributeValue(TT("frequency"), 1000.0);
+	setAttributeValue(TT("q"), 1.0);
+	setProcessMethod(processAudio);
 }
 
 
+// Destructor
+TTFilter::~TTFilter()
+{
+	;
+}
+
+
+/*******************************************************************************/
 
 int main(void)
 {
 	TTMulticoreInit();
 
-	// First, we have to register our custom subclass with the TTBlue framework.
-	TTClassRegister(TT("filter"), "audio, processor, effect, lydbaer", &instantiateTTFilter);
+	// First, we have to register our custom subclass with the Jamoma Foundation runtime.
+	TTFilter::registerClass();
+	
 	// Then we are able to wrap it as a Max class.
 	return wrapAsMaxbaer(TT("filter"), "filter≈", NULL);
 }
-
