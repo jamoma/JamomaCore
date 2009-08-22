b3fd8701eba0b41dddbcbd2aad4af77b9b9ca149
Timothy Place <tim@electrotap.com>
2009-08-22 09:46:12 -0500

    EffectsLib: now builds on Linux

--- extensions/EffectsLib/TTOverdrive.h
+++ extensions/EffectsLib/TTOverdrive.h
@@ -1,9 +1,9 @@
-/* 
- * TTBlue Overdrive / Soft Saturation Effect 
+/*
+ * TTBlue Overdrive / Soft Saturation Effect
  * Copyright © 2008, Timothy Place
- * 
+ *
  * License: This code is licensed under the terms of the GNU LGPL
- * http://www.gnu.org/licenses/lgpl.html 
+ * http://www.gnu.org/licenses/lgpl.html
  */
 
 #ifndef __TT_OVERDRIVE_H__
@@ -12,7 +12,7 @@
 #include "TTDSP.h"
 
 // silly stuff to get symbols exported so we can subclass this class in external code
-#ifdef TT_PLATFORM_MAC
+#if defined( TT_PLATFORM_MAC ) or defined ( TT_PLATFORM_LINUX )
 #define TT_OVERDRIVE_EXPORT TTEXPORT
 #else
 #ifdef TT_EFFECTS_LIB
@@ -23,7 +23,7 @@
 #endif
 
 
-/**	TTOverdrive is an audio processor that provides a soft saturation or overdrive effect to "warm" a sound up. */ 
+/**	TTOverdrive is an audio processor that provides a soft saturation or overdrive effect to "warm" a sound up. */
 class TT_OVERDRIVE_EXPORT TTOverdrive : public TTAudioObject {
 	TTCLASS_SETUP(TTOverdrive)
 
@@ -31,16 +31,16 @@ class TT_OVERDRIVE_EXPORT TTOverdrive : public TTAudioObject {
 	TTBoolean			dcBlocker;		///< Attribute:
 	TTUInt8				mode;			///< Attribute:
 	TTFloat64			preamp;			///< Attribute: linear gain for preamp (attr setter sets it in dB)
-	TTFloat64			s, 
-						b, 
-						nb, 
-						z, 
+	TTFloat64			s,
+						b,
+						nb,
+						z,
 						scale;
 	TTAudioObjectPtr	dcBlockerUnit;	///< A TTDCBlock object
 
 	/**	This method gets called when the inherited maxNumChannels attribute is changed. */
 	TTErr updateMaxNumChannels(const TTValue& oldMaxNumChannels);
-	
+
 	/**	Process method when mode == 0	*/
 	TTErr processMode0(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs);
 
@@ -51,18 +51,18 @@ public:
 
 	/**	Setter for the mode attribute. */
 	TTErr setdrive(const TTValue& value);
-	
+
 	/**	Setter for the mode attribute. */
 	TTErr setdcBlocker(const TTValue& value);
-	
+
 	/**	Setter for the mode attribute. */
 	TTErr setmode(const TTValue& value);
-	
+
 	/**	Getter for the mode attribute. */
 	TTErr getpreamp(TTValue& value);
 	/**	Setter for the mode attribute. */
 	TTErr setpreamp(const TTValue& value);
-	
+
 	/**	Reset the DC Blocker.			*/
 	TTErr clear();
 };
