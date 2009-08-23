4f95159851a47d9c504def7362b37b590c3d4dd4
Timothy Place <tim@electrotap.com>
2009-08-23 12:40:05 -0500

    work started on porting the DataspaceLib to become Max-independent

--- extensions/DataspaceLib/DataspaceLib.cpp
+++ extensions/DataspaceLib/DataspaceLib.cpp
@@ -1,16 +1,17 @@
-/* 
+/*
  * Jamoma FunctionLib Base Class
  * Copyright © 2007
- * 
+ *
  * License: This code is licensed under the terms of the GNU LGPL
- * http://www.gnu.org/licenses/lgpl.html 
+ * http://www.gnu.org/licenses/lgpl.html
  */
 
 #include "DataspaceLib.h"
 
-DataspaceUnit::DataspaceUnit(const char *cName)
+DataspaceUnit::DataspaceUnit(const TTSymbolPtr newName) :
+    name(newName)
 {
-	name = SymbolGen(cName);
+	;
 }
 
 
@@ -22,41 +23,54 @@ DataspaceUnit::~DataspaceUnit()
 
 /***********************************************************************************/
 
-DataspaceLib::DataspaceLib(const char *cName, const char *cNativeUnit)
-	: inUnit(NULL), outUnit(NULL)
+DataspaceLib::DataspaceLib(const TTSymbolPtr newName, const TTSymbolPtr newNativeUnit) :
+    inUnit(NULL),
+    outUnit(NULL),
+    neutralUnit(newNativeUnit),
+    name(newName)
 {
-	unitHash = hashtab_new(0);
-	name = SymbolGen(cName);
-	neutralUnit = SymbolGen(cNativeUnit);
+	unitHash = new TTHash;
 }
 
 
 DataspaceLib::~DataspaceLib()
 {
-	t_symbol		**keys = NULL;
+//	t_symbol		**keys = NULL;
+    TTValue         keys;
 	long			numKeys = 0;
 	long			i;
 	DataspaceUnit	*unit;
 
-	hashtab_getkeys(unitHash, &numKeys, &keys);
+    unitHash->getKeys(keys);
+    numKeys = unitHash->getSize();
+
+//	hashtab_getkeys(unitHash, &numKeys, &keys);
 	for(i=0; i<numKeys; i++){
-		hashtab_lookup(unitHash, keys[i], (t_object**)&unit);
-		delete unit;
+//		hashtab_lookup(unitHash, keys[i], (t_object**)&unit);
+        TTSymbolPtr key;
+        TTValue     v;
+
+        keys.get(i, &key);
+        unitHash->lookup(key, value);
+        unit = value;
+//		delete unit;
+        TTObjectRelease(&unit);
 	}
-	
-	if(keys)
-		sysmem_freeptr(keys);
-		
-	hashtab_chuck(unitHash);
+
+//	if(keys)
+//		sysmem_freeptr(keys);
+
+//	hashtab_chuck(unitHash);
+    delete unitHash;
 }
 
 
-// remember, we are relying on memory passed in for the outputAtoms		
+// remember, we are relying on memory passed in for the outputAtoms
 JamomaError DataspaceLib::convert(long inputNumArgs, t_atom *inputAtoms, long *outputNumArgs, t_atom **outputAtoms)
 {
 	double	value[3];	// right now we only handle a maximum of 3 values in the neutral unit passing
 	long	numvalues;
-	
+
 	if(inUnit->name == outUnit->name){
 		*outputNumArgs = inputNumArgs;
 		sysmem_copyptr(inputAtoms, *outputAtoms, sizeof(t_atom) * inputNumArgs);
@@ -67,13 +81,15 @@ JamomaError DataspaceLib::convert(long inputNumArgs, t_atom *inputAtoms, long *o
 	}
 	return JAMOMA_ERR_NONE;
 }
+TTErr convert(const TTValue& input, TTValue& output);
 
-		
+
+
 JamomaError DataspaceLib::setInputUnit(t_symbol *inUnitName)
 {
 	t_object*	newUnit = NULL;
 	JamomaError	err;
-	
+
 	if(inUnit && inUnitName == inUnit->name)	// already have this one loaded
 		return JAMOMA_ERR_NONE;
 	else{
@@ -83,13 +99,15 @@ JamomaError DataspaceLib::setInputUnit(t_symbol *inUnitName)
 		return err;
 	}
 }
+TTErr setInputUnit(TTSymbolPtr inUnitName);
+
 
 
 JamomaError DataspaceLib::setOutputUnit(t_symbol *outUnitName)
 {
 	t_object*	newUnit = NULL;
 	JamomaError	err;
-	
+
 	if(outUnit && outUnitName == outUnit->name)	// already have this one loaded
 		return JAMOMA_ERR_NONE;
 	else{
@@ -99,19 +117,20 @@ JamomaError DataspaceLib::setOutputUnit(t_symbol *outUnitName)
 		return err;
 	}
 }
+TTErr setOutputUnit(TTSymbolPtr outUnitName);
 
 
+void DataspaceLib::registerUnit(void *unit, const TTSymbolPtr unitName)
 void DataspaceLib::registerUnit(void *unit, t_symbol *unitName)
 {
 	hashtab_store(unitHash, unitName, (t_object*)unit);
 }
 
 
-void DataspaceLib::getAvailableUnits(long *numUnits, t_symbol ***unitNames)
+TTErr DataspaceLib::getAvailableUnits(TTValue& unitNames)
 {
-	hashtab_getkeys(unitHash, numUnits, unitNames);
+    return unitHash->getKeys(unitNames);
 }
-
 
 
 
@@ -124,66 +143,28 @@ void DataspaceLib::getAvailableUnits(long *numUnits, t_symbol ***unitNames)
 #include "GainDataspace.h"
 #include "NoneDataspace.h"
 #include "PitchDataspace.h"
-#include "PositionDataspace.h" 
+#include "PositionDataspace.h"
 #include "TemperatureDataspace.h"
 #include "TimeDataspace.h"
 
 
-JamomaError jamoma_getDataspace(t_symbol *dataspaceName, DataspaceLib **dataspace)
-{	
-	if(*dataspace){
-		if(dataspaceName == (*dataspace)->name)
-			return JAMOMA_ERR_NONE;	// already have this one, do nothing...
-		else{
-			delete *dataspace;
-			*dataspace = NULL;
-		}
-	}
-
-	// These should be alphabetized
-	if(dataspaceName == SymbolGen("angle"))
-		*dataspace = (DataspaceLib*) new AngleDataspace;
-	else if(dataspaceName == SymbolGen("color"))
-		*dataspace = (DataspaceLib*) new ColorDataspace;
-	else if(dataspaceName == SymbolGen("distance"))
-		*dataspace = (DataspaceLib*) new DistanceDataspace;
-	else if(dataspaceName == SymbolGen("gain"))
-		*dataspace = (DataspaceLib*) new GainDataspace;
-	else if(dataspaceName == SymbolGen("none"))
-		*dataspace = (DataspaceLib*) new NoneDataspace;
-	else if(dataspaceName == SymbolGen("pitch"))
-		*dataspace = (DataspaceLib*) new PitchDataspace;
-	else if(dataspaceName == SymbolGen("position")) 
-		*dataspace = (DataspaceLib*) new PositionDataspace;
-	else if(dataspaceName == SymbolGen("temperature"))
-		*dataspace = (DataspaceLib*) new TemperatureDataspace;
-	else if(dataspaceName == SymbolGen("time"))
-		*dataspace = (DataspaceLib*) new TimeDataspace;
-	else 
-		// Invalid -- default to temperature
-		*dataspace = (DataspaceLib*) new TemperatureDataspace;
-	
-	return JAMOMA_ERR_NONE;
-}
-
-
-// This function allocates memory -- caller must free it!
-void jamoma_getDataspaceList(long *numDataspaces, t_symbol ***dataspaceNames)
+extern "C" TT_EXTENSION_EXPORT TTErr loadTTExtension(void)
 {
-	*numDataspaces = 9; // must be the length of the Dataspace
-	*dataspaceNames = (t_symbol**)sysmem_newptr(sizeof(t_symbol*) * *numDataspaces);
-	
-	// These should be alphabetized
-	if(*numDataspaces){
-		*(*dataspaceNames+0) = SymbolGen("angle");
-		*(*dataspaceNames+1) = SymbolGen("color");
-		*(*dataspaceNames+2) = SymbolGen("distance");
-		*(*dataspaceNames+3) = SymbolGen("gain");
-		*(*dataspaceNames+4) = SymbolGen("none");
-		*(*dataspaceNames+5) = SymbolGen("pitch");
-		*(*dataspaceNames+6) = SymbolGen("position"); 
-		*(*dataspaceNames+7) = SymbolGen("temperature");
-		*(*dataspaceNames+8) = SymbolGen("time");
-	}
+	TTFoundationInit();
+
+    Dataspace::registerClass();
+
+	AngleDataspace::registerClass();
+	ColorDataspace::registerClass();
+	DistanceDataspace::registerClass();
+	GainDataspace::registerClass();
+	NoneDataspace::registerClass();
+	PitchDataspace::registerClass();
+	PositionDataspace::registerClass();
+	TemperatureDataspace::registerClass();
+	TimeDataspace::registerClass();
+
+	return kTTErrNone;
 }
+
 
