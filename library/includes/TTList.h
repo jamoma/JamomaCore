d0652616041b31fccc8edf74db89b1bbda7bd2e4
Timothy Place <tim@electrotap.com>
2009-09-06 21:48:36 -0500

    TTList: added new 'getIndex' method.

--- library/includes/TTList.h
+++ library/includes/TTList.h
@@ -51,6 +51,9 @@ public:
 		@return	The last value in the list.				*/
 	TTValue& getTail();
 	
+	/**	Return a value by it's location in the list.	*/
+	TTErr getIndex(TTUInt32 index, TTValue& returnedValue);
+	
 	/** Appends a value to the list. 
 		@param	newValue	The value to add to the list.  */
 	void append(const TTValue& newValue);
