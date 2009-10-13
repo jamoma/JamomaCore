25897a47dc5b8eece6f2c1e520d72e8fab03b805
Timothy Place <tim@electrotap.com>
2009-10-12 19:44:54 -0500

    jcom.snapshot: it ain't pretty, but it does store and recall global snapshots of all int & float parameters in the top-level of their modules.

--- Jamoma/documentation/jamoma-help/jcom.snapshot.maxhelp
+++ Jamoma/documentation/jamoma-help/jcom.snapshot.maxhelp
@@ -1,9 +1,9 @@
 {
 	"patcher" : 	{
 		"fileversion" : 1,
-		"rect" : [ 219.0, 248.0, 640.0, 506.0 ],
+		"rect" : [ 219.0, 248.0, 853.0, 620.0 ],
 		"bglocked" : 0,
-		"defrect" : [ 219.0, 248.0, 640.0, 506.0 ],
+		"defrect" : [ 219.0, 248.0, 853.0, 620.0 ],
 		"openrect" : [ 0.0, 0.0, 0.0, 0.0 ],
 		"openinpresentation" : 0,
 		"default_fontsize" : 12.0,
@@ -21,12 +21,128 @@
 				"box" : 				{
 					"fontname" : "Arial",
 					"fontsize" : 12.0,
+					"id" : "obj-9",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 395.0, 175.0, 49.0, 18.0 ],
+					"presentation_rect" : [ 393.0, 176.0, 0.0, 0.0 ],
+					"text" : "recall 3"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-10",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 345.0, 175.0, 49.0, 18.0 ],
+					"presentation_rect" : [ 343.0, 176.0, 0.0, 0.0 ],
+					"text" : "recall 2"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-11",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 295.0, 175.0, 49.0, 18.0 ],
+					"presentation_rect" : [ 293.0, 176.0, 0.0, 0.0 ],
+					"text" : "recall 1"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-8",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 395.0, 145.0, 47.0, 18.0 ],
+					"presentation_rect" : [ 393.0, 145.0, 0.0, 0.0 ],
+					"text" : "store 3"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-5",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 345.0, 145.0, 47.0, 18.0 ],
+					"presentation_rect" : [ 347.0, 145.0, 0.0, 0.0 ],
+					"text" : "store 2"
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"args" : [  ],
+					"id" : "obj-7",
+					"maxclass" : "bpatcher",
+					"name" : "jmod.echo~.maxpat",
+					"numinlets" : 3,
+					"numoutlets" : 3,
+					"outlettype" : [ "", "signal", "signal" ],
+					"patching_rect" : [ 420.0, 285.0, 300.0, 70.0 ],
+					"presentation_rect" : [ 0.0, 0.0, 300.0, 70.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"args" : [ "/output~" ],
+					"id" : "obj-6",
+					"maxclass" : "bpatcher",
+					"name" : "jmod.output~.maxpat",
+					"numinlets" : 3,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 90.0, 415.0, 300.0, 140.0 ],
+					"presentation_rect" : [ 0.0, 0.0, 300.0, 140.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"args" : [ "/input~" ],
+					"id" : "obj-4",
+					"maxclass" : "bpatcher",
+					"name" : "jmod.input~.maxpat",
+					"numinlets" : 1,
+					"numoutlets" : 3,
+					"outlettype" : [ "", "signal", "signal" ],
+					"patching_rect" : [ 65.0, 250.0, 300.0, 140.0 ],
+					"presentation_rect" : [ 0.0, 0.0, 300.0, 140.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
 					"id" : "obj-2",
 					"maxclass" : "newobj",
 					"numinlets" : 1,
 					"numoutlets" : 0,
 					"patching_rect" : [ 195.0, 205.0, 88.0, 20.0 ],
-					"presentation_rect" : [ 197.0, 185.0, 0.0, 0.0 ],
 					"text" : "jcom.snapshot"
 				}
 
@@ -45,13 +161,81 @@
 				}
 
 			}
+, 			{
+				"box" : 				{
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"id" : "obj-1",
+					"maxclass" : "message",
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 295.0, 145.0, 47.0, 18.0 ],
+					"text" : "store 1"
+				}
+
+			}
  ],
 		"lines" : [ 			{
 				"patchline" : 				{
 					"destination" : [ "obj-2", 0 ],
 					"hidden" : 0,
 					"midpoints" : [  ],
+					"source" : [ "obj-1", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ],
+					"source" : [ "obj-10", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ],
+					"source" : [ "obj-11", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ],
 					"source" : [ "obj-3", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ],
+					"source" : [ "obj-5", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ],
+					"source" : [ "obj-8", 0 ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"destination" : [ "obj-2", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ],
+					"source" : [ "obj-9", 0 ]
 				}
 
 			}
--- SourceCode/MaxObjects/jcom.snapshot/jcom.snapshot.cpp
+++ SourceCode/MaxObjects/jcom.snapshot/jcom.snapshot.cpp
@@ -1,69 +1,81 @@
-/* 
+/*
  * jcom.snapshot
  * External for Jamoma: capture, recall, transform, and manipulate global snapshots
  * By Timothy Place, Copyright 2009
- * 
+ *
  * License: This code is licensed under the terms of the GNU LGPL
  * http://www.gnu.org/licenses/lgpl.html
  */
 
 #include "Jamoma.h"
 
+class SnapshotParameterValue {
+public:
+    TTFloat64   value;
+    ObjectPtr   parameter;
+
+    SnapshotParameterValue(TTFloat64& f, ObjectPtr o):
+        value(f),
+        parameter(o)
+    {;}
+
+
+};
 
 // A vector of 64-bit floats used to represent a given snapshot
-typedef vector<TTFloat64>				SnapshotVec;
-typedef SnapshotVec::iterator			SnapshotIter;
-typedef SnapshotVec*					SnapshotPtr;
+typedef vector<SnapshotParameterValue>  Snapshot;
+typedef Snapshot::iterator              SnapshotIter;
+typedef Snapshot*                       SnapshotPtr;
 
 // A vector (or collection) of snapshots
-typedef vector<SnapshotPtr>				SnapshotCollectionVec;
-typedef SnapshotCollectionVec::iterator	SnapshotCollectionIter;
-typedef SnapshotCollectionVec*			SnapshotCollectionPtr;
+typedef vector<SnapshotPtr>             SnapshotCollection;
+typedef SnapshotCollection::iterator    SnapshotCollectionIter;
+typedef SnapshotCollection*             SnapshotCollectionPtr;
 
 // Data Structure for this object
 typedef struct {
-	Object					ob;
-	TTTreePtr				tree;
-	SnapshotCollectionPtr	snapshots;
+    Object                  ob;
+    TTTreePtr               tree;
+    SnapshotCollectionPtr   snapshots;
 } TTModSnapshot;
 typedef TTModSnapshot* TTModSnapshotPtr;
 
 
 // Prototypes
-TTPtr		TTModSnapshotNew	(SymbolPtr name, AtomCount argc, AtomPtr argv);
-void		TTModSnapshotFree	(TTModSnapshotPtr self);
-MaxErr		TTModSnapshotNotify	(TTModSnapshotPtr self, SymbolPtr s, SymbolPtr msg, void *sender, void *data);
-void		TTModSnapshotAssist	(TTModSnapshotPtr self, void *b, long m, long a, char *s);
-void		TTModSnapshotDump	(TTModSnapshotPtr self);
-void		TTModSnapshotStore	(TTModSnapshotPtr self, SymbolPtr s, AtomCount argc, AtomPtr argv);
-void		TTModSnapshotRecall	(TTModSnapshotPtr self, SymbolPtr s, AtomCount argc, AtomPtr argv);
+TTPtr       TTModSnapshotNew    (SymbolPtr name, AtomCount argc, AtomPtr argv);
+void        TTModSnapshotFree   (TTModSnapshotPtr self);
+MaxErr      TTModSnapshotNotify (TTModSnapshotPtr self, SymbolPtr s, SymbolPtr msg, void *sender, void *data);
+void        TTModSnapshotAssist (TTModSnapshotPtr self, void *b, long m, long a, char *s);
+void        TTModSnapshotDump   (TTModSnapshotPtr self);
+void        TTModSnapshotStore  (TTModSnapshotPtr self, SymbolPtr s, AtomCount argc, AtomPtr argv);
+void        TTModSnapshotRecall (TTModSnapshotPtr self, SymbolPtr s, AtomCount argc, AtomPtr argv);
 
 
 // Shared
-static ClassPtr	sMaxClass;
+static ClassPtr sMaxClass;
 
 
 // Class Definition
 int JAMOMA_EXPORT_MAXOBJ main(void)
 {
-	ClassPtr c = class_new("jcom.snapshot", 
-						   (method)TTModSnapshotNew, 
-						   (method)TTModSnapshotFree, 
-						    sizeof(TTModSnapshot), 
-						    NULL, A_GIMME, 0);
-	
-	jamoma_init();
-	common_symbols_init();
-		
-	class_addmethod(c, (method)TTModSnapshotNotify,		"notify",		A_CANT, 0);
-	class_addmethod(c, (method)TTModSnapshotAssist,		"assist",		A_CANT, 0);
-	class_addmethod(c, (method)TTModSnapshotDump,		"dump",			0);
-	class_addmethod(c, (method)TTModSnapshotStore,		"store",		A_GIMME, 0);
-	class_addmethod(c, (method)TTModSnapshotRecall,		"recall",		A_GIMME, 0);
-	
-	class_register(_sym_box, c);
-	sMaxClass = c;
-	return 0;
+    ClassPtr c = class_new("jcom.snapshot",
+                           (method)TTModSnapshotNew,
+                           (method)TTModSnapshotFree,
+                            sizeof(TTModSnapshot),
+                            NULL, A_GIMME, 0);
+
+    jamoma_init();
+    common_symbols_init();
+
+    class_addmethod(c, (method)TTModSnapshotNotify,     "notify",       A_CANT, 0);
+    class_addmethod(c, (method)TTModSnapshotAssist,     "assist",       A_CANT, 0);
+    class_addmethod(c, (method)TTModSnapshotDump,       "dump",         0);
+    class_addmethod(c, (method)TTModSnapshotStore,      "store",        A_GIMME, 0);
+    class_addmethod(c, (method)TTModSnapshotRecall,     "recall",       A_GIMME, 0);
+
+    class_register(_sym_box, c);
+    sMaxClass = c;
+    return 0;
 }
 
 
@@ -74,18 +86,18 @@ int JAMOMA_EXPORT_MAXOBJ main(void)
 
 TTPtr TTModSnapshotNew(SymbolPtr name, AtomCount argc, AtomPtr argv)
 {
-	TTModSnapshotPtr self = (TTModSnapshotPtr)object_alloc(sMaxClass);
-	
-	if (self) {
-		self->snapshots = new SnapshotCollectionVec;
-		self->tree = jamoma_tree_init();
-	}
-	return self;
+    TTModSnapshotPtr self = (TTModSnapshotPtr)object_alloc(sMaxClass);
+
+    if (self) {
+        self->snapshots = new SnapshotCollection;
+        self->tree = jamoma_tree_init();
+    }
+    return self;
 }
 
 void TTModSnapshotFree(TTModSnapshotPtr self)
 {
-	delete self->snapshots;
+    delete self->snapshots;
 }
 
 
@@ -96,37 +108,131 @@ void TTModSnapshotFree(TTModSnapshotPtr self)
 
 MaxErr TTModSnapshotNotify(TTModSnapshotPtr self, SymbolPtr s, SymbolPtr msg, TTPtr sender, TTPtr data)
 {
-	object_post(SELF, "notification : %s", msg->s_name);
-	return MAX_ERR_NONE;
+    object_post(SELF, "notification : %s", msg->s_name);
+    return MAX_ERR_NONE;
 }
 
 
 void TTModSnapshotAssist(TTModSnapshotPtr self, void* b, long msg, long arg, char* dst)
 {
-	if (msg == ASSIST_INLET) {	// inlet
-		if (arg == 0)
-			strcpy(dst, "inlet");
-	} 
-	else {						// outlet
-		if (arg == 0)
-			strcpy(dst, "outlet");
-	}		
+    if (msg == ASSIST_INLET) {  // inlet
+        if (arg == 0)
+            strcpy(dst, "inlet");
+    }
+    else {                      // outlet
+        if (arg == 0)
+            strcpy(dst, "outlet");
+    }
 }
 
 
 void TTModSnapshotDump(TTModSnapshotPtr self)
 {
-	jamoma_tree_dump();	// dump all the address of the tree in the Max window
+    jamoma_tree_dump(); // dump all the address of the tree in the Max window
 }
 
 
 void TTModSnapshotStore(TTModSnapshotPtr self, SymbolPtr s, AtomCount argc, AtomPtr argv)
 {
-	;
+    TTNodePtr   rootNode = self->tree->getRoot();
+    TTValue     moduleNodes;
+    TTUInt32    numModules;
+    TTListPtr   returnedChildren = NULL;
+    TTErr       err;
+    SnapshotPtr snapshot = NULL;
+    TTUInt32    snapshotIndex = 0;
+
+    if (argc && argv)
+        snapshotIndex = atom_getlong(argv);
+
+    if (snapshotIndex >= self->snapshots->size()) {
+        if (snapshotIndex >= self->snapshots->capacity()) {
+            self->snapshots->reserve(snapshotIndex+1);
+        }
+		self->snapshots->resize(snapshotIndex+1);
+    }
+    else {
+        snapshot = self->snapshots->at(snapshotIndex);
+        delete snapshot;
+        snapshot = NULL;
+    }
+    snapshot = new Snapshot;
+
+
+    err = rootNode->getChildren(TT("*"), TT("*"), &returnedChildren);
+
+    returnedChildren->assignToValue(moduleNodes);
+    numModules = moduleNodes.getSize();
+    for (TTUInt32 i=0; i<numModules; i++) {
+        TTNodePtr   module = NULL;
+        TTSymbolPtr type;
+
+        moduleNodes.get(i, (TTPtr*)(&module));
+        if (module) {
+            type = module->getType();
+            if (type == TT("hub")) {
+                TTValue     parameterNodes;
+                TTUInt32    numParameters;
+
+                post("  Module: %s", module->getName()->getCString());
+                err = module->getChildren(TT("*"), TT("*"), &returnedChildren);
+                returnedChildren->assignToValue(parameterNodes);
+                numParameters = parameterNodes.getSize();
+                for (TTUInt32 i=0; i<numParameters; i++) {
+                    TTNodePtr   parameter = NULL;
+                    TTSymbolPtr childType;
+
+                    parameterNodes.get(i, (TTPtr*)(&parameter));
+                    if (parameter) {
+                        childType = parameter->getType();
+                        if (childType == TT("subscribe_parameter")) {   // FIXME: this name sucks for the type.
+                            ObjectPtr maxObject = (ObjectPtr)parameter->getObject();
+							SymbolPtr maxType = object_attr_getsym(maxObject, SymbolGen("type"));
+
+                            if (maxType == SymbolGen("decimal") || maxType == SymbolGen("integer")) {
+                                TTFloat64               value = object_attr_getfloat(maxObject, SymbolGen("value"));
+                                SnapshotParameterValue  spv(value, maxObject);
+
+                                snapshot->push_back(spv);
+                                post("    parameter: %s -- value: %lf", parameter->getName()->getCString(), value);
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+//    ((*self->snapshots)[snapshotIndex]).reserve(snapshot.size()+1);
+  (*self->snapshots)[snapshotIndex] = snapshot;
+
+//  ((*self->snapshots)[snapshotIndex]).assign(snapshot.begin(), snapshot.end());
+
+//  ((*self->snapshots)[snapshotIndex]).clear();
+//  ((*self->snapshots)[snapshotIndex]).insert(((*self->snapshots)[snapshotIndex]).begin(), snapshot.begin(), snapshot.end());
+}
+
+
+void TTModSnapshotRecallOne(const SnapshotParameterValue& spv)
+{
+    //object_attr_setfloat(spv.parameter, _sym_value, spv.value);
+	object_method(spv.parameter, _sym_float, spv.value);
 }
 
 void TTModSnapshotRecall(TTModSnapshotPtr self, SymbolPtr s, AtomCount argc, AtomPtr argv)
 {
-	;
+    SnapshotPtr	snapshot;
+    TTUInt32    snapshotIndex = 0;
+
+    if (argc && argv)
+        snapshotIndex = atom_getlong(argv);
+
+    if (snapshotIndex >= self->snapshots->size()) {
+        object_error(SELF, "preset recall out of range");
+        return;
+    }
+
+    snapshot = (*self->snapshots)[snapshotIndex];
+    for_each((*snapshot).begin(), (*snapshot).end(), TTModSnapshotRecallOne);
 }
 
