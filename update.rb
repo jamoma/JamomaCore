cbfe4854cdc8808540c1df1777d59792ee642503
Timothy Place <tim@electrotap.com>
2009-10-29 15:45:22 -0500

    folder re-organization as per discussions on skype today

---
+++ update.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby -wKU
+
+#TODO: checkout the correct branch here, based on an arg!
+puts `git checkout master`
+puts `git pull`
+puts `git submodule update --init`
+
+puts `cd supports; git submodule update --init`
+puts `git submodule foreach git checkout master`
+puts `git submodule foreach git pull`
