bef5307c72ba78d19e0cfdbd39b732994caa5e39
theod <theod@gmea.net>
2009-11-03 12:45:53 +0100

    MultiMapper : result of a pull

---
+++ SourceCode/Framework/source/TTModularVersion.h
@@ -0,0 +1,17 @@
+/* 
+ * Jamoma Modular Shared Library
+ * Functions and resources used by Jamoma Modular objects.
+ * By Tim Place, Copyright © 2009
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+
+#ifndef __TT_MODULAR_VERSION_H__
+#define __TT_MODULAR_VERSION_H__
+
+#define TT_MODULAR_VERSION "0.5.5 a1"
+#define TT_MODULAR_REV "b80d5"
+
+#endif //__TT_MODULAR_VERSION_H__
