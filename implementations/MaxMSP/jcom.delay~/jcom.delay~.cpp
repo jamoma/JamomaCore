acdeffd1c8166321aee78cd9cc87a7889383fe59
Timothy Place <tim@74objects.com>
2009-12-06 16:50:07 -0600

    {new jcom.delay~: a wrapper for the TTDelay class}

---
+++ implementations/MaxMSP/jcom.delay~/jcom.delay~.cpp
@@ -0,0 +1,19 @@
+/* 
+ *	tt.dcblock~
+ *	External object for Max/MSP
+ *	Remove DC Offsets from a signal
+ *	Example project for TTBlue
+ *	Copyright © 2008 by Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "TTClassWrapperMax.h"
+
+int TTCLASSWRAPPERMAX_EXPORT main(void)
+{
+	TTDSPInit();
+	return wrapTTClassAsMaxClass(TT("delay"), "jcom.delay~", NULL);
+}
+
