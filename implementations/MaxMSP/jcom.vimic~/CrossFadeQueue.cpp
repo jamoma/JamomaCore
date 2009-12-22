3d8a1bc82210c75511b890ad509f5b93610a5225
Nilson <peters.nils@gmx.net>
2009-12-22 12:03:34 -0500

    some corrections after reviewing Tim's changes

--- implementations/MaxMSP/jcom.vimic~/CrossFadeQueue.cpp
+++ implementations/MaxMSP/jcom.vimic~/CrossFadeQueue.cpp
@@ -138,24 +138,15 @@ void CrossFadeQueue::computeFadeTbl()
     switch(fadeType_)
     {
         case Properties::COS: //cosine 
-#ifdef WIN_VERSION
-			// WARNING: This might be the wrong substitution, but M_PI_2 is not available on Windows
-			for (i = 0.0, tableIdx = 0; tableIdx < fadeLength_; i += (double) (kTTTwoPi / (fadeLength_ - 1)), tableIdx++)
-#else
-			for (i = 0.0, tableIdx = 0; tableIdx < fadeLength_; i += (double) (M_PI_2 / (fadeLength_ - 1)), tableIdx++)
-#endif
-				fadeTbl_[tableIdx] = (double) cos(i);	// read forward to fade out, backward to fade in
+			
+				for (i = 0.0, tableIdx = 0; tableIdx < fadeLength_; i += (double) (kTTPi_2 / (fadeLength_ - 1)), tableIdx++) //kTTPi_2 was M_PI_2
+				fadeTbl_[tableIdx] = (double) cos(i);  // read forward to fade out, backward to fade in			
             break;		
 
         case Properties::COS_SQUARED: //cosine squared
-#ifdef WIN_VERSION
-			// WARNING: This might be the wrong substitution, but M_PI_2 is not available on Windows
-			for (i = 0.0, tableIdx = 0; tableIdx < fadeLength_; i += (double) (kTTTwoPi / (fadeLength_ - 1)), tableIdx++)
-#else
-			for (i = 0.0, tableIdx = 0; tableIdx < fadeLength_; i += (double) (M_PI_2 / (fadeLength_ - 1)), tableIdx++)
-#endif
+			
+			for (i = 0.0, tableIdx = 0; tableIdx < fadeLength_; i += (double) (kTTPi_2 / (fadeLength_ - 1)), tableIdx++)	//kTTPi_2 was M_PI_2		
                 fadeTbl_[tableIdx] = (double) pow(cos(i),2.0);	// read forward to fade out, backward to fade in
-
             break;
 
         case Properties::LINEAR: //linear
