3f271e7705b6961c3d077cce786696ecd5c6cacc
Timothy Place <tim@74objects.com>
2010-01-18 15:26:07 -0600

    dcblocker≈: ported to multicore v3

--- implementations/MaxMSP/dcblocker/dcblocker.cpp
+++ implementations/MaxMSP/dcblocker/dcblocker.cpp
@@ -1,6 +1,6 @@
 /* 
  *	dcblocker≈
- *	External object for Max/MSP to perform basic mathematical operations on objects in a lydbær dsp chain.
+ *	DC-Blocking Filter object for Jamoma Multicore
  *	Copyright © 2008 by Timothy Place
  * 
  *	License: This code is licensed under the terms of the GNU LGPL
@@ -11,28 +11,28 @@
 
 
 // Data Structure for this object
-typedef struct DCBlockerBaer {
-    t_object			obj;
-	MCoreObjectPtr		lydbaer;
-	TTPtr				lydbaerOutlet;
-	long				attrBypass;
+typedef struct DCBlocker {
+    t_object				obj;
+	TTMulticoreObjectPtr	multicoreObject;
+	TTPtr					multicoreOutlet;
+	long					attrBypass;
 };
-typedef DCBlockerBaer* DCBlockerBaerPtr;
+typedef DCBlocker* DCBlockerPtr;
 
 
 // Prototypes for methods
-DCBlockerBaerPtr	dcBlockerBaerNew(SymbolPtr msg, AtomCount argc, AtomPtr argv);
-void				dcBlockerBaerFree(DCBlockerBaerPtr x);
-void				dcBlockerBaerAssist(DCBlockerBaerPtr x, void* b, long msg, long arg, char* dst);
-void				dcBlockerBaerClear(DCBlockerBaerPtr x);
-TTErr				dcBlockerBaerReset(DCBlockerBaerPtr x, long vectorSize);
-TTErr				dcBlockerBaerSetup(DCBlockerBaerPtr x);
-TTErr				dcBlockerBaerObject(DCBlockerBaerPtr x, MCoreObjectPtr audioSourceObject, long sourceOutletNumber);
-MaxErr				dcBlockerBaerSetBypass(DCBlockerBaerPtr x, void *attr, AtomCount argc, AtomPtr argv);
+DCBlockerPtr	DCBlockerNew(SymbolPtr msg, AtomCount argc, AtomPtr argv);
+void			DCBlockerFree(DCBlockerPtr self);
+void			DCBlockerAssist(DCBlockerPtr self, void* b, long msg, long arg, char* dst);
+void			DCBlockerClear(DCBlockerPtr self);
+TTErr			DCBlockerReset(DCBlockerPtr self);
+TTErr			DCBlockerSetup(DCBlockerPtr self);
+TTErr			DCBlockerConnect(DCBlockerPtr self, TTMulticoreObjectPtr audioSourceObject, long sourceOutletNumber);
+MaxErr			DCBlockerSetBypass(DCBlockerPtr self, void* attr, AtomCount argc, AtomPtr argv);
 
 
 // Globals
-static ClassPtr sDCBlockerBaerClass;
+static ClassPtr sDCBlockerClass;
 
 
 /************************************************************************************/
@@ -45,21 +45,21 @@ int main(void)
 	TTMulticoreInit();	
 	common_symbols_init();
 	
-	c = class_new("dcblocker≈", (method)dcBlockerBaerNew, (method)dcBlockerBaerFree, sizeof(DCBlockerBaer), (method)0L, A_GIMME, 0);
+	c = class_new("dcblocker≈", (method)DCBlockerNew, (method)DCBlockerFree, sizeof(DCBlocker), (method)0L, A_GIMME, 0);
 	
-	class_addmethod(c, (method)dcBlockerBaerClear,		"clear",			0);
-	class_addmethod(c, (method)dcBlockerBaerReset,		"multicore.reset",	A_CANT, 0);
-	class_addmethod(c, (method)dcBlockerBaerSetup,		"multicore.setup",	A_CANT, 0);
-	class_addmethod(c, (method)dcBlockerBaerObject,		"multicore.signal",	A_OBJ, A_LONG, 0);
- 	class_addmethod(c, (method)dcBlockerBaerAssist,		"assist",			A_CANT, 0); 
-    class_addmethod(c, (method)object_obex_dumpout,		"dumpout",			A_CANT, 0);  
+	class_addmethod(c, (method)DCBlockerClear,		"clear",				0);
+	class_addmethod(c, (method)DCBlockerReset,		"multicore.reset",		A_CANT, 0);
+	class_addmethod(c, (method)DCBlockerSetup,		"multicore.setup",		A_CANT, 0);
+	class_addmethod(c, (method)DCBlockerConnect,	"multicore.connect",	A_OBJ, A_LONG, 0);
+ 	class_addmethod(c, (method)DCBlockerAssist,		"assist",				A_CANT, 0); 
+    class_addmethod(c, (method)object_obex_dumpout,	"dumpout",				A_CANT, 0);  
 	
-	CLASS_ATTR_LONG(c,		"bypass",	0,		DCBlockerBaer,	attrBypass);
+	CLASS_ATTR_LONG(c,		"bypass",	0,		DCBlocker,	attrBypass);
 	CLASS_ATTR_STYLE(c,		"bypass",	0,		"onoff");
-	CLASS_ATTR_ACCESSORS(c,	"bypass",	NULL,	dcBlockerBaerSetBypass);
+	CLASS_ATTR_ACCESSORS(c,	"bypass",	NULL,	DCBlockerSetBypass);
 	
 	class_register(_sym_box, c);
-	sDCBlockerBaerClass = c;
+	sDCBlockerClass = c;
 	return 0;
 }
 
@@ -67,39 +67,39 @@ int main(void)
 /************************************************************************************/
 // Object Creation Method
 
-DCBlockerBaerPtr dcBlockerBaerNew(SymbolPtr msg, AtomCount argc, AtomPtr argv)
+DCBlockerPtr DCBlockerNew(SymbolPtr msg, AtomCount argc, AtomPtr argv)
 {
-    DCBlockerBaerPtr	x;
-	TTValue				v;
-	TTErr				err;
+    DCBlockerPtr	self;
+	TTValue			v;
+	TTErr			err;
 	
-    x = (DCBlockerBaerPtr)object_alloc(sDCBlockerBaerClass);
-    if(x){
-    	object_obex_store((void *)x, _sym_dumpout, (ObjectPtr)outlet_new(x, NULL));
-		x->lydbaerOutlet = outlet_new(x, "multicore.signal");
+    self = (DCBlockerPtr)object_alloc(sDCBlockerClass);
+    if (self) {
+    	object_obex_store((void*)self, _sym_dumpout, (ObjectPtr)outlet_new(self, NULL));
+		self->multicoreOutlet = outlet_new(self, "multicore.connect");
 		
 		// TODO: we need to update objects to work with the correct number of channels when the network is configured
 		// Either that, or when we pull we just up the number of channels if when we need to ???
 		v.setSize(2);
 		v.set(0, TT("dcblock"));
-		v.set(1., 1.);
-		err = TTObjectInstantiate(TT("multicore.object"), (TTObjectPtr*)&x->lydbaer, v);
+		v.set(1, 1.);
+		err = TTObjectInstantiate(TT("multicore.object"), (TTObjectPtr*)&self->multicoreObject, v);
 
-		if(!x->lydbaer->audioObject){
-			object_error(ObjectPtr(x), "cannot load TTBlue object");
+		if (!self->multicoreObject->mUnitGenerator) {
+			object_error(SELF, "cannot load TTBlue object");
 			return NULL;
 		}
 		
-		attr_args_process(x, argc, argv);
+		attr_args_process(self, argc, argv);
 	}
-	return x;
+	return self;
 }
 
 // Memory Deallocation
-void dcBlockerBaerFree(DCBlockerBaerPtr x)
+void DCBlockerFree(DCBlockerPtr self)
 {
-	if(x->lydbaer)
-		TTObjectRelease((TTObjectPtr*)&x->lydbaer);
+	if (self->multicoreObject)
+		TTObjectRelease((TTObjectPtr*)&self->multicoreObject);
 }
 
 
@@ -107,12 +107,12 @@ void dcBlockerBaerFree(DCBlockerBaerPtr x)
 // Methods bound to input/inlets
 
 // Method for Assistance Messages
-void dcBlockerBaerAssist(DCBlockerBaerPtr x, void* b, long msg, long arg, char* dst)
+void DCBlockerAssist(DCBlockerPtr self, void* b, long msg, long arg, char* dst)
 {
-	if(msg==1)			// Inlets
+	if (msg==1)			// Inlets
 		strcpy(dst, "multichannel input and control messages");		
-	else if(msg==2){	// Outlets
-		if(arg == 0)
+	else if (msg==2) {	// Outlets
+		if (arg == 0)
 			strcpy(dst, "multichannel output");
 		else
 			strcpy(dst, "dumpout");
@@ -120,44 +120,44 @@ void dcBlockerBaerAssist(DCBlockerBaerPtr x, void* b, long msg, long arg, char* 
 }
 
 
-void dcBlockerBaerClear(DCBlockerBaerPtr x)
+void DCBlockerClear(DCBlockerPtr self)
 {
-	x->lydbaer->audioObject->sendMessage(TT("clear"));
+	self->multicoreObject->mUnitGenerator->sendMessage(TT("clear"));
 }
 
 
-// METHODS SPECIFIC TO LYDBAER EXTERNALS
+// METHODS SPECIFIC TO MULTICORE EXTERNALS
 
-TTErr dcBlockerBaerReset(DCBlockerBaerPtr x, long vectorSize)
+TTErr DCBlockerReset(DCBlockerPtr self)
 {
-	return x->lydbaer->resetSources(vectorSize);
+	return self->multicoreObject->reset();
 }
 
 
-TTErr dcBlockerBaerSetup(DCBlockerBaerPtr x)
+TTErr DCBlockerSetup(DCBlockerPtr self)
 {
 	Atom a[2];
 	
-	atom_setobj(a+0, ObjectPtr(x->lydbaer));
+	atom_setobj(a+0, ObjectPtr(self->multicoreObject));
 	atom_setlong(a+1, 0);
-	outlet_anything(x->lydbaerOutlet, gensym("multicore.signal"), 2, a);
+	outlet_anything(self->multicoreOutlet, gensym("multicore.connect"), 2, a);
 	return kTTErrNone;
 }
 
 
-TTErr dcBlockerBaerObject(DCBlockerBaerPtr x, MCoreObjectPtr audioSourceObject, long sourceOutletNumber)
+TTErr DCBlockerConnect(DCBlockerPtr self, TTMulticoreObjectPtr audioSourceObject, long sourceOutletNumber)
 {
-	return x->lydbaer->addSource(audioSourceObject, sourceOutletNumber);
+	return self->multicoreObject->connect(audioSourceObject, sourceOutletNumber);
 }
 
 
 // ATTRIBUTE SETTERS
 
-MaxErr dcBlockerBaerSetBypass(DCBlockerBaerPtr x, void *attr, AtomCount argc, AtomPtr argv)
+MaxErr DCBlockerSetBypass(DCBlockerPtr self, void* attr, AtomCount argc, AtomPtr argv)
 {
-	if(argc){
-		x->attrBypass = atom_getlong(argv);
-		x->lydbaer->audioObject->setAttributeValue(kTTSym_bypass, (TTBoolean)x->attrBypass);
+	if (argc) {
+		self->attrBypass = atom_getlong(argv);
+		self->multicoreObject->mUnitGenerator->setAttributeValue(kTTSym_bypass, (TTBoolean)self->attrBypass);
 	}
 	return MAX_ERR_NONE;
 }
