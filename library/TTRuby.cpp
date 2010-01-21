b47ac2770c9c3936f598f24f928c0ae6d0fe44f9
Timothy Place <tim@74objects.com>
2010-01-20 19:34:25 -0600

    stubbing in ruby support for multicore

--- library/TTRuby.cpp
+++ library/TTRuby.cpp
@@ -2,7 +2,7 @@
 // Copyright © 2009, Timothy Place
 // GNU LGPL
 
-#include "TTDSP.h"
+#include "TTMulticoreAPI.h"
 #include "ruby.h"
 
 
@@ -25,17 +25,26 @@ typedef VALUE (*TTRubyMethod)(...);
 // Prototypes
 extern "C" {
 	void Init_TTRuby();
+
 	VALUE TTRubyInitialize(VALUE self, VALUE className);
 	VALUE TTRubySendMessage(int argc, VALUE* argv, VALUE self);
 	VALUE TTRubySetAttribute(VALUE self, VALUE attributeName, VALUE attributeValue);
 	VALUE TTRubyGetAttribute(VALUE self, VALUE attributeName);
 	VALUE TTRubyCalculate(VALUE self, VALUE x);
+
+	VALUE TTAudioInitialize(VALUE self, VALUE className);
+	VALUE TTAudioSendMessage(int argc, VALUE* argv, VALUE self);
+	VALUE TTAudioSetAttribute(VALUE self, VALUE attributeName, VALUE attributeValue);
+	VALUE TTAudioGetAttribute(VALUE self, VALUE attributeName);
 }
 
 
 // Globals and Statics
 VALUE		TTRuby_class;
 TTHashPtr	gTTRubyInstances = NULL;
+
+VALUE		TTAudio_class;
+TTHashPtr	gTTAudioInstances = NULL;
 
 
 /*****************************************************************************************************/
@@ -56,8 +65,23 @@ void Init_TTRuby()
 	rb_define_method(c, "calculate",		TTRubyMethod(TTRubyCalculate), 1);
 	
 	TTRuby_class = c;
-	
 	gTTRubyInstances = new TTHash;
+	
+	
+	// MULTICORE / AUDIO
+	
+	TTMulticoreInit();
+
+	c = rb_define_class("TTAudio", rb_cObject);
+	
+	rb_define_method(c, "initialize",		TTRubyMethod(TTAudioInitialize), 1);	// called to initialize a new object that has been created or cloned
+	rb_define_method(c, "send",				TTRubyMethod(TTAudioSendMessage), -1);	// send a message to the wrapped object
+	rb_define_method(c, "set",				TTRubyMethod(TTAudioSetAttribute), 2);	// set attribute value
+	rb_define_method(c, "get",				TTRubyMethod(TTAudioGetAttribute), 1);	// get attribute value
+	//rb_define_method(c, "calculate",		TTRubyMethod(TTRubyCalculate), 1);
+	
+	TTAudio_class = c;
+	gTTAudioInstances = new TTHash;
 }
 
 
@@ -94,6 +118,27 @@ VALUE TTRubyInitialize(VALUE self, VALUE className)
 
 /*****************************************************************************************************/
 // Methods
+
+VALUE TTAudioInitialize(VALUE self, VALUE className)
+{	
+	TTRubyInstance* instance = new TTRubyInstance;
+	TTValue*		v = new TTValue;
+	TTValue			args;
+	TTErr			err = kTTErrNone;
+	VALUE			classNameStr = StringValue(className);
+	
+	args.clear();
+	err = TTObjectInstantiate(TT(RSTRING(classNameStr)->ptr), &instance->obj, args);
+	if (!err) {
+		v->setSize(1);
+		v->set(0, TTPtr(instance));
+		gTTRubyInstances->append(TTSymbolPtr(self), *v);
+		return self;
+	}
+	else
+		return NULL;
+}
+
 
 VALUE TTRubySendMessage(int argc, VALUE* argv, VALUE self)
 {
@@ -292,6 +337,190 @@ VALUE TTRubyCalculate(VALUE self, VALUE x)
 	}
 	return returnValue;
 }
+
+
+
+/**************************************************************************************
+ * MULTICORE SUPPORT
+ **************************************************************************************/
+
+
+VALUE TTAudioSendMessage(int argc, VALUE* argv, VALUE self)
+{
+	TTRubyInstance* instance = NULL;
+	TTErr			err = kTTErrNone;
+	VALUE			messageNameStr;
+	VALUE			messageArgStr;
+	TTValue			v;
+
+	if (argc < 1) {
+		cout << "ERROR -- TTRuby.send requires at least 1 argument (the name of the message to send)" << endl;
+		goto bye;
+	}
+
+	messageNameStr = StringValue(argv[0]);	
+	err = gTTRubyInstances->lookup(TTSymbolPtr(self), v);
+	if (!err) {
+		instance = (TTRubyInstance*)TTPtr(v);
+		if (instance) {
+			if (argc == 1) {		// no arguments...
+				err = instance->obj->sendMessage(TT(RSTRING(messageNameStr)->ptr));
+			}
+			else {					// we have arguments...
+				v.clear();
+
+				for (int i=1; i<argc; i++) {
+					int t = TYPE(argv[i]);
+
+					cout << "the type of the message arg is " << t << endl;
+					switch (t) {
+						case T_FLOAT:
+							v.append(NUM2DBL(argv[i]));
+							break;
+						case T_FIXNUM:
+							v.append((int)FIX2LONG(argv[i]));
+							break;
+						case T_BIGNUM:
+							v.append((TTInt64)NUM2LL(argv[i]));
+							break;
+						case T_STRING:
+							messageArgStr = StringValue(argv[i]);
+							v.append(TT(RSTRING(messageArgStr)->ptr));
+							break;
+						case T_ARRAY:
+							cout << "TTError: Array arguments for messages not yet supported in Ruby" << endl;
+							err = kTTErrGeneric;
+							break;
+						case T_OBJECT:
+							cout << "TTError: Object arguments for messages not yet supported in Ruby" << endl;
+							err = kTTErrGeneric;
+							break;
+						default:
+							// assume no arguments for now...
+							break;
+					}
+				}				
+
+				if (!err)
+					err = instance->obj->sendMessage(TT(RSTRING(messageNameStr)->ptr), v);				
+			}
+
+			if (err)
+				cout << "TTRubySendMessage: Error " << err << endl;
+		}
+	}
+bye:
+	;
+}
+
+
+VALUE TTAudioSetAttribute(VALUE self, VALUE attributeName, VALUE attributeValue)
+{
+	TTRubyInstance* instance = NULL;
+	TTErr			err = kTTErrNone;
+	VALUE			attributeNameStr = StringValue(attributeName);
+	VALUE			attributeValueStr;
+	TTValue			v;
+
+	err = gTTRubyInstances->lookup(TTSymbolPtr(self), v);
+	if (!err) {
+		instance = (TTRubyInstance*)TTPtr(v);
+		if (instance) {
+			int t = TYPE(attributeValue);
+
+			v.clear();
+			//cout << "the type of the attr arg is " << t << endl;
+			switch (t) {
+				case T_FLOAT:
+					v.append(NUM2DBL(attributeValue));
+					break;
+				case T_FIXNUM:
+					v.append((int)FIX2LONG(attributeValue));
+					break;
+				case T_BIGNUM:
+					v.append((TTInt64)NUM2LL(attributeValue));
+					break;
+				case T_STRING:
+					attributeValueStr = StringValue(attributeValue);
+					v.append(TT(RSTRING(attributeValueStr)->ptr));
+					break;
+				case T_ARRAY:
+					cout << "TTError: Array arguments for attributes not yet supported in Ruby" << endl;
+					err = kTTErrGeneric;
+					break;
+				case T_OBJECT:
+					cout << "TTError: Object arguments for attributes not yet supported in Ruby" << endl;
+					err = kTTErrGeneric;
+					break;
+				default:
+					cout << "TTError: Unknown type for arguments to attribute" << endl;
+					err = kTTErrGeneric;
+					break;
+			}
+			if (!err)
+				err = instance->obj->setAttributeValue(TT(RSTRING(attributeNameStr)->ptr), v);
+			if (err)
+				cout << "TTRubySetAttribute: Error " << err << endl;
+		}
+	}
+}
+
+
+VALUE TTAudioGetAttribute(VALUE self, VALUE attributeName)
+{
+	TTRubyInstance* instance = NULL;
+	TTErr			err = kTTErrNone;
+	VALUE			attributeNameStr = StringValue(attributeName);
+	VALUE			attributeValueStr;
+	TTValue			v;
+	VALUE			returnValue = rb_float_new(0.0);
+	TTSymbolPtr		s;
+	TTCString		c;
+
+	err = gTTRubyInstances->lookup(TTSymbolPtr(self), v);
+	if (!err) {
+		instance = (TTRubyInstance*)TTPtr(v);
+		if (instance) {
+			err = instance->obj->getAttributeValue(TT(RSTRING(attributeNameStr)->ptr), v);
+			if (err) {
+				cout << "TTRubyGetAttribute: Error " << err << endl;
+				goto out;
+			}
+
+			// TODO: not handling array attrs yet...
+
+			switch (v.getType(0)) {
+				case kTypeFloat64:
+				case kTypeFloat32:
+					returnValue = rb_float_new(v);
+					break;
+				case kTypeInt8:
+				case kTypeUInt8:
+				case kTypeInt16:
+				case kTypeUInt16:
+				case kTypeInt32:
+				case kTypeUInt32:
+					returnValue = INT2NUM(v);
+					break;
+				case kTypeInt64:
+				case kTypeUInt64:
+					returnValue = LL2NUM((TTInt64)v);
+					break;
+				case kTypeBoolean:\
+					returnValue = TTBoolean(v);
+					break;
+				case kTypeSymbol:
+					s = v;
+					c = (TTCString)s->getCString();
+					returnValue = rb_str_new(c, strlen(c));
+					break;
+			}
+		}
+	}
+out:
+	return returnValue;
+}
+
 
 
 
--- library/extconf.rb
+++ library/extconf.rb
@@ -9,18 +9,21 @@ Dir.chdir "../../Foundation/library/includes"
 inc = Dir.pwd
 Dir.chdir "#{inc}/../../../DSP/library/includes"
 dsp = Dir.pwd
+Dir.chdir "#{inc}/../../../Multicore/library/includes"
+multicore = Dir.pwd
 Dir.chdir wd
 
 puts "config: #{inc}"
 puts "dsp: #{dsp}"
+puts "multicore: #{multicore}"
 #dir_config("#{inc}")
 #find_header('TTFoundationAPI.h', "#{inc}")
 #find_header('TTFoundationAPI.h', "../../library/includes")
 have_func("Init_TTRuby", "TTRuby.cpp")
 
-$CFLAGS = $CFLAGS + "-I #{inc} -I #{dsp}"
-$CPPFLAGS = $CPPFLAGS + "-I #{inc} -I #{dsp}"
-$LDFLAGS = $LDFLAGS + " -framework JamomaFoundation"
+$CFLAGS = $CFLAGS + "-I #{inc} -I #{dsp} -I #{multicore}"
+$CPPFLAGS = $CPPFLAGS + "-I #{inc} -I #{dsp} -I #{multicore}"
+$LDFLAGS = $LDFLAGS + " -framework JamomaFoundation -framework JamomaDSP -framework JamomaMulticore"
 #$ARCHFLAGS = $ARCHFLAGS + "-arch i386"
 
 create_makefile("TTRuby")
