9b91849257e6921fc76b96f6cb75f10ae2a8ac6c
Timothy Place <tim@74objects.com>
2010-01-24 11:08:29 -0600

    Foundation Library: can now be compiled as static lib for the iPhone.

--- library/includes/TTElement.h
+++ library/includes/TTElement.h
@@ -12,11 +12,13 @@
 // Platform Sniffing
 // Ideally the platform would already be set with a -D option to gcc...
 #ifndef TT_PLATFORM_LINUX
-	#ifndef TT_PLATFORM_WIN
-		#ifdef WIN_VERSION
-			#define TT_PLATFORM_WIN
-		#else
-			#define TT_PLATFORM_MAC
+	#ifndef TT_PLATFORM_IPHONE
+		#ifndef TT_PLATFORM_WIN
+			#ifdef WIN_VERSION
+				#define TT_PLATFORM_WIN
+			#else
+				#define TT_PLATFORM_MAC
+			#endif
 		#endif
 	#endif
 #endif
