8cd319f96c47919de22f4d2e6ca064ce9edede88
Nilson <peters.nils@gmx.net>
2010-02-03 16:39:38 -0500

    some window function optimization

--- extensions/WindowFunctionLib/HammingWindow.cpp
+++ extensions/WindowFunctionLib/HammingWindow.cpp
@@ -29,7 +29,7 @@ HammingWindow::~HammingWindow()
 // hanning(x) = 0.5 + 0.5*cos(2*PI*(x-0.5))
 TTErr HammingWindow::calculateValue(const TTFloat64& x, TTFloat64& y, TTPtrSizedInt data)
 {
-	y = 0.54 + 0.46*cos(2.0*kTTPi*(x-0.5));
+	y = 0.54 + 0.46*cos(kTTwoTPi*(x-0.5));
 	return kTTErrNone;
 }
 
