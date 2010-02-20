69995ec4b878d8c19cbe1b7b3c594a3b1cca3448
Timothy Place <tim@74objects.com>
2010-02-19 18:20:46 -0600

    fxlib≈ is no more -- replaced by individual projects.  The theory behind the fxlib≈ was that it would save us maintenance work.  Instead it just ended up being a pain in the you-know-what.

---
+++ implementations/MaxMSP/limiter/jcom.limiter.cpp
@@ -0,0 +1,17 @@
+/* 
+ *	limiter≈
+ *	Jamoma Multicore external object for Max
+ *	Copyright © 2008 by Timothy Place
+ * 
+ *	License: This code is licensed under the terms of the GNU LGPL
+ *	http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "maxMulticore.h"
+
+int main(void)
+{
+	TTMulticoreInit();
+	return wrapAsMaxMulticore(TT("limiter"), "jcom.limiter≈", NULL);
+}
+
