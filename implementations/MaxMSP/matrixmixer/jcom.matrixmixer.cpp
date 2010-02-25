a4fda9a74c92aa87d5000f0d5156544a58b1d537
Timothy Place <tim@74objects.com>
2010-02-25 16:04:52 -0600

    renaming jcom.matrix≈ to jcom.matrixmixer≈

---
+++ implementations/MaxMSP/matrixmixer/jcom.matrixmixer.cpp
@@ -0,0 +1,23 @@
+/* 
+ *	matrixmixer≈
+ *	External object for Jamoma Multicore
+ *	Copyright © 2009 by Timothy Place
+ * 
+ *	License: This code is licensed under the terms of the GNU LGPL
+ *	http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "maxMulticore.h"
+
+
+int main(void)
+{
+	WrappedClassOptionsPtr	options = new WrappedClassOptions;
+	TTValue					value(0);
+
+	TTMulticoreInit();
+	options->append(TT("argumentDefinesNumInlets"), value);
+	value = 1L;
+	options->append(TT("argumentDefinesNumOutlets"), value);
+	return wrapAsMaxMulticore(TT("matrixmixer"), "jcom.matrixmixer≈", NULL, options);
+}
