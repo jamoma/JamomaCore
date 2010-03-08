c80f8535701e0d4998c2b2d1469750624f61fc43
Timothy Place <tim@74objects.com>
2010-03-07 18:26:40 -0600

    oops: missing file for the MoR example

---
+++ examples/MulticoreOnRails/config/initializers/jamoma_multicore.rb
@@ -0,0 +1,18 @@
+require 'TTRuby'
+
+#def CreateMulticoreGraph
+    # build the multicore graph here
+     # TODO: need to clear all of this up when we are done!
+  # def initialize
+
+   	  puts "creating objects"
+       $tt_dac = TTAudio.new "multicore.output"
+       $tt_gain = TTAudio.new "gain"
+       $tt_op = TTAudio.new "operator"
+       $tt_oscil = TTAudio.new "wavetable"
+       $tt_op.connect_audio $tt_oscil
+       $tt_gain.connect_audio $tt_op
+       $tt_dac.connect_audio $tt_gain
+  # end
+#end
+
