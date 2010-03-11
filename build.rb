77e0cbecb63cd0095ab02b6ae139722cd9f0c1d0
Timothy Place <tim@74objects.com>
2010-03-10 18:38:41 -0600

    build.rb: attempt at installing the compiled the Ruby extension when we build it

--- build.rb
+++ build.rb
@@ -3,7 +3,12 @@
 puts "Building Ruby Language Extensions..."
 
 `cd library; ruby extconf.rb; make clean; make`
-# TODO: need to do "sudo make install" to actually use it -- not sure how to do that in the script
+
+# TODO: what if the person is using Ruby 1.9? Will this still work?
+
+puts `rm -rf /Library/Ruby/Site/1.8/universal-darwin10.0/TTRuby.bundle`
+puts `cp library/TTRuby.bundle /Library/Ruby/Site/1.8/universal-darwin10.0/TTRuby.bundle`
+
 
 puts
 puts "=================DONE===================="
