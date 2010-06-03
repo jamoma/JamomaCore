c575bd590bdda9534f20a7bfb6935e8ee154152a
Timothy Place <tim@74objects.com>
2010-06-03 11:33:08 -0400

    {TTOnePole: new class that implements a one-pole filter with a mode attribute to switch between highpass and lowpass frequency responses.  The older TTLowpassOnepole is now implemented as a subclass of the new TTOnePole}.

--- extensions/FilterLib/TTLowpassOnePole.cpp
+++ extensions/FilterLib/TTLowpassOnePole.cpp
@@ -13,77 +13,26 @@
 #define thisTTClassTags		"audio, processor, filter, lowpass"
 
 
-TT_AUDIO_CONSTRUCTOR
+extern "C" void TTLowpassOnePole::registerClass()
 {
-	// register attributes
-	addAttributeWithSetter(Frequency,	kTypeFloat64);
-	addAttributeProperty(Frequency,		range,			TTValue(2.0, sr*0.475));
-	addAttributeProperty(Frequency,		rangeChecking,	TT("clip"));
+	TTClassRegister(TT(thisTTClassName), thisTTClassTags, TTLowpassOnePole::instantiate);
+}
 
-	// register for notifications from the parent class so we can allocate memory as required
-	addMessageWithArgument(updateMaxNumChannels);
-	// register for notifications from the parent class so we can recalculate coefficients as required
-	addMessage(updateSr);
-	// make the clear method available to the outside world
-	addMessage(Clear);
 
-	// Set Defaults...
-	setAttributeValue(TT("MaxNumChannels"),	arguments);			// This attribute is inherited
-	setAttributeValue(TT("Frequency"),		1000.0);
-	setProcessMethod(processAudio);
-	setCalculateMethod(calculateValue);
+TTObjectPtr TTLowpassOnePole::instantiate(TTSymbolPtr name, TTValue& arguments)
+{
+	return new TTLowpassOnePole(arguments);
+}
 
+
+TTLowpassOnePole::TTLowpassOnePole(TTValue& arguments):
+	TTOnePole(arguments)
+{
+	;
 }
 
 
 TTLowpassOnePole::~TTLowpassOnePole()
 {
-	; // Nothing special to do for this class
-}
-
-
-TTErr TTLowpassOnePole::updateMaxNumChannels(const TTValue& oldMaxNumChannels)
-{
-	mFeedback.resize(maxNumChannels);
-	Clear();
-	return kTTErrNone;
-}
-
-
-TTErr TTLowpassOnePole::updateSr()
-{
-	TTValue	v(mFrequency);
-	return setFrequency(v);
-}
-
-
-TTErr TTLowpassOnePole::Clear()
-{
-	mFeedback.assign(maxNumChannels, 0.0);
-	return kTTErrNone;
-}
-
-
-TTErr TTLowpassOnePole::setFrequency(const TTValue& newValue)
-{
-	TTFloat64	radians;
-	
-	mFrequency = newValue;
-	radians = hertzToRadians(mFrequency);
-	mCoefficient = TTClip(radians / kTTPi, 0.0, 1.0);
-	mOneMinusCoefficient = 1.0 - mCoefficient;
-	return kTTErrNone;
-}
-
-
-inline TTErr TTLowpassOnePole::calculateValue(const TTFloat64& x, TTFloat64& y, TTPtrSizedInt channel)
-{
-	y = mFeedback[channel] = TTAntiDenormal((x * mCoefficient) + (mFeedback[channel] * mOneMinusCoefficient));
-	return kTTErrNone;
-}
-
-
-TTErr TTLowpassOnePole::processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs)
-{
-	TT_WRAP_CALCULATE_METHOD(calculateValue);
+	;
 }
--- extensions/FilterLib/TTLowpassOnePole.h
+++ extensions/FilterLib/TTLowpassOnePole.h
@@ -9,41 +9,12 @@
 #ifndef __TT_LOWPASS_ONEPOLE_H__
 #define __TT_LOWPASS_ONEPOLE_H__
 
-#include "TTDSP.h"
+#include "TTOnePole.h"
 
 
 /**	The simplest of lowpass filters: a single-pole, no-zero algorithm. */
-class TTLowpassOnePole : public TTAudioObject {
+class TTLowpassOnePole : public TTOnePole {
 	TTCLASS_SETUP(TTLowpassOnePole)
-
-	TTFloat64			mFrequency;					///< filter cutoff frequency
-	TTFloat64			mCoefficient;				///< filter coefficients
-	TTFloat64			mOneMinusCoefficient;		///< filter coefficients
-	TTSampleVector		mFeedback;					///< previous output sample for each channel
-
-	/**	Receives notifications when there are changes to the inherited 
-		maxNumChannels attribute.			*/
-	TTErr updateMaxNumChannels(const TTValue& oldMaxNumChannels);
-
-	/** Receives notifications when there are changes to the inherited 
-		sr attribute.						*/
-	TTErr updateSr();
-	
-	/**	This algorithm uses an IIR filter, meaning that it relies on feedback.  If the filter should
-	 *	not be producing any signal (such as turning audio off and then back on in a host) or if the
-	 *	feedback has become corrupted (such as might happen if a NaN is fed in) then it may be 
-	 *	neccesary to clear the filter by calling this method.
-	 *	@return Returns a TTErr error code.												*/
-	TTErr Clear();	
-
-	/**	Setter for the frequency attribute. */
-	TTErr setFrequency(const TTValue& value);
-	
-	/**	Standard single value calculate method as used by DSP objects. */
-	inline TTErr calculateValue(const TTFloat64& x, TTFloat64& y, TTPtrSizedInt channel);
-
-	/**	Standard audio processing method as used by DSP objects. */
-	TTErr processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs);
 };
 
 
