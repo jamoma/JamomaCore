46ee9622cd33a10314ca0453a925fb1141ad5ae3
Timothy Place <tim@74objects.com>
2010-06-11 18:26:44 -0500

    {jcom.clip~: new extern to verify the proper function of the 'clipper' extension class.}

---
+++ implementations/MaxMSP/jcom.clip~/jcom.clip~.cpp
@@ -0,0 +1,15 @@
+/* 
+ *	External object for Max/MSP
+ *	Copyright © 2010 by Timothy Place
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "TTClassWrapperMax.h"
+
+int TTCLASSWRAPPERMAX_EXPORT main(void)
+{
+	TTDSPInit();
+	return wrapTTClassAsMaxClass(TT("clipper"), "jcom.clip~", NULL);
+}
