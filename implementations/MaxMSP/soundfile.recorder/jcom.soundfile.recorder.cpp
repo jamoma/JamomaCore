5f76815d6298eff5b41a2ec6d5f8b50d02f28034
Timothy Place <tim@74objects.com>
2010-06-23 16:36:17 -0500

    jcom.soundfile.recorder≈: records sound files -- see help patcher for usage and example of recording a FLAC file.

---
+++ implementations/MaxMSP/soundfile.recorder/jcom.soundfile.recorder.cpp
@@ -0,0 +1,17 @@
+/* 
+ *	soundfile.recorder≈
+ *	Jamoma AudioGraph external object for Max
+ *	Copyright © 2010 by Timothy Place
+ * 
+ *	License: This code is licensed under the terms of the GNU LGPL
+ *	http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#include "maxAudioGraph.h"
+
+int main(void)
+{
+	TTAudioGraphInit();
+	return wrapAsMaxAudioGraph(TT("soundfile.recorder"), "jcom.soundfile.recorder≈", NULL);
+}
+
