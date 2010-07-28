39fad346d2519fc715ebad600e09b66319732755
@timothyplace <tim@74objects.com>
2010-07-28 16:22:07 -0500

    license LGPL->BSD

--- implementations/MaxMSP/dcblocker/jcom.dcblocker.cpp
+++ implementations/MaxMSP/dcblocker/jcom.dcblocker.cpp
@@ -3,8 +3,8 @@
  *	DC-Blocking Filter object for Jamoma AudioGraph
  *	Copyright © 2008 by Timothy Place
  * 
- *	License: This code is licensed under the terms of the GNU LGPL
- *	http://www.gnu.org/licenses/lgpl.html 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
  */
 
 #include "maxAudioGraph.h"
--- implementations/PureData/op/op.cpp
+++ implementations/PureData/op/op.cpp
@@ -3,8 +3,8 @@
  *	External object for Pd to perform basic mathematical operations on objects in a Jamoma AudioGraph dsp chain.
  *	Copyright © 2010 by Timothy Place
  * 
- *	License: This code is licensed under the terms of the GNU LGPL
- *	http://www.gnu.org/licenses/lgpl.html 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
  */
 
 #include "PureAudioGraph.h"
