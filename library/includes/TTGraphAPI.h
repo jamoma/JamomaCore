86a6118054431b97429a5dc25fbbb62aa7d29438
@timothyplace <tim@74objects.com>
2010-07-28 16:34:02 -0500

    license LGPL->BSD

--- library/includes/TTGraphAPI.h
+++ library/includes/TTGraphAPI.h
@@ -3,8 +3,8 @@
  * Creates a wrapper for TTObjects that can be used to build a control graph for asynchronous message passing.
  * Copyright © 2010, Timothy Place
  * 
- * License: This code is licensed under the terms of the GNU LGPL
- * http://www.gnu.org/licenses/lgpl.html 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
  */
 
 #ifndef __TTGRAPH_API_H__
