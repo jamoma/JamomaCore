f41a72363e815dec999df78b50653abbc28cdb48
Theo de la Hogue <theod@gmea.net>
2010-08-04 04:06:52 +0200

    { Adding baton destruction and a comment about that }

--- library/source/TTCallback.cpp
+++ library/source/TTCallback.cpp
@@ -29,6 +29,8 @@ TT_OBJECT_CONSTRUCTOR,
 TTCallback::~TTCallback()
 {
 	mFunction = NULL;
+	
+	delete mBaton;		// to -- there is a warning here... However mBaton is often a TTValue we have to delete... 
 	mBaton = NULL;
 }
 
