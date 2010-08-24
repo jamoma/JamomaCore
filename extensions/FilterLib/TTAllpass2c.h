28956c10a8fcd43888cabdf9ab5069e90a7ad779
@timothyplace <tim@74objects.com>
2010-08-24 08:42:26 -0500

    TTAllpass2c: adding difference and transfer function equations into the header to help clarify this filter's operation because the sign-flipping can be very confusing.

--- extensions/FilterLib/TTAllpass2c.h
+++ extensions/FilterLib/TTAllpass2c.h
@@ -15,6 +15,14 @@
 /**	A second-order building-block allpass filter.
 	Essentially the same as TTAllpass2a, but with some signs flipped.
 	Based on Multirate Signal Processing for Communication Systems, Chapter 10, Figure 43. 
+ 
+	difference equation:
+		y(n) = -e_2 x(n) - e_1 x(n-1) - x(n-2  -  e_1 y(n-1) - e_2 y(n-2)
+
+	transfer function:
+				-e_2  -  e_1 Z^(-1)  -  Z^(-2)
+		H(Z) = ---------------------------------
+				1  +  e_1 Z^(-1)  +  e_2 Z^(-2)
  */
 class TTAllpass2c : public TTAudioObject {
 	TTCLASS_SETUP(TTAllpass2c)
