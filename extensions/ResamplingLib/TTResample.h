7c33f1698cb2af0559f160aa67d6083cf881e2e5
@timothyplace <tim@74objects.com>
2010-08-26 14:45:50 -0500

    merging a local branch

---
+++ extensions/ResamplingLib/TTResample.h
@@ -0,0 +1,35 @@
+/* 
+ * Resample
+ * Extension Class for Jamoma DSP
+ * Copyright © 2010, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
+ */
+
+#ifndef __TT_RESAMPLE_H__
+#define __TT_RESAMPLE_H__
+
+#include "TTDSP.h"
+
+
+/**	Convert input to an output with different block size, sample rate, or other characteristics. */
+class TTResample : TTAudioObject {
+	TTCLASS_SETUP(TTResample)
+	
+	TTAudioObjectPtr	mResamplingFilter;
+	TTSymbolPtr			mDirection;	// up or down
+	TTSymbolPtr			mMode;		// staircase, third, fifth
+	
+	
+	TTErr setDirection(const TTValue& newDirection);
+	TTErr setMode(const TTValue& newMode);
+	
+	TTErr updateMaxNumChannels(const TTValue& oldMaxNumChannels);
+	
+	TTErr processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs);
+	
+};
+
+
+#endif // __TT_RESAMPLE_H__
---
+++ extensions/ResamplingLib/TTStaircase.cpp
@@ -0,0 +1,97 @@
+/* 
+ * Resample
+ * Extension Class for Jamoma DSP
+ * Copyright © 2010, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
+ */
+
+#include "TTStaircase.h"
+
+#define thisTTClass			TTStaircase
+#define thisTTClassName		"staircase"
+#define thisTTClassTags		"audio, processor, resample"
+
+
+TT_AUDIO_CONSTRUCTOR
+{
+	addAttributeWithSetter(Mode, kTypeSymbol);
+	setAttributeValue(TT("Mode"), TT("down"));					  
+}
+
+
+TTStaircase::~TTStaircase()
+{
+	;
+}
+
+
+TTErr TTStaircase::setMode(const TTValue& newMode)
+{
+	mMode = newMode;
+	if (mMode == TT("upsample"))
+		setProcessMethod(processUpsample);
+	else // mMode == TT("downsample")
+		setProcessMethod(processDownsample);
+	return kTTErrNone;
+}
+
+
+TTErr TTStaircase::processDownsample(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs)
+{
+	TTAudioSignal&	in = inputs->getSignal(0);
+	TTAudioSignal&	out = outputs->getSignal(0);
+	TTSampleValue*	inSample;
+	TTSampleValue*	outSample;
+	TTUInt16		numchannels = TTAudioSignal::getMinChannelCount(in, out);
+	TTPtrSizedInt	channel;
+	TTUInt16		targetVectorSize = in.getVectorSizeAsInt() / 2;
+	TTErr			err;
+	
+	err = out.changeVectorSize(targetVectorSize);
+	for (channel=0; channel<numchannels; channel++) {
+		TTUInt16 n = targetVectorSize;
+		
+		inSample = in.mSampleVectors[channel];
+		outSample = out.mSampleVectors[channel];
+		
+		while (n--) {
+			*outSample++ = * inSample++;
+			inSample++;
+		}
+	}
+	return kTTErrNone;
+}
+
+
+TTErr TTStaircase::processUpsample(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs)
+{
+	TTAudioSignal&	in = inputs->getSignal(0);
+	TTAudioSignal&	out = outputs->getSignal(0);
+	TTSampleValue*	inSample;
+	TTSampleValue*	outSample;
+	TTUInt16		numchannels = TTAudioSignal::getMinChannelCount(in, out);
+	TTPtrSizedInt	channel;
+	TTUInt16		targetVectorSize = in.getVectorSizeAsInt() * 2;
+	TTErr			err;
+	
+	err = out.changeVectorSize(targetVectorSize);
+	if (!err) {
+		for (channel=0; channel<numchannels; channel++) {
+			TTUInt16		n = in.getVectorSizeAsInt();
+			TTSampleValue	x;
+			
+			inSample = in.mSampleVectors[channel];
+			outSample = out.mSampleVectors[channel];
+			
+			while (n--) {
+				x = *inSample++;
+				*outSample++ = x;
+				*outSample++ = x;
+			}
+		}
+	}
+	return kTTErrNone;
+}
+
