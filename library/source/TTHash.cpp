d684f28ae5d822ca854c53cefdb78a5412e09ed2
Theo de la Hogue <theod@gmea.net>
2010-10-28 00:47:19 +0200

    { Adding a none shared thread protecion (disabled by default) copying the TTList mecanism }

--- library/source/TTHash.cpp
+++ library/source/TTHash.cpp
@@ -15,7 +15,10 @@ static TTMutex* sHashMutex=NULL;
 /****************************************************************************************************/
 
 TTHash::TTHash()
+			:mThreadProtection(NO)
 {
+	mMutex = new TTMutex(false);
+	
 	if (!sHashMutex)
 		sHashMutex = new TTMutex(false);
 }
@@ -29,19 +32,25 @@ TTHash::~TTHash()
 
 TTErr TTHash::append(const TTSymbolPtr key, const TTValue& value)
 {
+	lock();
 	hashMap.insert(TTKeyVal(TTPtrSizedInt(key), value));
+	unlock();
 	return kTTErrNone;
 }
 
 
 TTErr TTHash::lookup(const TTSymbolPtr key, TTValue& value)
 {
+	lock();
 	TTHashMapIter iter = hashMap.find(TTPtrSizedInt(key));
 
-	if (iter == hashMap.end())
+	if (iter == hashMap.end()) {
+		unlock();
 		return kTTErrValueNotFound;
+	}
 	else {
 		value = iter->second;
+		unlock();
 		return kTTErrNone;
 	}
 }
@@ -49,24 +58,29 @@ TTErr TTHash::lookup(const TTSymbolPtr key, TTValue& value)
 
 TTErr TTHash::remove(const TTSymbolPtr key)
 {
+	lock();
 	hashMap.erase(TTPtrSizedInt(key));
+	unlock();
 	return kTTErrNone;
 }
 
 
 TTErr TTHash::clear()
 {
+	lock();
 	hashMap.clear();
+	unlock();
 	return kTTErrNone;
 }
 
 
 TTErr TTHash::getKeys(TTValue& hashKeys)
 {
-// TODO: we need to have some thread protection for TTHash!
+	lock();
 	hashKeys.clear();
 	for (TTHashMapIter iter = hashMap.begin(); iter != hashMap.end(); iter++)	
 		hashKeys.append(TTSymbolPtr(iter->first));
+	unlock();
 	return kTTErrNone;
 }
 
@@ -82,3 +96,16 @@ TTBoolean TTHash::isEmpty()
 	return hashMap.empty();
 }
 
+
+void TTHash::lock()
+{
+	if (mThreadProtection)
+		mMutex->lock();
+}
+
+
+void TTHash::unlock()
+{
+	if (mThreadProtection)
+		mMutex->unlock();
+}
