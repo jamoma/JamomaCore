ad3db38e5e94f3cec6003616fd350a17957f4266
Theo de la Hogue <theod@gmea.net>
2010-12-06 11:45:30 +0100

    Changing attribute comments

--- library/PeerObject/TTData.h
+++ library/PeerObject/TTData.h
@@ -48,13 +48,13 @@ class TTMODULAR_EXPORT TTData : public TTObject
 	
 private:
 	
-	TTValue			mValue;						///< ATTRIBUTE: The data's value
-	TTValue			mValueDefault;				///< ATTRIBUTE: The data's default value
+	TTValue			mValue;						///< ATTRIBUTE: data's value
+	TTValue			mValueDefault;				///< ATTRIBUTE: data's default value
 	TTFloat32		mValueStepsize;				///< ATTRIBUTE: amount to increment or decrement by
 	
 	TTSymbolPtr		mType;						///< ATTRIBUTE: type of this data's value
 	TTUInt8			mPriority;					///< ATTRIBUTE: does this data have a priority over other datas ?
-	TTSymbolPtr		mDescription;				///< ATTRIBUTE: a text label to describe the role of this data
+	TTSymbolPtr		mDescription;				///< ATTRIBUTE: text to describe the role of this data
 	TTBoolean		mRepetitionsAllow;			///< ATTRIBUTE: is the same value can be update twice ?
 	TTBoolean		mReadonly;					///< ATTRIBUTE: 
 	TTBoolean		mInitialized;				///< ATTRIBUTE: is the Value attribute has been initialized ?
