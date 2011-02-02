4ce33213b87e65f425b2e26a56be834ac221351b
+1m place <tim@74objects.com>
2011-02-02 17:19:56 +0100

    SpatLib: initial

---
+++ extensions/SpatLib/SpatLib.h
@@ -0,0 +1,12 @@
+/* 
+ * SpatLib
+ * Extension Class for Jamoma DSP
+ * Copyright © 2011 by Trond Lossius, Nils Peters, and Timothy Place
+ * 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
+ */
+
+#include "TTDSP.h"
+
+
