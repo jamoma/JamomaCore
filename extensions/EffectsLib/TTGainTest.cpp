1a568e3e5e1cf9e6418bdd92def3aca25563738b
+1m place <tim@74objects.com>
2011-02-06 12:04:35 +0100

    TTGain: common unit test code moved into the Foundation's new TTUnitTest functions.

--- extensions/EffectsLib/TTGainTest.cpp
+++ extensions/EffectsLib/TTGainTest.cpp
@@ -1,6 +1,6 @@
 /* 
- * TTBlue Gain Control Object
- * Copyright © 2008, Timothy Place
+ * Unit tests for the Jamoma DSP gain object
+ * Copyright © 2011, Timothy Place and Trond Lossius
  * 
  * License: This code is licensed under the terms of the "New BSD License"
  * http://creativecommons.org/licenses/BSD/
@@ -9,50 +9,6 @@
 #include "TTGain.h"
 
 
-void TTTestLog(const char *msg)
-{
-	TTLogMessage("	");
-	TTLogMessage(msg);
-	TTLogMessage("\n");
-}
-
-
-void TTTestAssertion(const char* aTestName, TTBoolean aTestResult, int& testAssertionCount, int& errorCount)
-{
-	testAssertionCount++;
-	
-	if (aTestResult)
-		TTLogMessage("	PASS -- ");
-	else {
-		TTLogMessage("	FAIL -- ");
-		errorCount++;
-	}	
-	TTLogMessage(aTestName);
-	TTLogMessage("\n");	
-}
-
-
-static const TTFloat32 kTTTestFloat32Epsilon = 0.00001;
-
-TTBoolean TTTestFloatEquivalence(TTFloat32 a, TTFloat32 b)
-{
-	if (fabs(a-b) < fabs(a*kTTTestFloat32Epsilon))
-		return true;
-	else
-		return false;
-}
-
-
-static const TTFloat64 kTTTestFloat64Epsilon = 0.000000001;
-
-TTBoolean TTTestFloatEquivalence(TTFloat64 a, TTFloat64 b)
-{
-	if (fabs(a-b) < fabs(a*kTTTestFloat64Epsilon))
-		return true;
-	else
-		return false;
-}
-
 TTErr TTGain::test(TTValue& returnedTestInfo)
 {
 	// preliminary setup
@@ -60,7 +16,7 @@ TTErr TTGain::test(TTValue& returnedTestInfo)
 	int	errorCount = 0;
 	int testAssertionCount = 0;
 	
-	TTLogMessage("Testing Parameter value conversions:\n");
+	TTTestLog("Testing Parameter value conversions");
 	
 	
 	// N test assertions
@@ -106,29 +62,16 @@ TTErr TTGain::test(TTValue& returnedTestInfo)
 	for (int i=0; i<64; i++) {
 		badSampleCount += TTTestFloatEquivalence(0.501187, samples[i]);
 	}
-	TTLogMessage("		badSampleCount is %i\n", badSampleCount);
 	TTTestAssertion("accumulated audio error at gain = -6 dB", 
 					badSampleCount == 0, 
 					testAssertionCount, 
 					errorCount);
+	TTTestLog("badSampleCount is %i", badSampleCount);
 	
 	TTObjectRelease(&input);
 	TTObjectRelease(&output);
 	
-		
+	
 	// Wrap up the test results to pass back to whoever called this test
-
-	TTDictionaryPtr d = new TTDictionary;
-	d->setSchema(TT("TestInfo"));
-	d->append(TT("testAssertionCount"), testAssertionCount);
-	d->append(TT("errorCount"), errorCount);
-	returnedTestInfo = d;
-	
-	// TODO: log the test summary to the console
-	// TODO: DRY this stuff at the footer with a macro or function or something
-	
-	if (errorCount)
-		return kTTErrGeneric;
-	else
-		return kTTErrNone;
+	return TTTestFinish(testAssertionCount, errorCount, returnedTestInfo);
 }
