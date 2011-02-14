09b2d14538083251f9ccb710100c4945844e8517
+1m place <tim@74objects.com>
2011-02-14 11:57:55 -0500

    updates related to TTRuby changes

--- library/source/TTGraphDescription.cpp
+++ library/source/TTGraphDescription.cpp
@@ -24,7 +24,7 @@ void TTGraphDescription::exportRuby(const TTString& fullpathToFile)
 	TTStringVector	nodeNames;
 	ofstream		rubyFile(fullpathToFile.c_str());
 	
-	rubyContent += "require \"TTRuby\"\n\n";
+	rubyContent += "require \"Jamoma\"\n\n";
 	exportRubyNode(rubyContent, index, nodeNames);
 	rubyFile.write(rubyContent.c_str(), rubyContent.size());
 	rubyFile.close();
