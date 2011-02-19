7eee3c12bd4ced2ac8827a109ab39faa6bc1979d
+1m place <tim@74objects.com>
2011-02-18 21:08:53 -0600

    TTMatrix: unit testing checks size calculations -- currently fails on the 3D case...

--- library/tests/TTMatrixTest.cpp
+++ library/tests/TTMatrixTest.cpp
@@ -9,53 +9,78 @@
 #include "TTMatrix.h"
 
 
-void TTMatrixTestBasic(int& errorCount, int&testAssertionCount)
-{
-	TTTestLog("\n");
-	TTTestLog("Testing TTMatrix Basics...");
-	
-	TTMatrixPtr matrix = NULL;
-	TTErr err;
-	
-	err = TTObjectInstantiate(TT("matrix"), (TTObjectPtr*)&matrix, kTTValNONE);
-	TTTestAssertion("instantiates successfully", 
-					err == kTTErrNone, 
-					testAssertionCount,
-					errorCount);
-
-	TTTestLog("Setting to a 1D Matrix with a length of 16");
-	matrix->setAttributeValue(TT("dimensions"), 16);
-	
-	
-	TTTestLog("Setting to a 2D Matrix with a size of 160 x 120");
-	TTValue dims(160, 120);
-	matrix->setAttributeValue(TT("dimensions"), dims);
-	
-	
-	TTTestLog("Setting to a 3D Matrix with a size of 5 x 3 x 4");
-	dims.setSize(3);
-	dims.set(0, 5);
-	dims.set(1, 3);
-	dims.set(2, 4);
-	matrix->setAttributeValue(TT("dimensions"), dims);
-	
-	
-	
-	err = TTObjectRelease((TTObjectPtr*)&matrix);
-	TTTestAssertion("frees successfully", 
-					err == kTTErrNone, 
-					testAssertionCount,
-					errorCount);
-	
-}	
-
-
 TTErr TTMatrix::test(TTValue& returnedTestInfo)
 {
 	int	errorCount = 0;
 	int testAssertionCount = 0;
 	
-	TTMatrixTestBasic(errorCount, testAssertionCount);
+
+	
+	
+	{
+		TTTestLog("\n");
+		TTTestLog("Testing TTMatrix Basics...");
+		
+		TTMatrixPtr matrix = NULL;
+		TTErr err;
+		
+		err = TTObjectInstantiate(TT("matrix"), (TTObjectPtr*)&matrix, kTTValNONE);
+		TTTestAssertion("instantiates successfully", 
+						err == kTTErrNone, 
+						testAssertionCount,
+						errorCount);
+		
+		
+		TTTestLog("Setting to a 1D, float64, matrix with a length of 16 for complex numbers (2 elements per value)");
+		matrix->setAttributeValue(TT("dimensions"), 16);
+		matrix->setAttributeValue(TT("type"), TT("float64"));
+		matrix->setAttributeValue(TT("elementCount"), 2);
+		
+		TTTestAssertion("correct amount of data storage calculated", 
+						matrix->mDataSize == sizeof(TTFloat64) * 16 * 2, 
+						testAssertionCount,
+						errorCount);
+		
+		
+		
+		TTTestLog("Setting to a 2D image matrix (8-bit int, 4 elements per value for rgba color) with a size of 160 x 120");
+		TTValue dims(160, 120);
+		matrix->setAttributeValue(TT("dimensions"), dims);
+		matrix->setAttributeValue(TT("type"), TT("float64"));
+		matrix->setAttributeValue(TT("elementCount"), 2);
+		
+		TTTestAssertion("correct amount of data storage calculated", 
+						matrix->mDataSize == sizeof(TTUInt8) * 160 * 120 * 4, 
+						testAssertionCount,
+						errorCount);
+
+		
+		
+		TTTestLog("Setting to a 3D matrix with a size of 5 x 3 x 4 using float32 data and a single element per value");
+		dims.setSize(3);
+		dims.set(0, 5);
+		dims.set(1, 3);
+		dims.set(2, 4);
+		matrix->setAttributeValue(TT("dimensions"), dims);
+		matrix->setAttributeValue(TT("type"), TT("float32"));
+		matrix->setAttributeValue(TT("elementCount"), 1);
+
+		TTTestAssertion("correct amount of data storage calculated", 
+						matrix->mDataSize == sizeof(TTFloat32) * 5 * 3 * 4 * 1, 
+						testAssertionCount,
+						errorCount);
+		
+		
+		err = TTObjectRelease((TTObjectPtr*)&matrix);
+		TTTestAssertion("frees successfully", 
+						err == kTTErrNone, 
+						testAssertionCount,
+						errorCount);
+		
+	}	
+	
+	
+	
 	
 	return TTTestFinish(testAssertionCount, errorCount, returnedTestInfo);
 }
