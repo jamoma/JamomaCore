db285b1dd31eeec83ee868fe3cceb213529b5adc
@mr_nilson <peters.nils@gmx.net>
2011-02-25 14:29:33 -0800

    adding jcom.array≈

---
+++ implementations/MaxMSP/array/jcom.array.cpp
@@ -0,0 +1,18 @@
+/* 
+ *	array≈
+ *	Array object for Jamoma AudioGraph
+ *	Copyright © 2011 by Nils Peters
+ * 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
+ */
+
+#include "maxAudioGraph.h"
+
+
+int main(void)
+{
+	TTAudioGraphInit();
+	wrapAsMaxAudioGraph(TT("array"), "jcom.array≈", NULL);
+	return wrapAsMaxAudioGraph(TT("array"), "array≈", NULL);
+}
