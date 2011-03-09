8d6f6136e701cbf05fbd3dcb585c59d1d6a53829
@mr_nilson <peters.nils@gmx.net>
2011-03-09 12:34:19 -0800

    attempt to add negative frequencies to the phasor

--- extensions/GeneratorLib/includes/TTPhasor.h
+++ extensions/GeneratorLib/includes/TTPhasor.h
@@ -16,8 +16,8 @@
 class TTPhasor : TTAudioObject {
 	TTCLASS_SETUP(TTPhasor)
 
-	TTFloat64 			mFrequency;			///< ramp time in milliseconds
-	TTSampleValue		mPhase;				///< current location in the ramp
+	TTFloat64 			mFrequency;				///< ramp time in milliseconds
+	TTSampleValue		mPhase, phaseInternal;	///< current location in the ramp
 	
 	TTFloat64			rampMilliseconds;		///< ramp time in milliseconds
 	TTUInt32			rampSamples;			///< ramp time in samples
@@ -36,6 +36,8 @@ class TTPhasor : TTAudioObject {
 	
 	/**	Setter for the frequency attribute in Hertz.   */
 	TTErr setFrequency(const TTValue& newValue);
+	/**	Setter for the frequency attribute in Hertz.   */
+	TTErr setPhase(const TTValue& newValue);
 	
 	/**	Setter the gain attribute in dB.   */
 	TTErr setGain(const TTValue& newValue);
