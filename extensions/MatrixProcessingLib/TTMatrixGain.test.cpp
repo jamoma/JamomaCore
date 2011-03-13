57b34f9e01e2663956c9077cb0a143cfe18c015d
+1m place <tim@74objects.com>
2011-03-12 22:07:35 -0600

    TTMatrixGain: now passes its first unit test

--- extensions/MatrixProcessingLib/TTMatrixGain.test.cpp
+++ extensions/MatrixProcessingLib/TTMatrixGain.test.cpp
@@ -18,6 +18,7 @@ TTErr TTMatrixGain::test(TTValue& returnedTestInfo)
 	TTMatrixPtr	y = NULL;				// output matrix
 	TTErr		err = kTTErrNone;
 	TTValue		v(16, 16);
+	TTValue		aColor;
 	
 	err = TTObjectInstantiate(kTTSym_matrix, (TTObjectPtr*)&x, kTTValNONE);
 	err = TTObjectInstantiate(kTTSym_matrix, (TTObjectPtr*)&y, kTTValNONE);
@@ -25,7 +26,12 @@ TTErr TTMatrixGain::test(TTValue& returnedTestInfo)
 	x->setAttributeValue(kTTSym_dimensions,		v);
 	x->setAttributeValue(kTTSym_type,			kTTSym_uint8);
 	x->setAttributeValue(kTTSym_elementCount,	4);
-	x->fill(200);
+	aColor.setSize(4);
+	aColor.set(0, 200);
+	aColor.set(1, 196);
+	aColor.set(2, 24);
+	aColor.set(3, 240);
+	x->fill(aColor);
 	
 	this->setAttributeValue(kTTSym_gain, 0.125);
 	err = this->calculate(x, y);
@@ -35,15 +41,17 @@ TTErr TTMatrixGain::test(TTValue& returnedTestInfo)
 	// 2. Iterate through y and check all of the values
 	TTUInt8* data = (TTUInt8*)y->getLockedPointer();
 	
-	for (int i=0; i<(16*16); i++) {
-		TTBoolean result = !(data[i] == 25);
-		badValueCount += result;
-		if (result)
-			TTTestLog("BAD VALUE @ i=%i  ( value=%.10f   expected=%.10f )", i, data[i], 0.25);
+	for (int i=0; i<(16*16*4); i += 4) {
+		TTUInt8 *pixel = data+i;
+		
+		if (pixel[0] != 25 || pixel[1] != 24 || pixel[2] != 3 || pixel[3] != 30 ) {
+			badValueCount++;
+			TTTestLog("BAD VALUE @ i=%i  ( value = [%c,%c,%c,%c]   expected = [25,24,3,30]", i, pixel[0], pixel[1], pixel[2], pixel[3]);
+		}
 	}
 	y->releaseLockedPointer();
 	
-	TTTestAssertion("Test 1: Produces correct ramp from 0 to 1 when a positive Frequency is defined", 
+	TTTestAssertion("Test 2: All values in a 16x16 matrix (type uint8, element count 4) are properly scaled by 1/8.", 
 					badValueCount == 0, 
 					testAssertionCount, 
 					errorCount);
