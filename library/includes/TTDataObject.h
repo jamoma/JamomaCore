129521112f8a4059c7acfde0747defb0c5399598
+1m place <tim@74objects.com>
2011-03-12 20:46:06 -0600

    TTDataObject: stole a huge variety of functionality from JamomaDSP's TTAudioObject except focused on processing TTMatrix data.

--- library/includes/TTDataObject.h
+++ library/includes/TTDataObject.h
@@ -13,6 +13,15 @@
 #include "TTSymbol.h"
 #include "TTValue.h"
 
+// Forward declaration of TTAudioObject for the typedef that follows...
+class TTDataObject;
+class TTMatrixArray;
+
+/**	A type that can be used to store a pointer to a calculate method (which calculates an array of matrices). */
+typedef TTErr (TTDataObject::*TTMatrixCalculateMethod)(const TTMatrixArray* inputMatrices, TTMatrixArray* outputMatrices);
+
+#define setMatrixCalculateMethod(methodName)	setMatrixCalculate((TTMatrixCalculateMethod)& thisTTClass ::methodName )
+
 
 /****************************************************************************************************/
 // Class Specification
@@ -22,17 +31,35 @@
  */
 class TTFOUNDATION_EXPORT TTDataObject : public TTObject {
 protected:
-	TTPtrSizedInt		reserved1;			///< Reserved -- May be used for something in the future without changing the size of the struct.
-	TTPtrSizedInt		reserved2;			///< Reserved -- May be used for something in the future without changing the size of the struct.
+	TTMatrixCalculateMethod		mMatrixCalculateMethod;			///< This function pointer points to the active (non-bypass) calculate routine.
+	TTMatrixCalculateMethod		mCurrentMatrixCalculateMethod;	///< This function pointer always points to the current calculate routine.
+	TTBoolean					mBypass;						///< Are we bypassing the matrix calculation?
+	TTMatrixArray*				mInputArray;					///< If the calculate method is passed a matrix, rather than an array of matrices, we wrap it in this array.
+	TTMatrixArray*				mOutputArray;					///< If the calculate method is passed a matrix, rather than an array of matrices, we wrap it in this array.
+	TTPtrSizedInt				mReserved1;						///< Reserved -- May be used for something in the future without changing the size of the struct.
+	TTPtrSizedInt				mReserved2;						///< Reserved -- May be used for something in the future without changing the size of the struct.
 
 protected:
 	//** Constructor.	*/
 	TTDataObject(TTValue& arguments);
 
-public:	
 	/** Destructor. */
 	virtual ~TTDataObject();	
+
+	/** Set the sample calculate routine to point to a method that is defined as an arg to this function.	*/
+	TTErr setMatrixCalculate(TTMatrixCalculateMethod newMatrixCalculateMethod);
 	
+	/** Bypass the audio processing routine and copy all input samples to the output unchanged.				*/
+	TTErr setBypass(const TTValue& value);
+
+	// Built-in matrix calculation methods.
+	TTErr bypassMatrixCalculate(const TTMatrixArray* inputs, TTMatrixArray* outputs);
+	TTErr defaultMatrixCalculateMethod(const TTMatrixArray* inputs, TTMatrixArray* outputs);
+	
+public:
+	TTErr calculate(const TTMatrixArray* inputs, TTMatrixArray* outputs);
+	TTErr calculate(const TTMatrix& x, TTMatrix& y);
+
 };
 
 
--- library/source/TTDataObject.cpp
+++ library/source/TTDataObject.cpp
@@ -8,6 +8,7 @@
 
 #include "TTDataObject.h"
 #include "TTEnvironment.h"
+#include "TTMatrixArray.h"
 
 
 /****************************************************************************************************/
@@ -15,12 +16,108 @@
 TTDataObject::TTDataObject(TTValue& arguments)
 	: TTObject(arguments)
 {
+	registerAttribute(TT("bypass"),	kTypeBoolean,	&mBypass,	(TTSetterMethod)&TTDataObject::setBypass);
+
+//	registerMessage(TT("calculate"), (TTMethod)&TTAudioObject::calculateMessage);
 	registerMessage(TT("test"), TTMethod(&TTObject::test));
+//	registerMessage(TT("resetBenchmarking"), (TTMethod)&TTAudioObject::resetBenchmarking, kTTMessagePassNone);
+//	registerMessage(TT("getProcessingBenchmark"), (TTMethod)&TTAudioObject::getProcessingBenchmark);
+
+	TTObjectInstantiate(kTTSym_matrixarray, (TTObjectPtr*)&mInputArray, 2);
+	TTObjectInstantiate(kTTSym_matrixarray, (TTObjectPtr*)&mOutputArray, 2);
+	
+	// Set Defaults...	
+    setMatrixCalculate(&TTDataObject::defaultMatrixCalculateMethod);
+	setAttributeValue(TT("bypass"),			kTTBoolNo);	
 }
 
 
 TTDataObject::~TTDataObject()
 {
-	;
+	TTObjectRelease((TTObjectPtr*)&mInputArray);
+	TTObjectRelease((TTObjectPtr*)&mOutputArray);
+}
+
+
+TTErr TTDataObject::setMatrixCalculate(TTMatrixCalculateMethod newMatrixCalculateMethod)
+{
+	mMatrixCalculateMethod = newMatrixCalculateMethod;
+	if (!mBypass)
+		mCurrentMatrixCalculateMethod = mMatrixCalculateMethod;
+	return kTTErrNone;
+}
+
+
+TTErr TTDataObject::setBypass(const TTValue& value)
+{
+	mBypass = value;
+	if (mBypass) {
+		mCurrentMatrixCalculateMethod = &TTDataObject::bypassMatrixCalculate;
+	}
+//	else if (attrMute) {
+//		currentProcessMethod = &TTAudioObject::muteProcess;
+//	}
+	else {
+		if (mMatrixCalculateMethod)
+			mCurrentMatrixCalculateMethod = mMatrixCalculateMethod;
+		else
+			mCurrentMatrixCalculateMethod = &TTDataObject::defaultMatrixCalculateMethod;
+	}
+	return kTTErrNone;
+}
+
+
+TTErr TTDataObject::bypassMatrixCalculate(const TTMatrixArray* inputs, TTMatrixArray* outputs)
+{
+	for (TTUInt16 i=0; i < outputs->getMatrixCount(); i++) {
+		TTMatrixPtr out = outputs->getMatrix(i);
+		
+		if (i < inputs->getMatrixCount()) {
+			TTMatrixPtr in = inputs->getMatrix(i);
+			TTMatrix::copy(in, out);
+		}
+		else
+			out->clear();
+	}
+	return kTTErrNone;
+}
+
+
+TTErr TTDataObject::defaultMatrixCalculateMethod(const TTMatrixArray* inputs, TTMatrixArray* outputs)
+{
+	return bypassMatrixCalculate(inputs, outputs);
+}
+
+
+TTErr TTDataObject::calculate(const TTMatrixArray* inputs, TTMatrixArray* outputs)
+{
+	TTErr	err = kTTErrGeneric;
+	
+	if (valid) {
+		lock();
+		err = (this->*mCurrentMatrixCalculateMethod)(inputs, outputs);
+		unlock();
+	}
+	return err;
+}
+
+
+TTErr TTDataObject::calculate(const TTMatrix& x, TTMatrix& y)
+{
+	TTErr	err = kTTErrGeneric;
+	
+	if (valid) {
+		lock();
+		mInputArray->setMatrixCount(1);
+		mInputArray->setMatrix(0, (TTMatrix&)x);
+		mOutputArray->setMatrixCount(1);
+		mOutputArray->setMatrix(0, y);
+//		startProcessingTime = TTGetTimeInMicroseconds();
+		err = (this->*mCurrentMatrixCalculateMethod)(mInputArray, mOutputArray);
+//		accumulatedProcessingTime += (TTGetTimeInMicroseconds() - startProcessingTime);
+//		accumulatedProcessingCalls++;
+		unlock();
+	}
+	return err;
 }
 
