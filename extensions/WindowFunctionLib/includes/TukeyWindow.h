1b77dd3960271840b8def54cc5d8a0278d801bb0
Nathan Wolek <nw@nathanwolek.com>
2011-03-22 22:11:34 -0400

    first pass at a Tukey test function

--- extensions/WindowFunctionLib/includes/TukeyWindow.h
+++ extensions/WindowFunctionLib/includes/TukeyWindow.h
@@ -14,7 +14,7 @@
 
 /**	This implements the Tukey window function described here:
  http://en.wikipedia.org/wiki/File:Window_function_(Tukey;_alpha_%3D_0.5).png
- This is alternate implementation of what Roads (1996 p 170, 2001 p 88) typically refers to as a Gaussian window.
+ This is alternate implementation of what Roads (1996 p 170, 2001 p 88) typically refers to as a quasi-Gaussian window.
  It is easier to ensure that x and y values will scale properly with this formula as opposed to the one given by Roads.
  
  Where 0. <= x <= 1. and 0. <= mAlpha <= 1.:
@@ -25,7 +25,7 @@
 class TukeyWindow : TTAudioObject {
 	TTCLASS_SETUP(TukeyWindow)
 	
-	TTFloat64	mAlpha;			///< ratio of window fades to total window duration. 0 <= alpho <= 1., 0 = no fades, 1 = no sustain
+	TTFloat64	mAlpha;			///< ratio of window fades to total window duration. 0 <= alpha <= 1., 0 = no fades, 1 = no sustain
 	
 	/** y = f(x) for a single value */
 	inline TTErr calculateValue(const TTFloat64& x, TTFloat64& y, TTPtrSizedInt data);
