3efb336801872c649baa4e4c3aadc7401bb54806
Nathan Wolek <nw@nathanwolek.com>
2011-04-05 15:04:02 -0400

    basic Gauss source files created

---
+++ extensions/WindowFunctionLib/source/GaussWindow.cpp
@@ -0,0 +1,10 @@
+/*
+ * Gauss Window Function Unit for Jamoma DSP
+ * Copyright © 2011 by Nathan Wolek
+ * 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
+ */
+
+#include "GaussWindow.h"
+
