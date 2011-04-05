d6d261948c0741ae263c33ca3d98d24b054c9d34
Nathan Wolek <nw@nathanwolek.com>
2011-04-05 15:18:21 -0400

    gauss cpp code compiles

--- extensions/WindowFunctionLib/source/GaussWindow.cpp
+++ extensions/WindowFunctionLib/source/GaussWindow.cpp
@@ -8,3 +8,43 @@
 
 #include "GaussWindow.h"
 
+#define thisTTClass GaussWindow
+#define thisTTClassName		"gauss"
+#define thisTTClassTags		"audio, porcessor, function, window"
+
+
+TT_AUDIO_CONSTRUCTOR
+{
+	// Register Attributes...
+	addAttribute(Sigma, kTypeFloat64);
+	addAttributeProperty(Sigma,	range,			TTValue(kTTEpsilon, 0.5));	// Avoid dividing by 0
+	addAttributeProperty(Sigma,	rangeChecking,	TT("clip"));
+	
+	// Set Defaults:
+	setAttributeValue(TT("sigma"), 0.);
+	
+	setProcessMethod(processAudio);
+	setCalculateMethod(calculateValue);
+
+}
+
+GaussWindow::~GaussWindow()
+{
+	;
+}
+
+TTErr GaussWindow::calculateValue(const TTFloat64& x, TTFloat64& y, TTPtrSizedInt data)
+{
+	TTFloat64 superscript = -0.5 * pow( ( (2. * x) - 1. ) / mSigma, 2 );
+	
+	y = exp(superscript);
+	
+	TTLimit(y, 0.0, 1.0 ); // just in case 
+	
+	return kTTErrNone;
+}
+
+TTErr GaussWindow::processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs)
+{
+	TT_WRAP_CALCULATE_METHOD(calculateValue);
+}
