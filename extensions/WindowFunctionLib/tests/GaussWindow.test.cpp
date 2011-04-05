39ed76d810450b42d83af5c51eded37ee201cee1
Nathan Wolek <nw@nathanwolek.com>
2011-04-05 15:27:52 -0400

    gauss test method compiles

---
+++ extensions/WindowFunctionLib/tests/GaussWindow.test.cpp
@@ -0,0 +1,11 @@
+/*
+ *  GaussWindow.test.cpp
+ *  WindowFunctionLib
+ *
+ *  Created by Nathan Wolek on 4/5/11.
+ *  Copyright 2011 Stetson University. All rights reserved.
+ *
+ */
+
+#include "GaussWindow.test.h"
+
