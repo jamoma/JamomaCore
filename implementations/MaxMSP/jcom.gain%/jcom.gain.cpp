520414a6ae50bf92e49fcdb129749021b770cf3e
+1m place <tim@74objects.com>
2011-04-19 07:47:39 -0500

    jcom.gain% : adding a couple comments

--- implementations/MaxMSP/jcom.gain%/jcom.gain.cpp
+++ implementations/MaxMSP/jcom.gain%/jcom.gain.cpp
@@ -108,8 +108,8 @@ t_jit_err GainMatrixCalc(GainObjectPtr self, void *inputs, void *outputs)
 	out_matrix 	= jit_object_method(outputs,_jit_sym_getindex,0);
 
 	if (self && in_matrix && out_matrix) {
-		in_savelock = TTMatrixReferenceJitterMatrix(self->x, in_matrix);
-		out_savelock = TTMatrixReferenceJitterMatrix(self->y, out_matrix);
+		in_savelock = TTMatrixReferenceJitterMatrix(self->x, in_matrix, false);		// we're just scaling, and this is a trivial example, so we don't copy
+		out_savelock = TTMatrixReferenceJitterMatrix(self->y, out_matrix, false);	// we're just scaling, and this is a trivial example, so we don't copy
 		
 		self->gainObject->calculate(self->x, self->y);
 		
