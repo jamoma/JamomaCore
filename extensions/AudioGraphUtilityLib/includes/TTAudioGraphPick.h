2022d492b27719e24bc24880383b45bbcdb0ec08
@mr_nilson <peters.nils@gmx.net>
2011-06-17 19:05:03 -0700

    new AudioGraph external: jcom.pick≈: extracts specific channels from a multichannel signal

---
+++ extensions/AudioGraphUtilityLib/includes/TTAudioGraphPick.h
@@ -0,0 +1,77 @@
+/* 
+ * TTAudioGraphSplit Object
+ * Copyright © 2008, Timothy Place
+ * 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
+ */
+
+#ifndef __TT_PICK_H__
+#define __TT_PICK_H__
+
+#include "TTDSP.h"
+
+
+class TTAudioGraphPick : public TTAudioObject {
+	TTCLASS_SETUP(TTAudioGraphPick)
+	
+	vector<TTUInt16>	mPickChannels;	///< The number of channels in each of N groups of signals
+	TTUInt16			mNumPickChannels;
+	
+	TTErr setPicks(const TTValueRef args)
+	{
+		mNumPickChannels = args.getSize();
+		
+		mPickChannels.resize(mNumPickChannels);
+		for (TTUInt16 i=0; i<mNumPickChannels; i++) 
+			args.get(i, mPickChannels[i]);
+		return kTTErrNone;
+	}
+	
+	TTErr getPicks(TTValueRef args)
+	{
+		args.setSize(mNumPickChannels);
+		for (TTUInt16 i=0; i<mNumPickChannels; i++)
+			args.set(i, mPickChannels[i]);
+		return kTTErrNone;
+	}
+	
+	TTErr processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs)
+	{
+		TTAudioSignal&	in = inputs->getSignal(0);
+		TTAudioSignal&	out = outputs->getSignal(0);
+		TTSampleValue	*inSample, *outSample;
+		TTUInt16		inputChannelCount = in.getNumChannelsAsInt();		
+		TTUInt16		vs = out.getVectorSizeAsInt();
+		TTUInt16		currentPick, n;
+		
+		// TODO: we don't really want to alloc this memory every time!
+		
+		out.setMaxNumChannels(mNumPickChannels);
+		out.setNumChannels(mNumPickChannels);
+		
+		
+		for (TTUInt16 i=0; i < mNumPickChannels; i++) {
+			outSample = out.mSampleVectors[i];
+			n = vs;
+			currentPick = mPickChannels[i];
+			if (currentPick <= inputChannelCount) {
+				inSample = in.mSampleVectors[mPickChannels[i]-1];					
+				while (n--)
+					*outSample++ = *inSample++;
+			}
+			else {
+				while (n--)
+					*outSample++ = 0.0;
+			}
+				
+		}
+
+		return kTTErrNone;
+	}
+	
+};
+
+
+
+#endif // __TT_PICK_H__
