03793a55d9b3fc06051f981db858281eba3c118e
@mr_nilson <peters.nils@gmx.net>
2011-06-20 08:00:13 -0700

    using 'nonadaptive' and 'userCanSetNumChannels' ClassOptions to prevent that number of outputs is the same than number of inputs

--- implementations/MaxMSP/pick/jcom.pick.cpp
+++ implementations/MaxMSP/pick/jcom.pick.cpp
@@ -12,21 +12,20 @@
 
 int TTCLASSWRAPPERMAX_EXPORT main(void)
 {
-	/*MaxAudioGraphWrappedClassOptionsPtr	options = new MaxAudioGraphWrappedClassOptions;
+	MaxAudioGraphWrappedClassOptionsPtr	options = new MaxAudioGraphWrappedClassOptions;
 	TTValue								value(0);
 
 	TTAudioGraphInit();
 	options->append(TT("nonadapting"), value); // don't change the number of out-channels in response to changes in the number of in-channels
-	options->append(TT("argumentDefinesNumInlets"), value);
-	options->append(TT("argumentDefinesNumOutlets"), value);
+	options->append(TT("userCanSetNumChannels"), kTTBoolNo);
 	wrapAsMaxAudioGraph(TT("audio.pick"), "jcom.pick≈", NULL, options);
-	return wrapAsMaxAudioGraph(TT("audio.pick"), "pick≈", NULL, options);*/
+	return wrapAsMaxAudioGraph(TT("audio.pick"), "pick≈", NULL, options);
 	
 	
-		TTAudioGraphInit();
-		wrapAsMaxAudioGraph(TT("audio.pick"), "jcom.pick≈", NULL);
-		wrapAsMaxAudioGraph(TT("audio.pick"), "pick≈", NULL);
-		return 0;
+		//TTAudioGraphInit();
+		//wrapAsMaxAudioGraph(TT("audio.pick"), "jcom.pick≈", NULL);
+		//wrapAsMaxAudioGraph(TT("audio.pick"), "pick≈", NULL);
+		//return 0;
 	
 	
 }
