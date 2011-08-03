c36cf4d3c57c4333d304b0790b104ea59c1432d9
@timothyplace <tim@74objects.com>
2011-08-03 13:02:09 -0500

    updated build.rb to match other jamoma projects

--- build.rb
+++ build.rb
@@ -6,9 +6,3 @@ glibdir = Dir.pwd
 
 Dir.chdir "#{glibdir}/supports"
 load "build.rb"
- 
-
-#if  win32?
-#else
-#	`cp -r "../../../Builds/MaxMSP/jcom.fxlib≈.mxo" "/Applications/Max5/Cycling '74/extensions/jcom.fxlib≈.mxo"`
-#end
