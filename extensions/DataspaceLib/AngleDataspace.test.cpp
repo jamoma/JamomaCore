6710f4b9f1aaf123559c710cfd5e8fb0ed8f45ef
Trond Lossius <trond.lossius@bek.no>
2011-08-09 15:58:38 +0200

    Adding tests for Angle dataspace

---
+++ extensions/DataspaceLib/AngleDataspace.test.cpp
@@ -0,0 +1,91 @@
+/* 
+ * Jamoma DataspaceLib: AngleDataspace test
+ * Copyright © 2011 Trond Lossius
+ * 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
+ */
+
+#include "AngleDataspace.h"
+
+
+TTErr AngleDataspace::test(TTValue& returnedTestInfo)
+{
+    int					errorCount = 0;
+	int					testAssertionCount = 0;
+    
+    // Create dataspace object and set to angle
+    TTObjectPtr         myDataspace;
+    TTErr err;
+    err = TTObjectInstantiate(TT("dataspace"), (TTObjectPtr*)&myDataspace, kTTValNONE);
+	myDataspace->setAttributeValue(TT("dataspace"), TT("angle"));
+    
+    TTValue v;
+    TTValue expected;
+    
+    
+    /************************************************/
+    /*                                              */
+    /* Test conversions to neutral unit             */
+    /*                                              */
+    /************************************************/
+    
+    
+    // Radian => Radian
+    
+    myDataspace->setAttributeValue(TT("inputUnit"), TT("radian"));
+    myDataspace->setAttributeValue(TT("outputUnit"), TT("radian"));    
+    
+    v = TTValue(kTTPi);
+    expected = TTValue(kTTPi);
+    
+    myDataspace->sendMessage(TT("convert"), v);
+    
+    TTTestAssertion("Radian to Radian", 
+					TTTestFloatEquivalence(TTFloat64(v), TTFloat64(expected)),
+					testAssertionCount, 
+					errorCount);
+    
+    
+    
+    // Degree => Radian
+    
+    myDataspace->setAttributeValue(TT("inputUnit"), TT("degree"));
+    myDataspace->setAttributeValue(TT("outputUnit"), TT("radian"));
+    
+    v = TTValue(180.);
+    expected = TTValue(kTTPi);
+    
+    myDataspace->sendMessage(TT("convert"), v);
+    
+    TTTestAssertion("Degree to Radian", 
+					TTTestFloatEquivalence(TTFloat64(v), TTFloat64(expected)),
+					testAssertionCount, 
+					errorCount);
+    
+    
+    /************************************************/
+    /*                                              */
+    /* Test conversions from neutral unit           */
+    /*                                              */
+    /************************************************/
+    
+    // Radian => Degree
+    // Expected value according to Google search: "0 Celsius to Kelvin"
+    
+    myDataspace->setAttributeValue(TT("inputUnit"), TT("radian"));
+    myDataspace->setAttributeValue(TT("outputUnit"), TT("degree"));
+    
+    v = TTValue(kTTPi);
+    expected = TTValue(180.0);
+    
+    myDataspace->sendMessage(TT("convert"), v);
+    
+    TTTestAssertion("Radian to Degree", 
+					TTTestFloatEquivalence(TTFloat64(v), TTFloat64(expected)),
+					testAssertionCount, 
+					errorCount);
+    
+    
+    return TTTestFinish(testAssertionCount, errorCount, returnedTestInfo);
+}
--- tests/dataspacelib.test.rb
+++ tests/dataspacelib.test.rb
@@ -2,15 +2,16 @@
 
 require 'Jamoma'
 
-cpu_total = 0.0;
 
 puts
 puts "TESTING TEMPERATURE DATASPACE"
 o = TTObject.new "dataspace.temperature"
 o.send "test"
-err, cpu = o.send "getProcessingBenchmark", 1
-cpu_total += cpu
 
 puts
-puts "time spent calculating audio process method: #{cpu_total} µs"
+
+puts "TESTING DEGREE DATASPACE"
+o = TTObject.new "dataspace.angle"
+o.send "test"
+
 puts
