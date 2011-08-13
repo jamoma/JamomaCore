20ba4914816e95e7b701108d7b3b31b948be7d9f
Trond Lossius <trond.lossius@bek.no>
2011-08-13 12:11:28 +0200

    Adding some more midi-related tests for gain dataspace
    - Testing for the critical values of MIDI = 0, 100 and 127

--- extensions/DataspaceLib/tests/GainDataspace.test.cpp
+++ extensions/DataspaceLib/tests/GainDataspace.test.cpp
@@ -116,7 +116,72 @@ TTErr GainDataspace::test(TTValue& returnedTestInfo)
 					TTTestFloatEquivalence(TTFloat64(v), TTFloat64(expected)),
 					testAssertionCount, 
 					errorCount);
+
+    /************************************************/
+    /*                                              */
+    /* Some additional important midi relationships */
+    /*                                              */
+    /************************************************/
     
+    // 127 midi => 10 dB
+    
+    myDataspace->setAttributeValue(TT("inputUnit"), TT("midi"));
+    myDataspace->setAttributeValue(TT("outputUnit"), TT("dB"));    
+    
+    v = TTValue(127.0);
+    expected = TTValue(10.0);
+    
+    myDataspace->sendMessage(TT("convert"), v);    
+    
+    TTTestAssertion("127 MIDI to 10 dB", 
+					TTTestFloatEquivalence(TTFloat64(v), TTFloat64(expected)),
+					testAssertionCount, 
+					errorCount);
+    
+    // 10 dB => 127 midi
+    
+    myDataspace->setAttributeValue(TT("inputUnit"), TT("dB"));
+    myDataspace->setAttributeValue(TT("outputUnit"), TT("midi"));    
+    
+    v = TTValue(10.0);
+    expected = TTValue(127.0);
+    
+    myDataspace->sendMessage(TT("convert"), v);    
+    
+    TTTestAssertion("10 dB to 127 MIDI", 
+					TTTestFloatEquivalence(TTFloat64(v), TTFloat64(expected)),
+					testAssertionCount, 
+					errorCount);
+    
+    // 0 midi => 0 linear
+    
+    myDataspace->setAttributeValue(TT("inputUnit"), TT("midi"));
+    myDataspace->setAttributeValue(TT("outputUnit"), TT("linear"));    
+    
+    v = TTValue(0.0);
+    expected = TTValue(0.0);
+    
+    myDataspace->sendMessage(TT("convert"), v);    
+    
+    TTTestAssertion("0 MIDI to 0 linear", 
+					TTTestFloatEquivalence(TTFloat64(v), TTFloat64(expected)),
+					testAssertionCount, 
+					errorCount);
+
+    // 0 linear => 0 midi
+    
+    myDataspace->setAttributeValue(TT("inputUnit"), TT("linear"));
+    myDataspace->setAttributeValue(TT("outputUnit"), TT("midi"));    
+    
+    v = TTValue(0.0);
+    expected = TTValue(0.0);
+    
+    myDataspace->sendMessage(TT("convert"), v);    
+    
+    TTTestAssertion("0 linear to 0 midi", 
+					TTTestFloatEquivalence(TTFloat64(v), TTFloat64(expected)),
+					testAssertionCount, 
+					errorCount);
     
     return TTTestFinish(testAssertionCount, errorCount, returnedTestInfo);
 }
