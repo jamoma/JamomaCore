aa5264dacc944d1aa653f5549530860f59582643
@mr_nilson <peters.nils@gmx.net>
2011-08-12 22:33:58 -0700

    plug.loader is now copied into Jamoma's build folder after compiling

--- build.rb
+++ build.rb
@@ -6,3 +6,14 @@ glibdir = Dir.pwd
 
 Dir.chdir "#{glibdir}/supports"
 load "build.rb"
+
+puts "post-build..."
+Dir.chdir "#{glibdir}"
+
+if  win32?
+else
+	`cp -r "../../Builds/MaxMSP/plug.loader.mxo" "/Applications/Max5/Cycling '74/max-startup/plug.loader.mxo"`
+end
+
+puts "done"
+puts
