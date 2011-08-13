13e2f1bb79a5c785c22769529f8cc8787977f8ab
Trond Lossius <trond.lossius@bek.no>
2011-08-13 15:47:39 +0200

    Removing file that shouldn't be there

--- extensions/DataspaceLib/PositionDataspace.test.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  PositionDataspace.test.cpp
-//  DataspaceLib
-//
-//  Created by Trond Lossius on 8/13/11.
-//  Copyright 2011 BEK - Bergen senter for elektronisk kunst. All rights reserved.
-//
-
-#include <iostream>
