9361d5ff334f4346b1347fe755fde5f2fdacff8a
+1m place <tim@74objects.com>
2011-09-22 21:26:39 -0500

    MaxAudioGraph.h: updated header documentation as per request from Trond

--- implementations/MaxMSP/MaxAudioGraph.h
+++ implementations/MaxMSP/MaxAudioGraph.h
@@ -129,12 +129,13 @@ TTErr wrapAsMaxAudioGraph(TTSymbolPtr ttblueClassName, char* maxClassName, MaxAu
 TTErr wrapAsMaxAudioGraph(TTSymbolPtr ttblueClassName, char* maxClassName, MaxAudioGraphWrappedClassPtr* c, TTValidityCheckFunction validityCheck, MaxAudioGraphWrappedClassOptionsPtr options);
 
 /** 
- This version can be passed a method that is called to make sure it is legit to instantiate the class. 
- * @param ttClassName           Name of the Jamoma DSP class that will be wrapped.
+ This version can be passed a method that is called to make sure it is okay to instantiate the class. 
+ This is useful, for example, if you need to have copy-protection for your external.
+ * @param ttClassName           Name of the Jamoma Audio Graph class that will be wrapped.
  * @param maxClassName          Name of the resulting Max external.
- * @param c                     Pointer to the wrapped class.
- * @param validityCheck
- * @param validityCheckArgument
+ * @param c                     Address to a variable to hold the wrapped Max class upon return.
+ * @param validityCheck			Pointer to a function that will return a true or false regarding whether or not it is okay to instantiate the object.
+ * @param validityCheckArgument	An argument that will be passed to the validity check function, or NULL if you don't wish to pass an argument.
  * @param options               Pointer to additional options that will be forwarded to the wrapped object when instantiated.
  */
 TTErr wrapAsMaxAudioGraph(TTSymbolPtr ttblueClassName, char* maxClassName, MaxAudioGraphWrappedClassPtr* c, TTValidityCheckFunction validityCheck, TTPtr validityCheckArgument, MaxAudioGraphWrappedClassOptionsPtr options);
@@ -143,23 +144,23 @@ TTErr wrapAsMaxAudioGraph(TTSymbolPtr ttblueClassName, char* maxClassName, MaxAu
 
 /** 
  Method called when a connection from an upstream node is dropped
- * @param self                  This object.
+ * @param self                  This Max object.
  * @param inletNumber           The inlet of this object who is now loosing a connection.
  * @param sourceMaxObject       The upstream (source) object who is now disconnecting.
- * @param sourceOutletNumber    The outlet of the source obejct that is now being disconnected.
+ * @param sourceOutletNumber    The outlet of the source object that is now being disconnected.
  */
 TTErr MaxAudioGraphDrop(ObjectPtr self, long inletNumber, ObjectPtr sourceMaxObject, long sourceOutletNumber);
 
 /**
- What do this one do?
- * @param self                      This object.
- * @param returnedAudioGraphObject
+ Returns a pointer to the Jamoma Audio Graph object that is wrapped by this Max object.
+ * @param self                      This Max object.
+ * @param returnedAudioGraphObject	Pointer to the wrapped Jamoma Audio Graph object.
  */
 TTErr MaxAudioGraphObject(ObjectPtr self, TTAudioGraphObjectPtr* returnedAudioGraphObject);
 
 /** 
  Method called when an upstream node is connected to this node
- * @param x
+ * @param x						This Max object.
  * @param audioSourceObject     The upstream object/node that is now connecting
  * @param sourceOutletNumber    What outlet of the upstream object that is now connecting to this object
  */
@@ -167,14 +168,14 @@ TTErr MaxAudioGraphConnect(ObjectPtr x, TTAudioGraphObjectPtr audioSourceObject,
 
 /** 
  Clear the list of source objects from which this object will try to pull audio
- * @param x
+ * @param x				This Max object.
  * @param vectorSize    Initial vector size, might be overruled by subsequent process calls.
  */
 TTErr MaxAudioGraphReset(ObjectPtr x, long vectorSize);
 
 /** 
  Set up fresh connections from this object to nodes that are connected downstream
- * @param self          This object
+ * @param self          This Max object.
  */
 TTErr MaxAudioGraphSetup(ObjectPtr self);
 
