90c1277cd169323c02d926200315e82795791dd6
+1m place <tim@74objects.com>
2011-10-12 22:08:43 -0500

    new ruby file that shows how use the 'environment' object and then run tests on classes in Jamoma

---
+++ Tests/environment.test.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require 'Jamoma'
+
+environment = TTObject.new "environment"              # create an access point to the TTEnvironment class
+classnames = environment.send "getAllClassNames", 0   # query the environment to get a list of all registered classes in the system
+error_code = classnames.delete_at 0                   # the first value returned is an error code, which we don't care about in this case
+
+puts
+puts "Printing all classnames..."
+classnames.each do |name|
+  puts "class: #{name}"
+end
+puts
+
+
+classnames = environment.send "getClassNamesForTags", "lowpass"   # query the environment to get a list of all registered classes in the system
+error_code = classnames.delete_at 0                               # the first value returned is an error code, which we don't care about in this case
+
+puts
+puts "Printing classnames for all objects that have the 'lowpass' tag..."
+classnames.each do |name|
+  puts "class: #{name}"
+end
+puts
+
+puts
+puts "Running unit tests for all objects that have the 'lowpass' tag..."
+puts
+classnames.each do |name|
+  puts "class: #{name}"
+  o = TTObject.new name
+  o.send "test"
+  puts
+end
+puts
+
