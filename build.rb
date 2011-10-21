59ec70c0dce474a41b4a1a240f82a8b800b49a4d
@mr_nilson <peters.nils@gmx.net>
2011-10-21 12:35:30 -0500

    simplifying build script

--- build.rb
+++ build.rb
@@ -7,14 +7,3 @@ glibdir = Dir.pwd
 
 Dir.chdir "#{glibdir}/supports"
 load "build.rb"
-
-puts "post-build..."
-Dir.chdir "#{glibdir}"
-
-if  win32?
-else
-	`cp -r "../../Builds/MaxMSP/jcom.plug.loader.mxo" "/Applications/Max5/Cycling '74/max-startup/jcom.plug.loader.mxo"`
-end
-
-puts "done"
-puts
