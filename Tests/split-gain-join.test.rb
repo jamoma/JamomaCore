6cb7d8c16b8502ef9e63a6bcb20cecb8573b3e69
+1m place <tim@74objects.com>
2011-10-25 21:41:20 -0500

    UnitTest: turning off benchmarking since it makes no sense in this case.

--- Tests/split-gain-join.test.rb
+++ Tests/split-gain-join.test.rb
@@ -6,8 +6,3 @@ require 'Jamoma'
 o = TTObject.new "audio.generator"
 o.send "test"
 
-err, cpu = o.send "getProcessingBenchmark", 1
-
-puts
-puts "time spent calculating audio process method: #{cpu} µs"
-puts
