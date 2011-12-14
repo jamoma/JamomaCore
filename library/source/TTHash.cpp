3e5eb81113e1d2c696f05a223a45d8df06938a92
+1m place <tim@74objects.com>
2011-12-14 13:51:07 -0600

    TTHash: on Linux we use the C++0x standard unordered_map instead of the Boost unordered_map. The Boost unordered_map is now deprecated in favor of the C++0x and also it doesn't compile on the BeagleBoard.

--- library/source/TTHash.cpp
+++ library/source/TTHash.cpp
@@ -15,8 +15,13 @@
 	using namespace stdext;	// Visual Studio 2008 puts the hash_map in this namespace
 	typedef hash_map<TTPtrSizedInt,TTValue>			TTHashMap;
 #else
-	#include "boost/unordered_map.hpp"
-	using namespace boost;
+	#ifdef TT_PLATFORM_LINUX
+		// at least for GCC 4.6 on the BeagleBoard, the unordered map is standard
+		#include <unordered_map>
+	#else
+		#include "boost/unordered_map.hpp"
+		using namespace boost;
+	#endif
 	typedef unordered_map<TTPtrSizedInt,TTValue>	TTHashMap;
 #endif
 
