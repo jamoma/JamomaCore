e741ec7f4be4c906f8e88f7db97aa3a28db68548
Trond Lossius <trond.lossius@bek.no>
2011-12-31 00:35:56 +0100

    Removing posting that have been commented out for a long period of time and deemed annoying

--- implementations/MaxMSP/jcom.hub/jcom.hub.cpp
+++ implementations/MaxMSP/jcom.hub/jcom.hub.cpp
@@ -1404,9 +1404,6 @@ t_max_err hub_attr_setname(t_hub* x, t_object* attr, long argc, t_atom* argv)
 
 		// No arg is present -- try to invent something intelligent for a name
 		if (x->osc_name == _sym_nothing) {
-			// it's annoying when doing demos to have to see this, so it is now silent when doing this:
-			// object_post((t_object*)x, "%s: this module was not given an osc name as an argument!  making up something that will hopefully work.", x->attr_name->s_name);
-
 			// Strip jmod. from the beginning of patch names, this happens if you drag a module from browser to bpatcher
 			if (strncmp(x->attr_name->s_name, "jmod.", 5) == 0)
 				x->osc_name = gensym(x->attr_name->s_name + 5);
