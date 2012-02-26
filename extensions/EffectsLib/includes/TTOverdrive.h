jamoma/JamomaCore

commit a71f88681c365c47c9baf7df51547e5f008d0dfa

TTOverdrive.cpp: using addAttributeProperty macro for clipping drive value

extensions/EffectsLib/sources/TTOverdrive.cpp
@@ -21,6 +21,8 @@ TT_AUDIO_CONSTRUCTOR
 	
 	// Register Attributes
 	addAttributeWithSetter(Drive,				kTypeFloat64);
+		addAttributeProperty(Drive,			range,			TTValue(1.0, 32.0));
+		addAttributeProperty(Drive,			rangeChecking,	TT("clip")); // options are "clip" "cliphigh" "cliplow"
 	addAttributeWithSetter(DcBlocker,			kTypeBoolean);
 	addAttributeWithSetter(Mode,				kTypeUInt8);
 	addAttributeWithGetterAndSetter(Preamp,		kTypeFloat64);
@@ -56,9 +58,7 @@ TTErr TTOverdrive::updateMaxNumChannels(const TTValue& oldMaxNumChannels, TTValu
 TTErr TTOverdrive::setDrive(const TTValue& newValue)
 {
 	TTFloat64 	f;
-	int			i;
-		
-	mDrive = TTClip(TTFloat32(newValue), TTFloat32(1.0), TTFloat32(10.0));
+	int			i;		
 	
 	// These calculations really only apply to mode 1...
 	f = (mDrive - 0.999) * 0.111;	// range is roughly [0.001 to 0.999]

commit 21abc203dfc42d14ec37512cced524e74b63c056

fixing bug I introduced with commit a71f88681c365c47c9baf7df51547e5f008d0dfa

extensions/EffectsLib/includes/TTOverdrive.h
@@ -33,7 +33,7 @@ class TT_OVERDRIVE_EXPORT TTOverdrive : public TTAudioObject {
 
 	TTFloat64			mDrive;			///< Attribute: The amount of saturation to apply
 	TTBoolean			mDcBlocker;		///< Attribute: Pass processed signal through a DC blocking filter?
-	TTUInt8				mMode;			///< Attribute: What distortion function to use
+	TTBoolean			mMode;			///< Attribute: What distortion function to use
 	TTFloat64			mPreamp;		///< Attribute: linear gain for preamp (attr setter sets it in dB)
 	TTFloat64			s,
 						b,

extensions/EffectsLib/sources/TTOverdrive.cpp
@@ -21,10 +21,10 @@ TT_AUDIO_CONSTRUCTOR
 	
 	// Register Attributes
 	addAttributeWithSetter(Drive,				kTypeFloat64);
-		addAttributeProperty(Drive,			range,			TTValue(1.0, 32.0));
+		addAttributeProperty(Drive,			range,			TTValue(1.0, 10.0));
 		addAttributeProperty(Drive,			rangeChecking,	TT("clip")); // options are "clip" "cliphigh" "cliplow"
 	addAttributeWithSetter(DcBlocker,			kTypeBoolean);
-	addAttributeWithSetter(Mode,				kTypeUInt8);
+	addAttributeWithSetter(Mode,				kTypeBoolean);// IMPORTANT: if we have more modes, the datatype need to change here
 	addAttributeWithGetterAndSetter(Preamp,		kTypeFloat64);
 	
 	// Register Messages
@@ -58,16 +58,17 @@ TTErr TTOverdrive::updateMaxNumChannels(const TTValue& oldMaxNumChannels, TTValu
 TTErr TTOverdrive::setDrive(const TTValue& newValue)
 {
 	TTFloat64 	f;
-	int			i;		
+	int			i;	
 	
+	mDrive = newValue;
 	// These calculations really only apply to mode 1...
 	f = (mDrive - 0.999) * 0.111;	// range is roughly [0.001 to 0.999]
 	
 	z = kTTPi * f;
 	s = 1.0 / sin(z);
 	b = 1.0 / f;
-	b = TTClip(b, 0.0, 1.0);
-	nb = b * -1.;
+    TTLimit(b, 0.0, 1.0);
+	nb = b * -1.0;
 	i = int(f);
 	if ((f-(TTFloat64)i) > 0.5) 
 		scale = sin(z); // sin(f * kTTPi);