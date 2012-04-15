c6d0d3dd6d1a5ea0729736a058e9ba466ecc0ddc
@mr_nilson <peters.nils@gmx.net>
2012-04-15 15:21:19 -0700

    new JAG external: jcom.clip≈

---
+++ implementations/MaxMSP/jcom.clip≈/jcom.clip.cpp
@@ -0,0 +1,19 @@
+/* 
+ *	limiter≈
+ *	Jamoma AudioGraph external object for Max
+ *	Copyright © 2008 by Timothy Place
+ * 
+ * License: This code is licensed under the terms of the "New BSD License"
+ * http://creativecommons.org/licenses/BSD/
+ */
+
+#include "maxAudioGraph.h"
+
+int TTCLASSWRAPPERMAX_EXPORT main(void)
+{
+	TTAudioGraphInit();
+	wrapAsMaxAudioGraph(TT("clipper"), "jcom.clip≈", NULL);
+	wrapAsMaxAudioGraph(TT("clipper"), "clip≈", NULL);
+	return 0;
+}
+
