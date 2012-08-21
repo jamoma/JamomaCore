413a549cd96eadc307c9f601d87ac05cc591e450
Nathan Wolek <nw@nathanwolek.com>
2012-08-21 12:58:30 +0200

    looking at interaction between dimensions and elementCount

--- Tests/unit/samplematrix.test.rb
+++ Tests/unit/samplematrix.test.rb
@@ -12,7 +12,7 @@ p = TTObject.new "samplematrix"
 pName = "TTSampleMatrix"
 
 puts
-puts "Testing the differences between #{oName} and its superclass #{pName} . . ."
+puts "Testing the differences between #{pName} and its superclass #{oName} . . ."
 
 #####
 puts
@@ -41,8 +41,10 @@ puts p.messages?
 #####
 puts
 puts "**********"
+puts "I want to know if setting the dimensions impacts the elementCount..."
 
 attributeTest = "dimensions"
+attributeTest2 = "elementCount"
 attributeValue = 2
 
 o.set attributeTest, attributeValue
@@ -51,17 +53,28 @@ temp = o.get attributeTest
 puts
 puts "#{oName} #{attributeTest} were set to #{temp}"
 
+temp = o.get attributeTest2
+
+puts "#{oName} #{attributeTest2} has a value of #{temp}"
+
 p.set attributeTest, attributeValue
 temp = p.get attributeTest
 
 puts
 puts "#{pName} #{attributeTest} were set to #{temp}"
 
+temp = p.get attributeTest2
+
+puts "#{pName} #{attributeTest2} has a value of #{temp}"
+
+
 #####
 puts
 puts "**********"
+puts "Now the opposite..."
 
 attributeTest = "elementCount"
+attributeTest2 = "dimensions"
 attributeValue = 65
 
 o.set attributeTest, attributeValue
@@ -70,11 +83,19 @@ temp = o.get attributeTest
 puts
 puts "#{oName} #{attributeTest} was set to #{temp}"
 
+temp = o.get attributeTest2
+
+puts "#{oName} #{attributeTest2} has a value of #{temp}"
+
 p.set attributeTest, attributeValue
 temp = p.get attributeTest
 
 puts
 puts "#{pName} #{attributeTest} was set to #{temp}"
+
+temp = p.get attributeTest2
+
+puts "#{pName} #{attributeTest2} has a value of #{temp}"
 
 #####
 puts
