6db4cd22feb81fd04fcc502db75401faf0b9e3a2
Theo de la Hogue <theod@gmea.net>
2012-09-05 09:59:35 +0200

    Moving the try/catch part into the TTRegex constructor

--- library/source/TTNodeLib.cpp
+++ library/source/TTNodeLib.cpp
@@ -8,23 +8,15 @@
 
 #include "TTNodeLib.h"
 #include "TTRegex.h"
-#include <regex>
 
 void TTNodeLibInit()
 {
 	// Global regex for TTNodeAddress parsing
-	try {
-		ttRegexForDirectory = new TTRegex("([\\w]+)\\:\\/"); // had to escape the forward /  [tap]
-		ttRegexForAttribute = new TTRegex(":+");
-		ttRegexForParent = new TTRegex("(.*)\\/+(\\S+)");	// had to escape the forward /  [tap]
-		ttRegexForInstance = new TTRegex("[.]");
-		ttRegexForInstanceZero = new TTRegex("\\.0");
-	}
-	catch (const std::regex_error& e) {
-        std::cout << "regex_error caught: " << e.what() << '\n';
-        if (e.code() == std::regex_constants::error_brack)
-			std::cout << "The code was error_brack\n";
-	}
+	ttRegexForDirectory = new TTRegex(":/");			// old syntax : "([\\w]+)\\:\\/"	(had to escape the forward /  [tap])
+	ttRegexForAttribute = new TTRegex(":+");
+	ttRegexForParent = new TTRegex("(.*)\/+(\S+)");		// old syntax : "(.*)\\/+(\\S+)"	(had to escape the forward /  [tap])
+	ttRegexForInstance = new TTRegex("[.]");
+	ttRegexForInstanceZero = new TTRegex("\\.0");
 	
 	ttNodeAddressTable = new TTNodeAddressTable;
 }
