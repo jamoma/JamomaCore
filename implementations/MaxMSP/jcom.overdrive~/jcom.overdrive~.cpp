7a97eba4a0a0e47a5afed710dbd23e74821bdcc1
+1m place <tim@74objects.com>
2012-10-26 17:34:55 -0500

    jcom.overdrive~: can now control the drive attribute with an audio signal.

--- implementations/MaxMSP/jcom.overdrive~/jcom.overdrive~.cpp
+++ implementations/MaxMSP/jcom.overdrive~/jcom.overdrive~.cpp
@@ -13,6 +13,14 @@
 
 int TTCLASSWRAPPERMAX_EXPORT main(void)
 {
+	WrappedClassOptionsPtr	options = new WrappedClassOptions;
+	TTValue					value;
+
 	TTDSPInit();
-	return wrapTTClassAsMaxClass(TT("overdrive"), "jcom.overdrive~", NULL);
+	
+	value.clear();
+	value.append(TT("drive"));
+	options->append(TT("additionalSignalInputSetsAttribute"), value);	
+	
+	return wrapTTClassAsMaxClass(TT("overdrive"), "jcom.overdrive~", NULL, options);
 }
