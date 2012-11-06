01022cf575dc7b467ddd019dea7461101737933a
Trond Lossius <trond.lossius@bek.no>
2012-11-06 15:20:46 -0800

    Improving documentation for TTDictionary

--- library/includes/TTDictionary.h
+++ library/includes/TTDictionary.h
@@ -1,10 +1,19 @@
-/* 
- * Foundation Dictionary Class
- * Copyright © 2010, Timothy Place
- * 
- * License: This code is licensed under the terms of the "New BSD License"
+/** @file
+ * @ingroup foundation
+ *
+ * @brief Foundation Dictionary Class.
+ *
+ * @details The dictionary is a data structure that combines the fast lookup of a hashtable,
+ * but may be sorted like a linked-list.
+ * The linked list contains the key-value pairs of the hash a linked-list of TTKeyValues.
+ *
+ * @authors Timothy Place, Trond Lossius
+ *
+ * @copyright Copyright © 2010, Timothy Place @n
+ * This code is licensed under the terms of the "New BSD License" @n
  * http://creativecommons.org/licenses/BSD/
  */
+
 
 #ifndef __TT_DICTIONARY_H__
 #define __TT_DICTIONARY_H__
@@ -17,10 +26,10 @@
 // Class Specification
 
 /**	
-	The dictionary is a data structure that combines the fast lookup of a hashtable,
-	but may be sorted like a linked-list.
+ The dictionary is a data structure that combines the fast lookup of a hashtable,
+but may be sorted like a linked-list.
  
-	The linked list contains the key-value pairs of the hash a linked-list of TTKeyValues.
+The linked list contains the key-value pairs of the hash a linked-list of TTKeyValues.
 */
 class TTFOUNDATION_EXPORT TTDictionary : TTBase {
 private:
@@ -44,37 +53,83 @@ public:
 		mList = new TTList(*source.mList);
 		return *this;
 	}
-		
 	
+	
+	/** TODO: Add documentation
+	 @para, schemaName			TODO: Add documentation
+	 @return					#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr setSchema(const TTSymbol& schemaName);
+	
+	
+	/** TODO: Add documentation
+	 @return					TODO: Add documentation
+	 */
 	const TTSymbol getSchema() const;
 	
+	
+	/** TODO: Add documentation
+	 @param newValue			TODO: Add documentation
+	 @return					#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr setValue(const TTValue& newValue);
+	
+	
+	/** TODO: Add documentation
+	 @param returnedValue		TODO: Add documentation
+	 @return					#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr getValue(TTValue& returnedValue) const;
 	
 	
-	/** Insert an item into the hash table. */
+	/** Insert an item into the hash table. 
+	 @param value	The value to instert.
+	 @return					#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr append(const TTSymbol& key, const TTValue& value);
 	
-	/** Find the value for the given key. */
+	
+	/** Find the value for the given key.
+	 @return					#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr lookup(const TTSymbol& key, TTValue& value) const;
 	
-	/** Remove an item from the hash table. */
+	
+	/** Remove an item from the hash table.
+	 @param key					The key of the item to remove.
+	 @return					#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr remove(const TTSymbol& key);
 	
-	/** Remove all items from the hash table. */
+	
+	/** Remove all items from the hash table.
+	 @return					#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr clear();
 	
-	/** Get an array of all of the keys for the hash table. */
+	
+	/** Get an array of all of the keys for the hash table.
+	 @param hashKeys			Used to return an array of all of the keys for the hash table
+	 @return					#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr getKeys(TTValue& hashKeys);
 	
-	/** Return the number of keys in the hash table. */
+	
+	/** Return the number of keys in the hash table.
+	 @return					The number of keys in the hash table.
+	 */
 	TTUInt32 getSize();
 	
-	/** Return true if the hash has nothing stored in it. */
+	
+	/** Return true if the hash has nothing stored in it. 
+	 @return					TRUE if the hash has nothing stored in it.
+	 */
 	TTBoolean isEmpty();
 };
 
+/** Pointer to a #TTDictionary.
+ @ingroup typedefs
+ */
 typedef TTDictionary* TTDictionaryPtr;
 
 
