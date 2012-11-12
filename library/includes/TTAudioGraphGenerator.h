c0aa621e62a63ab08f4b21bc5697826ba21fe51b
Trond Lossius <trond.lossius@bek.no>
2012-11-12 10:21:10 -0800

    I'm not entirely sure about all the comments committed here, we'll need to double-check if they are correct or need further adjustments

--- library/includes/TTAudioGraphGenerator.h
+++ library/includes/TTAudioGraphGenerator.h
@@ -32,20 +32,52 @@ protected:
 
 	TTUInt16	mVectorSize;
 	
+	
+	/** Set the vector size for audio processing.
+	 @param newVectorSize					The new vector size.
+	 @return								#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr setVectorSize(const TTValue& newVectorSize);
+	
+	
+	/** Update the maximim number of channels to process.
+	 @param oldMaxNumChannels				TODO: This doesn't seem to be used
+	 @return								#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr updateMaxNumChannels(const TTValue& oldMaxNumChannels, TTValue&);
+	
+	
+	
+	/** Update the sample rate.
+	 @param oldSampleRate					TODO: This doesn't seem to be used.
+	 @return								#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr updateSampleRate(const TTValue& oldSampleRate, TTValue&);
 	
 public:
-	TTAudioSignalPtr		mBuffer;		///< storage for the audioSignal that we provide
+	TTAudioSignalPtr		mBuffer;		///< Storage for the audioSignal that we provide
 
-	/**	A standard audio processing method as used by TTBlue objects.
-		@param	inputs	unused.				*/
+	
+	/**	A standard audio processing method as used by Jamoma DSP objects.
+	 @param	inputs							Unused.
+	 @param outputs							
+	 @return								#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	TTErr processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs);	
 	
+	
+	/** Unit test for the window function unit.
+	 @param returnedTestInfo				The outcome from the performed unit test.
+	 @return								#TTErr error code if the method fails to execute, else #kTTErrNone.
+	 */
 	virtual TTErr test(TTValue& returnedTestInfo);
+	
 };
 
+
+/** Pointer to a TTAudioGraphGenerator.
+ @ingroup typedefs
+ */
 typedef TTAudioGraphGenerator*	TTAudioGraphGeneratorPtr;
 
 
