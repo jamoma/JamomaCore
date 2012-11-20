8453b4fc01a4800aac7575a48b25aa02be4195c1
Trond Lossius <trond.lossius@bek.no>
2012-11-20 13:12:03 -0800

    Elaborating on doxygen for TTSpatSnap

--- DSP/extensions/SpatLib/includes/TTSpatSnap.h
+++ DSP/extensions/SpatLib/includes/TTSpatSnap.h
@@ -21,7 +21,7 @@
 #include "TTSampleMatrix.h"
 #include "TTSpatEntity.h"
 
-/** The TTSpatSnapRenderer class keeps 
+/** TTSpatSnapRenderer contains attributes and methods that are specific to this particular spatialisation renderer.
  */
 class TTSpatSnapRenderer {
 	
@@ -40,70 +40,155 @@ public:
 	
 	/** This method is called whenever matrix coefficients need to be updated.
 	 @details This method also takes care of matrix resizing if the number of sources or sinks change.
-	 @param sources
-	 @param sinks
+	 @param sources						A vector of sources
+	 @param sinks						A vector of sinks
 	 */
 	void recalculateMatrixCoefficients(TTSpatEntityVector& sources, TTSpatEntityVector& sinks);
 	
-	/**	A standard audio processing method as used by TTBlue objects.*/
+	/**	A standard audio processing method as used by Jamoma DSP objects.
+	 @param inputs						Incomming audio signals to process from sound sources.
+	 @param outputs						Processed audio signals passed to the sinks.
+	 */
 	TTErr processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs);
 		
 };
 
 
 
-/**	TODO
+/**	This class is eventually intended to provide a generalised interface for spatial renderers.
+ @todo The class might be further generalised so that it can be reused for other spatial renderers.
  */
 class TTSpatSnap : TTAudioObject {
 	TTCLASS_SETUP(TTSpatSnap)
 	
-	TTSpatEntityVector	mSources;					///< A vector describing the geometry of the sources
-	TTSpatEntityVector	mSinks;						///< A vector describing the geometry of the sinks (e.g., speakers)
-	TTSpatSnapRenderer	mRenderer;					///< The actual spatial renderer for this class
+	TTSpatEntityVector	mSources;		///< A vector describing the geometry of the sources
+	TTSpatEntityVector	mSinks;			///< A vector describing the geometry of the sinks (e.g., speakers)
+	TTSpatSnapRenderer	mRenderer;		///< The actual spatial renderer for this class
+
 	
-	/**	A standard audio processing method as used by TTBlue objects.*/
+	/**	A standard audio processing method as used by TTBlue objects.
+	 @param inputs						Incomming audio signals to process from sound sources.
+	 @param outputs						Processed audio signals passed to the sinks.
+	 */
 	TTErr processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs);
 
-	/**	Unit Tests	*/
+	/**	Unit Tests.	
+	 */
 	virtual TTErr test(TTValue& returnedTestInfo);
 	
 public:
 	
+	/** Get the number of sources.
+	 @param value						Used to return the number of sources.
+	 */
 	TTErr getSourceCount(TTValue& value);
 	
+	
+	/** Set the number of sources.
+	 @param value						The desired number of sources.
+	 */
 	TTErr setSourceCount(const TTValue& value);
 	
+	
+	/** Get the number of sinks.
+	 @param value						Used to return the number of sinks.
+	 */
 	TTErr getSinkCount(TTValue& value);
 	
+	
+	/** Set the number of sinks.
+	 @param value						The desired number of sinks.
+	 */
 	TTErr setSinkCount(const TTValue& value);
 	
-
+	
+	/** Get the position of one source.
+	 @param sourceNumber				The source (counting from 1) that we want to query the position of.
+	 @param x							Cartesian x-coordinate of the position.
+	 @param y							Cartesian y-coordinate of the position.
+	 @param z							Cartesian z-coordinate of the position.
+	 */
 	void getOneSourcePosition(TTInt32 sourceNumber, TTFloat64& x, TTFloat64& y, TTFloat64& z);
 	
+	
+	/** Get the position of one source.
+	 @param requestedChannel			The channel (counting from 1) that we want to query the position of.
+	 @param aPosition					Used to return the position.
+	 */
 	TTErr getSourcePosition(const TTValue& requestedChannel, TTValue& aPosition);
 	
+	
+	/** Set the position of one source.
+	 @param sourceNumber				The source (counting from 1) that we want to set the position of.
+	 @param x							Cartesian x-coordinate of the position.
+	 @param y							Cartesian y-coordinate of the position.
+	 @param z							Cartesian z-coordinate of the position.
+	 */
 	void setOneSourcePosition(TTInt32 sourceNumber, TTFloat64 x, TTFloat64 y, TTFloat64 z);
 
+	
+	/** Get the position of one source.
+	 @param requestedChannel			The channel (counting from 1) that we want to set the position of.
+	 @param aPosition					Used to return the position.
+	 */
 	TTErr setSourcePosition(const TTValue& aPosition, TTValue& unused);
 	
+	
+	/** Get the position of one sink.
+	 @param sourceNumber				The sink (counting from 1) that we want to query the position of.
+	 @param x							Cartesian x-coordinate of the position.
+	 @param y							Cartesian y-coordinate of the position.
+	 @param z							Cartesian z-coordinate of the position.
+	 */
 	void getOneSinkPosition(TTInt32 sinkNumber, TTFloat64& x, TTFloat64& y, TTFloat64& z);
 	
+	
+	/** Get the position of one sink.
+	 @param requestedChannel			The channel (counting from 1) that we want to query the position of.
+	 @param aPosition					Used to return the position.
+	 */
 	TTErr getSinkPosition(const TTValue& requestedChannel, TTValue& aPosition);
 
+	
+	/** Set the position of one sink.
+	 @param sourceNumber				The sink (counting from 1) that we want to set the position of.
+	 @param x							Cartesian x-coordinate of the position.
+	 @param y							Cartesian y-coordinate of the position.
+	 @param z							Cartesian z-coordinate of the position.
+	 */
 	void setOneSinkPosition(TTInt32 sinkNumber, TTFloat64 x, TTFloat64 y, TTFloat64 z);
 	
+	
+	/** Set the position of one sink.
+	 @param requestedChannel			The channel (counting from 1) that we want to set the position of.
+	 @param aPosition					Used to return the position.
+	 */
 	TTErr setSinkPosition(const TTValue& aPosition, TTValue& unused);
 	
 private:
 	
+	/** Unit tests of the getters and setters for the attributes sourceCont and sinkCount.
+	 */
 	TTErr testSourceAndSinkCountSetterAndGetter(int& testAssertionCount, int& errorCount, TTValue& returnedTestInfo);
 
+	
+	/** Unit tests for messages to set and get source positions"
+	 */
 	TTErr testSourcePositionSetterAndGetter(int& testAssertionCount, int& errorCount, TTValue& returnedTestInfo);
 	
+	
+	/** Unit tests for messages to set and get source positions"
+	 */
 	TTErr testSinkPositionSetterAndGetter(int& testAssertionCount, int& errorCount, TTValue& returnedTestInfo);
 	
+	
+	/** Unit test for checking that we have correct matrix coefficient values.
+	 */
 	TTErr testMatrixCoefficients(int& testAssertionCount, int& errorCount, TTValue& returnedTestInfo);
 	
+	
+	/** Unit test that pass multichannel audio to the object and test that we get correct audo for all sink channels.
+	 */
 	TTErr testAudioProcessing(int& testAssertionCount, int& errorCount, TTValue& returnedTestInfo);
 	
 };
