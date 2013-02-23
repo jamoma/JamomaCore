887c3f73ddf698663d8407e72dd69f7e8069b350
+1m place <tim@74objects.com>
2013-02-23 10:46:20 -0600

    TTClipper extension: whitespace

--- DSP/extensions/Clipper/TTClipper.cpp
+++ DSP/extensions/Clipper/TTClipper.cpp
@@ -16,41 +16,37 @@
 
 
 /**	Hard-clip signals to a low and high bound. */
+ 
 class Clipper : TTAudioObjectBase {
 	TTCLASS_SETUP(Clipper)
 
 	TTFloat64	mLowBound;		///< Attribute: low bound for clipping
 	TTFloat64	mHighBound;		///< Attribute: high bound for clipping
 	
-	
-	inline TTErr calculateValue(const TTFloat64& x, TTFloat64& y, TTPtrSizedInt channel)
+	TTErr calculateValue(const TTFloat64& x, TTFloat64& y, TTPtrSizedInt channel)
 	{
 		y = x;
 		TTLimit(y, mLowBound, mHighBound);
 		return kTTErrNone;
-	}
-	
+	}	
 	
 	/** Audio Processing Method */
 	TTErr processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs)
 	{
 		TT_WRAP_CALCULATE_METHOD(calculateValue);
 	}
-	
 };
-
 
 TT_AUDIO_CONSTRUCTOR_EXPORT(Clipper)
 {
-	addAttribute(LowBound,	kTypeFloat64);
-	addMessageProperty(LowBound,	description,	TT("Sets the minimum amplitude."));
+	addAttribute(		LowBound,	kTypeFloat64);
+	addMessageProperty(	LowBound,	description,	TT("Sets the minimum amplitude."));
 	
 	addAttribute(HighBound,	kTypeFloat64);
 	addMessageProperty(LowBound,	description,	TT("Sets the maximum amplitude."));
 	
 	setAttributeValue(TT("lowBound"), -1.0);	
 	setAttributeValue(TT("highBound"), 1.0);
-	
 	setProcessMethod(processAudio);	
 }
 
