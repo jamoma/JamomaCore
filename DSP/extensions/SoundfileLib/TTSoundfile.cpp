3cd6c70ed3fa63680056fd68d608f4de328c8b31
nwolek <nw@nathanwolek.com>
2013-06-08 16:01:26 +0200

    upgrading Doxygen headers and basic definitions

--- DSP/extensions/SoundfileLib/TTSoundfile.cpp
+++ DSP/extensions/SoundfileLib/TTSoundfile.cpp
@@ -1,9 +1,19 @@
-//
-//  TTSoundfile.cpp
-//  SoundfileLibIOS
-//
-//  Created by Nathan Wolek on 6/8/13.
-//  Copyright (c) 2013 org.jamoma. All rights reserved.
-//
+/** @file
+ *
+ * @ingroup dspSoundFileLib
+ *
+ * @brief Provides a common interface to soundfile data
+ *
+ * @details This object provides a common set of attributes and methods for working with soundfiles at a specific filepath.
+ * This allows us to access metadata and copy values in a common way without duplicating code. As with the rest of the
+ * SoundfileLib, the third-party <a href="http://www.mega-nerd.com/libsndfile/">libsndfile library</a> is used for actual
+ * access.
+ *
+ * @authors Nathan Wolek
+ *
+ * @copyright Copyright © 2013 by Nathan Wolek @n
+ * This code is licensed under the terms of the "New BSD License" @n
+ * http://creativecommons.org/licenses/BSD/
+ */
 
 #include "TTSoundfile.h"
--- DSP/extensions/SoundfileLib/TTSoundfile.h
+++ DSP/extensions/SoundfileLib/TTSoundfile.h
@@ -1,14 +1,35 @@
-//
-//  TTSoundfile.h
-//  SoundfileLibIOS
-//
-//  Created by Nathan Wolek on 6/8/13.
-//  Copyright (c) 2013 org.jamoma. All rights reserved.
-//
+/** @file
+ *
+ * @ingroup dspSoundFileLib
+ *
+ * @brief Provides a common interface to soundfile data
+ *
+ * @details This object provides a common set of attributes and methods for working with soundfiles at a specific filepath.
+ * This allows us to access metadata and copy values in a common way without duplicating code. As with the rest of the 
+ * SoundfileLib, the third-party <a href="http://www.mega-nerd.com/libsndfile/">libsndfile library</a> is used for actual
+ * access.
+ *
+ * @authors Nathan Wolek
+ *
+ * @copyright Copyright © 2013 by Nathan Wolek @n
+ * This code is licensed under the terms of the "New BSD License" @n
+ * http://creativecommons.org/licenses/BSD/
+ */
 
-#ifndef SoundfileLibIOS_TTSoundfile_h
-#define SoundfileLibIOS_TTSoundfile_h
+#ifndef __TT_SOUNDFILE_H__
+#define __TT_SOUNDFILE_H__
+
+#include "TTDSP.h"
+
+#ifdef uint
+#undef uint
+#endif
+#include "libsndfile/sndfile.h"
 
 
+
+
+//** Send a file path to the object and attempt to load the file *//
+TTErr TTSoundfileToSampleMatrix::setFilePath(const TTValue& newValue);
 
 #endif
