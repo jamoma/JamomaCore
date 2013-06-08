fc265c6298c35b4612c19d2917187882e4717e26
nwolek <nw@nathanwolek.com>
2013-06-08 16:00:42 +0200

    goal is to collect common features into new TTSoundfile class. beginning the process.

---
+++ DSP/extensions/SoundfileLib/TTSoundfile.cpp
@@ -0,0 +1,9 @@
+//
+//  TTSoundfile.cpp
+//  SoundfileLibIOS
+//
+//  Created by Nathan Wolek on 6/8/13.
+//  Copyright (c) 2013 org.jamoma. All rights reserved.
+//
+
+#include "TTSoundfile.h"
---
+++ DSP/extensions/SoundfileLib/TTSoundfile.h
@@ -0,0 +1,14 @@
+//
+//  TTSoundfile.h
+//  SoundfileLibIOS
+//
+//  Created by Nathan Wolek on 6/8/13.
+//  Copyright (c) 2013 org.jamoma. All rights reserved.
+//
+
+#ifndef SoundfileLibIOS_TTSoundfile_h
+#define SoundfileLibIOS_TTSoundfile_h
+
+
+
+#endif
