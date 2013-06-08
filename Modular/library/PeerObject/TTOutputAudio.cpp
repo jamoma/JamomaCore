7523c28b46b4e7c7c03d72341fd55a67eb9fcc0f
+1m place <tim@74objects.com>
2013-06-08 12:24:40 +0200

    Modular: refactoring of code for jcom.in~ and jcom.out~ into TTInputAudio and TTOutputAudio for better scoping and life-cycle management and more dry code.

--- Modular/library/PeerObject/TTOutputAudio.cpp
+++ Modular/library/PeerObject/TTOutputAudio.cpp
@@ -34,9 +34,70 @@ extern "C" void TTOutputAudio::registerClass()
 TTOutputAudio::TTOutputAudio(TTValue& arguments) :
 TTOutput(arguments)
 {
+	TTValue				args;
+	
+	mType = "audio";
+
+	// the only argument is the owner, which is used as a baton to hand to the callback
+	if (arguments.size())
+		mReturnSignalCallback = TTCallbackPtr((TTObjectBasePtr)arguments[0]);
+	if (arguments.size() > 1)
+		mReturnLinkCallback = TTCallbackPtr((TTObjectBasePtr)arguments[1]);
+	
+	TTObjectBaseInstantiate(kTTSym_audiosignal, &mSignalIn, 1);
+	TTObjectBaseInstantiate(kTTSym_audiosignal, &mSignalOut, 1);
+	TTObjectBaseInstantiate(kTTSym_audiosignal, &mSignalTemp, 1);
+	TTObjectBaseInstantiate(kTTSym_audiosignal, &mSignalZero, 1);
+	TTObjectBaseInstantiate(TTSymbol("crossfade"), &mMixUnit, 1);
+	mMixUnit->setAttributeValue(TTSymbol("position"), 1.0);
+	TTObjectBaseInstantiate(TTSymbol("gain"), &mGainUnit, 1);
+	mGainUnit->setAttributeValue(TTSymbol("linearGain"), 1.0);
+	TTObjectBaseInstantiate(TTSymbol("ramp"), &mRampMixUnit, 1);
+	TTObjectBaseInstantiate(TTSymbol("ramp"), &mRampGainUnit, 1);
 }
 
 
 TTOutputAudio::~TTOutputAudio()
 {
+	TTObjectBaseRelease(&mSignalIn);
+	TTObjectBaseRelease(&mSignalOut);
+	TTObjectBaseRelease(&mSignalTemp);
+	TTObjectBaseRelease(&mSignalZero);
+	TTObjectBaseRelease(&mMixUnit);
+	TTObjectBaseRelease(&mGainUnit);
+	TTObjectBaseRelease(&mRampMixUnit);
+	TTObjectBaseRelease(&mRampGainUnit);
 }
+
+
+void TTOutputAudio::process(TTSampleValue* anInputSampleVector, TTSampleValue* anOutputSampleVector, TTUInt16 aVectorSize)
+{
+	// Store the audio vector as a proper audio signal
+	TTAudioSignalPtr(mSignalIn)->setVector64Copy(0, aVectorSize, anInputSampleVector);
+	
+	// if the output signal is muted
+	if (mMute)
+		TTAudioSignal::copy(*TTAudioSignalPtr(mSignalZero), *TTAudioSignalPtr(mSignalOut));
+	
+	// if input signal exists
+	else if (mInputObject) {
+		
+		// if input signal is bypassed : copy input (in Temp)
+		if (mInputObject->mBypass)
+			TTAudioSignal::copy(*TTAudioSignalPtr(mInputObject->mSignalIn), *TTAudioSignalPtr(mSignalTemp));
+		
+		// otherwise mix input and output signals (in Temp)
+		else
+			TTAudioObjectBasePtr(mMixUnit)->process(TTAudioSignalPtr(mInputObject->mSignalOut), TTAudioSignalPtr(mSignalIn), TTAudioSignalPtr(mSignalTemp));
+		
+		// then perform gain control (from Temp)
+		TTAudioObjectBasePtr(mGainUnit)->process(TTAudioSignalPtr(mSignalTemp), TTAudioSignalPtr(mSignalOut));
+	}
+	// otherwise just perform gain control
+	else
+		TTAudioObjectBasePtr(mGainUnit)->process(TTAudioSignalPtr(mSignalIn), TTAudioSignalPtr(mSignalOut));
+	
+	// Send the input on to the outlets for the algorithm
+	TTAudioSignalPtr(mSignalOut)->getVectorCopy(0, aVectorSize, anOutputSampleVector);
+}
+
--- Modular/library/PeerObject/TTOutputAudio.h
+++ Modular/library/PeerObject/TTOutputAudio.h
@@ -17,6 +17,7 @@
 #define __TT_OUTPUT_AUDIO_H__
 
 #include "TTOutput.h"
+#include "TTInputAudio.h"
 
 /** An audio output component for Jamoma models.
  */
@@ -25,66 +26,31 @@ class TTMODULAR_EXPORT TTOutputAudio : public TTOutput
 	TTCLASS_SETUP(TTOutputAudio)
 	
 public:	// use public for quick acces during signal processing
-/*
-	TTSymbol					mType;						///< ATTRIBUTE : the type of signal output (like audio, video, ...)
-	TTAddress                   mInputAddress;				///< ATTRIBUTE : address of the input to bind
-	
-	TTBoolean					mMute;						///< ATTRIBUTE : to mute signal output
-	TTFloat32					mMix;						///< ATTRIBUTE : to mix signal output and  a signal coming from TTInput object
-	TTFloat32					mGain;						///< ATTRIBUTE : apply again on signal output
-	TTBoolean					mFreeze;					///< ATTRIBUTE : to freeze signal output (ex : for video signal)
-	TTBoolean					mPreview;					///< ??? : this attribute should not be here (it is relative to view part)
-	
-	TTObjectBasePtr				mSignalIn;					///< any data structure to receive complex signal
-	TTObjectBasePtr				mSignalOut;					///< any data structure to send complex signal
-	
-	TTObjectBasePtr				mSignalTemp;				///< a temporary signal
-	TTObjectBasePtr				mSignalZero;				///< a zero signal
-	
-	TTObjectBasePtr				mMixUnit;					///< the unit used to mix the signal
-	TTObjectBasePtr				mGainUnit;					///< the unit used to apply the gain value on the signal
-	
-	TTObjectBasePtr				mRampMixUnit;				///< the unit used to ramp the mix value
-	TTObjectBasePtr				mRampGainUnit;				///< the unit used to ramp the gain value
-	
-	TTInputPtr					mInputObject;				///< TTInput object to get a signal passing through
-	
-private:
-	
-	TTCallbackPtr				mReturnSignalCallback;		///< a way to return back signal to the owner of this output
-	TTCallbackPtr				mReturnLinkCallback;		///< a way to return back the input link status to the owner of this output
-	TTValue						mLast;						///< keep a copy of the last sent signal for each index
-	TTCallbackPtr				mAddressObserver;			///< to observe mInputAddress creation/destruction
-	
-	TTValue						mSignal;					///< ATTRIBUTE : a hidden attribute to observe signal preview
-	TTAttributePtr				mSignalAttr;				///< a direct access to the signal preview attribute to notify observers
-	*/
-	
-	/** Send signal */
-//	TTErr Send(const TTValue& inputValue, TTValue& outputValue);
-//	TTErr SendBypassed(const TTValue& inputValue, TTValue& outputValue);
-	
-	/** Set outputObject */
-//	TTErr Link(const TTValue& inputValue, TTValue& outputValue);
-	
-	/** Remove outputObject */
-//	TTErr Unlink();
-	
-	/** Set the outputAddress attribute */
-//	TTErr setInputAddress(const TTValue& value);
-	
-	/** Set the mute attribute */
-//	TTErr setMute(const TTValue& value);
-	
-	/** Set the mix attribute */
-//	TTErr setMix(const TTValue& value);
-	
-	/** Set the gain attribute */
-//	TTErr setGain(const TTValue& value);
-	
-	/** Notify signal observer */
-//	TTErr notifySignalObserver(const TTValue& value);
 
+	void process(TTSampleValue* anInputSampleVector, TTSampleValue* anOutputSampleVector, TTUInt16 aVectorSize);
+
+	
+	void setupAudioSignals(TTUInt16 aVectorSize, TTFloat64 aSampleRate)
+	{
+		mRampGainUnit->setAttributeValue(kTTSym_sampleRate, aSampleRate);	// convert midi to db for tap_gain
+		mGainUnit->setAttributeValue(TTSymbol("interpolated"), 1);
+		mRampMixUnit->setAttributeValue(kTTSym_sampleRate, aSampleRate);	// convert midi to db for tap_gain
+		
+		mSignalIn->setAttributeValue(kTTSym_numChannels, 1);
+		mSignalOut->setAttributeValue(kTTSym_numChannels, 1);
+		mSignalTemp->setAttributeValue(kTTSym_numChannels, 1);
+		mSignalZero->setAttributeValue(kTTSym_numChannels, 1);
+		
+		mSignalIn->setAttributeValue(kTTSym_vectorSize, aVectorSize);
+		mSignalOut->setAttributeValue(kTTSym_vectorSize, aVectorSize);
+		mSignalTemp->setAttributeValue(kTTSym_vectorSize, aVectorSize);
+		mSignalZero->setAttributeValue(kTTSym_vectorSize, aVectorSize);
+		
+		mSignalOut->sendMessage(kTTSym_alloc);
+		mSignalTemp->sendMessage(kTTSym_alloc);
+		mSignalZero->sendMessage(kTTSym_alloc);
+		mSignalZero->sendMessage(kTTSym_clear);
+	}
 };
 
 typedef TTOutputAudio* TTOutputAudioPtr;
