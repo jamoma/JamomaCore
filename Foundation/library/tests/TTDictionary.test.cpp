9211be00f61d6a68b1c0901eb7718402f59e3bf3
theod <theod@gmea.net>
2013-07-02 10:21:27 +0200

    Adding TTDictionary test files

---
+++ Foundation/library/tests/TTDictionary.test.cpp
@@ -0,0 +1,51 @@
+/** @file
+ *
+ * @ingroup foundationLibrary
+ *
+ * @brief #TTDictionaryTest is an class used for unit tests of the #TTDictionary class.
+ *
+ * @details
+ *
+ * @authors Théo de la Hogue, Tim Place
+ *
+ * @copyright Copyright © 2013, Théo de la Hogue, Tim Place @n
+ * This code is licensed under the terms of the "New BSD License" @n
+ * http://creativecommons.org/licenses/BSD/
+ */
+
+#include "TTDictionary.test.h"
+
+#define thisTTClass			TTDictionaryTest
+#define thisTTClassName		"dictionary.test"
+#define thisTTClassTags		"test, foundation"
+
+
+TT_OBJECT_CONSTRUCTOR
+{;}
+
+
+TTDictionaryTest::~TTDictionaryTest()
+{;}
+
+
+void TTDictionaryTest______(int& errorCount, int&testAssertionCount)
+{
+	TTTestLog("\n");
+	TTTestLog("Testing something");
+	
+	TTTestAssertion("TTDictionary: Test fails if ...",
+					NO,
+					testAssertionCount,
+					errorCount);
+}
+
+
+TTErr TTDictionaryTest::test(TTValue& returnedTestInfo)
+{
+	int	errorCount = 0;
+	int testAssertionCount = 0;
+	
+	TTDictionaryTest______(errorCount, testAssertionCount);
+		
+	return TTTestFinish(testAssertionCount, errorCount, returnedTestInfo);
+}
