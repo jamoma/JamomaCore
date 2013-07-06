9e45bfc7ce169b590e0ce96b8b7c76d231e29e4f
+1m place <tim@74objects.com>
2013-07-05 20:18:16 -0500

    adding unit test source file for the dataspace lib. see #131.

---
+++ Foundation/extensions/DataspaceLib/test.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  JamomaTest
+//
+//  Created by Timothy Place on 7/3/13.
+//
+//
+
+#include <iostream>
+#include "TTFoundationAPI.h"
+
+extern "C" TTErr TTLoadJamomaExtension_DataspaceLib(void);
+
+int main(int argc, const char * argv[])
+{
+	//	TTFoundationInit();
+	std::cout << "Hi, World!\n";
+	TTLoadJamomaExtension_DataspaceLib();
+	
+	TTValue classNames;
+	
+	TTObject::GetRegisteredClassNames(classNames);
+	
+	for (int i=0; i<classNames.size(); i++) {
+		TTSymbol name = classNames[i];
+		
+		try {
+			TTObject obj(name);
+			obj.send("test");
+		}
+		catch (...) {
+			TTLogMessage("UnitTest Failure to instantiate object of class %s! \n", name.c_str());
+			continue;
+		}
+	}
+	
+	// insert code here...
+	std::cout << "Goodbye, World!\n";
+    return 0;
+}
