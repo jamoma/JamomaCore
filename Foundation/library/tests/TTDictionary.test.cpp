13d4c7b4278957776277a95198c77813a5f818aa
theod <theod@gmea.net>
2013-07-11 14:03:02 +0200

    adding more basic tests on TTDictionary

--- Foundation/library/tests/TTDictionary.test.cpp
+++ Foundation/library/tests/TTDictionary.test.cpp
@@ -44,8 +44,18 @@ void TTDictionaryTestBasic(int& errorCount, int& testAssertionCount)
     // creation using a specific string name
     TTDictionary   d2("dictionary2");
     
+    TTTestAssertion("TTDictionary string name : Test fails if the dictionary name is not \"dictionary2\"",
+					d2.name() != TTSymbol("dictionary2"),
+					testAssertionCount,
+					errorCount);
+    
     // creation using a specific symbol name
     TTDictionary   d3(kTTSym_symbol);
+    
+    TTTestAssertion("TTDictionary symbol name : Test fails if the dictionary name is not \"symbol\"",
+					d2.name() != kTTSym_symbol,
+					testAssertionCount,
+					errorCount);
     
     
     
@@ -65,7 +75,7 @@ void TTDictionaryTestBasic(int& errorCount, int& testAssertionCount)
 					errorCount);
     
     
-    
+
     TTTestLog("\n");
 	TTTestLog("Testing dictionary value");
     
@@ -82,6 +92,53 @@ void TTDictionaryTestBasic(int& errorCount, int& testAssertionCount)
     
     TTTestAssertion("TTDictionary value : Test fails if the dictionary value is not kTTVal1",
 					v == kTTVal1,
+					testAssertionCount,
+					errorCount);
+    
+    
+    
+    TTTestLog("\n");
+	TTTestLog("Testing dictionary keys");
+    
+    d1.append(kTTSym_gain, kTTVal1);
+    TTErr err = d1.lookup(kTTSym_gain, v);
+    
+    TTTestAssertion("TTDictionary append key : Test fails if the dictionary key \"gain\" doesn't exist or its value is not kTTVal1",
+                    err == kTTErrNone &&
+					v == kTTVal1,
+					testAssertionCount,
+					errorCount);
+    
+    d1.getKeys(v);
+    TTSymbol k1 = v[0];
+    TTSymbol k2 = v[1];
+    TTSymbol k3 = v[2];
+    
+    TTTestAssertion("TTDictionary get keys : Test fails if the dictionary keys are not \"schema\", \"value\" and \"gain\" or the size is not 3",
+                    k1 == kTTSym_schema &&
+                    k2 == kTTSym_value &&
+                    k3 == kTTSym_gain &&
+                    d1.size() == 3,
+					testAssertionCount,
+					errorCount);
+    
+    d1.remove(kTTSym_gain);
+    d1.getKeys(v);
+    k1 = v[0];
+    k2 = v[1];
+    
+    TTTestAssertion("TTDictionary remove key : Test fails if the dictionary keys are not \"schema\" and \"value\" or the size is not 2",
+                    k1 == kTTSym_schema &&
+                    k2 == kTTSym_value  &&
+                    d1.size() == 2,
+					testAssertionCount,
+					errorCount);
+    
+    d1.clear();
+    
+    TTTestAssertion("TTDictionary clear keys : Test fails if the dictionary keys are not empty or the size is not 0",
+                    d1.empty() &&
+                    d1.size() == 0,
 					testAssertionCount,
 					errorCount);
 }
