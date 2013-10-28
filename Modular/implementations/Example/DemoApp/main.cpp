1c897b7066480d00ef0df772961f6ab79281ecfc
laugre <laurent@blueyeti.fr>
2013-10-28 19:15:20 +0100

    update c++ DemoApp

--- Modular/implementations/Example/DemoApp/main.cpp
+++ Modular/implementations/Example/DemoApp/main.cpp
@@ -1,14 +1,15 @@
 #include "TTModular.h"
-//
+
 //#include <iostream>
-//using namespace std;
-//
-//static TTApplicationPtr	mDemoApp = NULL;
-//static TTSymbol			kTTSym_DemoApp;
-//
+using namespace std;
+
+static TTApplicationPtr	mDemoApp = NULL;
+static TTSymbol			kTTSym_DemoApp;
+
 void	init();
-//TTErr	instantiate_Data(TTDataPtr *returnedData);
-//void	myData_return_value_callback(TTPtr baton, TTValue& v);
+void    runProtocol();
+TTErr	instantiate_Data(TTDataPtr *returnedData);
+void	myData_return_value_callback(TTPtr baton, TTValue& v);
 
 int 
 main(int argc, char **argv) 
@@ -17,9 +18,14 @@ main(int argc, char **argv)
 	
 	// Initialize Jamoma environment
 	/////////////////////////////////////////////////////////
+
 	TTLogMessage("/n*** Starting my DemoApp application *** /n");
-	
+
 	init();
+    
+    runProtocol();
+    
+    getchar();  // Wait until user hits "enter"
 
 //	test();
 
@@ -38,15 +44,22 @@ void init()
 	// Init the Modular library
 	TTModularInit();
     
-//    // Edit the path to the JamomaConfiguration.xml file
-//	const char *name = "DemoApp";
-//	const char *config = "JamomaConfiguration.xml";
-//    
-//    // Create a local application named DemoApp and get it
-//    TTModularCreateLocalApplication(name, config);
-//    mDemoApp = getLocalApplication;
-//	  kTTSym_DemoApp = TTSymbol(APP_NAME);
+    // Edit the path to the JamomaConfiguration.xml file
+	const char *name = "DemoApp";
+	const char *config = "JamomaConfiguration.xml";
+    
+    // Create a local application named DemoApp and get it
+    TTModularCreateLocalApplication(name, config);
+    mDemoApp = getLocalApplication;
+    kTTSym_DemoApp = TTSymbol("DemoApp");
 }
+
+void runProtocol()
+{
+    TTValue  v1, v2;
+    getProtocol("WebSocket")->Run(v1, v2);
+}
+
 
 //void test()
 //{
@@ -65,88 +78,88 @@ void init()
 //	
 //	testNodeLib->sendMessage(TTSymbol("test"), args);
 //}
-//
-//void createData()
-//{
-//	// Create a TTData object an use it
-//	/////////////////////////////////////////////////////////
-//	TTLogMessage("/n*** Instantiate myData as a TTData object ***/n");
-//	
-//	TTDataPtr myData = NULL;
-//	
-//	instantiate_Data(&myData);
-//
-//	// set TTData object attributes
-//	myData->setAttributeValue(kTTSym_type, kTTSym_decimal);
-//	myData->setAttributeValue(kTTSym_valueDefault, 0);
-//	myData->setAttributeValue(kTTSym_description, TTSymbol("a data for the demo"));
-//
-//	
-//	// Register a TTObject into the DemoApp directory
-//	/////////////////////////////////////////////////////////
-//	TTLogMessage("/n*** Register myData into the DemoApp directory ***/n");
-//	TTNodePtr		returnedNode;
-//	TTBoolean		newInstanceCreated;
-//
-//	getDirectoryFrom(kTTAdrsRoot)->TTNodeCreate(TTADRS("/mydata"), myData, NULL, &returnedNode, &newInstanceCreated);
-//
-//	// 
-//	// Use a TTData object
-//	/////////////////////////////////////////////////////////
-//
-//	
-//	// set the value
-//	do {
-//		TTString s;
-//		
-//		cout << "Set /myData value : ";
-//		cin >> s;
-//		
-//		if (s == "exit")
-//			break;
-//		
-//		v.clear();
-//		v = s;
-//		v.fromString();
-//		myData->sendMessage(kTTSym_Command, v);
-//	} while (true);
-//
-//
-//	// Delete every TTObject and TTValuePtr
-//	//////////////////////////////////////////////////////////////////
-//	TTObjectRelease(TTObjectHandle(&myData));
-//
-//}
-//
-//TTErr instantiate_Data(TTDataPtr *returnedData)
-//{
-//	TTValue			args;
-//	TTCallbackPtr	p_returnValueCallback = NULL;
-//	TTValuePtr		p_returnValueBaton;
-//	
-//	// prepare arguments : see TTData.h to know which args are needed
-//	TTObjectInstantiate(TTSymbol("callback"), TTObjectHandle(&p_returnValueCallback), kTTValNONE);
-//	p_returnValueBaton = new TTValue(NULL);
-//	p_returnValueCallback->setAttributeValue(TTSymbol("baton"), TTPtr(p_returnValueBaton));
-//	p_returnValueCallback->setAttributeValue(TTSymbol("function"), TTPtr(myData_return_value_callback));
-//	args.append(p_returnValueCallback);
-//	
-//	// create an instance of a TTData object
-//	return TTObjectInstantiate(TTSymbol("Data"), TTObjectHandle(returnedData), args);
-//}
-//
-//void myData_return_value_callback(TTPtr baton, TTValue& v)
-//{
-//	TTValuePtr	b;
-//	TTPtr		x;
-//	TTString	s;	
-//
-//	// unpack baton
-//	b = (TTValuePtr)baton;
-//	b->get(0, (TTPtr*)&x);
-//
-//	// print the returned value
-//	v.toString();
-//	v.get(0, s);
-//	TTLogMessage("myData has been updated to %s /n", s.data());
-//}
+
+void createData()
+{
+	// Create a TTData object an use it
+	/////////////////////////////////////////////////////////
+	TTLogMessage("/n*** Instantiate myData as a TTData object ***/n");
+	
+	TTDataPtr myData = NULL;
+	
+	instantiate_Data(&myData);
+
+	// set TTData object attributes
+	myData->setAttributeValue(kTTSym_type, kTTSym_decimal);
+	myData->setAttributeValue(kTTSym_valueDefault, 0);
+	myData->setAttributeValue(kTTSym_description, TTSymbol("a data for the demo"));
+
+	
+	// Register a TTObject into the DemoApp directory
+	/////////////////////////////////////////////////////////
+	TTLogMessage("/n*** Register myData into the DemoApp directory ***/n");
+	TTNodePtr		returnedNode;
+	TTBoolean		newInstanceCreated;
+
+	getDirectoryFrom(kTTAdrsRoot)->TTNodeCreate(TTADRS("/mydata"), myData, NULL, &returnedNode, &newInstanceCreated);
+
+	// 
+	// Use a TTData object
+	/////////////////////////////////////////////////////////
+
+	
+	// set the value
+	do {
+		TTString s;
+		
+		cout << "Set /myData value : ";
+		cin >> s;
+		
+		if (s == "exit")
+			break;
+		
+		v.clear();
+		v = s;
+		v.fromString();
+		myData->sendMessage(kTTSym_Command, v);
+	} while (true);
+
+
+	// Delete every TTObject and TTValuePtr
+	//////////////////////////////////////////////////////////////////
+	TTObjectRelease(TTObjectHandle(&myData));
+
+}
+
+TTErr instantiate_Data(TTDataPtr *returnedData)
+{
+	TTValue			args;
+	TTCallbackPtr	p_returnValueCallback = NULL;
+	TTValuePtr		p_returnValueBaton;
+	
+	// prepare arguments : see TTData.h to know which args are needed
+	TTObjectInstantiate(TTSymbol("callback"), TTObjectHandle(&p_returnValueCallback), kTTValNONE);
+	p_returnValueBaton = new TTValue(NULL);
+	p_returnValueCallback->setAttributeValue(TTSymbol("baton"), TTPtr(p_returnValueBaton));
+	p_returnValueCallback->setAttributeValue(TTSymbol("function"), TTPtr(myData_return_value_callback));
+	args.append(p_returnValueCallback);
+	
+	// create an instance of a TTData object
+	return TTObjectInstantiate(TTSymbol("Data"), TTObjectHandle(returnedData), args);
+}
+
+void myData_return_value_callback(TTPtr baton, TTValue& v)
+{
+	TTValuePtr	b;
+	TTPtr		x;
+	TTString	s;
+
+	// unpack baton
+	b = (TTValuePtr)baton;
+	b->get(0, (TTPtr*)&x);
+
+	// print the returned value
+	v.toString();
+	v.get(0, s);
+	TTLogMessage("myData has been updated to %s /n", s.data());
+}
