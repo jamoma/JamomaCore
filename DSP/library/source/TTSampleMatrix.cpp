6ac0acf97960e7942c9bd75e965867b5d5a0048c
nwolek <nw@nathanwolek.com>
2013-12-19 09:01:46 -0500

    TTSampleMatrix - adding load message that I apparently forgot

--- DSP/library/source/TTSampleMatrix.cpp
+++ DSP/library/source/TTSampleMatrix.cpp
@@ -59,6 +59,8 @@ TTSampleMatrix::TTSampleMatrix(const TTValue& arguments) :
 
 	addMessageWithArguments(setValueAtIndex);
 	registerMessage("poke", (TTMethod)&TTSampleMatrix::setValueAtIndex);
+    
+    addMessageWithArguments(load);
 
 	// TODO: more messages to implement
 	//	"readFile"   (requires libsndfile straightening-out)
