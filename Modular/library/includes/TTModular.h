7b774d2b2aa96e393c57c954c36788b52aced352
theod <theod@gmea.net>
2014-02-03 14:20:44 +0100

    More work on TTModularAPI

--- Modular/library/includes/TTModular.h
+++ Modular/library/includes/TTModular.h
@@ -51,7 +51,7 @@ public:
         @param binaries                 path to the Jamoma libraries and extensions binaries folder to load them */
 	Modular(const char* binaries = NULL);
     
-    /** Destructor : release all local datas registered and all receivers */
+    /** Destructor : release all local datas registered and all senders and receivers */
 	~Modular(void);
     
 #if 0
@@ -62,33 +62,52 @@ public:
     /** Declare its application as the Modular local application
      @param applicationName             the name used to declare its application
      @param applicationVersion          the version of its application
-     @param applicationCreator          the creator name of its application
-     @param applicationConfiguration    the path to the xml configuration file
-     */
+     @param applicationAuthor           the author name of its application
+     @param applicationConfiguration    the path to the xml configuration file 
+     @return kTTErrGeneric if the application cannot be created */
     TTErr   ApplicationCreateLocal(const string applicationName,
                                    const string applicationVersion,
-                                   const string applicationCreator,
+                                   const string applicationAuthor,
                                    const string applicationConfiguration = "");
     
-    /** */
+    /** Declare another application as a Modular distant application
+     @param applicationName             the name used to declare the application
+     @return kTTErrGeneric if the application cannot be created */
     TTErr   ApplicationCreateDistant(const string applicationName);
     
-    /** Write an application namespace in console */
-    TTErr   ApplicationDumpDirectory(const string applicationName);
+    /** Write an application namespace in console 
+     @param applicationName             any application name 
+     @return kTTErrGeneric if the application namespace cannot be dumpr */
+    TTErr   ApplicationDump(const string applicationName);
     
-    /** Save the namespace in a xml file */
+    /** Save the namespace in a xml file. This function doesn't write senders and receivers configuration.
+     @param filepath             the path where to save the namespace
+     @return kTTErrInvalidFilepath if the file cannot be written at the given path, kTTErrGeneric for any other reason */
 	TTErr   ApplicationWriteAsXml(const string filepath);
     
-	/** Load and build a namespace tree by parsing a xml file
-     Note that namespace observers are not created */
+	/** Load and build a namespace tree by parsing a xml file. This function doesn't read senders and receivers configuration.
+     @param filepath             the path to the file from where to load the namespace
+     @return kTTErrInvalidFilepath if the file cannot be read at the given path, kTTErrGeneric for any other reasons */
 	TTErr   ApplicationReadFromXml(const string filepath);
     
-    /** */
+    /** Set the value of an application
+     @param applicationName             any application name 
+     @param attribute                   the attribute name to set :
+                                            - "version" : a string value to change the version
+                                            - "author"  : a string value to change the author
+                                            - "activity": a boolean value to enable the activity mechanism before to observe "/:activity/in" or "/:actvity/out" with a receiver
+     @return kTTErrInvalidAttribute if the attribute doesn't exist, kTTErrGneric for any other reasons */
     TTErr   ApplicationAttributeSet(const string applicationName,
                                     const string attribute,
                                     const TTValue& value);
     
-	/** */
+	/** Get the value of an application
+     @param applicationName             any application name
+     @param attribute                   the attribute name to get :
+                                            - "version" : a string value to change the version
+                                            - "author"  : a string value to change the author
+                                            - "activity": a boolean value to enable the activity mechanism before to observe "/:activity/in" or "/:actvity/out" with a receiver 
+     @return kTTErrInvalidAttribute if the attribute doesn't exist, kTTErrGneric for any other reasons */
 	TTErr   ApplicationAttributeGet(const string applicationName,
                                     const string attribute,
                                     TTValue& value);
@@ -98,28 +117,48 @@ public:
 #pragma mark Protocols management
 #endif
     
-    /** */
+    /** Get all loaded protocol names 
+     @param names                       the returned protocol names
+     @return kTTErrGeneric if no protocol have been loaded */
     TTErr   ProtocolGetNames(TTValue& names);
     
-    /** */
+    /** Register an application to a protocol 
+     @param protocolName                the protocol to use 
+     @param applicationName             the application to register 
+     @return kTTErrGeneric if the application cannot be registered */
     TTErr   ProtocolRegisterApplication(string protocolName, string applicationName);
     
-    /** */
+    /** Unregister an application to a protocol
+     @param protocolName                the protocol to not use anymore
+     @param applicationName             the application to unregister 
+     @return kTTErrGeneric if the application cannot be unregistered */
     TTErr   ProtocolUnregisterApplication(string protocolName, string applicationName);
     
-    /** */
+    /** Get all attributes of a protocol
+     @param names                       the returned attribute names
+     @return kTTErrGeneric if the protocol have no attribute */
     TTErr   ProtocolGetAttributeNames(TTValue& names);
     
-    /** */
-    bool    ProtocolIsAttributeNumeric(string attribute);
+    /** Ask if a protocol attribute is an integer
+     @param attribute                   an attribute name
+     @return true if the attribute is an integer */
+    bool    ProtocolIsAttributeInteger(string attribute);
     
-    /** */
+    /** Ask if a protocol attribute is a string
+     @param attribute                   an attribute name
+     @return true if the attribute is a string */
     bool    ProtocolIsAttributeString(string attribute);
 
-    /** */
-    TTErr   ProtocolSetNumericAttribute(string applicationName, string attribute, int value);
+    /** Set a protocol integer attribute
+     @param attribute                   an attribute name
+     @param value                       an integer value
+     @return kTTErrInvalidAttribute if the attribute doesn't exist, kTTErrGeneric for any other reasons */
+    TTErr   ProtocolSetIntegerAttribute(string applicationName, string attribute, int value);
     
-    /** */
+    /** Set a protocol string attribute
+     @param attribute                   an attribute name
+     @param value                       an string value
+     @return kTTErrInvalidAttribute if the attribute doesn't exist, kTTErrGeneric for any other reasons */
     TTErr   ProtocolSetStringAttribute(string applicationName, string attribute, string value);
 
 #if 0
@@ -137,58 +176,87 @@ public:
      @param service                     a data can be "parameter", "message" or a "return" :
                                             - "parameter" handles the state of the application (e.g. a position or a gain).
                                             - "message" handles a command of the application (e.g. a play, a stop).
-                                            - "return" handles a notification of the application (e.g. a fps, the end of a loop). */
+                                            - "return" handles a notification of the application (e.g. a fps, the end of a loop). 
+     @return kTTErrGeneric if the data cannot be created */
 	TTErr   DataCreate(string address,
                        void (*returnValueCallback) (void*, const TTValue&) = NULL,
                        void* owner = NULL,
                        string service = "parameter");
     
+    /** Delete a data object registered under an address into the local or a distant application
+     @param address                     address of the data into an application directory :
+                                            - /any/level/name.instance to create data into the local application
+                                            - /any/level/name.[N] to create N instances of this data under N different addresses /any/level/name.1, /any/level/name.2, ..., /any/level/name.N
+                                            - distantApp:/any/level/name.instance to create a data into the distantApp application 
+     @return kTTErrGeneric if the data cannot be deleted */
     TTErr   DataDelete(string address);
     
-    /** Set the value of a data attribute (usefull to init it)
+    /** Set the value of a data attribute
      @param address                     address of the data into an application directory :
                                             - /any/level/name.instance to acces to a data into the local application
                                             - /any/level/name.[N] to acces to N data instances registered under N different addresses /any/level/name.1, /any/level/name.2, ..., /any/level/name.N
                                             - distantApp:/any/level/name.instance to access to a data into the distantApp application
-     @param attribute		:           the attribute name to set :
+     @param attribute                   the attribute name to set :
+                                            - "value"                   :   set the data value
+                                            - "valueDefault"            :   set the default value recalled on "Init" method
+                                            - "valueStepsize"           :   amount to increment or decrement by
      
-                                            "value"         :   set the data value
-                                            "valueDefault"  :   set the default value recalled on "Init" method
-                                            "valueStepsize" :   amount to increment or decrement by
+                                            - "type"                    :   type of this data's value ("none", "generic" (default), "integer", "decimal", "string", "array")
+                                            - "tag"                     :   tag list for this data
+                                            - "priority"                :   does this data have a priority over other datas ? (0 means no priority then 1 is the highest priority)
+                                            - "description"             :   text to describe the role of this data
+                                            - "repetitionsFilter"       :   is repetitions are filtered out ?
+                                            - "active"                  :   to stop notifying any value changes
+                                            - "initialized"             :   is the value attribute has been initialized ?
      
-                                            "type";						///< ATTRIBUTE: type of this data's value
-     TTValue			mTag;						///< ATTRIBUTE: tag list for this data
-     TTInt32			mPriority;					///< ATTRIBUTE: does this data have a priority over other datas ?
-     TTSymbol		mDescription;				///< ATTRIBUTE: text to describe the role of this data
-     TTBoolean		mRepetitionsFilter;			///< ATTRIBUTE: is repetitions are filtered out ?
-     TTBoolean		mActive;					///< ATTRIBUTE: this used by return only to stop sending values
-     TTBoolean		mInitialized;				///< ATTRIBUTE: is the Value attribute has been initialized ?
+                                            - "rangeBounds"             :   low and a high bounds values
+                                            - "rangeClipmode"           :   behavior at the bounds ("low", "high", "wrap", "fold")
      
-     TTValue			mRangeBounds;				///< ATTRIBUTE:
-     TTSymbol		mRangeClipmode;				///< ATTRIBUTE:
+                                            - "rampDrive"               :   the scheduler unit to use for the ramp ("none or "System")
+                                            - "rampFunction"            :   the function used by the ramping
+                                            - "rampFunctionParameters"  :   names of parameter's function
+                                            - "rampStatus"              :   is the ramp running ?
      
-     TTBoolean		mDynamicInstances;			///< ATTRIBUTE: is the data can be dynamically instanciated
-     TTValue			mInstanceBounds;			///< ATTRIBUTE: two TTValues for a range of dynamic instances (-1 = infini)
+                                            - "dynamicInstances"        :   is the data can be dynamically instanciated ?
+                                            - "instanceBounds"          :   low and a high bounds values to range dynamic instances (-1 means no bounds)
      
-     TTSymbol		mRampDrive;					///< ATTRIBUTE: ramp mode
-     #ifndef TT_NO_DSP
-     TTSymbol		mRampFunction;				///< ATTRIBUTE: for setting the function used by the ramping
-     #endif
-     TTValue			mRampFunctionParameters;	///< ATTRIBUTE: names of parameter's function
-     TTBoolean		mRampStatus;				///< ATTRIBUTE: is the ramp running ?
-
-                                            
-     @param value			:	the value to set */
+     @param value                       the value to set 
+     @return kTTErrInvalidAttribute if the attribute doesn't exist, kTTErrGeneric for any other reasons */
 	TTErr   DataAttributeSet(const string address,
                              const string attribute,
                              const TTValue& value);
     
-	/** Get the value of a parameter attribute
-     address			:	osc like address of the parameter data in the namespace directory
-     application name is automatically added at the beginning of the address : /"appName"
-     instance		:	number of the instance you want to get the value
-     attribute		:	symbol representing the attribute name to set (attribute symbols are defined on top)
-     value			:	the value that will be got */
+	/** Get the value of a data attribute
+     @param address                     address of the data into an application directory :
+                                            - /any/level/name.instance to acces to a data into the local application
+                                            - /any/level/name.[N] to acces to N data instances registered under N different addresses /any/level/name.1, /any/level/name.2, ..., /any/level/name.N
+                                            - distantApp:/any/level/name.instance to access to a data into the distantApp application
+     @param attribute                   the attribute name to get :
+                                            - "value"                   :   set the data value
+                                            - "valueDefault"            :   set the default value recalled on "Init" method
+                                            - "valueStepsize"           :   amount to increment or decrement by
+     
+                                            - "type"                    :   type of this data's value ("none", "generic" (default), "integer", "decimal", "string", "array")
+                                            - "tag"                     :   tag list for this data
+                                            - "priority"                :   does this data have a priority over other datas ? (0 means no priority then 1 is the highest priority)
+                                            - "description"             :   text to describe the role of this data
+                                            - "repetitionsFilter"       :   is repetitions are filtered out ?
+                                            - "active"                  :   to stop notifying any value changes
+                                            - "initialized"             :   is the value attribute has been initialized ?
+     
+                                            - "rangeBounds"             :   low and a high bounds values
+                                            - "rangeClipmode"           :   behavior at the bounds ("low", "high", "wrap", "fold")
+     
+                                            - "rampDrive"               :   the scheduler unit to use for the ramp ("none or "System")
+                                            - "rampFunction"            :   the function used by the ramping
+                                            - "rampFunctionParameters"  :   names of parameter's function
+                                            - "rampStatus"              :   is the ramp running ?
+     
+                                            - "dynamicInstances"        :   is the data can be dynamically instanciated ?
+                                            - "instanceBounds"          :   low and a high bounds values to range dynamic instances (-1 means no bounds)
+     
+     @param value                       the returned attribute value
+     @return kTTErrInvalidAttribute if the attribute doesn't exist, kTTErrGeneric for any other reasons */
 	TTErr   DataAttributeGet(const string address,
                              const string attribute,
                              TTValue& value);
