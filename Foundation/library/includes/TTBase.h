6c71d64c131b43914934186f8b95755250e309bc
timothy place <tim@74objects.com>
2014-03-23 21:43:15 -0500

    TTBase: adding the TTAddress type -- that way we can properly store these in TTValue instead of using the hack currently employed

--- Foundation/library/includes/TTBase.h
+++ Foundation/library/includes/TTBase.h
@@ -277,6 +277,7 @@ enum TTDataType {
 	kTypeLocalValue,	///< This is a special type used by TTAttribute to indicate that a value is a TTValue and is locally maintained.
 	kTypeMatrix,		///< An instance of a TTMatrix object
 	kTypeDictionary,
+	kTypeAddress,
 	kNumTTDataTypes
 };
 
