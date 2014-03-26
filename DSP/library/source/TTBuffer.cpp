5d958a4c60f986566b2956291248a3242df2a211
timothy place <tim@74objects.com>
2014-03-25 20:18:19 -0500

    TTBuffer: build fix because the assignment to/from TTValue for TTObjects must use real TTObjects now and not the old API where ambiguous pointers were used. @nwolek could you please give this commit a quick code review to make sure I didn't mess something up? Thanks.

--- DSP/library/source/TTBuffer.cpp
+++ DSP/library/source/TTBuffer.cpp
@@ -221,16 +221,16 @@ TTErr TTBuffer::checkInMatrix(TTSampleMatrixPtr& doneUsingThisMatrix)
 
 TTErr TTBuffer::checkInMatrixValues(const TTValueRef input, const TTValueRef unusedOutput)
 {
-    TTObjectBasePtr doneUsingThisObject = input[0];
+    TTObject doneUsingThisObject = input[0];
     
     // let's make sure input is a samplematrix first
-    if (doneUsingThisObject->getName() != TT("samplematrix"))
+    if (doneUsingThisObject.name() != TT("samplematrix"))
     {
         // if no: return an error
         return kTTErrInvalidValue;
     } else {
         // if yes: get a generic TTPtr, then cast to TTSampleMatrixPtr
-        TTSampleMatrixPtr doneUsingThisMatrix = (TTSampleMatrixPtr)(TTPtr(doneUsingThisObject));
+        TTSampleMatrixPtr doneUsingThisMatrix = (TTSampleMatrixPtr)doneUsingThisObject.instance();
         return checkInMatrix(doneUsingThisMatrix);
     }
     
