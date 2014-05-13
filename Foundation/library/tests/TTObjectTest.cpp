3be51483264356778d9c3677c023cf9dd1a9aa9a
theod <theod@gmea.net>
2014-05-13 23:15:08 +0200

    Adding tests for TTObject reference count mechanism

--- Foundation/library/tests/TTObjectTest.cpp
+++ Foundation/library/tests/TTObjectTest.cpp
@@ -93,6 +93,111 @@ void TTObjectTestWithTTValue(int& errorCount, int& testAssertionCount)
 }
 
 
+void TTObjectTestIterateTTList(int& errorCount, int& testAssertionCount)
+{
+    TTTestLog("\n");
+	TTTestLog("Testing iteration on TTObjects stored inside a TTList");
+    
+    // create an object
+	TTObject callback("callback");
+    
+    TTTestAssertion("1 object -- refcount should be 1",
+					callback.instance()->getReferenceCount() == 1,
+					testAssertionCount,
+					errorCount);
+	
+    // store the object into a list
+    TTList testList;
+	testList.appendUnique(callback);
+    
+    TTTestAssertion("1 object and 1 stored inside the list -- refcount should be 2",
+					callback.instance()->getReferenceCount() == 2,
+					testAssertionCount,
+					errorCount);
+    
+    // iterate the list to send it a message
+    TTValue v;
+    testList.iterateObjectsSendingMessage("nothing", v);
+    
+    TTTestAssertion("1 object and 1 stored inside the list -- refcount should be 2",
+					callback.instance()->getReferenceCount() == 2,
+					testAssertionCount,
+					errorCount);
+    
+    // clear the list
+    testList.clear();
+    
+    TTTestAssertion("1 object -- refcount should be 1",
+					callback.instance()->getReferenceCount() == 1,
+					testAssertionCount,
+					errorCount);
+}
+
+
+void TTObjectTestAsClassMemberCallback(const TTValue& baton, const TTValue& value)
+{
+    int *errorCount = (int*)TTPtr(baton[0]);
+    int *testAssertionCount = (int*)TTPtr(baton[1]);
+    TTObject anObject = value[0];
+    
+    TTTestAssertion("no more object as class member -- should be not valid",
+                    !anObject.instance()->valid,
+                    *testAssertionCount,
+                    *errorCount);
+}
+
+void TTObjectTestAsClassMemberAndObjectFreeing(int& errorCount, int& testAssertionCount)
+
+{
+    TTTestLog("\n");
+	TTTestLog("Testing TTObject as class member and object freeing observation");
+    
+    // create a Test instance
+    Test* testInstance = new Test(errorCount, testAssertionCount);
+    
+    // register for objectFreeing notification
+    TTObject    callback("callback");
+    TTValue     v = TTValue(TTPtr(&errorCount), TTPtr(&testAssertionCount));
+    callback.set("baton", v);
+    callback.set("function", TTPtr(TTObjectTestAsClassMemberCallback));
+    callback.set("notification", "objectFreeing");
+    
+    testInstance->mObject.registerObserverForNotifications(callback);
+    
+    // now the observer should be referenced also by the observers list of Test::mObject
+    TTTestAssertion("1 observer on a TTObject class member -- refcount should be 2",
+                    callback.instance()->getReferenceCount() == 2,
+                    testAssertionCount,
+                    errorCount);
+    
+    // don't unregister for objectFreeing notification as we need to be notified when the object is freeing
+    
+    // delete the Test instance and its mObject (and we expect the Test::mObject to forget its observers)
+    delete testInstance;
+    
+    // now the observer should be referenced ony one time
+    TTTestAssertion("no more observer on a TTObject class member -- refcount should be 1",
+                    callback.instance()->getReferenceCount() == 1,
+                    testAssertionCount,
+                    errorCount);
+}
+
+
+Test::Test(int& errorCount, int& testAssertionCount) :
+mObject("callback")
+{
+    TTTestAssertion("1 object as class member -- refcount should be 1",
+                    mObject.instance()->getReferenceCount() == 1,
+                    testAssertionCount,
+                    errorCount);
+}
+
+Test::~Test()
+{
+    ; // we do nothing here as we expect the mObject member will be destroyed with the instance of the Test class
+}
+
+
 TTErr TTObjectTest::test(TTValue& returnedTestInfo)
 {
 	int	errorCount = 0;
@@ -100,6 +205,8 @@ TTErr TTObjectTest::test(TTValue& returnedTestInfo)
 	
 	TTObjectTestBasic(errorCount, testAssertionCount);
 	TTObjectTestWithTTValue(errorCount, testAssertionCount);
+    TTObjectTestIterateTTList(errorCount, testAssertionCount);
+    TTObjectTestAsClassMemberAndObjectFreeing(errorCount, testAssertionCount);
 	
 	return TTTestFinish(testAssertionCount, errorCount, returnedTestInfo);
 }
