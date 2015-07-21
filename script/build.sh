5c7bd47e48904464ea28d6231123dce89a113da2
Antoine Villeret <antoine.villeret@gmail.com>
2015-07-21 17:27:34 +0200

    use container based infrastructure to speed up the build
    Signed-off-by: Antoine Villeret <antoine.villeret@gmail.com>

--- script/build.sh
+++ script/build.sh
@@ -2,11 +2,14 @@
 
 mkdir -p build
 cd build
+if [ "x$TRAVIS_OS_NAME" = "xlinux" ]; then
+  export CC=gcc-4.9
+  export CXX=g++-4.9
+fi
+
 if [ "x$ARCH" = "xrpi" ]; then
-	if [ "$TRAVIS_OS_NAME" != "osx" ]; then
-		/tmp/cmake/bin/cmake -DCMAKE_TOOLCHAIN_FILE=../Shared/CMake/toolchains/arm-linux-gnueabihf.cmake -DCROSS_COMPILER_PATH=${PWD}/../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/ ..
-		make
-	fi
+  /tmp/cmake/bin/cmake -DCMAKE_TOOLCHAIN_FILE=../Shared/CMake/toolchains/arm-linux-gnueabihf.cmake -DCROSS_COMPILER_PATH=${PWD}/../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/ ..
+	make
 else
 	/tmp/cmake/bin/cmake ..
 	make
