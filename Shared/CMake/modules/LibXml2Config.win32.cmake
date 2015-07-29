7b845f9e3d63a0c5fc32ccfb6893f784a2787b10
Antoine Villeret <antoine.villeret@gmail.com>
2015-07-29 13:25:39 +0200

    try to fix libxml2 search

--- Shared/CMake/modules/LibXml2Config.win32.cmake
+++ Shared/CMake/modules/LibXml2Config.win32.cmake
@@ -8,44 +8,54 @@ find_path(LIBXML2_INCLUDE_DIR NAMES libxml/xpath.h
 	PATHS 
 	"${WIN32_LIBXML2_FOLDER}//include"
 	PATH_SUFFIXES libxml2)
-
+message("LIBXML2_INCLUDE_DIR : ${LIBXML2_INCLUDE_DIR}")
 # Find the correct .lib / .dll.
 set(CMAKE_FIND_LIBRARY_SUFFIXES ".lib")
 if(MINGW)
-   find_library(LIBXML2_LIBRARIES NAMES libxml2 xml2
+   find_library(LIBXML2_LIBRARIES NAMES libxml2
                 PATHS "${WIN32_LIBXML2_FOLDER}//libmingw")
 	set(LIBXML2_DLL "${WIN32_LIBXML2_FOLDER}//libmingw//libxml2.dll")
 elseif(WIN64)
+		message("looking for 64bit XML library")
 	if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-	    find_library(LIBXML2_LIBRARIES NAMES libxml2 xml2
+	    find_library(LIBXML2_LIBRARIES NAMES libxml2
 					 PATHS "${WIN32_LIBXML2_FOLDER}//lib64//debug")
 		set(LIBXML2_DLL "${WIN32_LIBXML2_FOLDER}//lib64//debug//libxml2.dll")
 	else()
-	    find_library(LIBXML2_LIBRARIES NAMES libxml2 xml2
+	    find_library(LIBXML2_LIBRARIES NAMES libxml2
 					 PATHS "${WIN32_LIBXML2_FOLDER}//lib64//release")
 		set(LIBXML2_DLL "${WIN32_LIBXML2_FOLDER}//lib64//release//libxml2.dll")
 	endif()
 	
 elseif(WIN32)
-   find_library(LIBXML2_LIBRARIES_DEBUG_STATIC NAMES libxml2_s xml2_s
+	message("looking for 32bit XML library")
+	message("LIBXML2_LIBRARIES ${LIBXML2_LIBRARIES}")
+   find_library(LIBXML2_LIBRARIES_DEBUG_STATIC NAMES libxml2_s
 		PATHS 
 		"${WIN32_LIBXML2_FOLDER}//lib//debug")
-	find_library(LIBXML2_LIBRARIES_RELEASE_STATIC NAMES libxml2_s xml2_s
+   message("LIBXML2_LIBRARIES_DEBUG_STATIC ${LIBXML2_LIBRARIES_DEBUG_STATIC}")
+	find_library(LIBXML2_LIBRARIES_RELEASE_STATIC NAMES libxml2_s
 		PATHS 
 		"${WIN32_LIBXML2_FOLDER}//lib//release")
-   find_library(LIBXML2_LIBRARIES_DEBUG_DYNAMIC NAMES libxml2 xml2
+   message("LIBXML2_LIBRARIES_RELEASE_STATIC ${LIBXML2_LIBRARIES_RELEASE_STATIC}")
+   find_library(LIBXML2_LIBRARIES_DEBUG_DYNAMIC NAMES libxml2
 		PATHS 
 		"${WIN32_LIBXML2_FOLDER}//lib//debug")
-	find_library(LIBXML2_LIBRARIES_RELEASE_DYNAMIC NAMES libxml2 xml2
+   message("LIBXML2_LIBRARIES_DEBUG_DYNAMIC ${LIBXML2_LIBRARIES_DEBUG_DYNAMIC}")
+	find_library(LIBXML2_LIBRARIES_RELEASE_DYNAMIC NAMES libxml2
 		PATHS 
 		"${WIN32_LIBXML2_FOLDER}//lib//release")
+   message("LIBXML2_LIBRARIES_RELEASE_DYNAMIC ${LIBXML2_LIBRARIES_RELEASE_DYNAMIC}")
 	if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-		set(LIBXML2_LIBRARIES ${LIBXML2_LIBRARIES_DEBUG_DYNAMIC})
+		set(LIBXML2_LIBRARIES ${LIBXML2_LIBRARIES_DEBUG_STATIC})
 		set(LIBXML2_DLL "${WIN32_LIBXML2_FOLDER}//lib//debug//libxml2.dll")
 	else()
-		set(LIBXML2_LIBRARIES ${LIBXML2_LIBRARIES_RELEASE_DYNAMIC})
+		set(LIBXML2_LIBRARIES ${LIBXML2_LIBRARIES_RELEASE_STATIC})
 		set(LIBXML2_DLL "${WIN32_LIBXML2_FOLDER}//lib//release//libxml2.dll")
 	endif()
+
+	
+	message("after search : LIBXML2_LIBRARIES ${LIBXML2_LIBRARIES}")
 	
 endif()
 
