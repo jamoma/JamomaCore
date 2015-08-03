62ed0f20947f6d0be6bee1ec24d3664c321fc16e
Trond Lossius <trond.lossius@bek.no>
2015-08-03 14:51:31 +0200

    Fixing Xcode project

--- DSP/extensions/SpatLib/source/TTSpatBaseRenderer.cpp
+++ DSP/extensions/SpatLib/source/TTSpatBaseRenderer.cpp
@@ -1,9 +1,88 @@
-//
-//  TTSpatBaseRenderer.cpp
-//  SpatLib
-//
-//  Created by lossius on 03/08/15.
-//  Copyright (c) 2015 org.jamoma. All rights reserved.
-//
+/** @file
+ *
+ * @ingroup dspSpatLib
+ *
+ * @brief Base class for Spat renderers
+ *
+ * @details
+ *
+ * @authors Trond Lossius, Nils Peters, Timothy Place, Jan Schacher
+ *
+ * @copyright Copyright © 2011 by Trond Lossius, Nils Peters, Timothy Place, Jan Schacher @n
+ * This code is licensed under the terms of the "New BSD License" @n
+ * http://creativecommons.org/licenses/BSD/
+ */
+
 
 #include "TTSpatBaseRenderer.h"
+
+
+
+
+/// Methods for TTSpatSnapRenderer: //////////////////////
+
+TTSpatBaseRenderer::TTSpatBaseRenderer():
+mMixerMatrixCoefficients(NULL)
+{
+	// TODO: Must set up pointer to associated TTSpat class
+	TTObjectBaseInstantiate("samplematrix", (TTObjectBasePtr*)&mMixerMatrixCoefficients, kTTValNONE);
+}
+
+
+TTSpatBaseRenderer::~TTSpatBaseRenderer()
+{
+	TTObjectBaseRelease((TTObjectBasePtr*)&mMixerMatrixCoefficients);
+}
+
+
+TTErr TTSpatBaseRenderer::processAudio(TTAudioSignalArrayPtr inputs, TTAudioSignalArrayPtr outputs)
+{
+	TTAudioSignal&		in = inputs->getSignal(0);
+	TTAudioSignal&		out = outputs->getSignal(0);
+	TTUInt16			vs = in.getVectorSizeAsInt();
+	TTSampleValuePtr	inSample;
+	TTSampleValuePtr	outSample;
+	TTChannelCount		numInputChannels = in.getNumChannelsAsInt();
+	TTChannelCount		numOutputChannels = out.getNumChannelsAsInt();
+	TTChannelCount		outChannel;
+	TTChannelCount		inChannel;
+	TTSampleValue       gainValue;
+	
+	TTInt16				sourceCount = mMixerMatrixCoefficients->getRowCount();
+	TTInt16				sinkCount	= mMixerMatrixCoefficients->getColumnCount();
+	
+	// If the input signal has more channels than we have sources, the additional channels are ignored.
+	if (numInputChannels > sourceCount) {
+		numInputChannels = sourceCount;
+	}
+	
+	// Force the right number of sinks
+	if ( numOutputChannels != sinkCount ) {
+		TTValue v = sinkCount;
+		
+		out.setMaxNumChannels(v);
+		out.setNumChannels(v);
+		numOutputChannels = sinkCount;
+	}
+	// Setting all output signals to zero.
+	out.clear();
+	
+	// TODO: Make sure that when we iterate over the matrix, this is done in an efficient way.
+	
+	for (outChannel=0; outChannel<numOutputChannels; outChannel++) {
+		outSample = out.mSampleVectors[outChannel];
+		for (inChannel=0; inChannel<numInputChannels; inChannel++) {
+			
+			mMixerMatrixCoefficients->get2d(inChannel, outChannel, gainValue);
+			
+			if (gainValue != 0.0){
+				inSample = in.mSampleVectors[inChannel];
+				for (int i=0; i<vs; i++) {
+					outSample[i] += inSample[i] * gainValue;
+				}
+			}
+		}
+	}
+	return kTTErrNone;
+	
+}
