f114a8ce755e0d4f7b29577fd14c070b3d68e879
Antoine Villeret <antoine.villeret@gmail.com>
2015-08-30 19:57:59 +0200

    don't need to export env. variable and make it work on Linux

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ rm -rf *
 cmake -DCMAKE_INSTALL_PREFIX=${PWD}/JamomaInstallation -DCMAKE_OSX_ARCHITECTURES="x86_64;i386" .. # change installation directory, enable FAT Binary
 make -j 2 # "-j 2" to use 2 threads to build
 make install
-export DYLD_LIBRARY_PATH=${PWD}/JamomaInstallation/jamoma/lib
-make test
+LD_LIBRARY_PATH=${PWD}/JamomaInstallation/jamoma/lib DYLD_LIBRARY_PATH=${PWD}/JamomaInstallation/jamoma/lib make test
