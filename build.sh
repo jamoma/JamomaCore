6f2ec8002f99da0299fbac66fc04327d74837143
Antoine Villeret <antoine.villeret@gmail.com>
2015-08-30 19:57:30 +0200

    it could dangerous to use too much threads...

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ mkdir -p build
 cd build
 rm -rf *
 cmake -DCMAKE_INSTALL_PREFIX=${PWD}/JamomaInstallation -DCMAKE_OSX_ARCHITECTURES="x86_64;i386" .. # change installation directory, enable FAT Binary
-make -j 16 # "-j 16" to speed up the build
+make -j 2 # "-j 2" to use 2 threads to build
 make install
 export DYLD_LIBRARY_PATH=${PWD}/JamomaInstallation/jamoma/lib
 make test
