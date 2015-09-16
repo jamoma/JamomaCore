e812b7f2e6c0d9a22d25baa2ada1318f282772bf
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-09-16 14:50:46 +0200

    Update JamomaGitInfo.cpp

--- Shared/JamomaGitInfo.cpp
+++ Shared/JamomaGitInfo.cpp
@@ -3,9 +3,12 @@
 /**
 * Will check that the current extension can be loaded
 * by the version given in argument in git_tag.
+* 
+* Returns true if the extension is compatible with the loading 
+* dylib.
 */
 bool TTExtensionCompatibilityCheck(const char* git_tag)
 {
     static const std::string rev(JAMOMACORE_REV);
-    return rev != git_tag;
+    return rev == git_tag;
 }
